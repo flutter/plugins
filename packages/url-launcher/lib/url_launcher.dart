78e9303cc360095286217df5a0fdddea2fda1bc3
Sarah Zakarias <zarah@google.com>
2017-04-20 13:16:27 +0200

    indent

--- packages/url-launcher/lib/url_launcher.dart
+++ packages/url-launcher/lib/url_launcher.dart
@@ -1,11 +1,10 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class UrlLauncher {
   static const MethodChannel _channel =
-  const MethodChannel('plugins.flutter.io/URLLauncher');
+      const MethodChannel('plugins.flutter.io/URLLauncher');
 
   /// Parse the specified URL string and delegate handling of the same to the
   /// underlying platform.
