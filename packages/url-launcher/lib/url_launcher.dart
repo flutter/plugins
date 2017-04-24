9b5cd44aa3c8e9d2d7b355d545aab5e4e2a1dbf1
Sarah Zakarias <zarah@google.com>
2017-04-24 09:41:47 +0200

    comments

--- packages/url-launcher/lib/url_launcher.dart
+++ packages/url-launcher/lib/url_launcher.dart
@@ -4,6 +4,9 @@ import 'package:flutter/services.dart';
 
 /// Parse the specified URL string and delegate handling of the same to the
 /// underlying platform.
-Future<Null> launch(String urlString) =>
-    const MethodChannel('plugins.flutter.io/URLLauncher')
-        .invokeMethod('UrlLauncher.launch', urlString);
+Future<Null> launch(String urlString) {
+  return const MethodChannel('plugins.flutter.io/URLLauncher').invokeMethod(
+    'UrlLauncher.launch',
+    urlString,
+  );
+}
