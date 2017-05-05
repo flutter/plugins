b9e70d58b016809a330df1b1eef56b48e7bafc43
Xiao Yu <xster@google.com>
2017-05-05 15:15:17 -0700

    builds on Android

--- packages/share/android/src/main/java/io/flutter/plugins/share/SharePlugin.java
+++ packages/share/android/src/main/java/io/flutter/plugins/share/SharePlugin.java
@@ -2,53 +2,52 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package com.google.android.flutter.plugins.share;
+package io.flutter.plugins.share;
 
 import android.content.Context;
 import android.content.Intent;
 
 import io.flutter.app.FlutterActivity;
-import io.flutter.plugin.common.FlutterMethodChannel;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
 
 /** Plugin method host for presenting a share sheet via Intent */
-public class SharePlugin {
+public class SharePlugin implements MethodChannel.MethodCallHandler {
 
   private static final String PLATFORM_CHANNEL = "plugins.flutter.io/share";
 
-  public static void register(FlutterActivity flutterActivity) {
-    new FlutterMethodChannel(flutterActivity.getFlutterView(), PLATFORM_CHANNEL)
-        .setMethodCallHandler(new ShareMethodHandler(flutterActivity));
+  public static SharePlugin register(FlutterActivity flutterActivity) {
+    return new SharePlugin(flutterActivity);
   }
 
-  private static class ShareMethodHandler implements FlutterMethodChannel.MethodCallHandler{
+  private Context context;
 
-    private Context context;
+  private SharePlugin(FlutterActivity flutterActivity) {
+    context = flutterActivity;
+    new MethodChannel(flutterActivity.getFlutterView(), PLATFORM_CHANNEL)
+        .setMethodCallHandler(this);
+  }
 
-    private ShareMethodHandler(Context context) {
-      this.context = context;
-    }
-
-    @Override
-    public void onMethodCall(MethodCall call, FlutterMethodChannel.Response response) {
-      if (call.method.equals("share")) {
-        if (!(call.arguments instanceof String)) {
-          response.error("ARGUMENT_ERROR", "String argument expected", null);
-          return;
-        }
-        final String text = (String) call.arguments;
-
-        Intent shareIntent = new Intent();
-        shareIntent.setAction(Intent.ACTION_SEND);
-        shareIntent.putExtra(Intent.EXTRA_TEXT, text);
-        shareIntent.setType("text/plain");
-        context.startActivity(Intent.createChooser(shareIntent, null /* dialog title optional */));
-        response.success(null);
-      } else {
-        response.error("UNKNOWN_METHOD", "Unknown share method called", null);
+  @Override
+  public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+    if (call.method.equals("share")) {
+      if (!(call.arguments instanceof String)) {
+        result.error("ARGUMENT_ERROR", "String argument expected", null);
+        return;
       }
-    }
+      final String text = (String) call.arguments;
 
+      Intent shareIntent = new Intent();
+      shareIntent.setAction(Intent.ACTION_SEND);
+      shareIntent.putExtra(Intent.EXTRA_TEXT, text);
+      shareIntent.setType("text/plain");
+      context.startActivity(Intent.createChooser(shareIntent, null /* dialog title optional */));
+      result.success(null);
+    } else {
+      result.error("UNKNOWN_METHOD", "Unknown share method called", null);
+    }
   }
 
 }
