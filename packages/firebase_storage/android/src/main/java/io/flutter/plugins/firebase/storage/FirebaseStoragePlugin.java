a70e748ae13290f56056a01edcc588d93e0b69f3
Michael Goderbauer <goderbauer@google.com>
2017-05-11 10:31:08 -0700

    Added firebase_storage

---
+++ packages/firebase_storage/android/src/main/java/io/flutter/plugins/firebase/storage/FirebaseStoragePlugin.java
@@ -0,0 +1,69 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugins.firebase.storage;
+
+import android.app.Activity;
+import android.net.Uri;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnFailureListener;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+import java.util.Map;
+import java.io.File;
+
+/**
+ * FirebaseStoragePlugin
+ */
+public class FirebaseStoragePlugin implements MethodCallHandler {
+  private FirebaseStorage firebaseStorage;
+
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "firebase_storage");
+    channel.setMethodCallHandler(new FirebaseStoragePlugin(registrar.activity()));
+  }
+
+
+  private FirebaseStoragePlugin(Activity activity) {
+    FirebaseApp.initializeApp(activity);
+    this.firebaseStorage = FirebaseStorage.getInstance();
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, final Result result) {
+    if (call.method.equals("StorageReference#putFile")) {
+      Map<String, String> arguments = (Map<String, String>) call.arguments;
+      String filename = arguments.get("filename");
+      String path = arguments.get("path");
+      File file = new File(filename);
+      StorageReference ref = firebaseStorage.getReference().child(path);
+      UploadTask uploadTask = ref.putFile(Uri.fromFile(file));
+      uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+        @Override
+        public void onSuccess(UploadTask.TaskSnapshot snapshot) {
+          result.success(snapshot.getDownloadUrl().toString());
+        }
+      });
+      uploadTask.addOnFailureListener(new OnFailureListener() {
+        @Override
+        public void onFailure(Exception e) {
+          result.error("upload_error", e.getMessage(), e.getStackTrace());
+        }
+      });
+    } else {
+      result.notImplemented();
+    }
+  }
+}
---
+++ packages/firebase_storage/ios/Classes/FirebaseStoragePlugin.m
@@ -0,0 +1,64 @@
+#import "FirebaseStoragePlugin.h"
+
+#import <Firebase/Firebase.h>
+
+@interface NSError (FlutterError)
+@property(readonly, nonatomic) FlutterError *flutterError;
+@end
+
+@implementation NSError (FlutterError)
+- (FlutterError *)flutterError {
+  return [FlutterError
+      errorWithCode:[NSString stringWithFormat:@"Error %ld", (long)self.code]
+            message:self.domain
+            details:self.localizedDescription];
+}
+@end
+
+@implementation FirebaseStoragePlugin {
+}
+
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
+  FlutterMethodChannel *channel =
+  [FlutterMethodChannel methodChannelWithName:@"firebase_storage"
+                              binaryMessenger:[registrar messenger]];
+  FirebaseStoragePlugin *instance = [[FirebaseStoragePlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    if (![FIRApp defaultApp]) {
+      [FIRApp configure];
+    }
+  }
+  return self;
+}
+
+- (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
+  if ([@"StorageReference#putFile" isEqualToString:call.method]) {
+    NSData *data =
+        [NSData dataWithContentsOfFile:call.arguments[@"filename"]];
+    NSString *path = call.arguments[@"path"];
+    FIRStorageReference *fileRef =
+        [[FIRStorage storage].reference child:path];
+    [fileRef putData:data
+            metadata:nil
+          completion:^(FIRStorageMetadata *metadata, NSError *error) {
+            if (error != nil) {
+              result(error.flutterError);
+            } else {
+              // Metadata contains file metadata such as size,
+              // content-type, and download URL.
+              NSURL *downloadURL = metadata.downloadURL;
+              result(downloadURL.absoluteString);
+            }
+          }];
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
