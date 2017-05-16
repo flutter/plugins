f041d816556b8eac91a60666ef41cbdc17c4e12f
Collin Jackson <jackson@google.com>
2017-05-16 11:18:36 -0700

    Update google_sign_in example to use the default project template on iOS (#51)
    * Update google_sign_in example to use the default project template
    
    * Review feedback

--- packages/google_sign_in/example/ios/Runner/AppDelegate.m
+++ packages/google_sign_in/example/ios/Runner/AppDelegate.m
@@ -6,7 +6,18 @@
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
+// Regression test for https://github.com/flutter/flutter/issues/10106
+- (BOOL)application:(UIApplication *)application
+              openURL:(NSURL *)url
+    sourceApplication:(NSString *)sourceApplication
+           annotation:(id)annotation {
+  return [super application:application
+                    openURL:url
+          sourceApplication:sourceApplication
+                 annotation:annotation];
+}
 @end
