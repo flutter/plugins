a1b49cc05b022a3322cb0038f0ad57e422e2de08
Mehmet Fidanboylu <mehmetf@google.com>
2017-06-06 10:38:06 -0700

    Add back the iOS dummy code for android_intent as per discussion on github.com/flutter/flutter/issues/10530.

--- packages/android_intent/example/android/app/src/main/java/io/flutter/plugins/androidintentexample/MainActivity.java
+++ packages/android_intent/example/android/app/src/main/java/io/flutter/plugins/androidintentexample/MainActivity.java
@@ -1,6 +1,7 @@
 package io.flutter.plugins.androidintentexample;
 
 import android.os.Bundle;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
---
+++ packages/android_intent/ios/Classes/AndroidIntentPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface AndroidIntentPlugin : NSObject<FlutterPlugin>
+@end
