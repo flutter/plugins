bb8327edb5deddbb3bf5b603f78c3538e4cca3c7
Mehmet Fidanboylu <mehmetf@google.com>
2017-06-07 16:06:21 -0700

    First connectivity commit

---
+++ packages/connectivity/README.md
@@ -0,0 +1,22 @@
+# connectivity
+
+This plugin allows Flutter apps to discover network connectivity and configure
+themselves accordingly. It can distinguish between cellular vs wifi connection.
+
+Sample usage:
+
+```dart
+var connectivityResult = await checkConnectivity();
+if (connectivityResult == ConnectivityResult.mobile) {
+  // I am connected to a mobile network.
+} else if (connectivityResult == ConnectivityResult.wifi) {
+  // I am connected to a wifi network.
+}
+```
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](http://flutter.io/).
+
+For help on editing plugin code, view the [documentation](https://flutter.io/platform-plugins/#edit-code).
---
+++ packages/connectivity/lib/connectivity.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+/// Connection Status Check Result
+///
+/// Wifi: Device connected via Wi-Fi
+/// Mobile: Device connected to cellular network
+/// None: Device not connected to any network
+enum ConnectivityResult { wifi, mobile, none }
+
+class Connectivity {
+  static const MethodChannel _channel =
+      const MethodChannel('plugins.flutter.io/connectivity');
+
+  /// Checks the connection status of the device.
+  Future<ConnectivityResult> checkConnectivity() async {
+    String result =
+        await _channel.invokeMethod('check');
+    switch (result) {
+      case 'wifi':
+        return ConnectivityResult.wifi;
+      case 'mobile':
+        return ConnectivityResult.mobile;
+      case 'none':
+      default:
+        return ConnectivityResult.none;
+    }
+  }
+}
