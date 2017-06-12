dd0b211f3ce2f86d14cb4e3eb5732041e335f6c7
Mehmet Fidanboylu <mehmetf@users.noreply.github.com>
2017-06-12 10:05:17 -0700

    Quick actions initial commit (#112)
    * Quick Actions plugin
    
    * Add licensing
    
    * Fix icon refs
    
    * Add licensing and fix usage based on engine update.
    
    * Add Android Support too. Update README
    
    * Remove more comments
    
    * Make analyzer and formatter happy
    
    * Review comments

---
+++ packages/quick_actions/example/lib/main.dart
@@ -0,0 +1,64 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:quick_actions/quick_actions.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    final QuickActions quickActions = const QuickActions();
+    quickActions.initialize((String shortcutType) {
+      if (shortcutType == 'action_main') {
+        print('The user tapped on the "Main view" action.');
+      }
+    });
+
+    quickActions.setShortcutItems(<ShortcutItem>[
+      const ShortcutItem(
+          type: 'action_main', localizedTitle: 'Main view', icon: 'AppIcon'),
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text('Plugin example app'),
+      ),
+      body: const Center(
+          child: const Text('On home screen, long press the icon to '
+              'get Main view action. Tapping on that action should print '
+              'a message to the log.')),
+    );
+  }
+}
