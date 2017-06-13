f7e47fb90a39f5db1c165b77681bf953ac281ac3
Todd Volkert <tvolkert@google.com>
2017-06-12 23:13:47 -0700

    Minor fixes in iOS sign-in plugin
    * Ignore "Broken pipe" errors affecting simulator
    * Apply suggested workaround in https://github.com/flutter/flutter/issues/9961
    * Update to non-deprecated `application:openURL:options:`

--- packages/google_sign_in/example/ios/Runner/AppDelegate.m
+++ packages/google_sign_in/example/ios/Runner/AppDelegate.m
@@ -14,14 +14,4 @@
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-// Regression test for https://github.com/flutter/flutter/issues/10106
-- (BOOL)application:(UIApplication *)application
-              openURL:(NSURL *)url
-    sourceApplication:(NSString *)sourceApplication
-           annotation:(id)annotation {
-  return [super application:application
-                    openURL:url
-          sourceApplication:sourceApplication
-                 annotation:annotation];
-}
 @end
