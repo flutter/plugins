2a7f4e7612ac3e18004dc37fa574fbcf173baf22
Mehmet Fidanboylu <mehmetf@google.com>
2017-06-14 16:42:58 -0700

    Remove setup script which is useless. Fix logic for not touching any package files

--- script/plugin_tools.sh
+++ script/plugin_tools.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -ev
+
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  pub global run flutter_plugin_tools "$@"
+  export FLUTTER_CHANGED_PACKAGES=""
 else
   export FLUTTER_CHANGED_PACKAGES=`git diff --name-only $TRAVIS_COMMIT_RANGE | grep -o "packages/[^/]*" | sed -e "s/packages\///g" | sort | uniq | paste -s -d, -`
+fi
+
+if [ "${FLUTTER_CHANGED_PACKAGES}" = ""]; then
+  echo "Running for all packages"
+  pub global run flutter_plugin_tools "$@"
+else
   pub global run flutter_plugin_tools "$@" --plugins=$FLUTTER_CHANGED_PACKAGES
 fi
--- script/travis_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ev
-git clone https://github.com/flutter/flutter.git --depth 1
-./flutter/bin/flutter doctor
-export PATH=`pwd`/flutter/bin:`pwd`/flutter/bin/cache/dart-sdk/bin:$PATH
-./flutter/bin/cache/dart-sdk/bin/pub global activate flutter_plugin_tools
