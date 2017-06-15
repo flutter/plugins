b80f545d81c7935d3c3ce05a2d92ca160f0c2a11
Mehmet Fidanboylu <mehmetf@google.com>
2017-06-15 16:36:19 -0700

    Test new script

--- script/plugin_tools.sh
+++ script/plugin_tools.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -ev
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  export FLUTTER_CHANGED_PACKAGES=""
-else
-  export FLUTTER_CHANGED_PACKAGES=`git diff --name-only $TRAVIS_COMMIT_RANGE | grep -o "packages/[^/]*" | sed -e "s/packages\///g" | sort | uniq | paste -s -d, -`
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+  set FLUTTER_CHANGED_GLOBAL=`git diff --name-only $TRAVIS_COMMIT_RANGE | grep -v packages`
+  set FLUTTER_CHANGED_PACKAGES=`git diff --name-only $TRAVIS_COMMIT_RANGE | grep -o "packages/[^/]*" | sed -e "s/packages\///g" | sort | uniq | paste -s -d, -`
 fi
 
-if [ "${FLUTTER_CHANGED_PACKAGES}" = "" ]; then
+if [ "${FLUTTER_CHANGED_PACKAGES}" = "" ] || [ "${FLUTTER_CHANGED_GLOBAL}" != "" ]; then
   echo "Running for all packages"
-  pub global run flutter_plugin_tools "$@"
+  #pub global run flutter_plugin_tools "$@"
 else
   echo "Running only for $FLUTTER_CHANGED_PACKAGES"
-  pub global run flutter_plugin_tools "$@" --plugins=$FLUTTER_CHANGED_PACKAGES
+  #pub global run flutter_plugin_tools "$@" --plugins=$FLUTTER_CHANGED_PACKAGES
 fi
