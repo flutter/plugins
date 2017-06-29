5eff85fac012cc0ce66e61a81ef136abbdd06e18
Yegor Jbanov <yjbanov@google.com>
2017-06-29 13:18:33 -0700

    proper clang-format

--- packages/device_info/ios/Classes/DeviceInfoPlugin.m
+++ packages/device_info/ios/Classes/DeviceInfoPlugin.m
@@ -6,23 +6,23 @@
 
 @implementation DeviceInfoPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"plugins.flutter.io/device_info"
-            binaryMessenger:[registrar messenger]];
+  FlutterMethodChannel* channel =
+      [FlutterMethodChannel methodChannelWithName:@"plugins.flutter.io/device_info"
+                                  binaryMessenger:[registrar messenger]];
   DeviceInfoPlugin* instance = [[DeviceInfoPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getIosDeviceInfo" isEqualToString:call.method]) {
-    UIDevice *device = [UIDevice currentDevice];
+    UIDevice* device = [UIDevice currentDevice];
     result(@{
-      @"name": [device name],
-      @"systemName": [device systemName],
-      @"systemVersion": [device systemVersion],
-      @"model": [device model],
-      @"localizedModel": [device localizedModel],
-      @"identifierForVendor": [[device identifierForVendor] UUIDString],
+      @"name" : [device name],
+      @"systemName" : [device systemName],
+      @"systemVersion" : [device systemVersion],
+      @"model" : [device model],
+      @"localizedModel" : [device localizedModel],
+      @"identifierForVendor" : [[device identifierForVendor] UUIDString],
     });
   } else {
     result(FlutterMethodNotImplemented);
