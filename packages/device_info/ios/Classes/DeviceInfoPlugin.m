b9a730b752815745b8916628aff48dfe7af97928
Yegor <yjbanov@google.com>
2017-06-30 14:22:57 -0700

    device_info plugin to get information about the device and OS (#158)
    * flutter create; first approximation of the API
    
    * implement Android bits
    
    * remove references to "yourcompany" in Android code
    
    * implement iOS bits
    
    * fix licenses; format code
    
    * more licenses; address comments
    
    * proper clang-format
    
    * add class container
    
    * fix null-ckeck-assignment

---
+++ packages/device_info/ios/Classes/DeviceInfoPlugin.m
@@ -0,0 +1,32 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "DeviceInfoPlugin.h"
+
+@implementation DeviceInfoPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel =
+      [FlutterMethodChannel methodChannelWithName:@"plugins.flutter.io/device_info"
+                                  binaryMessenger:[registrar messenger]];
+  DeviceInfoPlugin* instance = [[DeviceInfoPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getIosDeviceInfo" isEqualToString:call.method]) {
+    UIDevice* device = [UIDevice currentDevice];
+    result(@{
+      @"name" : [device name],
+      @"systemName" : [device systemName],
+      @"systemVersion" : [device systemVersion],
+      @"model" : [device model],
+      @"localizedModel" : [device localizedModel],
+      @"identifierForVendor" : [[device identifierForVendor] UUIDString],
+    });
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
