7d171d6eeb1fbc31bcca3f71c103292aa4684a25
Todd Volkert <tvolkert@google.com>
2017-06-30 16:56:37 -0700

    Add `getCurrentAccount()` to `GoogleSignInPlugin.Delegate`

--- packages/google_sign_in/android/src/main/java/io/flutter/plugins/googlesignin/GoogleSignInPlugin.java
+++ packages/google_sign_in/android/src/main/java/io/flutter/plugins/googlesignin/GoogleSignInPlugin.java
@@ -129,11 +129,17 @@ public class GoogleSignInPlugin implements MethodCallHandler {
     private GoogleApiClient googleApiClient;
     private List<String> requestedScopes;
     private PendingOperation pendingOperation;
+    private volatile GoogleSignInAccount currentAccount;
 
     public Delegate(PluginRegistry.Registrar registrar) {
       activity = registrar.activity();
       activity.getApplication().registerActivityLifecycleCallbacks(handler);
       registrar.addActivityResultListener(handler);
+    }
+
+    /** Returns the most recently signed-in account, or null if there was none. */
+    public GoogleSignInAccount getCurrentAccount() {
+      return currentAccount;
     }
 
     private void checkAndSetPendingOperation(String method, Result result) {
@@ -303,6 +309,7 @@ public class GoogleSignInPlugin implements MethodCallHandler {
     private void onSignInResult(GoogleSignInResult result) {
       if (result.isSuccess()) {
         GoogleSignInAccount account = result.getSignInAccount();
+        currentAccount = account;
         Map<String, Object> response = new HashMap<>();
         response.put("displayName", account.getDisplayName());
         response.put("email", account.getEmail());
