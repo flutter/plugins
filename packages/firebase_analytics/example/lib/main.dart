9a066a8c025fa29da004d8d245877a58aa79ddc2
Collin Jackson <jackson@google.com>
2017-07-17 08:50:23 -0700

    Support for long values on Android Firebase Analytics (#175)

--- packages/firebase_analytics/example/lib/main.dart
+++ packages/firebase_analytics/example/lib/main.dart
@@ -44,7 +44,16 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<Null> _sendAnalyticsEvent() async {
-    await analytics.logEvent(name: 'test_event');
+    await analytics.logEvent(
+      name: 'test_event',
+      parameters: <String, dynamic>{
+        'string': 'string',
+        'int': 42,
+        'long': 12345678910,
+        'double': 42.0,
+        'bool': true,
+      },
+    );
     setMessage('logEvent succeeded');
   }
 
