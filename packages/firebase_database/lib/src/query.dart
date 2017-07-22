469b5415a4e52638a6383d7ddabb0d886d8c822d
German Saprykin <saprykin.h@gmail.com>
2017-07-22 18:30:11 +0700

    Fix format.

--- packages/firebase_database/lib/src/query.dart
+++ packages/firebase_database/lib/src/query.dart
@@ -6,14 +6,15 @@ part of firebase_database;
 
 /// Represents a query over the data at a particular location.
 class Query {
-  Query._({
-    @required FirebaseDatabase database,
-    @required List<String> pathComponents,
-    Map<String, dynamic> parameters
-  }): _database = database,
-    _pathComponents = pathComponents,
-    _parameters = parameters ?? new Map<String, dynamic>.unmodifiable(<String, dynamic>{}),
-    assert(database != null);
+  Query._(
+      {@required FirebaseDatabase database,
+      @required List<String> pathComponents,
+      Map<String, dynamic> parameters})
+      : _database = database,
+        _pathComponents = pathComponents,
+        _parameters = parameters ??
+            new Map<String, dynamic>.unmodifiable(<String, dynamic>{}),
+        assert(database != null);
 
   final FirebaseDatabase _database;
   final List<String> _pathComponents;
@@ -33,9 +34,10 @@ class Query {
   }
 
   Map<String, dynamic> buildArguments() {
-    return new Map<String, dynamic>.from(_parameters)..addAll(<String, dynamic>{
-      'path': path,
-    });
+    return new Map<String, dynamic>.from(_parameters)
+      ..addAll(<String, dynamic>{
+        'path': path,
+      });
   }
 
   Stream<Event> _observe(_EventType eventType) {
@@ -46,7 +48,8 @@ class Query {
     controller = new StreamController<Event>.broadcast(
       onListen: () {
         _handle = _database._channel.invokeMethod(
-          'Query#observe', <String, dynamic>{
+          'Query#observe',
+          <String, dynamic>{
             'path': path,
             'parameters': _parameters,
             'eventType': eventType.toString(),
@@ -60,7 +63,7 @@ class Query {
         _handle.then((int handle) async {
           await _database._channel.invokeMethod(
             'Query#removeObserver',
-            <String, dynamic>{ 'handle': handle },
+            <String, dynamic>{'handle': handle},
           );
           FirebaseDatabase._observers.remove(handle);
         });
@@ -91,10 +94,10 @@ class Query {
   /// than or equal to the given value, using the given orderBy directive or
   /// priority as default, and optionally only child nodes with a key greater
   /// than or equal to the given key.
-  Query startAt(dynamic value, { String key }) {
+  Query startAt(dynamic value, {String key}) {
     assert(!_parameters.containsKey('startAt'));
     assert(value is String || value is bool || value is double || value is int);
-    final Map<String, dynamic> parameters = <String, dynamic>{ 'startAt': value };
+    final Map<String, dynamic> parameters = <String, dynamic>{'startAt': value};
     if (key != null) parameters['startAtKey'] = key;
     return _copyWithParameters(parameters);
   }
@@ -103,10 +106,10 @@ class Query {
   /// than or equal to the given value, using the given orderBy directive or
   /// priority as default, and optionally only child nodes with a key less
   /// than or equal to the given key.
-  Query endAt(dynamic value, { String key }) {
+  Query endAt(dynamic value, {String key}) {
     assert(!_parameters.containsKey('endAt'));
     assert(value is String || value is bool || value is double || value is int);
-    final Map<String, dynamic> parameters = <String, dynamic>{ 'endAt': value };
+    final Map<String, dynamic> parameters = <String, dynamic>{'endAt': value};
     if (key != null) parameters['endAtKey'] = key;
     return _copyWithParameters(parameters);
   }
@@ -115,24 +118,24 @@ class Query {
   /// `value` (and `key`, if provided).
   ///
   /// If a key is provided, there is at most one such child as names are unique.
-  Query equalTo(dynamic value, { String key }) {
+  Query equalTo(dynamic value, {String key}) {
     assert(!_parameters.containsKey('equalTo'));
     assert(value is String || value is bool || value is double || value is int);
     return _copyWithParameters(
-      <String, dynamic>{ 'equalTo': value, 'equalToKey': key },
+      <String, dynamic>{'equalTo': value, 'equalToKey': key},
     );
   }
 
   /// Create a query with limit and anchor it to the start of the window.
   Query limitToFirst(int limit) {
     assert(!_parameters.containsKey('limitToFirst'));
-    return _copyWithParameters(<String, dynamic>{ 'limitToFirst': limit });
+    return _copyWithParameters(<String, dynamic>{'limitToFirst': limit});
   }
 
   /// Create a query with limit and anchor it to the end of the window.
   Query limitToLast(int limit) {
     assert(!_parameters.containsKey('limitToLast'));
-    return _copyWithParameters(<String, dynamic>{ 'limitToLast': limit });
+    return _copyWithParameters(<String, dynamic>{'limitToLast': limit});
   }
 
   /// Generate a view of the data sorted by values of a particular child key.
@@ -143,7 +146,7 @@ class Query {
     assert(key != null);
     assert(!_parameters.containsKey('orderBy'));
     return _copyWithParameters(
-      <String, dynamic>{ 'orderBy': 'child', 'orderByChildKey': key },
+      <String, dynamic>{'orderBy': 'child', 'orderByChildKey': key},
     );
   }
 
@@ -153,7 +156,7 @@ class Query {
   /// [equalTo].
   Query orderByKey() {
     assert(!_parameters.containsKey('orderBy'));
-    return _copyWithParameters(<String, dynamic>{ 'orderBy': 'key' });
+    return _copyWithParameters(<String, dynamic>{'orderBy': 'key'});
   }
 
   /// Generate a view of the data sorted by value.
@@ -162,7 +165,7 @@ class Query {
   /// [equalTo].
   Query orderByValue() {
     assert(!_parameters.containsKey('orderBy'));
-    return _copyWithParameters(<String, dynamic>{ 'orderBy': 'value' });
+    return _copyWithParameters(<String, dynamic>{'orderBy': 'value'});
   }
 
   /// Generate a view of the data sorted by priority.
@@ -171,11 +174,12 @@ class Query {
   /// [equalTo].
   Query orderByPriority() {
     assert(!_parameters.containsKey('orderBy'));
-    return _copyWithParameters(<String, dynamic>{ 'orderBy': 'priority' });
+    return _copyWithParameters(<String, dynamic>{'orderBy': 'priority'});
   }
 
   /// Obtains a DatabaseReference corresponding to this query's location.
-  DatabaseReference reference() => new DatabaseReference._(_database, _pathComponents);
+  DatabaseReference reference() =>
+      new DatabaseReference._(_database, _pathComponents);
 
   /// By calling keepSynced(true) on a location, the data for that location will
   /// automatically be downloaded and kept in sync, even when no listeners are
@@ -184,7 +188,11 @@ class Query {
   Future<Null> keepSynced(bool value) {
     return _database._channel.invokeMethod(
       'Query#keepSynced',
-      <String, dynamic>{ 'path': path, 'parameters': _parameters, 'value': value },
+      <String, dynamic>{
+        'path': path,
+        'parameters': _parameters,
+        'value': value
+      },
     );
   }
 }
