5eb1949f7deabbbbb92919d0c5b0f8c82b7f5ec7
Alex Rudyk <alr@me.com>
2017-09-12 09:34:56 -0700

    added FirebaseStorage getData func

--- packages/firebase_storage/android/src/main/java/io/flutter/plugins/firebase/storage/FirebaseStoragePlugin.java
+++ packages/firebase_storage/android/src/main/java/io/flutter/plugins/firebase/storage/FirebaseStoragePlugin.java
@@ -6,8 +6,10 @@ package io.flutter.plugins.firebase.storage;
 
 import android.app.Activity;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -37,29 +39,61 @@ public class FirebaseStoragePlugin implements MethodCallHandler {
 
   @Override
   public void onMethodCall(MethodCall call, final Result result) {
-    if (call.method.equals("StorageReference#putFile")) {
-      Map<String, String> arguments = (Map<String, String>) call.arguments;
-      String filename = arguments.get("filename");
-      String path = arguments.get("path");
-      File file = new File(filename);
-      StorageReference ref = firebaseStorage.getReference().child(path);
-      UploadTask uploadTask = ref.putFile(Uri.fromFile(file));
-      uploadTask.addOnSuccessListener(
-          new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot snapshot) {
-              result.success(snapshot.getDownloadUrl().toString());
-            }
-          });
-      uploadTask.addOnFailureListener(
-          new OnFailureListener() {
-            @Override
-            public void onFailure(Exception e) {
-              result.error("upload_error", e.getMessage(), null);
-            }
-          });
-    } else {
-      result.notImplemented();
+    switch (call.method) {
+      case "StorageReference#putFile":
+        putFile(call, result);
+        break;
+      case "StorageReference#getData":
+        getData(call, result);
+        break;
+      default:
+        result.notImplemented();
+        break;
     }
+  }
+
+  private void putFile(MethodCall call, final Result result) {
+    Map<String, String> arguments = (Map<String, String>) call.arguments;
+    String filename = arguments.get("filename");
+    String path = arguments.get("path");
+    File file = new File(filename);
+    StorageReference ref = firebaseStorage.getReference().child(path);
+    UploadTask uploadTask = ref.putFile(Uri.fromFile(file));
+    uploadTask.addOnSuccessListener(
+        new OnSuccessListener<UploadTask.TaskSnapshot>() {
+          @Override
+          public void onSuccess(UploadTask.TaskSnapshot snapshot) {
+            result.success(snapshot.getDownloadUrl().toString());
+          }
+        });
+    uploadTask.addOnFailureListener(
+        new OnFailureListener() {
+          @Override
+          public void onFailure(Exception e) {
+            result.error("upload_error", e.getMessage(), null);
+          }
+        });
+  }
+
+  private void getData(MethodCall call, final Result result) {
+    Map<String, Object> arguments = (Map<String, Object>) call.arguments;
+    Integer maxSize = (Integer) arguments.get("maxSize");
+    String path = (String) arguments.get("path");
+    StorageReference ref = firebaseStorage.getReference().child(path);
+    Task<byte[]> downloadTask = ref.getBytes(maxSize);
+    downloadTask.addOnSuccessListener(
+        new OnSuccessListener<byte[]>() {
+          @Override
+          public void onSuccess(byte[] bytes) {
+            result.success(bytes);
+          }
+        });
+    downloadTask.addOnFailureListener(
+        new OnFailureListener() {
+          @Override
+          public void onFailure(@NonNull Exception e) {
+            result.error("download_error", e.getMessage(), null);
+          }
+        });
   }
 }
