3cd77573ccbb983ecfe83ac06ba4b338fba79462
Alex Rudyk <alr@me.com>
2017-09-14 20:02:53 -0700

    added getData tests

---
+++ packages/firebase_storage/test/firebase_storage_test.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+import 'dart:typed_data';
+import 'package:test/test.dart';
+import 'package:flutter/services.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+void main() {
+  group('StorageReference', () {
+    group('getData', () {
+      const MethodChannel channel = const MethodChannel(
+        'firebase_storage',
+      );
+
+      final List<MethodCall> log = <MethodCall>[];
+
+      StorageReference ref;
+
+      setUp(() {
+        channel.setMockMethodCallHandler((MethodCall methodCall) {
+          log.add(methodCall);
+          return new Future<dynamic>.value(
+              new Uint8List.fromList([1, 2, 3, 4]));
+        });
+        ref = FirebaseStorage.instance
+            .ref()
+            .child('avatars')
+            .child('large')
+            .child('image.jpg');
+      });
+
+      test('invokes correct method', () async {
+        await ref.getData(10);
+
+        expect(
+            log,
+            equals(<MethodCall>[
+              new MethodCall('StorageReference#getData', <String, dynamic>{
+                'maxSize': 10,
+                'path': 'avatars/large/image.jpg',
+              }),
+            ]));
+      });
+
+      test('returns correct result', () async {
+        expect(await ref.getData(10),
+            equals(new Uint8List.fromList([1, 2, 3, 4])));
+      });
+    });
+  });
+}
