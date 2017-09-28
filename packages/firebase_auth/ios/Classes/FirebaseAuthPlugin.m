11dbdbf3eca74296d0beeb6fcb82d327b9547dff
Marcin Szałek <marcin.szalek1@gmail.com>
2017-09-28 10:19:24 +0200

    Minor changes in updating user profile for iOS

--- packages/firebase_auth/ios/Classes/FirebaseAuthPlugin.m
+++ packages/firebase_auth/ios/Classes/FirebaseAuthPlugin.m
@@ -117,13 +117,12 @@ NSDictionary *toDictionary(id<FIRUserInfo> userInfo) {
                                           [self sendResult:result forUser:user error:error];
                                         }];
   } else if ([@"updateUserProfile" isEqualToString:call.method]) {
-        NSDictionary<NSString*, NSString*> *dict = call.arguments; //Tutaj moze byc potrzebne jakies rzutowanie, nie jestem w stanie tego sprawdzic
         FIRUserProfileChangeRequest *changeRequest = [[FIRAuth auth].currentUser profileChangeRequest];
-        if (dict[@"displayName"]) {
-            changeRequest.displayName = dict[@"displayName"];
+        if (call.arguments[@"displayName"]) {
+            changeRequest.displayName = call.arguments[@"displayName"];
         }
-        if (dict[@"photoUrl"]) {
-            changeRequest.photoUrl = dict[@"photoUrl"];
+        if (call.arguments[@"photoUrl"]) {
+            changeRequest.photoURL = call.arguments[@"photoUrl"];
         }
         [changeRequest commitChangesWithCompletion:^(NSError *error) {
             [self sendResult:result forUser:nil error:error];
