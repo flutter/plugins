36fee3a34a818d92501072d577e2020094b4cc33
Iiro Krankka <iiro.krankka@codemate.com>
2017-10-18 14:51:38 +0300

    Added some documentation for the pickImage method.

--- packages/image_picker/lib/image_picker.dart
+++ packages/image_picker/lib/image_picker.dart
@@ -10,7 +10,14 @@ import 'package:flutter/services.dart';
 class ImagePicker {
   static const MethodChannel _channel = const MethodChannel('image_picker');
 
-  // Returns the URL of the picked image
+  /// Returns a [File] object pointing to the image that was picked.
+  ///
+  /// On both Android & iOS, the user can choose to either:
+  ///
+  /// * pick an image from the gallery
+  /// * take a photo using the device camera.
+  ///
+  /// If specified, image will be resized to [desiredWidth] and [desiredHeight].
   static Future<File> pickImage({double desiredWidth, double desiredHeight}) async {
     final String path = await _channel.invokeMethod('pickImage',
       <String, double> {
