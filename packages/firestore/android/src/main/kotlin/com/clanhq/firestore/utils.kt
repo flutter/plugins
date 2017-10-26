5a93aef4b701ba6e97f3a911b372c6cffc31e8f3
Unknown <philipp.comans@gmail.com>
2017-10-26 11:34:08 -0700

    Convert Date objects to long before sending them over the bridge

--- packages/firestore/android/src/main/kotlin/com/clanhq/firestore/utils.kt
+++ packages/firestore/android/src/main/kotlin/com/clanhq/firestore/utils.kt
@@ -2,14 +2,23 @@ package com.clanhq.firestore
 
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.DocumentSnapshot
-import java.util.HashMap
+import java.util.*
 
 /**
  * Created by philipp on 10/20/17.
  */
 fun documentSnapshotToMap(it: DocumentSnapshot): Map<String, Any> {
     val m = HashMap<String, Any>()
-    m.put("data", it.data)
+
+    val cleandata: HashMap<String, Any> = HashMap()
+    it.data.forEach {
+        if (it.value is Date) {
+            cleandata.put(it.key, (it.value as Date).time)
+        } else {
+            cleandata.put(it.key, it.value)
+        }
+    }
+    m.put("data", cleandata)
     m.put("id", it.id)
 
     return m
