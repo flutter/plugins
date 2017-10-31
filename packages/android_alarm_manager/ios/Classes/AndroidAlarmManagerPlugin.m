9717578d4373b45145e1ae269d0ade80c70ef4b7
Zachary Anderson <zanderso@users.noreply.github.com>
2017-10-31 08:47:18 -0700

    Add a plugin for the Android AlarmManager (#250)

---
+++ packages/android_alarm_manager/ios/Classes/AndroidAlarmManagerPlugin.m
@@ -0,0 +1,21 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "AndroidAlarmManagerPlugin.h"
+
+@implementation AndroidAlarmManagerPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel =
+      [FlutterMethodChannel methodChannelWithName:@"plugins.flutter.io/android_alarm_manager"
+                                  binaryMessenger:[registrar messenger]
+                                            codec:[FlutterJSONMessageCodec sharedInstance]];
+  AndroidAlarmManagerPlugin* instance = [[AndroidAlarmManagerPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  result(FlutterMethodNotImplemented);
+}
+
+@end
