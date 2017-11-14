3adc2416efcb6a0f90d30c5cb764efb416e78cb2
Zachary Anderson <zanderso@users.noreply.github.com>
2017-11-14 13:46:29 -0800

    Re-use main activity isolate (#270)

--- packages/android_alarm_manager/android/src/main/java/io/flutter/androidalarmmanager/AlarmService.java
+++ packages/android_alarm_manager/android/src/main/java/io/flutter/androidalarmmanager/AlarmService.java
@@ -4,20 +4,25 @@
 
 package io.flutter.androidalarmmanager;
 
+import android.app.Activity;
 import android.app.AlarmManager;
+import android.app.Application;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
+import io.flutter.app.FlutterActivity;
+import io.flutter.app.FlutterApplication;
 import io.flutter.view.FlutterMain;
-import io.flutter.view.FlutterView;
+import io.flutter.view.FlutterNativeView;
 
 public class AlarmService extends Service {
   public static final String TAG = "AlarmService";
+  private static FlutterNativeView sSharedFlutterView;
 
-  private FlutterView flutterView;
+  private FlutterNativeView mFlutterView;
   private String appBundlePath;
 
   public static void setOneShot(
@@ -56,14 +61,65 @@ public class AlarmService extends Service {
     manager.cancel(existingIntent);
   }
 
+  public static FlutterNativeView getSharedFlutterView() {
+    return sSharedFlutterView;
+  }
+
+  public static boolean setSharedFlutterView(FlutterNativeView view) {
+    if (sSharedFlutterView != null && sSharedFlutterView != view) {
+      Log.i(TAG, "setSharedFlutterView tried to overwrite an existing FlutterNativeView");
+      return false;
+    }
+    Log.i(TAG, "setSharedFlutterView set");
+    sSharedFlutterView = view;
+    return true;
+  }
+
+  private void ensureFlutterView() {
+    if (mFlutterView != null) {
+      return;
+    }
+
+    if (sSharedFlutterView != null) {
+      mFlutterView = sSharedFlutterView;
+      return;
+    }
+
+    // mFlutterView and sSharedFlutterView are both null. That likely means that
+    // no FlutterView has ever been created in this process before. So, we'll
+    // make one, and assign it to both mFlutterView and sSharedFlutterView.
+    mFlutterView = new FlutterNativeView();
+    sSharedFlutterView = mFlutterView;
+    return;
+  }
+
+  // This returns the FlutterView for the main FlutterActivity if there is one.
+  private static FlutterNativeView viewFromAppContext(Context context) {
+    Application app = (Application) context;
+    if (!(app instanceof FlutterApplication)) {
+      Log.i(TAG, "viewFromAppContext app not a FlutterApplication");
+      return null;
+    }
+    FlutterApplication flutterApp = (FlutterApplication) app;
+    Activity activity = flutterApp.getCurrentActivity();
+    if (activity == null) {
+      Log.i(TAG, "viewFromAppContext activity is null");
+      return null;
+    }
+    if (!(activity instanceof FlutterActivity)) {
+      Log.i(TAG, "viewFromAppContext activity is not a FlutterActivity");
+      return null;
+    }
+    FlutterActivity flutterActivity = (FlutterActivity) activity;
+    return flutterActivity.getFlutterView().getFlutterNativeView();
+  }
+
   @Override
   public void onCreate() {
-    Context context = getApplicationContext();
     super.onCreate();
+    Context context = getApplicationContext();
+    mFlutterView = viewFromAppContext(context);
     FlutterMain.ensureInitializationComplete(context, null);
-    if (flutterView == null) {
-      flutterView = new FlutterView(context);
-    }
     if (appBundlePath == null) {
       appBundlePath = FlutterMain.findAppBundlePath(context);
     }
@@ -71,20 +127,35 @@ public class AlarmService extends Service {
 
   @Override
   public void onDestroy() {
-    if (flutterView != null) {
-      flutterView.destroy();
+    // Try to find the native view of the main activity if there is one.
+    Context context = getApplicationContext();
+    FlutterNativeView nativeView = viewFromAppContext(context);
+
+    // Don't destroy mFlutterView if it is the same as the native view for the
+    // main activity, or the same as the shared native view.
+    if (mFlutterView != nativeView && mFlutterView != sSharedFlutterView) {
+      mFlutterView.destroy();
     }
+    mFlutterView = null;
+
+    // Don't destroy the shared native view if it is the same native view as
+    // for the main activity.
+    if (sSharedFlutterView != nativeView) {
+      sSharedFlutterView.destroy();
+    }
+    sSharedFlutterView = null;
   }
 
   @Override
   public int onStartCommand(Intent intent, int flags, int startId) {
+    ensureFlutterView();
     String entrypoint = intent.getStringExtra("entrypoint");
     if (entrypoint == null) {
       Log.i(TAG, "onStartCommand got a null entrypoint. Bailing out");
       return START_NOT_STICKY;
     }
     if (appBundlePath != null) {
-      flutterView.runFromBundle(appBundlePath, null, entrypoint, true);
+      mFlutterView.runFromBundle(appBundlePath, null, entrypoint, true);
     }
     return START_NOT_STICKY;
   }
