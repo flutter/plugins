c810900a2d898dce3ed2f42bf162cf6b27e4874a
Kyle Bradshaw <kyle@hotfixit.net>
2017-11-28 23:29:23 -0600

    First file change
    Git is weird to me, so forgive the clutter.

--- packages/cloud_firestore/android/src/main/java/io/flutter/plugins/firebase/CloudFirestorePlugin.java
+++ packages/cloud_firestore/android/src/main/java/io/flutter/plugins/firebase/CloudFirestorePlugin.java
@@ -38,7 +38,7 @@ public class CloudFirestorePlugin implements MethodCallHandler {
 
   public static void registerWith(PluginRegistry.Registrar registrar) {
     final MethodChannel channel =
-        new MethodChannel(registrar.messenger(), "plugins.flutter.io/cloud_firestore");
+        new MethodChannel(registrar.messenger(), "plugins.flutter.io/cloud_firestore", FirestoreMethodCodec.INSTANCE);
     channel.setMethodCallHandler(new CloudFirestorePlugin(channel));
   }
 
