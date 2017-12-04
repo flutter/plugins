dd00f126447a539c5c796dd96639c303a8f84928
Zachary Anderson <zanderso@users.noreply.github.com>
2017-12-04 15:45:52 -0800

    [android_alarm_manager] Use a firebase plugin (#290)

--- packages/android_alarm_manager/CHANGELOG.md
+++ packages/android_alarm_manager/CHANGELOG.md
@@ -1,3 +1,9 @@
+## [0.0.3] - 12/4/2017
+
+* Adds use of a Firebase plugin to the example. The example also now
+  demonstrates overriding the Application's onCreate method so that the
+  AlarmService can initialize plugin connections.
+
 ## [0.0.2] - 12/3/2017.
 
 * Add FLT prefix to iOS types.
--- packages/android_alarm_manager/example/lib/main.dart
+++ packages/android_alarm_manager/example/lib/main.dart
@@ -5,37 +5,73 @@
 import 'dart:async';
 import 'dart:isolate';
 
-import 'package:flutter/widgets.dart';
 import 'package:android_alarm_manager/android_alarm_manager.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+
+final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+FirebaseUser firebaseUser;
+
+Future<Null> ensureFirebaseUser() async {
+  if (firebaseUser == null) {
+    firebaseUser = await firebaseAuth.currentUser();
+    if (firebaseUser == null) {
+      firebaseUser = await firebaseAuth.signInAnonymously();
+    }
+  }
+}
+
+class HelloMessage {
+  final DateTime _now;
+  final String _msg;
+  final int _isolate;
+  final FirebaseUser _user;
+  final String _token;
+
+  HelloMessage(this._now, this._msg, this._isolate, this._user, this._token);
+
+  @override
+  String toString() {
+    return "[$_now] $_msg "
+        "isolate=$_isolate "
+        "user='$_user' "
+        "token=$_token";
+  }
+}
+
+void printHelloMessage(String msg) {
+  ensureFirebaseUser().then((_) {
+    firebaseUser.getIdToken().then((String idToken) {
+      print(new HelloMessage(
+        new DateTime.now(),
+        msg,
+        Isolate.current.hashCode,
+        firebaseUser,
+        idToken,
+      ));
+    });
+  });
+}
 
 void printHello() {
-  final DateTime now = new DateTime.now();
-  final int isolateId = Isolate.current.hashCode;
-  print("[$now] Hello, world! isolate=$isolateId function='$printHello'");
+  printHelloMessage("Hello, world!");
 }
 
 void printGoodbye() {
-  final DateTime now = new DateTime.now();
-  final int isolateId = Isolate.current.hashCode;
-  print("[$now] Goodbye, world! isolate=$isolateId function='$printGoodbye'");
+  printHelloMessage("Goodbye, world!");
 }
 
 bool oneShotFired = false;
 
 void printOneShot() {
-  oneShotFired = true;
-  final DateTime now = new DateTime.now();
-  final int isolateId = Isolate.current.hashCode;
-  print("[$now] Hello, once! isolate=$isolateId function='$printOneShot'");
+  printHelloMessage("Hello, once!");
 }
 
 Future<Null> main() async {
   final int helloAlarmID = 0;
   final int goodbyeAlarmID = 1;
   final int oneShotID = 2;
-  final DateTime now = new DateTime.now();
-  final int isolateId = Isolate.current.hashCode;
-  print("[$now] Hello, main()! isolate=$isolateId function='$main'");
+  printHelloMessage("Hello, main()!");
   runApp(const Center(
       child: const Text('Hello, world!', textDirection: TextDirection.ltr)));
   await AndroidAlarmManager.periodic(
