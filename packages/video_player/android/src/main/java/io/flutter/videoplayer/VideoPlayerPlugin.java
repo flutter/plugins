71fea09b155b4a6fe49cdce2f94edaa5f785db63
Sigurd Meldgaard <sigurdm@google.com>
2017-12-04 12:33:15 +0100

    Remove video player from list on dispose (#294)

--- packages/video_player/android/src/main/java/io/flutter/videoplayer/VideoPlayerPlugin.java
+++ packages/video_player/android/src/main/java/io/flutter/videoplayer/VideoPlayerPlugin.java
@@ -226,6 +226,7 @@ public class VideoPlayerPlugin implements MethodCallHandler {
         result.success(player.getPosition());
       } else if (call.method.equals("dispose")) {
         player.dispose();
+        videoPlayers.remove(textureId);
         result.success(null);
       } else {
         result.notImplemented();
