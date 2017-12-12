99e5afee4f2f9c137dbd48b1df81994b319956db
Mehmet Fidanboylu <mehmetf@google.com>
2017-12-12 15:29:02 -0800

    Make formatter happy

--- packages/local_auth/android/src/main/java/io/flutter/plugins/localauth/LocalAuthPlugin.java
+++ packages/local_auth/android/src/main/java/io/flutter/plugins/localauth/LocalAuthPlugin.java
@@ -47,20 +47,23 @@ public class LocalAuthPlugin implements MethodCallHandler {
               new AuthCompletionHandler() {
                 @Override
                 public void onSuccess() {
-                  if (authInProgress.compareAndSet(true, false))
+                  if (authInProgress.compareAndSet(true, false)) {
                     result.success(true);
+                  }
                 }
 
                 @Override
                 public void onFailure() {
-                  if (authInProgress.compareAndSet(true, false))
+                  if (authInProgress.compareAndSet(true, false)) {
                     result.success(false);
+                  }
                 }
 
                 @Override
                 public void onError(String code, String error) {
-                  if (authInProgress.compareAndSet(true, false))
+                  if (authInProgress.compareAndSet(true, false)) {
                     result.error(code, error, null);
+                  }
                 }
               });
       authenticationHelper.authenticate();
