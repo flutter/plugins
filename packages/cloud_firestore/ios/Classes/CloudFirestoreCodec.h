3bf63940c8eb906bcaa34943f9911e928df29a70
ananfang <ananfang@gmail.com>
2017-12-26 14:35:08 +0800

    Init commit

---
+++ packages/cloud_firestore/ios/Classes/CloudFirestoreCodec.h
@@ -0,0 +1,29 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+//  CloudFirestoreCodec.h
+//  cloud_firestore
+//
+//  Created by ananfang on 26/12/2017.
+//
+
+#import <Flutter/Flutter.h>
+
+#pragma mark - Enum: CloudFirestoreField
+typedef NS_ENUM(NSInteger, CloudFirestoreField) {
+    CloudFirestoreFieldNil,
+    CloudFirestoreFieldTrue,
+    CloudFirestoreFieldFalse,
+    CloudFirestoreFieldInt32,
+    CloudFirestoreFieldInt64,
+    CloudFirestoreFieldIntHex,
+    CloudFirestoreFieldFloat64,
+    CloudFirestoreFieldString,
+    CloudFirestoreFieldUInt8Data,
+    CloudFirestoreFieldInt32Data,
+    CloudFirestoreFieldInt64Data,
+    CloudFirestoreFieldFloat64Data,
+    CloudFirestoreFieldList,
+    CloudFirestoreFieldMap
+};
