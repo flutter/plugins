af59a7896e1771a764401d5f2a8290e5c94a0b0a
Kyle Bradshaw <kyle@hotfixit.net>
2017-12-26 08:54:54 -0600

    Standardize file headers. We ARE "The Chromium Authors" :)

--- packages/cloud_firestore/ios/Classes/CloudFirestoreCodec.h
+++ packages/cloud_firestore/ios/Classes/CloudFirestoreCodec.h
@@ -1,13 +1,6 @@
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//
-//  CloudFirestoreCodec.h
-//  cloud_firestore
-//
-//  Created by ananfang on 26/12/2017.
-//  ananfang@gmail.com
-//
 
 #import <Flutter/Flutter.h>
 #import <Firebase/Firebase.h>
--- packages/cloud_firestore/ios/Classes/CloudFirestoreCodec.m
+++ packages/cloud_firestore/ios/Classes/CloudFirestoreCodec.m
@@ -1,13 +1,6 @@
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//
-//  CloudFirestoreCodec.m
-//  cloud_firestore
-//
-//  Created by ananfang on 26/12/2017.
-//  ananfang@gmail.com
-//
 
 #import "CloudFirestoreCodec.h"
 
