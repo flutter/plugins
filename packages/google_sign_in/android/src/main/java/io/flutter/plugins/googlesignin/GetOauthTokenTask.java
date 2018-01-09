75ecbcb2eb5db2c3a7c7c4df42ea8fb72e1dd2e7
André Tietz <andre.tietz@moovel.com>
2018-01-09 14:38:06 +0100

    using android inbuild stringjoiner

--- packages/google_sign_in/android/src/main/java/io/flutter/plugins/googlesignin/GetOauthTokenTask.java
+++ packages/google_sign_in/android/src/main/java/io/flutter/plugins/googlesignin/GetOauthTokenTask.java
@@ -5,8 +5,9 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
 import com.google.android.gms.auth.GoogleAuthUtil;
-import java.util.Iterator;
 import java.util.List;
 
 /** Android inbuilt solution for calling async tasks */
@@ -19,40 +20,20 @@ class GetOauthTokenTask extends AsyncTask<GetOauthTokenTask.Request, Void, Strin
     this.listener = listener;
   }
 
-  /**
-   * This method avoids the guava dependency. Guava has about 15k methods, to avoid hitting the
-   * method limit we write this method ourselves.
-   *
-   * @param stringsToJoin a list of strings you want to join
-   * @return a string that joined the list of strings separated by a space character
-   * @see <a href="https://developer.android.com/studio/build/multidex.html#about">Android Method
-   *     Limit</a>
-   * @see <a href="http://www.methodscount.com/?lib=com.google.guava%3Aguava%3A20.0">Guava Method
-   *     count</a>
-   */
-  @NonNull
-  private static String stringJoiner(@NonNull List<String> stringsToJoin) {
-    StringBuilder sb = new StringBuilder();
-    Iterator<String> it = stringsToJoin.iterator();
-    while (it.hasNext()) {
-      sb.append(it.next());
-      if (it.hasNext()) sb.append(' ');
-    }
-    return sb.toString();
-  }
 
   @Override
   protected String doInBackground(GetOauthTokenTask.Request... requests) {
     if (requests.length == 1) {
       Request request = requests[0];
       Account account = new Account(request.email, "com.google");
-      String scopesStr = String.format("oauth2:%s", stringJoiner(request.scopes));
+      String scopesStr = String.format("oauth2:%s", TextUtils.join(" ", request.scopes));
       try {
         return GoogleAuthUtil.getToken(request.context, account, scopesStr);
       } catch (Exception e) {
         error = e;
       }
     }
+
     return null;
   }
 
