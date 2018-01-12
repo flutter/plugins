376a0d0a4536946c6db72014aa82792fe9ae6335
Mikkel Ravn <mravn@google.com>
2018-01-12 13:50:40 +0100

    Refresh alarm_manager project template. Move Java package

--- packages/android_alarm_manager/example/android/app/src/main/java/io/flutter/plugins/androidalarmmanagerexample/MainActivity.java
+++ packages/android_alarm_manager/example/android/app/src/main/java/io/flutter/plugins/androidalarmmanagerexample/MainActivity.java
@@ -1,10 +1,10 @@
-package io.flutter.androidalarmmanagerexample;
+package io.flutter.plugins.androidalarmmanagerexample;
 
 import android.os.Bundle;
 import android.util.Log;
-import io.flutter.androidalarmmanager.AlarmService;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugins.androidalarmmanager.AlarmService;
 import io.flutter.view.FlutterNativeView;
 
 public class MainActivity extends FlutterActivity {
