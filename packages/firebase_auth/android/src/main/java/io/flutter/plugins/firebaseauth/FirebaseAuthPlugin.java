e010b1b7ef8551ebdd17b0f713c70b51ce53859d
Mikkel Ravn <mravn@google.com>
2018-01-17 16:08:43 +0100

    Move firebase_auth to io.flutter.plugins org

--- packages/firebase_auth/android/src/main/java/io/flutter/plugins/firebaseauth/FirebaseAuthPlugin.java
+++ packages/firebase_auth/android/src/main/java/io/flutter/plugins/firebaseauth/FirebaseAuthPlugin.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package io.flutter.firebaseauth;
+package io.flutter.plugins.firebaseauth;
 
 import android.net.Uri;
 import android.support.annotation.NonNull;
