10233d57df9e7d8f88439353d5631ccb1137cc29
Mikkel Nygaard Ravn <mravn@google.com>
2018-01-26 11:09:31 +0100

    Update package_info.dart

--- packages/package_info/lib/package_info.dart
+++ packages/package_info/lib/package_info.dart
@@ -10,56 +10,41 @@ import 'package:flutter/services.dart';
 const MethodChannel _kChannel =
     const MethodChannel('plugins.flutter.io/package_info');
 
-/// [PackageInfo] wrap application Bundle (on iOS) and Package (on Android) information,
-/// providing cached property for package name verion and build number.
-///
-/// [PackgeInfo] implemented in singleton idiom. After first called
-/// [PackageInfo.getInstance], cached properties never changed.
+/// Application metadata. Provides application bundle information on iOS and
+/// application package information on Android.
 ///
 /// ```dart
-///     PackageInfo packageInfo = await PackageInfo.getInstance()
-///     print("Version is: ${packageInfo.version}");
+/// PackageInfo packageInfo = await PackageInfo.fromPlatform()
+/// print("Version is: ${packageInfo.version}");
 /// ```
-/// Or in async mode:
-///
-/// ```dart
-///   PackageInfo.getInstance().then((PackageInfo packageInfo) {
-///     String packageName = packageInfo.packageName;
-///     String version = packageInfo.version;
-///     String buildNumber = packageInfo.buildNumber;
-///   });
-/// ```
-/// All properties are type of [String].
 class PackageInfo {
-  PackageInfo._({
-    @required this.packageName,
-    @required this.version,
-    @required this.buildNumber,
-  })
-      : assert(packageName != null),
-        assert(version != null),
-        assert(buildNumber != null);
+  PackageInfo({
+    this.packageName,
+    this.version,
+    this.buildNumber,
+  });
 
-  static Future<PackageInfo> _instance;
+  static Future<PackageInfo> _fromPlatform;
 
-  /// Singleton instance.
-  static Future<PackageInfo> getInstance() async {
-    if (_instance == null) {
+  /// Retrieves package information from the platform.
+  /// The result is cached.
+  static Future<PackageInfo> fromPlatform() async {
+    if (_fromPlatform == null) {
       final Completer<PackageInfo> completer = new Completer<PackageInfo>();
 
       _kChannel.invokeMethod('getAll').then((dynamic result) {
-        final Map<String, String> fromSystem = result;
+        final Map<String, String> map = result;
 
-        completer.complete(new PackageInfo._(
-          packageName: fromSystem["packageName"],
-          version: fromSystem["version"],
-          buildNumber: fromSystem["buildNumber"],
+        completer.complete(new PackageInfo(
+          packageName: map["packageName"],
+          version: map["version"],
+          buildNumber: map["buildNumber"],
         ));
       }, onError: completer.completeError);
 
-      _instance = completer.future;
+      _fromPlatform = completer.future;
     }
-    return _instance;
+    return _fromPlatform;
   }
 
   /// The package name. `bundleIdentifier` on iOS, `getPackageName` on Android.
