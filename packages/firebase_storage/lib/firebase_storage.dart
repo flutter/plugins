ba88aae2fbe72d7972dbefe93c6612ac1f7f56d1
Luke <freeman.luke@gmail.com>
2018-02-08 09:00:18 -0800

    add getPath to storage ref object

--- packages/firebase_storage/lib/firebase_storage.dart
+++ packages/firebase_storage/lib/firebase_storage.dart
@@ -52,6 +52,8 @@ class StorageReference {
     return FirebaseStorage._channel.invokeMethod("StorageReference#delete",
         <String, String>{'path': _pathComponents.join("/")});
   }
+
+  String getPath() => _pathComponents.join("/");
 }
 
 class StorageUploadTask {
