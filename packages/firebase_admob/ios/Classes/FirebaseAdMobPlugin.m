0e8d2b5ba10d63e853a932929f91c4ad3dd6e296
Sarah Zakarias <zarah@google.com>
2018-02-14 09:10:57 +0100

    fix import

--- packages/firebase_admob/ios/Classes/FirebaseAdMobPlugin.m
+++ packages/firebase_admob/ios/Classes/FirebaseAdMobPlugin.m
@@ -2,10 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-#import "FirebaseAdmobPlugin.h"
 #import "FLTMobileAd.h"
 #import "FLTRewardedVideoAdWrapper.h"
 #import "Firebase/Firebase.h"
+#import "FirebaseAdMobPlugin.h"
 
 @interface NSError (FlutterError)
 @property(readonly, nonatomic) FlutterError *flutterError;
