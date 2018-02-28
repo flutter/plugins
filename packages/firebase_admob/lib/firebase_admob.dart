79ccd95239e63a9e5ef90d278d35907af9c48a43
Mikkel Ravn <mravn@google.com>
2018-02-28 21:13:37 +0100

    Fix Dart 2 type errors

--- packages/firebase_admob/lib/firebase_admob.dart
+++ packages/firebase_admob/lib/firebase_admob.dart
@@ -118,8 +118,6 @@ abstract class MobileAd {
   /// Plugin log messages will identify this property as the ad's `mobileAdId`.
   int get id => hashCode;
 
-  MethodChannel get _channel => FirebaseAdMob.instance._channel;
-
   /// Start loading this ad.
   Future<bool> load();
 
@@ -131,21 +129,21 @@ abstract class MobileAd {
   /// The [listener] will be notified when the ad has finished loading or fails
   /// to do so. An ad that fails to load will not be shown.
   Future<bool> show() {
-    return _channel.invokeMethod("showAd", <String, dynamic>{'id': id});
+    return _invokeBooleanMethod("showAd", <String, dynamic>{'id': id});
   }
 
   /// Free the plugin resources associated with this ad.
   ///
-  /// Disposing a banner ad that's been shown removes it from the screen. Interstitial
-  /// ads can't be programatically removed from view.
+  /// Disposing a banner ad that's been shown removes it from the screen.
+  /// Interstitial ads can't be programmatically removed from view.
   Future<bool> dispose() {
     assert(_allAds[id] != null);
     _allAds[id] = null;
-    return _channel.invokeMethod("disposeAd", <String, dynamic>{'id': id});
+    return _invokeBooleanMethod("disposeAd", <String, dynamic>{'id': id});
   }
 
   Future<bool> _doLoad(String loadMethod) {
-    return _channel.invokeMethod(loadMethod, <String, dynamic>{
+    return _invokeBooleanMethod(loadMethod, <String, dynamic>{
       'id': id,
       'adUnitId': adUnitId,
       'targetingInfo': targetingInfo?.toJson(),
@@ -278,18 +276,16 @@ class RewardedVideoAd {
   /// Callback invoked for events in the rewarded video ad lifecycle.
   RewardedVideoAdListener listener;
 
-  MethodChannel get _channel => FirebaseAdMob.instance._channel;
-
   /// Shows a rewarded video ad if one has been loaded.
   Future<bool> show() {
-    return _channel.invokeMethod("showRewardedVideoAd");
+    return _invokeBooleanMethod("showRewardedVideoAd");
   }
 
   /// Loads a rewarded video ad using the provided ad unit ID.
   Future<bool> load(
       {@required String adUnitId, MobileAdTargetingInfo targetingInfo}) {
     assert(adUnitId.isNotEmpty);
-    return _channel.invokeMethod("loadRewardedVideoAd", <String, dynamic>{
+    return _invokeBooleanMethod("loadRewardedVideoAd", <String, dynamic>{
       'adUnitId': adUnitId,
       'targetingInfo': targetingInfo?.toJson(),
     });
@@ -369,7 +365,7 @@ class FirebaseAdMob {
       bool analyticsEnabled: false}) {
     assert(appId != null && appId.isNotEmpty);
     assert(analyticsEnabled != null);
-    return _channel.invokeMethod("initialize", <String, dynamic>{
+    return _invokeBooleanMethod("initialize", <String, dynamic>{
       'appId': appId,
       'trackingId': trackingId,
       'analyticsEnabled': analyticsEnabled,
@@ -404,4 +400,12 @@ class FirebaseAdMob {
 
     return new Future<Null>(null);
   }
+}
+
+Future<bool> _invokeBooleanMethod(String method, [dynamic arguments]) async {
+  final bool result = await FirebaseAdMob.instance._channel.invokeMethod(
+    method,
+    arguments,
+  );
+  return result;
 }
