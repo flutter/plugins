08b31da6bd40b915023e0d825017b825a113ef62
Jonah Williams <jonahwilliams@google.com>
2018-03-14 16:38:07 -0700

    send error if permissions are denied and handle error in example code

--- packages/image_picker/android/src/main/java/io/flutter/plugins/imagepicker/ImagePickerPlugin.java
+++ packages/image_picker/android/src/main/java/io/flutter/plugins/imagepicker/ImagePickerPlugin.java
@@ -171,6 +171,7 @@ public class ImagePickerPlugin
         activity.startActivityForResult(
             cameraModule.getCameraIntent(activity), REQUEST_CODE_CAMERA);
       } else {
+        pendingResult.error("no_permissions", "image_picker plugin requires camera permissions", null);
         pendingResult = null;
         methodCall = null;
       }
--- packages/image_picker/example/lib/main.dart
+++ packages/image_picker/example/lib/main.dart
@@ -44,9 +44,11 @@ class _MyHomePageState extends State<MyHomePage> {
           child: new FutureBuilder<File>(
               future: _imageFile,
               builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.connectionState == ConnectionState.done && snapshot.error == null) {
                   return new Image.file(snapshot.data);
-                } else {
+                } else if (snapshot.error != null) {
+                  return const Text('error picking image.');
+                } else  {
                   return const Text('You have not yet picked an image.');
                 }
               })),
