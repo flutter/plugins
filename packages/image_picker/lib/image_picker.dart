e4c0093f4dd86e1995ce4a1eeb5d1f06e1d78d5d
Iiro Krankka <iiro.krankka@gmail.com>
2018-03-29 22:43:18 +0300

    [Image Picker] Import foundation for the @required annotations (#445)

--- packages/image_picker/lib/image_picker.dart
+++ packages/image_picker/lib/image_picker.dart
@@ -5,8 +5,8 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import 'package:meta/meta.dart';
 
 /// Specifies the source where the picked image should come from.
 enum ImageSource {
