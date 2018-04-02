e34ac23f3511ceed32489abaa92bbaff95ca8d15
Mikkel Nygaard Ravn <mravn@google.com>
2018-04-02 08:48:21 +0200

    Remove yourcompany.com from cloud_firestore (#447)

--- packages/cloud_firestore/example/lib/main.dart
+++ packages/cloud_firestore/example/lib/main.dart
@@ -11,19 +11,23 @@ void main() {
   runApp(new MaterialApp(title: 'Firestore Example', home: new MyHomePage()));
 }
 
-class BookList extends StatelessWidget {
+class MessageList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('books').snapshots,
+      stream: Firestore.instance.collection('messages').snapshots,
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (!snapshot.hasData) return const Text('Loading...');
-        return new ListView(
-          children: snapshot.data.documents.map((DocumentSnapshot document) {
+        final int messageCount = snapshot.data.documents.length;
+        return new ListView.builder(
+          itemCount: messageCount,
+          itemBuilder: (_, int index) {
+            final DocumentSnapshot document = snapshot.data.documents[index];
             return new ListTile(
               title: new Text(document['message'] ?? '<No message retrieved>'),
+              subtitle: new Text('Message ${index + 1} of $messageCount'),
             );
-          }).toList(),
+          },
         );
       },
     );
@@ -34,10 +38,7 @@ class MyHomePage extends StatelessWidget {
   CollectionReference get messages => Firestore.instance.collection('messages');
 
   Future<Null> _addMessage() async {
-    Firestore.instance
-        .collection('books')
-        .document()
-        .setData(<String, String>{'message': 'Hello world!'});
+    messages.document().setData(<String, String>{'message': 'Hello world!'});
   }
 
   @override
@@ -46,7 +47,7 @@ class MyHomePage extends StatelessWidget {
       appBar: new AppBar(
         title: const Text('Firestore Example'),
       ),
-      body: new BookList(),
+      body: new MessageList(),
       floatingActionButton: new FloatingActionButton(
         onPressed: _addMessage,
         tooltip: 'Increment',
