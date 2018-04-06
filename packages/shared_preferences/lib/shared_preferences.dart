40758f6fdca37ac829fe5cac05cafc6c910677cb
Kevin Walter <namanix@gmail.com>
2018-04-06 16:10:46 +0200

    Added getAll for debugging purposes

--- packages/shared_preferences/lib/shared_preferences.dart
+++ packages/shared_preferences/lib/shared_preferences.dart
@@ -45,6 +45,9 @@ class SharedPreferences {
   /// in sync since the setter method might fail for any reason.
   final Map<String, Object> _preferenceCache;
 
+  /// returns the entire set for debugging purposes.
+  Map<String,Object> getAll() => _preferenceCache;
+
   /// Reads a value of any type from persistent storage.
   dynamic get(String key) => _preferenceCache[key];
 
