eea281f4905ff639a2a38b973fd49d37c7c8fcfb
Stefano Rodriguez <sroddy@users.noreply.github.com>
2018-05-01 09:23:30 +0200

    Fix Dart2 Types (#514)

--- packages/firebase_messaging/CHANGELOG.md
+++ packages/firebase_messaging/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.5
+
+* Fixed Dart 2 type error.
+
 ## 0.2.4
 
 * Updated Google Play Services dependencies to version 15.0.0.
--- packages/firebase_messaging/lib/firebase_messaging.dart
+++ packages/firebase_messaging/lib/firebase_messaging.dart
@@ -108,8 +108,8 @@ class FirebaseMessaging {
         }
         return null;
       case "onIosSettingsRegistered":
-        _iosSettingsStreamController
-            .add(new IosNotificationSettings._fromMap(call.arguments));
+        _iosSettingsStreamController.add(new IosNotificationSettings._fromMap(
+            call.arguments.cast<String, bool>()));
         return null;
       case "onMessage":
         return _onMessage(call.arguments.cast<String, dynamic>());
