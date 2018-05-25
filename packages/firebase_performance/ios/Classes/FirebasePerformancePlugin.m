9e2a74462231fd4b9b0d793afe27540e316cdfae
Brandon Parrish <bparr2450@gmail.com>
2018-05-24 17:30:47 -0700

    Performance metrics (#577)
    * Dart side of HttpMetric
    
    * Android and example HttpMetric setup
    
    * IOS HttpMetric
    
    * Updated HttpMethod to work with iOS
    
    * Using constants from Trace for HttpMetric
    
    * Formatting
    
    * Use BaseClient as example network request
    
    * Changed some comments and formatting
    
    * Removed podfile

--- packages/firebase_performance/ios/Classes/FirebasePerformancePlugin.m
+++ packages/firebase_performance/ios/Classes/FirebasePerformancePlugin.m
@@ -8,6 +8,7 @@
 
 @interface FLTFirebasePerformancePlugin ()
 @property(nonatomic, retain) NSMutableDictionary *traces;
+@property(nonatomic, retain) NSMutableDictionary *httpMetrics;
 @end
 
 @implementation FLTFirebasePerformancePlugin
@@ -42,6 +43,10 @@
     [self handleTraceStart:call result:result];
   } else if ([@"Trace#stop" isEqualToString:call.method]) {
     [self handleTraceStop:call result:result];
+  } else if ([@"HttpMetric#start" isEqualToString:call.method]) {
+    [self handleHttpMetricStart:call result:result];
+  } else if ([@"HttpMetric#stop" isEqualToString:call.method]) {
+    [self handleHttpMetricStop:call result:result];
   } else {
     result(FlutterMethodNotImplemented);
   }
@@ -73,6 +78,78 @@
 
   [trace stop];
   [_traces removeObjectForKey:handle];
+  result(nil);
+}
+
+- (void)handleHttpMetricStart:(FlutterMethodCall *)call result:(FlutterResult)result {
+  NSNumber *handle = call.arguments[@"handle"];
+  NSURL *url = [NSURL URLWithString:call.arguments[@"url"]];
+
+  NSNumber *httpMethod = call.arguments[@"httpMethod"];
+  FIRHTTPMethod method;
+  switch ([httpMethod intValue]) {
+    case 0:
+      method = FIRHTTPMethodCONNECT;
+      break;
+    case 1:
+      method = FIRHTTPMethodDELETE;
+      break;
+    case 2:
+      method = FIRHTTPMethodGET;
+      break;
+    case 3:
+      method = FIRHTTPMethodHEAD;
+      break;
+    case 4:
+      method = FIRHTTPMethodOPTIONS;
+      break;
+    case 5:
+      method = FIRHTTPMethodPATCH;
+      break;
+    case 6:
+      method = FIRHTTPMethodPOST;
+      break;
+    case 7:
+      method = FIRHTTPMethodPUT;
+      break;
+    case 8:
+      method = FIRHTTPMethodTRACE;
+      break;
+    default:
+      method = [httpMethod intValue];
+      break;
+  }
+
+  FIRHTTPMetric *metric = [[FIRHTTPMetric alloc] initWithURL:url HTTPMethod:method];
+  [_httpMetrics setObject:metric forKey:handle];
+  [metric start];
+  result(nil);
+}
+
+- (void)handleHttpMetricStop:(FlutterMethodCall *)call result:(FlutterResult)result {
+  NSNumber *handle = call.arguments[@"handle"];
+  FIRHTTPMetric *metric = [_httpMetrics objectForKey:handle];
+
+  NSNumber *responseCode = call.arguments[@"httpResponseCode"];
+  NSNumber *requestPayloadSize = call.arguments[@"requestPayloadSize"];
+  NSString *responseContentType = call.arguments[@"responseContentType"];
+  NSNumber *responsePayloadSize = call.arguments[@"responsePayloadSize"];
+
+  if (![responseCode isEqual:[NSNull null]]) metric.responseCode = [responseCode integerValue];
+  if (![requestPayloadSize isEqual:[NSNull null]])
+    metric.requestPayloadSize = [requestPayloadSize longValue];
+  if (![responseContentType isEqual:[NSNull null]])
+    metric.responseContentType = responseContentType;
+  if (![responsePayloadSize isEqual:[NSNull null]])
+    metric.responsePayloadSize = [responsePayloadSize longValue];
+
+  NSDictionary *attributes = call.arguments[@"attributes"];
+  [attributes enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString *value, BOOL *stop) {
+    [metric setValue:key forAttribute:value];
+  }];
+
+  [metric stop];
+  [_httpMetrics removeObjectForKey:handle];
   result(nil);
 }
 
