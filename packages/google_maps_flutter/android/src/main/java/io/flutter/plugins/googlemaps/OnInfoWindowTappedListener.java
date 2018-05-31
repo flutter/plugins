bf01bfb2cb0c7377fa83544f9d3611247831be15
gerryhigh <gerryhigh@gmail.com>
2018-05-31 11:07:48 -0500

    Add Infowindow tapped handler; also rev. google play services to 15.+ to fix runtime issue with different versions

---
+++ packages/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/OnInfoWindowTappedListener.java
@@ -0,0 +1,7 @@
+package io.flutter.plugins.googlemaps;
+
+import com.google.android.gms.maps.model.Marker;
+
+public interface OnInfoWindowTappedListener {
+  void onInfoWindowTapped(Marker marker);
+}
