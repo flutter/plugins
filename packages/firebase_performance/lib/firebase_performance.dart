63de31ee1143ba5a0a9155d50195e9b497e26623
Brandon Parrish <bparr2450@gmail.com>
2018-06-06 11:46:57 -0700

    Updated plugin code and formatting to match other plugins (#591)
    * Added commas for dartfmt
    
    * Added finals
    
    * Use call.argument to use dynamic types
    
    * Removed local file
    
    * Removed unnecessary comments
    
    * double to single
    
    * Updated gradle and podspec
    
    * Added empty line at end of files and updated plugin version
    
    * Updated licenses to 2018
    
    * Added HttpMetric doc comment
    
    * Moves attributes to separate abstract class
    
    * Created separate test for PerformanceAttributable
    
    * Object => Number
    
    * Updated comments
    
    * Changed unmodifiable map to modifiable
    
    * formatting
    
    * PerformanceAttributable => PerformanceAttributes
    
    * Changed version 0.0.3 => 0.0.4
    
    * Fixed tests
    
    * Few comment changes

--- packages/firebase_performance/lib/firebase_performance.dart
+++ packages/firebase_performance/lib/firebase_performance.dart
@@ -1,4 +1,4 @@
-// Copyright 2017, the Flutter project authors.  Please see the AUTHORS file
+// Copyright 2018, the Flutter project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -12,4 +12,5 @@ import 'package:flutter/foundation.dart';
 
 part 'src/firebase_performance.dart';
 part 'src/http_metric.dart';
+part 'src/performance_attributes.dart';
 part 'src/trace.dart';
