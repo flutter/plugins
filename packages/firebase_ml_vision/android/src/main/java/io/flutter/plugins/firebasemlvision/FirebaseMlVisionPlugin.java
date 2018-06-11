c0303b385d0228a3954c24df375c46c281cda24f
Maurice Parrish <bmparr@google.com>
2018-06-11 15:56:14 -0700

    Initial 'fluter create' for Firebase ML Vision plugin

---
+++ packages/firebase_ml_vision/android/src/main/java/io/flutter/plugins/firebasemlvision/FirebaseMlVisionPlugin.java
@@ -0,0 +1,25 @@
+package io.flutter.plugins.firebasemlvision;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** FirebaseMlVisionPlugin */
+public class FirebaseMlVisionPlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "firebase_ml_vision");
+    channel.setMethodCallHandler(new FirebaseMlVisionPlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+}
