b11381c0daf7984420768e0a1850f7f823fa2d52
Brandon Parrish <bparr2450@gmail.com>
2018-06-12 15:12:40 -0700

    Initial 'flutter create' for Firebase ML Vision plugin (#609)
    * Initial 'fluter create' for Firebase ML Vision plugin
    
    * Formatting
    
    * More Formatting
    
    * Import ordering

---
+++ packages/firebase_ml_vision/example/README.md
@@ -0,0 +1,8 @@
+# firebase_ml_vision_example
+
+Demonstrates how to use the firebase_ml_vision plugin.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ packages/firebase_ml_vision/example/lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _platformVersion = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await FirebaseMlVision.platformVersion;
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: new Center(
+          child: new Text('Running on: $_platformVersion\n'),
+        ),
+      ),
+    );
+  }
+}
