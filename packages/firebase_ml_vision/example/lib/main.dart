418f646ba6ce9dce3a639dcde499d684a2a24769
Maurice Parrish <bmparr@google.com>
2018-06-15 15:46:57 -0700

    Added to test app

--- packages/firebase_ml_vision/example/lib/main.dart
+++ packages/firebase_ml_vision/example/lib/main.dart
@@ -17,22 +17,72 @@ class _MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<_MyHomePage> {
-  File _image;
+  Image _image;
+  List<TextBlock> _blocks;
 
-  Future<void> getImage() async {
+  Future<void> _getImage() async {
     final File image = await ImagePicker.pickImage(source: ImageSource.camera);
 
     final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(image);
     final TextDetector detector = FirebaseVision.instance.getTextDetector();
     final List<TextBlock> blocks = await detector.detectInImage(visionImage);
 
-    for (TextBlock block in blocks) {
-      print(block.text);
+    setState(() {
+      _image = Image.file(image);
+      _blocks = blocks;
+    });
+  }
+
+  Future<Size> _getImageSize(Image image) {
+    final Completer<Size> completer = new Completer<Size>();
+    image.image
+        .resolve(const ImageConfiguration())
+        .addListener((ImageInfo info, bool _) => completer.complete(Size(
+              info.image.width.toDouble(),
+              info.image.height.toDouble(),
+            )));
+    return completer.future;
+  }
+
+  List<Widget> buildTextDetectionStack(Size imageSize) {
+    final List<Widget> textDetection = <Widget>[];
+    textDetection.add(_image);
+
+    final Size screenSize = MediaQuery.of(context).size;
+    final Size scale = new Size(
+      screenSize.width / imageSize.width,
+      screenSize.height / imageSize.height,
+    );
+
+    for (TextBlock block in _blocks) {
+      print('Text block text: ${block.text}');
+
+      textDetection.add(new Positioned(
+        left: block.boundingBox.left * scale.width,
+        top: block.boundingBox.top * scale.height,
+        right: screenSize.width - (block.boundingBox.right * scale.width),
+        bottom: screenSize.height - (block.boundingBox.bottom * scale.height),
+        child: new Container(
+          decoration: new BoxDecoration(
+            color: Colors.transparent,
+            border: new Border.all(
+              width: 2.0,
+              color: Colors.red,
+            ),
+          ),
+        ),
+      ));
     }
 
-    setState(() {
-      _image = image;
-    });
+    return textDetection;
+  }
+
+  Future<Stack> _buildStack() async {
+    final Size size = await _getImageSize(_image);
+
+    return new Stack(
+      children: buildTextDetectionStack(size),
+    );
   }
 
   @override
@@ -41,13 +91,24 @@ class _MyHomePageState extends State<_MyHomePage> {
       appBar: new AppBar(
         title: const Text('Image Picker Example'),
       ),
-      body: new Center(
-        child: _image == null
-            ? const Text('No image selected.')
-            : new Image.file(_image),
-      ),
+      body: _image == null
+          ? const Center(
+              child: const Text("No image selected."),
+            )
+          : new FutureBuilder<Stack>(
+              future: _buildStack(),
+              builder: (BuildContext context, AsyncSnapshot<Stack> snapshot) {
+                if (snapshot.hasData) {
+                  return snapshot.data;
+                } else {
+                  return const Center(
+                    child: const Text("Scanning for text..."),
+                  );
+                }
+              },
+            ),
       floatingActionButton: new FloatingActionButton(
-        onPressed: getImage,
+        onPressed: _getImage,
         tooltip: 'Pick Image',
         child: const Icon(Icons.add_a_photo),
       ),
