80fb0c1e17d312258f5a7e5b7e03daea44b5fe6a
Mehmet Fidanboylu <mehmetf@users.noreply.github.com>
2018-06-16 07:23:10 -0700

    Make sensors package Dart 2 compliant. (#621)
    * Add sensors test.
    
    * Fix formatting and analyzer issues
    
    * Bump version and add changelog entry

---
+++ packages/sensors/test/sensors_test.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
+import 'package:sensors/sensors.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('$accelerometerEvents are streamed', () async {
+    const String channelName = 'plugins.flutter.io/sensors/accelerometer';
+    const List<double> sensorData = const <double>[1.0, 2.0, 3.0];
+
+    const StandardMethodCodec standardMethod = const StandardMethodCodec();
+
+    void emitEvent(ByteData event) {
+      BinaryMessages.handlePlatformMessage(
+        channelName,
+        event,
+        (ByteData reply) {},
+      );
+    }
+
+    bool isCanceled = false;
+    BinaryMessages.setMockMessageHandler(channelName, (ByteData message) async {
+      final MethodCall methodCall = standardMethod.decodeMethodCall(message);
+      if (methodCall.method == 'listen') {
+        emitEvent(standardMethod.encodeSuccessEnvelope(sensorData));
+        emitEvent(null);
+        return standardMethod.encodeSuccessEnvelope(null);
+      } else if (methodCall.method == 'cancel') {
+        isCanceled = true;
+        return standardMethod.encodeSuccessEnvelope(null);
+      } else {
+        fail('Expected listen or cancel');
+      }
+    });
+
+    final AccelerometerEvent event = await accelerometerEvents.first;
+    expect(event.x, 1.0);
+    expect(event.y, 2.0);
+    expect(event.z, 3.0);
+
+    await new Future<Null>.delayed(Duration.zero);
+    expect(isCanceled, isTrue);
+  });
+}
