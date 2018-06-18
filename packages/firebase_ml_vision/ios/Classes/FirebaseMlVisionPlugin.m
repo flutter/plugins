98de4cf856de891b09639e90f98be4796d2f0547
Maurice Parrish <bmparr@google.com>
2018-06-18 14:31:25 -0700

    Add permssions

--- packages/firebase_ml_vision/ios/Classes/FirebaseMlVisionPlugin.m
+++ packages/firebase_ml_vision/ios/Classes/FirebaseMlVisionPlugin.m
@@ -22,8 +22,8 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  if ([@"TextDetector#detectInImage" isEqualToString:call.method]) {
+    result(@[]);
   } else {
     result(FlutterMethodNotImplemented);
   }
