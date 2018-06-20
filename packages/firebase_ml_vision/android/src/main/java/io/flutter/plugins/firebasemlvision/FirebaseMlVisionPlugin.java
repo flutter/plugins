0bbd80053fcfef61a9d614dcd738c5d76a37183a
Maurice Parrish <bparr2450@gmail.com>
2018-06-20 14:07:32 -0700

    Setup Android side of MLVision text and created example app (#626)
    * Initial 'fluter create' for Firebase ML Vision plugin
    
    * Formatting
    
    * More Formatting
    
    * Import ordering
    
    * Added android app to firebase
    
    * Added ios app to firebase
    
    * Added ml vision to android and ios
    
    * WIP dart side of ml vision
    
    * finish dart side of ml vision and text detector
    
    * Added TextDetector tests
    
    * Added some comments
    
    * Finished comments
    
    * Removed usused dependency
    
    * Formatting and unused imports
    
    * Changed variable name to restart WIP test
    
    * Start of android text detection
    
    * Added text recognition to android channel
    
    * Create dart test app
    
    * Added to test app
    
    * Finished example ml vision app
    
    * Formatting
    
    * Fixed crash
    
    * Repaint on data change
    
    * Get image from gallery
    
    * Better error codes

--- packages/firebase_ml_vision/android/src/main/java/io/flutter/plugins/firebasemlvision/FirebaseMlVisionPlugin.java
+++ packages/firebase_ml_vision/android/src/main/java/io/flutter/plugins/firebasemlvision/FirebaseMlVisionPlugin.java
@@ -1,26 +1,122 @@
 package io.flutter.plugins.firebasemlvision;
 
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /** FirebaseMlVisionPlugin */
 public class FirebaseMlVisionPlugin implements MethodCallHandler {
+  private Registrar registrar;
+
+  private FirebaseMlVisionPlugin(Registrar registrar) {
+    this.registrar = registrar;
+  }
+
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel =
         new MethodChannel(registrar.messenger(), "plugins.flutter.io/firebase_ml_vision");
-    channel.setMethodCallHandler(new FirebaseMlVisionPlugin());
+    channel.setMethodCallHandler(new FirebaseMlVisionPlugin(registrar));
   }
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    if (call.method.equals("TextDetector#detectInImage")) {
+      handleTextDetectionResult(call, result);
     } else {
       result.notImplemented();
     }
+  }
+
+  private void handleTextDetectionResult(MethodCall call, final Result result) {
+    File file = new File((String) call.arguments);
+
+    FirebaseVisionImage image;
+    try {
+      image = FirebaseVisionImage.fromFilePath(registrar.context(), Uri.fromFile(file));
+    } catch (IOException exception) {
+      result.error("textDetectorError", exception.getLocalizedMessage(), null);
+      return;
+    }
+
+    FirebaseVisionTextDetector detector = FirebaseVision.getInstance().getVisionTextDetector();
+    detector
+        .detectInImage(image)
+        .addOnSuccessListener(
+            new OnSuccessListener<FirebaseVisionText>() {
+              @Override
+              public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                List<Map<String, Object>> blocks = new ArrayList<>();
+                for (FirebaseVisionText.Block block : firebaseVisionText.getBlocks()) {
+                  Map<String, Object> blockData = new HashMap<>();
+                  addTextData(
+                      blockData, block.getBoundingBox(), block.getCornerPoints(), block.getText());
+
+                  List<Map<String, Object>> lines = new ArrayList<>();
+                  for (FirebaseVisionText.Line line : block.getLines()) {
+                    Map<String, Object> lineData = new HashMap<>();
+                    addTextData(
+                        lineData, line.getBoundingBox(), line.getCornerPoints(), line.getText());
+
+                    List<Map<String, Object>> elements = new ArrayList<>();
+                    for (FirebaseVisionText.Element element : line.getElements()) {
+                      Map<String, Object> elementData = new HashMap<>();
+                      addTextData(
+                          elementData,
+                          element.getBoundingBox(),
+                          element.getCornerPoints(),
+                          element.getText());
+                      elements.add(elementData);
+                    }
+                    lineData.put("elements", elements);
+                    lines.add(lineData);
+                  }
+                  blockData.put("lines", lines);
+                  blocks.add(blockData);
+                }
+                result.success(blocks);
+              }
+            })
+        .addOnFailureListener(
+            new OnFailureListener() {
+              @Override
+              public void onFailure(@NonNull Exception e) {
+                result.error("textDetectorError", e.getLocalizedMessage(), null);
+              }
+            });
+  }
+
+  private void addTextData(
+      Map<String, Object> addTo, Rect boundingBox, Point[] cornerPoints, String text) {
+    addTo.put("text", text);
+
+    addTo.put("left", boundingBox.left);
+    addTo.put("top", boundingBox.top);
+    addTo.put("width", boundingBox.width());
+    addTo.put("height", boundingBox.height());
+
+    List<int[]> points = new ArrayList<>();
+    for (Point point : cornerPoints) {
+      points.add(new int[] {point.x, point.y});
+    }
+    addTo.put("points", points);
   }
 }
--- packages/firebase_ml_vision/example/lib/main.dart
+++ packages/firebase_ml_vision/example/lib/main.dart
@@ -2,32 +2,154 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new MaterialApp(home: _MyHomePage()));
 
-class MyApp extends StatefulWidget {
+class _MyHomePage extends StatefulWidget {
   @override
-  _MyAppState createState() => new _MyAppState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
+class _MyHomePageState extends State<_MyHomePage> {
+  File _imageFile;
+  Size _imageSize;
+  List<TextBlock> _textLocations;
+
+  Future<void> _getImage() async {
+    setState(() {
+      _imageFile = null;
+      _imageSize = null;
+      _textLocations = null;
+    });
+
+    final File imageFile =
+        await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _imageFile = imageFile;
+    });
+  }
+
+  Future<void> _scanImage() async {
+    final FirebaseVisionImage visionImage =
+        FirebaseVisionImage.fromFile(_imageFile);
+    final TextDetector detector = FirebaseVision.instance.getTextDetector();
+    final List<TextBlock> blocks = await detector.detectInImage(visionImage);
+
+    final Image image = Image.file(_imageFile);
+    final Size imageSize = await _getImageSize(image);
+
+    setState(() {
+      _textLocations = blocks;
+      _imageSize = imageSize;
+    });
+  }
+
+  Future<Size> _getImageSize(Image image) {
+    final Completer<Size> completer = new Completer<Size>();
+    image.image
+        .resolve(const ImageConfiguration())
+        .addListener((ImageInfo info, bool _) => completer.complete(Size(
+              info.image.width.toDouble(),
+              info.image.height.toDouble(),
+            )));
+    return completer.future;
+  }
+
+  Widget _buildImage() {
+    return new Container(
+      constraints: const BoxConstraints.expand(),
+      decoration: new BoxDecoration(
+        image: new DecorationImage(
+          image: Image.file(_imageFile).image,
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: _imageSize == null || _textLocations == null
+          ? const Center(
+              child: const Text(
+              "Scanning...",
+              style: const TextStyle(
+                color: Colors.green,
+                fontSize: 30.0,
+              ),
+            ))
+          : new CustomPaint(
+              painter: new ScannedTextPainter(_imageSize, _textLocations),
+            ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: const Center(
-          child: const Text('Hello, World!'),
-        ),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text('ML Vision Example'),
+      ),
+      body: _imageFile == null
+          ? const Center(child: const Text("No image selected."))
+          : _buildImage(),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () async {
+          await _getImage();
+          if (_imageFile != null) _scanImage();
+        },
+        tooltip: 'Pick Image',
+        child: const Icon(Icons.add_a_photo),
       ),
     );
+  }
+}
+
+class ScannedTextPainter extends CustomPainter {
+  ScannedTextPainter(this.absoluteImageSize, this.textLocations);
+
+  final Size absoluteImageSize;
+  final List<TextBlock> textLocations;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double scaleX = size.width / absoluteImageSize.width;
+    final double scaleY = size.height / absoluteImageSize.height;
+
+    Rect scaleRect(TextContainer container) {
+      return new Rect.fromLTRB(
+        container.boundingBox.left * scaleX,
+        container.boundingBox.top * scaleY,
+        container.boundingBox.right * scaleX,
+        container.boundingBox.bottom * scaleY,
+      );
+    }
+
+    final Paint paint = new Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0;
+
+    for (TextBlock block in textLocations) {
+      for (TextLine line in block.lines) {
+        for (TextElement element in line.elements) {
+          paint.color = Colors.green;
+          canvas.drawRect(scaleRect(element), paint);
+        }
+
+        paint.color = Colors.yellow;
+        canvas.drawRect(scaleRect(line), paint);
+      }
+
+      paint.color = Colors.red;
+      canvas.drawRect(scaleRect(block), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(ScannedTextPainter oldDelegate) {
+    return oldDelegate.absoluteImageSize != absoluteImageSize ||
+        oldDelegate.textLocations != textLocations;
   }
 }
