2e2b864391c17051a741cad92e1b0e7286b1ec9d
Maurice Parrish <bparr2450@gmail.com>
2018-06-22 16:11:53 -0700

    Setup ios text detector (#627)
    * Initial 'fluter create' for Firebase ML Vision plugin
    
    * Formatting
    
    * More Formatting
    
    * Import ordering
    
    * Added android app to firebase
    
    * Added ios app to firebase
    
    * Added ml vision to android and ios
    
    * WIP dart side of ml vision
    
    * finish dart side of ml vision and text detector
    
    * Added TextDetector tests
    
    * Added some comments
    
    * Finished comments
    
    * Removed usused dependency
    
    * Formatting and unused imports
    
    * Changed variable name to restart WIP test
    
    * Start of android text detection
    
    * Added text recognition to android channel
    
    * Create dart test app
    
    * Added to test app
    
    * Finished example ml vision app
    
    * Formatting
    
    * Fixed crash
    
    * Add permssions
    
    * Setup iOS side of text detector
    
    * Formatting and comments
    
    * More formattign
    
    * More More Formatting
    
    * For loops and a correction

--- packages/firebase_ml_vision/ios/Classes/FirebaseMlVisionPlugin.m
+++ packages/firebase_ml_vision/ios/Classes/FirebaseMlVisionPlugin.m
@@ -2,12 +2,28 @@
 
 #import "Firebase/Firebase.h"
 
-@implementation FirebaseMlVisionPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel =
+@interface NSError (FlutterError)
+@property(readonly, nonatomic) FlutterError *flutterError;
+@end
+
+@implementation NSError (FlutterError)
+- (FlutterError *)flutterError {
+  return [FlutterError errorWithCode:[NSString stringWithFormat:@"Error %d", (int)self.code]
+                             message:self.domain
+                             details:self.localizedDescription];
+}
+@end
+
+@interface FLTFirebaseMlVisionPlugin ()
+@property(nonatomic, retain) FIRVisionTextDetector *textDetector;
+@end
+
+@implementation FLTFirebaseMlVisionPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
+  FlutterMethodChannel *channel =
       [FlutterMethodChannel methodChannelWithName:@"plugins.flutter.io/firebase_ml_vision"
                                   binaryMessenger:[registrar messenger]];
-  FirebaseMlVisionPlugin* instance = [[FirebaseMlVisionPlugin alloc] init];
+  FLTFirebaseMlVisionPlugin *instance = [[FLTFirebaseMlVisionPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
@@ -21,12 +37,122 @@
   return self;
 }
 
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+- (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
+  NSString *filePath = call.arguments;
+  UIImage *image = [UIImage imageWithContentsOfFile:filePath];
+  FIRVisionImage *visionImage = [[FIRVisionImage alloc] initWithImage:image];
+
+  if ([@"TextDetector#detectInImage" isEqualToString:call.method]) {
+    [self handleTextDetectionResult:visionImage result:result];
   } else {
     result(FlutterMethodNotImplemented);
   }
 }
 
+- (void)handleTextDetectionResult:(FIRVisionImage *)image result:(FlutterResult)result {
+  FIRVision *vision = [FIRVision vision];
+  if (_textDetector == nil) _textDetector = [vision textDetector];
+
+  [_textDetector
+      detectInImage:image
+         completion:^(NSArray<id<FIRVisionText>> *_Nullable features, NSError *_Nullable error) {
+           if (error) {
+             result([error flutterError]);
+             return;
+           } else if (!features) {
+             result(@[]);
+             return;
+           }
+
+           NSMutableArray *blocks = [NSMutableArray array];
+           for (id<FIRVisionText> feature in features) {
+             NSMutableDictionary *blockData = [NSMutableDictionary dictionary];
+             if ([feature isKindOfClass:[FIRVisionTextBlock class]]) {
+               FIRVisionTextBlock *block = (FIRVisionTextBlock *)feature;
+
+               [blockData addEntriesFromDictionary:[self getTextData:block.frame
+                                                        cornerPoints:block.cornerPoints
+                                                                text:block.text]];
+               blockData[@"lines"] = [self getLineData:block.lines];
+             } else if ([feature isKindOfClass:[FIRVisionTextLine class]]) {
+               // We structure the return data to have the line be inside a FIRVisionTextBlock.
+               FIRVisionTextLine *line = (FIRVisionTextLine *)feature;
+
+               [blockData addEntriesFromDictionary:[self getTextData:line.frame
+                                                        cornerPoints:line.cornerPoints
+                                                                text:line.text]];
+               NSArray<FIRVisionTextLine *> *lines = @[ line ];
+               blockData[@"lines"] = [self getLineData:lines];
+             } else if ([feature isKindOfClass:[FIRVisionTextElement class]]) {
+               // We structure the return data to have the element inside a FIRVisionTextLine
+               // that is inside a FIRVisionTextBlock.
+               FIRVisionTextElement *element = (FIRVisionTextElement *)feature;
+
+               [blockData addEntriesFromDictionary:[self getTextData:element.frame
+                                                        cornerPoints:element.cornerPoints
+                                                                text:element.text]];
+
+               NSMutableDictionary *lineData = [NSMutableDictionary dictionary];
+               [lineData addEntriesFromDictionary:[self getTextData:element.frame
+                                                       cornerPoints:element.cornerPoints
+                                                               text:element.text]];
+
+               NSArray<FIRVisionTextElement *> *elements = @[ element ];
+               lineData[@"elements"] = [self getElementData:elements];
+
+               blockData[@"lines"] = lineData;
+             }
+
+             [blocks addObject:blockData];
+           }
+
+           result(blocks);
+         }];
+}
+
+- (NSDictionary *)getTextData:(CGRect)frame
+                 cornerPoints:(NSArray<NSValue *> *)cornerPoints
+                         text:(NSString *)text {
+  __block NSMutableArray<NSArray *> *points = [NSMutableArray array];
+
+  for (NSValue *point in points) {
+    [points addObject:@[ @(((__bridge CGPoint *)point)->x), @(((__bridge CGPoint *)point)->y) ]];
+  }
+
+  return @{
+    @"text" : text,
+    @"left" : @(frame.origin.x),
+    @"top" : @(frame.origin.y),
+    @"width" : @(frame.size.width),
+    @"height" : @(frame.size.height),
+    @"points" : points,
+  };
+}
+
+- (NSMutableArray *)getLineData:(NSArray<FIRVisionTextLine *> *)lines {
+  NSMutableArray *lineDataArray = [NSMutableArray array];
+
+  for (FIRVisionTextLine *line in lines) {
+    NSMutableDictionary *lineData = [NSMutableDictionary dictionary];
+    [lineData addEntriesFromDictionary:[self getTextData:line.frame
+                                            cornerPoints:line.cornerPoints
+                                                    text:line.text]];
+    lineData[@"elements"] = [self getElementData:line.elements];
+    [lineDataArray addObject:lineData];
+  }
+
+  return lineDataArray;
+}
+
+- (NSMutableArray *)getElementData:(NSArray<FIRVisionTextElement *> *)elements {
+  NSMutableArray *elementDataArray = [NSMutableArray array];
+
+  for (FIRVisionTextElement *element in elements) {
+    [elementDataArray addObject:[self getTextData:element.frame
+                                     cornerPoints:element.cornerPoints
+                                             text:element.text]];
+  }
+
+  return elementDataArray;
+}
 @end
