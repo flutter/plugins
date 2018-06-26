5db9148652cc489eef321f24f414d8412c6697dd
pyros2097 <pyros2097@gmail.com>
2018-06-26 14:00:12 +0530

    video_player: use big_buck_bunny for hls example

--- packages/video_player/example/lib/main.dart
+++ packages/video_player/example/lib/main.dart
@@ -385,7 +385,7 @@ void main() {
                         new AspectRatioVideo(controller),
                   ),
                   new NetworkPlayerLifeCycle(
-                    'http://live.cloud6.in:1935/simskasri/limitless/playlist.m3u8',
+                    'http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8',
                     (BuildContext context, VideoPlayerController controller) =>
                         new AspectRatioVideo(controller),
                   ),
