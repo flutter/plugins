56272e420a2f3be1469cf2fca4936d9563ca434c
dustin <dcgraham7@gmail.com>
2018-07-10 14:30:06 -0700

    ios: Add basic barcode scanning.

--- packages/firebase_ml_vision/example/ios/Runner/AppDelegate.m
+++ packages/firebase_ml_vision/example/ios/Runner/AppDelegate.m
@@ -1,10 +1,12 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+@import Firebase;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [FIRApp configure];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
--- packages/firebase_ml_vision/ios/Classes/BarcodeDetector.m
+++ packages/firebase_ml_vision/ios/Classes/BarcodeDetector.m
@@ -1,4 +1,51 @@
 #import "FirebaseMlVisionPlugin.h"
 
 @implementation BarcodeDetector
+static FIRVisionBarcodeDetector *barcodeDetector;
+
++ (void)handleDetection:(FIRVisionImage *)image result:(FlutterResult)result {
+  if (barcodeDetector == nil) {
+    FIRVision *vision = [FIRVision vision];
+    barcodeDetector = [vision barcodeDetector];
+  }
+  
+  [barcodeDetector
+   detectInImage:image
+   completion:^(NSArray<FIRVisionBarcode *> * _Nullable barcodes, NSError * _Nullable error) {
+     if (error) {
+       [FLTFirebaseMlVisionPlugin handleError:error result:result];
+       return;
+     } else if (!barcodes) {
+       result(@[]);
+       return;
+     }
+     
+     NSMutableArray *blocks = [NSMutableArray array];
+     for (FIRVisionBarcode *barcode in barcodes) {
+       NSDictionary *barcodeData = [BarcodeDetector getBarcodeData:barcode];
+       [blocks addObject:barcodeData];
+     }
+     
+     result(blocks);
+   }];
+}
+
++ (void)close {
+  barcodeDetector = nil;
+}
+
+
++ (NSDictionary *)getBarcodeData:(FIRVisionBarcode *)barcode {
+  CGRect frame = barcode.frame;
+  NSString *displayValue = barcode.displayValue == nil ? @"" : barcode.displayValue;
+  NSString *rawValue = barcode.rawValue == nil ? @"" : barcode.rawValue;
+  return @{
+    @"left" : @((int)frame.origin.x),
+    @"top" : @((int)frame.origin.y),
+    @"width" : @((int)frame.size.width),
+    @"height" : @((int)frame.size.height),
+    @"barcode_display_value" : displayValue,
+    @"barcode_raw_value" : rawValue
+  };
+}
 @end
