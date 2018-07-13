a9bb779b2c2a8db4db3cbb2a79e996b59cd026e8
Tobe O <thosakwe@gmail.com>
2018-07-13 19:19:34 -0400

    `closeWebView` simply finishes the current activity, if one is present

--- packages/url_launcher/android/src/main/java/io/flutter/plugins/urllauncher/UrlLauncherPlugin.java
+++ packages/url_launcher/android/src/main/java/io/flutter/plugins/urllauncher/UrlLauncherPlugin.java
@@ -84,7 +84,11 @@ public class UrlLauncherPlugin implements MethodCallHandler {
   }
 
   private void closeWebView(Result result) {
-    
+    Activity activity = mRegistrar.activity();
+
+    if (activity != null) {
+      activity.finish();
+    }
   }
 
   /*  Launches WebView activity */
