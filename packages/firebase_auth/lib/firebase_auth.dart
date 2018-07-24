212f608116fc2f2edcde4b702e81c2ff45ed9d9a
Feu <fabricio@feu.com.br>
2018-07-24 15:39:53 -0300

    Adding support to setLanguageCode to FirebaseAuth (#683)
    * Adding support to setLanguageCode to FirebaseAuth

--- packages/firebase_auth/lib/firebase_auth.dart
+++ packages/firebase_auth/lib/firebase_auth.dart
@@ -411,6 +411,16 @@ class FirebaseAuth {
     return currentUser;
   }
 
+  /// Sets the user-facing language code for auth operations that can be
+  /// internationalized, such as [sendEmailVerification]. This language
+  /// code should follow the conventions defined by the IETF in BCP47.
+  Future<void> setLanguageCode(String language) async {
+    assert(language != null);
+    await FirebaseAuth.channel.invokeMethod('setLanguageCode', <String, String>{
+      'language': language,
+    });
+  }
+
   Future<Null> _callHandler(MethodCall call) async {
     switch (call.method) {
       case 'onAuthStateChanged':
