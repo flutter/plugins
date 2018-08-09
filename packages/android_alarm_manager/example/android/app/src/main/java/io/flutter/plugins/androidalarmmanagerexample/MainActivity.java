0289c36b9c9cd87f6a154f5604c2d48079a9cdb2
Ben Konyi <bkonyi@google.com>
2018-08-09 09:09:49 -0700

    Update MainActivity.java

--- packages/android_alarm_manager/example/android/app/src/main/java/io/flutter/plugins/androidalarmmanagerexample/MainActivity.java
+++ packages/android_alarm_manager/example/android/app/src/main/java/io/flutter/plugins/androidalarmmanagerexample/MainActivity.java
@@ -1,11 +1,8 @@
 package io.flutter.plugins.androidalarmmanagerexample;
 
 import android.os.Bundle;
-import android.util.Log;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-import io.flutter.plugins.androidalarmmanager.AlarmService;
-import io.flutter.view.FlutterNativeView;
 
 public class MainActivity extends FlutterActivity {
   public static final String TAG = "AlarmExampleMainActivity";
