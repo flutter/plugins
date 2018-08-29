8fba0a957aa2aba12d3fe22d85fabd6dd3a7e2a9
Amir Hardon <amirha@google.com>
2018-08-29 15:41:49 -0700

    fix analyzer errors

--- packages/webview_flutter/example/lib/main.dart
+++ packages/webview_flutter/example/lib/main.dart
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
--- packages/webview_flutter/lib/webview_flutter.dart
+++ packages/webview_flutter/lib/webview_flutter.dart
@@ -7,8 +7,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class WebviewFlutter {
-  static const MethodChannel _channel =
-      const MethodChannel('webview_flutter');
+  static const MethodChannel _channel = MethodChannel('webview_flutter');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
