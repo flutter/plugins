404e37ba5184a6e5ef469dfa8a590b39179f8166
Amir Hardon <amirha@google.com>
2018-08-29 14:29:06 -0700

    add license to source files

--- packages/webview_flutter/example/test/widget_test.dart
+++ packages/webview_flutter/example/test/widget_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // This is a basic Flutter widget test.
 // To perform an interaction with a widget in your test, use the WidgetTester utility that Flutter
 // provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
