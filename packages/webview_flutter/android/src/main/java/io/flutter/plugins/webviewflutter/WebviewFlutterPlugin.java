e4f33da8660e1784396fe64330b45ed399ed5b2a
Amir Hardon <amirh@users.noreply.github.com>
2018-08-29 20:10:45 -0700

    Plugin template for webview_flutter (#746)

---
+++ packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/WebviewFlutterPlugin.java
@@ -0,0 +1,25 @@
+package io.flutter.plugins.webviewflutter;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** WebviewFlutterPlugin */
+public class WebviewFlutterPlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "webview_flutter");
+    channel.setMethodCallHandler(new WebviewFlutterPlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+}
