4f18bce851abbb0b60ed9092e3b1ade35c421de2
gildaswise <gildasiogx@gmail.com>
2018-09-03 09:22:13 -0300

    Added FirestoreList and AnimatedFirestoreList equivalent to their RDB counterparts

---
+++ packages/cloud_firestore/lib/src/ui/animated_firestore_list.dart
@@ -0,0 +1,247 @@
+// Copyright 2017, the Flutter project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../../cloud_firestore.dart';
+import 'firestore_list.dart';
+
+typedef Widget FirestoreAnimatedListItemBuilder(
+  BuildContext context,
+  DocumentSnapshot snapshot,
+  Animation<double> animation,
+  int index,
+);
+
+/// An AnimatedList widget that is bound to a query
+class FirestoreAnimatedList extends StatefulWidget {
+  /// Creates a scrolling container that animates items when they are inserted or removed.
+  FirestoreAnimatedList({
+    Key key,
+    @required this.query,
+    @required this.itemBuilder,
+    this.defaultChild,
+    this.errorChild,
+    this.emptyChild,
+    this.scrollDirection = Axis.vertical,
+    this.reverse = false,
+    this.controller,
+    this.primary,
+    this.physics,
+    this.shrinkWrap = false,
+    this.padding,
+    this.duration = const Duration(milliseconds: 300),
+  }) : super(key: key) {
+    assert(itemBuilder != null);
+  }
+
+  /// A Firestore query to use to populate the animated list
+  final Stream<QuerySnapshot> query;
+
+  /// A widget to display while the query is loading. Defaults to a
+  /// centered [CircularProgressIndicator];
+  final Widget defaultChild;
+
+  /// A widget to display if an error ocurred. Defaults to a
+  /// centered [Column] with `Icon.error` and the error itsef;
+  final Widget errorChild;
+
+  /// A widget to display if the query returns empty. Defaults to a
+  /// `Container()`;
+  final Widget emptyChild;
+
+  /// Called, as needed, to build list item widgets.
+  ///
+  /// List items are only built when they're scrolled into view.
+  ///
+  /// The [DocumentSnapshot] parameter indicates the snapshot that should be used
+  /// to build the item.
+  ///
+  /// Implementations of this callback should assume that [AnimatedList.removeItem]
+  /// removes an item immediately.
+  final FirestoreAnimatedListItemBuilder itemBuilder;
+
+  /// The axis along which the scroll view scrolls.
+  ///
+  /// Defaults to [Axis.vertical].
+  final Axis scrollDirection;
+
+  /// Whether the scroll view scrolls in the reading direction.
+  ///
+  /// For example, if the reading direction is left-to-right and
+  /// [scrollDirection] is [Axis.horizontal], then the scroll view scrolls from
+  /// left to right when [reverse] is false and from right to left when
+  /// [reverse] is true.
+  ///
+  /// Similarly, if [scrollDirection] is [Axis.vertical], then the scroll view
+  /// scrolls from top to bottom when [reverse] is false and from bottom to top
+  /// when [reverse] is true.
+  ///
+  /// Defaults to false.
+  final bool reverse;
+
+  /// An object that can be used to control the position to which this scroll
+  /// view is scrolled.
+  ///
+  /// Must be null if [primary] is true.
+  final ScrollController controller;
+
+  /// Whether this is the primary scroll view associated with the parent
+  /// [PrimaryScrollController].
+  ///
+  /// On iOS, this identifies the scroll view that will scroll to top in
+  /// response to a tap in the status bar.
+  ///
+  /// Defaults to true when [scrollDirection] is [Axis.vertical] and
+  /// [controller] is null.
+  final bool primary;
+
+  /// How the scroll view should respond to user input.
+  ///
+  /// For example, determines how the scroll view continues to animate after the
+  /// user stops dragging the scroll view.
+  ///
+  /// Defaults to matching platform conventions.
+  final ScrollPhysics physics;
+
+  /// Whether the extent of the scroll view in the [scrollDirection] should be
+  /// determined by the contents being viewed.
+  ///
+  /// If the scroll view does not shrink wrap, then the scroll view will expand
+  /// to the maximum allowed size in the [scrollDirection]. If the scroll view
+  /// has unbounded constraints in the [scrollDirection], then [shrinkWrap] must
+  /// be true.
+  ///
+  /// Shrink wrapping the content of the scroll view is significantly more
+  /// expensive than expanding to the maximum allowed size because the content
+  /// can expand and contract during scrolling, which means the size of the
+  /// scroll view needs to be recomputed whenever the scroll position changes.
+  ///
+  /// Defaults to false.
+  final bool shrinkWrap;
+
+  /// The amount of space by which to inset the children.
+  final EdgeInsets padding;
+
+  /// The duration of the insert and remove animation.
+  ///
+  /// Defaults to const Duration(milliseconds: 300).
+  final Duration duration;
+
+  @override
+  FirestoreAnimatedListState createState() => new FirestoreAnimatedListState();
+}
+
+class FirestoreAnimatedListState extends State<FirestoreAnimatedList> {
+  final GlobalKey<AnimatedListState> _animatedListKey =
+      new GlobalKey<AnimatedListState>();
+  List<DocumentSnapshot> _model;
+  String _error;
+  bool _loaded = false;
+
+  @override
+  void didChangeDependencies() {
+    _model = FirestoreList(
+      query: widget.query,
+      onDocumentAdded: _onDocumentAdded,
+      onDocumentRemoved: _onDocumentRemoved,
+      onDocumentChanged: _onDocumentChanged,
+      onValue: _onValue,
+      onError: _onError,
+      debug: true,
+    );
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    // Cancel the Firebase stream subscriptions
+    _model.clear();
+
+    super.dispose();
+  }
+
+  void _onError(Error error) {
+    setState(() {
+      error = error;
+    });
+  }
+
+  void _onDocumentAdded(int index, DocumentSnapshot snapshot) {
+    if (!_loaded) {
+      return; // AnimatedList is not created yet
+    }
+    _animatedListKey.currentState.insertItem(index, duration: widget.duration);
+  }
+
+  void _onDocumentRemoved(int index, DocumentSnapshot snapshot) {
+    // The child should have already been removed from the model by now
+    assert(index >= _model.length ||
+        _model[index].documentID != snapshot.documentID);
+    _animatedListKey.currentState.removeItem(
+      index,
+      (BuildContext context, Animation<double> animation) {
+        return widget.itemBuilder(context, snapshot, animation, index);
+      },
+      duration: widget.duration,
+    );
+  }
+
+  // No animation, just update contents
+  void _onDocumentChanged(int index, DocumentSnapshot snapshot) {
+    setState(() {});
+  }
+
+  void _onValue(DocumentSnapshot _) {
+    setState(() {
+      _loaded = true;
+    });
+  }
+
+  Widget _buildItem(
+      BuildContext context, int index, Animation<double> animation) {
+    return widget.itemBuilder(context, _model[index], animation, index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_model == null || _model.isEmpty) {
+      return widget.emptyChild ?? Container();
+    }
+
+    if (!_loaded) {
+      return widget.defaultChild ??
+          const Center(child: CircularProgressIndicator());
+    }
+
+    if (_error != null && _error.isNotEmpty) {
+      return widget.errorChild ??
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const Icon(Icons.error),
+                Text(_error),
+              ],
+            ),
+          );
+    }
+
+    return AnimatedList(
+      key: _animatedListKey,
+      itemBuilder: _buildItem,
+      initialItemCount: _model.length,
+      scrollDirection: widget.scrollDirection,
+      reverse: widget.reverse,
+      controller: widget.controller,
+      primary: widget.primary,
+      physics: widget.physics,
+      shrinkWrap: widget.shrinkWrap,
+      padding: widget.padding,
+    );
+  }
+}
---
+++ packages/cloud_firestore/lib/src/ui/firestore_list.dart
@@ -0,0 +1,153 @@
+// Copyright 2017, the Flutter project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:collection';
+
+import 'package:cloud_firestore/src/utils/stream_subscriber_mixin.dart';
+import 'package:meta/meta.dart';
+
+import '../../cloud_firestore.dart';
+
+typedef void DocumentCallback(int index, DocumentSnapshot snapshot);
+typedef void ValueCallback(DocumentSnapshot snapshot);
+typedef void ErrorCallback(Error error);
+
+/// Sorts the results of `query` on the client side using `DataSnapshot.key`.
+class FirestoreList extends ListBase<DocumentSnapshot>
+    with StreamSubscriberMixin<QuerySnapshot> {
+  FirestoreList({
+    @required this.query,
+    this.onDocumentAdded,
+    this.onDocumentRemoved,
+    this.onDocumentChanged,
+    this.onValue,
+    this.onError,
+    this.debug = false,
+  }) {
+    assert(query != null);
+    listen(query, _onData, onError: _onError);
+  }
+
+  /// Database query used to populate the list
+  final Stream<QuerySnapshot> query;
+
+  // Whether or not to show debug logs
+  final bool debug;
+
+  static const String TAG = "FIRESTORE_LIST";
+
+  /// Called when the Document has been added
+  final DocumentCallback onDocumentAdded;
+
+  /// Called when the Document has been removed
+  final DocumentCallback onDocumentRemoved;
+
+  /// Called when the Document has changed
+  final DocumentCallback onDocumentChanged;
+
+  /// Called when the data of the list has finished loading
+  final ValueCallback onValue;
+
+  /// Called when an error is reported (e.g. permission denied)
+  final ErrorCallback onError;
+
+  // ListBase implementation
+  final List<DocumentSnapshot> _snapshots = <DocumentSnapshot>[];
+
+  @override
+  int get length => _snapshots.length;
+
+  @override
+  set length(int value) {
+    throw new UnsupportedError("List cannot be modified.");
+  }
+
+  @override
+  DocumentSnapshot operator [](int index) => _snapshots[index];
+
+  @override
+  void operator []=(int index, DocumentSnapshot value) {
+    throw new UnsupportedError("List cannot be modified.");
+  }
+
+  @override
+  void clear() {
+    cancelSubscriptions();
+
+    // Do not call super.clear(), it will set the length, it's unsupported.
+  }
+
+  void _log(String message) {
+    if (debug) print("[$TAG] $message");
+  }
+
+  int _indexForKey(String key) {
+    assert(key != null);
+    for (int index = 0; index < _snapshots.length; index++) {
+      if (key == _snapshots[index].documentID) {
+        _log("Returning $index from _indexForKey($key)");
+        return index;
+      }
+    }
+    return null;
+  }
+
+  void _onData(QuerySnapshot snapshot) {
+    if (_snapshots.isEmpty) {
+      _log("Adding all values from query");
+      _snapshots.addAll(snapshot.documents.map(_onValue));
+    } else if (snapshot.documentChanges.isNotEmpty) {
+      for (DocumentChange change in snapshot.documentChanges) {
+        switch (change.type) {
+          case DocumentChangeType.added:
+            _onDocumentAdded(change);
+            break;
+          case DocumentChangeType.modified:
+            _onDocumentChanged(change);
+            break;
+          case DocumentChangeType.removed:
+            _onDocumentRemoved(change);
+            break;
+        }
+      }
+    }
+  }
+
+  void _onDocumentAdded(DocumentChange event) {
+    int index = 0;
+    if (event.oldIndex != null) {
+      index = event.oldIndex + 1;
+    }
+    _log("Calling _onDocumentAdded for document ${event.document.documentID}");
+    _snapshots.insert(index, event.document);
+    onDocumentAdded(index, event.document);
+  }
+
+  void _onDocumentRemoved(DocumentChange event) {
+    final int index = _indexForKey(event.document.documentID);
+    _log(
+        "Calling _onDocumentRemoved for document ${event.document.documentID}");
+    _snapshots.removeAt(index);
+    onDocumentRemoved(index, event.document);
+  }
+
+  void _onDocumentChanged(DocumentChange event) {
+    final int index = _indexForKey(event.document.documentID);
+    _log("Calling onDocumentChanged for document ${event.document.documentID}");
+    _snapshots[index] = event.document;
+    onDocumentChanged(index, event.document);
+  }
+
+  DocumentSnapshot _onValue(DocumentSnapshot document) {
+    _log("Calling onValue for document ${document.documentID}");
+    onValue(document);
+    return document;
+  }
+
+  void _onError(Object o) {
+    final Error error = o;
+    onError?.call(error);
+  }
+}
