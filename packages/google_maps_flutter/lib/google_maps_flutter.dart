f2d0bc7038c6c3fdb72fb455b6d29252fae2a1fa
Amir Hardon <amirh@users.noreply.github.com>
2018-09-06 13:18:27 -0700

    Add a gestureRecognizers parameter to GoogleMap. (#755)
    These gesture recognizers participates in gesture arenas on behalf of
    the map.
    This allows fine grain control over which gestures are dispatched to
    the map.

--- packages/google_maps_flutter/lib/google_maps_flutter.dart
+++ packages/google_maps_flutter/lib/google_maps_flutter.dart
@@ -8,6 +8,7 @@ import 'dart:async';
 import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
