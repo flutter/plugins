9d0e88049d905f903bee3dbbfed09c7b3e1c963a
Amir Hardon <amirh@users.noreply.github.com>
2018-09-06 13:18:27 -0700

    Add a gestureRecognizers parameter to GoogleMap. (#755)
    These gesture recognizers participates in gesture arenas on behalf of
    the map.
    This allows fine grain control over which gestures are dispatched to
    the map.

--- packages/google_maps_flutter/lib/src/google_map.dart
+++ packages/google_maps_flutter/lib/src/google_map.dart
@@ -1,13 +1,33 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 part of google_maps_flutter;
 
 typedef void MapCreatedCallback(GoogleMapController controller);
 
 class GoogleMap extends StatefulWidget {
-  GoogleMap({@required this.onMapCreated, GoogleMapOptions options})
-      : this.options = GoogleMapOptions.defaultOptions.copyWith(options);
+  GoogleMap({
+    @required this.onMapCreated,
+    GoogleMapOptions options,
+    this.gestureRecognizers = const <OneSequenceGestureRecognizer>[],
+  })  : assert(gestureRecognizers != null),
+        this.options = GoogleMapOptions.defaultOptions.copyWith(options);
 
   final MapCreatedCallback onMapCreated;
+
   final GoogleMapOptions options;
+
+  /// Which gestures should be consumed by the map.
+  ///
+  /// It is possible for other gesture recognizers to be competing with the map on pointer
+  /// events, e.g if the map is inside a [ListView] the [ListView] will want to handle
+  /// vertical drags. The map will claim gestures that are recognized by any of the
+  /// recognizers on this list.
+  ///
+  /// When this list is empty, the map will only handle pointer events for gestures that
+  /// were not claimed by any other gesture recognizer.
+  final List<OneSequenceGestureRecognizer> gestureRecognizers;
 
   @override
   State createState() => new _GoogleMapState();
@@ -20,6 +40,7 @@ class _GoogleMapState extends State<GoogleMap> {
       return AndroidView(
         viewType: 'plugins.flutter.io/google_maps',
         onPlatformViewCreated: onPlatformViewCreated,
+        gestureRecognizers: widget.gestureRecognizers,
         creationParams: widget.options._toJson(),
         creationParamsCodec: const StandardMessageCodec(),
       );
