a4ae6642e07b3610ca5e48bfa8b5647fc489f1b7
Dennis Alund <dennis.alund@gmail.com>
2018-09-07 11:22:50 +0800

    Fixing code formatting issues

--- packages/firebase_ml_vision/lib/src/vision_cloud_detector_options.dart
+++ packages/firebase_ml_vision/lib/src/vision_cloud_detector_options.dart
@@ -12,8 +12,7 @@ class VisionCloudDetectorOptions {
   const VisionCloudDetectorOptions(
       {this.maxResults = 10, this.modelType = modelTypeStable})
       : assert(maxResults > 0),
-        assert(
-            modelType == modelTypeLatest || modelType == modelTypeStable);
+        assert(modelType == modelTypeLatest || modelType == modelTypeStable);
 
   static const int modelTypeStable = 1;
   static const int modelTypeLatest = 2;
@@ -25,7 +24,7 @@ class VisionCloudDetectorOptions {
   final int maxResults;
 
   /// The type of model to use for the detection..
-  /// 
+  ///
   /// Defaults to [modelTypeStable]
   /// Required to be [modelTypeStable] or [modelTypeLatest].
   final int modelType;
