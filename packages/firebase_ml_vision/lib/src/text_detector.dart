1514db4c59a7ef0c8a423ddcfdc37a8475548d74
Maurice Parrish <bmparr@google.com>
2018-09-28 16:23:04 -0700

    PR to show filename diffs

--- packages/firebase_ml_vision/lib/src/text_detector.dart
+++ packages/firebase_ml_vision/lib/src/text_detector.dart
@@ -6,56 +6,83 @@ part of firebase_ml_vision;
 
 /// Detector for performing optical character recognition(OCR) on an input image.
 ///
-/// A text detector is created via `textDetector()` in [FirebaseVision]:
+/// A text recognizer is created via `textRecognizer()` in [FirebaseVision]:
 ///
 /// ```dart
-/// TextDetector textDetector = FirebaseVision.instance.textDetector();
+/// TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
 /// ```
-class TextDetector implements FirebaseVisionDetector {
-  TextDetector._();
+class TextRecognizer implements FirebaseVisionDetector {
+  TextRecognizer._();
 
-  /// Detects text in the input image.
+  /// Detects [VisionText] from a [FirebaseVisionImage].
   ///
   /// The OCR is performed asynchronously.
-  @override
-  Future<List<TextBlock>> detectInImage(FirebaseVisionImage visionImage) async {
-    final List<dynamic> reply = await FirebaseVision.channel.invokeMethod(
-      'TextDetector#detectInImage',
+  Future<VisionText> processImage(FirebaseVisionImage visionImage) async {
+    final Map<dynamic, dynamic> reply =
+    await FirebaseVision.channel.invokeMethod(
+      'TextRecognizer#detectInImage',
       <String, dynamic>{
         'path': visionImage.imageFile.path,
         'options': <String, dynamic>{},
       },
     );
 
-    final List<TextBlock> blocks = <TextBlock>[];
-    reply.forEach((dynamic block) {
-      blocks.add(TextBlock._(block));
-    });
-
-    return blocks;
+    return VisionText._(reply);
   }
+
+  /// Detects [VisionText] from a [FirebaseVisionImage].
+  ///
+  /// The OCR is performed asynchronously.
+  @override
+  Future<VisionText> detectInImage(FirebaseVisionImage visionImage) async {
+    return processImage(visionImage);
+  }
+}
+
+/// Recognized text in an image.
+class VisionText {
+  VisionText._(Map<dynamic, dynamic> data)
+      : text = data['text'],
+        blocks = List<TextBlock>.unmodifiable(data['blocks']
+      .map<TextBlock>((dynamic block) => TextBlock._(block)));
+
+  /// String representation of the recognized text.
+  final String text;
+
+  /// All recognized text broken down into individual blocks/paragraphs.
+  final List<TextBlock> blocks;
+}
+
+/// Detected language from text recognition.
+class RecognizedLanguage {
+  RecognizedLanguage._(dynamic data) : languageCode = data['languageCode'];
+
+  /// The BCP-47 language code, such as, en-US or sr-Latn. For more information,
+  /// see http://www.unicode.org/reports/tr35/#Unicode_locale_identifier.
+  final String languageCode;
 }
 
 /// Abstract class representing dimensions of recognized text in an image.
 abstract class TextContainer {
   TextContainer._(Map<dynamic, dynamic> data)
       : boundingBox = data['left'] != null
-            ? Rectangle<int>(
-                data['left'],
-                data['top'],
-                data['width'],
-                data['height'],
-              )
-            : null,
-        _cornerPoints = data['points']
-            .map<Point<int>>((dynamic item) => Point<int>(
-                  item[0],
-                  item[1],
-                ))
-            .toList(),
-        text = data['text'];
-
-  final List<Point<int>> _cornerPoints;
+      ? Rectangle<int>(
+    data['left'],
+    data['top'],
+    data['width'],
+    data['height'],
+  )
+      : null,
+        confidence = data['confidence'],
+        cornerPoints = List<Point<int>>.unmodifiable(
+  data['points'].map<Point<int>>((dynamic point) => Point<int>(
+  point[0],
+  point[1],
+  ))),
+  recognizedLanguages = List<RecognizedLanguage>.unmodifiable(
+  data['recognizedLanguages'].map<RecognizedLanguage>(
+  (dynamic language) => RecognizedLanguage._(language))),
+  text = data['text'];
 
   /// Axis-aligned bounding rectangle of the detected text.
   ///
@@ -64,11 +91,11 @@ abstract class TextContainer {
   /// Could be null even if text is found.
   final Rectangle<int> boundingBox;
 
-  /// The recognized text as a string.
+  /// The confidence of the recognized text block.
   ///
-  /// Returned in reading order for the language. For Latin, this is top to
-  /// bottom within a Block, and left-to-right within a Line.
-  final String text;
+  /// The value is null for all text recognizers except for cloud text
+  /// recognizers.
+  final double confidence;
 
   /// The four corner points in clockwise direction starting with top-left.
   ///
@@ -76,35 +103,42 @@ abstract class TextContainer {
   /// rectangle. Parts of the region could be outside of the image.
   ///
   /// Could be empty even if text is found.
-  List<Point<int>> get cornerPoints => List<Point<int>>.from(_cornerPoints);
+  final List<Point<int>> cornerPoints;
+
+  /// All detected languages from recognized text.
+  ///
+  /// On-device text recognizers only detect Latin-based languages, while cloud
+  /// text recognizers can detect multiple languages. If no languages are
+  /// recognized, the list is empty.
+  final List<RecognizedLanguage> recognizedLanguages;
+
+  /// The recognized text as a string.
+  ///
+  /// Returned in reading order for the language. For Latin, this is top to
+  /// bottom within a Block, and left-to-right within a Line.
+  final String text;
 }
 
 /// A block of text (think of it as a paragraph) as deemed by the OCR engine.
 class TextBlock extends TextContainer {
   TextBlock._(Map<dynamic, dynamic> block)
-      : _lines = block['lines']
-            .map<TextLine>((dynamic line) => TextLine._(line))
-            .toList(),
-        super._(block);
-
-  final List<TextLine> _lines;
+      : lines = List<TextLine>.unmodifiable(
+  block['lines'].map<TextLine>((dynamic line) => TextLine._(line))),
+  super._(block);
 
   /// The contents of the text block, broken down into individual lines.
-  List<TextLine> get lines => List<TextLine>.from(_lines);
+  final List<TextLine> lines;
 }
 
 /// Represents a line of text.
 class TextLine extends TextContainer {
   TextLine._(Map<dynamic, dynamic> line)
-      : _elements = line['elements']
-            .map<TextElement>((dynamic element) => TextElement._(element))
-            .toList(),
-        super._(line);
-
-  final List<TextElement> _elements;
+      : elements = List<TextElement>.unmodifiable(line['elements']
+      .map<TextElement>((dynamic element) => TextElement._(element))),
+  super._(line);
 
   /// The contents of this line, broken down into individual elements.
-  List<TextElement> get elements => List<TextElement>.from(_elements);
+  final List<TextElement> elements;
 }
 
 /// Roughly equivalent to a space-separated "word."
