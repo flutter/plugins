7d7168260c925fd1a2d4b469262e87ea918427d9
Maurice Parrish <bmparr@google.com>
2018-10-23 13:03:33 -0700

    Make variables private and change way to get app instance of auth

--- packages/firebase_auth/example/lib/main.dart
+++ packages/firebase_auth/example/lib/main.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-final FirebaseAuth _auth = FirebaseAuth();
+final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn _googleSignIn = GoogleSignIn();
 
 void main() {
