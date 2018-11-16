46e715ee324e64406872112f680728a54bf7fe8a
Patrick Boos <pboos@carecon.com>
2018-11-16 16:43:37 +0900

    Share Plugin: Add .shareFile() method (Android & iOS)
    On iOS file should be in getApplicationDocumentsDirectory()
    On Android the file should be in getExternalStorageDirectory()

--- packages/share/android/src/main/java/io/flutter/plugins/share/SharePlugin.java
+++ packages/share/android/src/main/java/io/flutter/plugins/share/SharePlugin.java
@@ -5,9 +5,12 @@
 package io.flutter.plugins.share;
 
 import android.content.Intent;
+import android.support.v4.content.FileProvider;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import android.net.Uri;
+import java.io.File;
 import java.util.Map;
 
 /** Plugin method host for presenting a share sheet via Intent */
@@ -30,14 +33,23 @@ public class SharePlugin implements MethodChannel.MethodCallHandler {
   @Override
   public void onMethodCall(MethodCall call, MethodChannel.Result result) {
     if (call.method.equals("share")) {
-      if (!(call.arguments instanceof Map)) {
-        throw new IllegalArgumentException("Map argument expected");
-      }
+      expectMapArguments(call);
       // Android does not support showing the share sheet at a particular point on screen.
       share((String) call.argument("text"));
       result.success(null);
+    } else if (call.method.equals("shareFile")) {
+      expectMapArguments(call);
+      // Android does not support showing the share sheet at a particular point on screen.
+      shareFile((String) call.argument("path"), (String) call.argument("mimeType"));
+      result.success(null);
     } else {
       result.notImplemented();
+    }
+  }
+
+  private void expectMapArguments(MethodCall call) throws IllegalArgumentException {
+    if (!(call.arguments instanceof Map)) {
+      throw new IllegalArgumentException("Map argument expected");
     }
   }
 
@@ -50,6 +62,31 @@ public class SharePlugin implements MethodChannel.MethodCallHandler {
     shareIntent.setAction(Intent.ACTION_SEND);
     shareIntent.putExtra(Intent.EXTRA_TEXT, text);
     shareIntent.setType("text/plain");
+    Intent chooserIntent = Intent.createChooser(shareIntent, null /* dialog title optional */);
+    if (mRegistrar.activity() != null) {
+      mRegistrar.activity().startActivity(chooserIntent);
+    } else {
+      chooserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      mRegistrar.context().startActivity(chooserIntent);
+    }
+  }
+
+  private void shareFile(String path, String mimeType) {
+    if (path == null || path.isEmpty()) {
+      throw new IllegalArgumentException("Non-empty path expected");
+    }
+
+    Uri uri = FileProvider.getUriForFile(
+      mRegistrar.context(),
+      mRegistrar.context().getPackageName() + ".flutter.share.provider",
+      new File(path));
+
+    Intent shareIntent = new Intent();
+    shareIntent.setAction(Intent.ACTION_SEND);
+    shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+    shareIntent.putExtra(Intent.EXTRA_SUBJECT, "subject");
+    shareIntent.putExtra(Intent.EXTRA_TEXT, "text");
+    shareIntent.setType(mimeType != null ? mimeType : "*/*");
     Intent chooserIntent = Intent.createChooser(shareIntent, null /* dialog title optional */);
     if (mRegistrar.activity() != null) {
       mRegistrar.activity().startActivity(chooserIntent);
--- packages/share/ios/Classes/SharePlugin.m
+++ packages/share/ios/Classes/SharePlugin.m
@@ -14,8 +14,19 @@ static NSString *const PLATFORM_CHANNEL = @"plugins.flutter.io/share";
                                   binaryMessenger:registrar.messenger];
 
   [shareChannel setMethodCallHandler:^(FlutterMethodCall *call, FlutterResult result) {
+    NSDictionary *arguments = [call arguments];
+    NSNumber *originX = arguments[@"originX"];
+    NSNumber *originY = arguments[@"originY"];
+    NSNumber *originWidth = arguments[@"originWidth"];
+    NSNumber *originHeight = arguments[@"originHeight"];
+
+    CGRect originRect;
+    if (originX != nil && originY != nil && originWidth != nil && originHeight != nil) {
+      originRect = CGRectMake([originX doubleValue], [originY doubleValue],
+                              [originWidth doubleValue], [originHeight doubleValue]);
+    }
+
     if ([@"share" isEqualToString:call.method]) {
-      NSDictionary *arguments = [call arguments];
       NSString *shareText = arguments[@"text"];
 
       if (shareText.length == 0) {
@@ -24,18 +35,22 @@ static NSString *const PLATFORM_CHANNEL = @"plugins.flutter.io/share";
         return;
       }
 
-      NSNumber *originX = arguments[@"originX"];
-      NSNumber *originY = arguments[@"originY"];
-      NSNumber *originWidth = arguments[@"originWidth"];
-      NSNumber *originHeight = arguments[@"originHeight"];
+      [self share:shareText
+          withController:[UIApplication sharedApplication].keyWindow.rootViewController
+                atSource:originRect];
+      result(nil);
+    } else if ([@"shareFile" isEqualToString:call.method]) {
+      NSString *path = arguments[@"path"];
+      NSString *mimeType = arguments[@"mimeType"];
 
-      CGRect originRect;
-      if (originX != nil && originY != nil && originWidth != nil && originHeight != nil) {
-        originRect = CGRectMake([originX doubleValue], [originY doubleValue],
-                                [originWidth doubleValue], [originHeight doubleValue]);
+      if (path.length == 0) {
+        result(
+            [FlutterError errorWithCode:@"error" message:@"Non-empty path expected" details:nil]);
+        return;
       }
 
-      [self share:shareText
+      [self shareFile:path
+            withMimeType:mimeType
           withController:[UIApplication sharedApplication].keyWindow.rootViewController
                 atSource:originRect];
       result(nil);
@@ -50,6 +65,31 @@ static NSString *const PLATFORM_CHANNEL = @"plugins.flutter.io/share";
           atSource:(CGRect)origin {
   UIActivityViewController *activityViewController =
       [[UIActivityViewController alloc] initWithActivityItems:@[ sharedItems ]
+                                        applicationActivities:nil];
+  activityViewController.popoverPresentationController.sourceView = controller.view;
+  if (!CGRectIsEmpty(origin)) {
+    activityViewController.popoverPresentationController.sourceRect = origin;
+  }
+  [controller presentViewController:activityViewController animated:YES completion:nil];
+}
+
++ (void)shareFile:(id)path
+      withMimeType:(id)mimeType
+    withController:(UIViewController *)controller
+          atSource:(CGRect)origin {
+  NSMutableArray *items = [[NSMutableArray alloc]init];
+
+  // TODO maybe we can just call share here?
+  if([mimeType hasPrefix:@"image/"]) {
+    UIImage *image = [UIImage imageWithContentsOfFile:path];
+    [items addObject:image];
+  } else {
+    NSURL *url = [NSURL fileURLWithPath:path];
+    [items addObject:url];
+  }
+
+  UIActivityViewController *activityViewController =
+      [[UIActivityViewController alloc] initWithActivityItems:items
                                         applicationActivities:nil];
   activityViewController.popoverPresentationController.sourceView = controller.view;
   if (!CGRectIsEmpty(origin)) {
