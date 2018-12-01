4f8d76e3de0d2cb88891963262ac96df97e48929
Tomas Radvansky <radvansky.tomas@gmail.com>
2018-12-01 22:00:31 +1300

    Update Firebase Messaging SDK, Remove deprecated methods/services

--- packages/firebase_messaging/android/src/main/java/io/flutter/plugins/firebasemessaging/FirebaseMessagingPlugin.java
+++ packages/firebase_messaging/android/src/main/java/io/flutter/plugins/firebasemessaging/FirebaseMessagingPlugin.java
@@ -52,7 +52,6 @@ public class FirebaseMessagingPlugin extends BroadcastReceiver
     FirebaseApp.initializeApp(registrar.context());
 
     IntentFilter intentFilter = new IntentFilter();
-    intentFilter.addAction(FlutterFirebaseInstanceIDService.ACTION_TOKEN);
     intentFilter.addAction(FlutterFirebaseMessagingService.ACTION_REMOTE_MESSAGE);
     LocalBroadcastManager manager = LocalBroadcastManager.getInstance(registrar.context());
     manager.registerReceiver(this, intentFilter);
@@ -67,8 +66,8 @@ public class FirebaseMessagingPlugin extends BroadcastReceiver
       return;
     }
 
-    if (action.equals(FlutterFirebaseInstanceIDService.ACTION_TOKEN)) {
-      String token = intent.getStringExtra(FlutterFirebaseInstanceIDService.EXTRA_TOKEN);
+    if (action.equals(FlutterFirebaseMessagingService.ACTION_TOKEN)) {
+      String token = intent.getStringExtra(FlutterFirebaseMessagingService.EXTRA_TOKEN);
       channel.invokeMethod("onToken", token);
     } else if (action.equals(FlutterFirebaseMessagingService.ACTION_REMOTE_MESSAGE)) {
       RemoteMessage message =
@@ -100,7 +99,7 @@ public class FirebaseMessagingPlugin extends BroadcastReceiver
   @Override
   public void onMethodCall(final MethodCall call, final Result result) {
     if ("configure".equals(call.method)) {
-      FlutterFirebaseInstanceIDService.broadcastToken(registrar.context());
+      FlutterFirebaseMessagingService.broadcastToken(registrar.context());
       if (registrar.activity() != null) {
         sendMessageFromIntent("onLaunch", registrar.activity().getIntent());
       }
--- packages/firebase_messaging/android/src/main/java/io/flutter/plugins/firebasemessaging/FlutterFirebaseMessagingService.java
+++ packages/firebase_messaging/android/src/main/java/io/flutter/plugins/firebasemessaging/FlutterFirebaseMessagingService.java
@@ -4,8 +4,16 @@
 
 package io.flutter.plugins.firebasemessaging;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -14,7 +22,8 @@ public class FlutterFirebaseMessagingService extends FirebaseMessagingService {
   public static final String ACTION_REMOTE_MESSAGE =
       "io.flutter.plugins.firebasemessaging.NOTIFICATION";
   public static final String EXTRA_REMOTE_MESSAGE = "notification";
-
+  public static final String ACTION_TOKEN = "io.flutter.plugins.firebasemessaging.TOKEN";
+  public static final String EXTRA_TOKEN = "token";
   /**
    * Called when message is received.
    *
@@ -25,5 +34,35 @@ public class FlutterFirebaseMessagingService extends FirebaseMessagingService {
     Intent intent = new Intent(ACTION_REMOTE_MESSAGE);
     intent.putExtra(EXTRA_REMOTE_MESSAGE, remoteMessage);
     LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+  }
+
+
+  public static void broadcastToken(final Context context)
+  {
+    FirebaseInstanceId.getInstance().getInstanceId()
+            .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+              @Override
+              public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if (!task.isSuccessful()) {
+                  Log.w("broadcastToken", "getInstanceId failed", task.getException());
+                  return;
+                }
+
+                // Get new Instance ID token
+                String token = task.getResult().getToken();
+
+                Intent intent = new Intent(ACTION_TOKEN);
+                intent.putExtra(EXTRA_TOKEN, token);
+                LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+              }
+            });
+  }
+
+  @Override
+  public void onNewToken(String s) {
+    super.onNewToken(s);
+    Intent intent = new Intent(ACTION_TOKEN);
+    intent.putExtra(EXTRA_TOKEN, s);
+    LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
   }
 }
