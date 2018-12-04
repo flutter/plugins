494778bc36941240c8f2c99b47aac4b04d4410c3
jike <jike@kezaihui.com>
2018-12-04 11:11:51 +0800

    update

--- packages/url_launcher/android/src/main/java/io/flutter/plugins/urllauncher/UrlLauncherPlugin.java
+++ packages/url_launcher/android/src/main/java/io/flutter/plugins/urllauncher/UrlLauncherPlugin.java
@@ -60,7 +60,9 @@ public class UrlLauncherPlugin implements MethodCallHandler {
         launchIntent = new Intent(Intent.ACTION_VIEW);
         launchIntent.setData(Uri.parse(url));
       }
-      launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      if (mRegistrar.activity() == null) {
+        launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      }
       context.startActivity(launchIntent);
       result.success(null);
     } else if (call.method.equals("closeWebView")) {
