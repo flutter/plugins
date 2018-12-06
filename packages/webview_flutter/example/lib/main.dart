0fe8df02a4be5881398b386b5515a718122616e2
kikuchy <kikuchy.h@gmail.com>
2018-12-06 13:35:02 +0900

    Use Completable and FutureBuilder instead of StatfulWidget.

--- packages/webview_flutter/example/lib/main.dart
+++ packages/webview_flutter/example/lib/main.dart
@@ -2,18 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() => runApp(MaterialApp(home: WebViewExample()));
 
-class WebViewExample extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => WebViewExampleState();
-}
-
-class WebViewExampleState extends State<WebViewExample> {
-  WebViewController controller;
+class WebViewExample extends StatelessWidget {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +19,7 @@ class WebViewExampleState extends State<WebViewExample> {
         title: const Text('Flutter WebView example'),
         // This drop down menu demonstrates that Flutter widgets can be shown over the web view.
         actions: <Widget>[
-          (controller != null) ? NavigationControls(controller) : Container(),
+          NavigationControls(_controller.future),
           const SampleMenu(),
         ],
       ),
@@ -30,9 +27,7 @@ class WebViewExampleState extends State<WebViewExample> {
         initialUrl: 'https://flutter.io',
         javaScriptMode: JavaScriptMode.unrestricted,
         onWebViewCreated: (WebViewController webViewController) {
-          setState(() {
-            controller = webViewController;
-          });
+          _controller.complete(webViewController);
         },
       ),
     );
@@ -64,37 +59,49 @@ class SampleMenu extends StatelessWidget {
 }
 
 class NavigationControls extends StatelessWidget {
-  const NavigationControls(this._webViewController)
-      : assert(_webViewController != null);
+  const NavigationControls(this._webViewControllerFuture)
+      : assert(_webViewControllerFuture != null);
 
-  final WebViewController _webViewController;
+  final Future<WebViewController> _webViewControllerFuture;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
-        IconButton(
-            icon: const Icon(Icons.arrow_back_ios),
-            onPressed: () async {
-              final bool canGoBack = await _webViewController.canGoBack();
-              if (canGoBack) {
-                _webViewController.goBack();
-              } else {
-                Scaffold.of(context).showSnackBar(
-                    const SnackBar(content: Text("No back history item")));
-              }
-            }),
-        IconButton(
-            icon: const Icon(Icons.arrow_forward_ios),
-            onPressed: () async {
-              final bool canGoForward = await _webViewController.canGoForward();
-              if (canGoForward) {
-                _webViewController.goForward();
-              } else {
-                Scaffold.of(context).showSnackBar(
-                    const SnackBar(content: Text("No forward history item")));
-              }
-            }),
+        FutureBuilder<WebViewController>(
+          builder: (BuildContext context,
+                  AsyncSnapshot<WebViewController> snapshot) =>
+              IconButton(
+                  icon: const Icon(Icons.arrow_back_ios),
+                  onPressed: () async {
+                    final WebViewController controller = snapshot.data;
+                    final bool canGoBack = await controller.canGoBack();
+                    if (canGoBack) {
+                      controller.goBack();
+                    } else {
+                      Scaffold.of(context).showSnackBar(const SnackBar(
+                          content: Text("No back history item")));
+                    }
+                  }),
+          future: _webViewControllerFuture,
+        ),
+        FutureBuilder<WebViewController>(
+          builder: (BuildContext context,
+                  AsyncSnapshot<WebViewController> snapshot) =>
+              IconButton(
+                  icon: const Icon(Icons.arrow_forward_ios),
+                  onPressed: () async {
+                    final WebViewController controller = snapshot.data;
+                    final bool canGoForward = await controller.canGoForward();
+                    if (canGoForward) {
+                      controller.goForward();
+                    } else {
+                      Scaffold.of(context).showSnackBar(const SnackBar(
+                          content: Text("No forward history item")));
+                    }
+                  }),
+          future: _webViewControllerFuture,
+        ),
       ],
     );
   }
