e7df772b982e3052439443ebda0e3f0ac81f2a63
Maurice Parrish <bmparr@google.com>
2018-12-07 16:40:07 +0000

    Documentation

--- packages/firebase_ml_vision/lib/src/firebase_vision.dart
+++ packages/firebase_ml_vision/lib/src/firebase_vision.dart
@@ -93,14 +93,18 @@ class FirebaseVisionImage {
 
   /// Construct a [FirebaseVisionImage] from a list of bytes.
   ///
-  /// Expects `ImageFormat.NV21` on Android and expects bytes from `NSData`
-  /// provided from a `UIImage` on iOS. (e.g. using
-  /// `UIImageJPEGRepresentation()` on the platform side).
+  /// Expects `android.graphics.ImageFormat.NV21` format on Android. Note:
+  /// Concatenating the planes of `android.graphics.ImageFormat.YUV_420_888`
+  /// into a single plane, converts it to `android.graphics.ImageFormat.NV21`.
+  ///
+  /// Expects `kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange` or any other
+  /// planar format on iOS.
   factory FirebaseVisionImage.fromBytes(
     Uint8List bytes,
     FirebaseVisionImageMetadata metadata,
   ) {
     assert(bytes != null);
+    assert(metadata != null);
     return FirebaseVisionImage._(
       type: _ImageType.bytes,
       bytes: bytes,
@@ -121,6 +125,7 @@ class FirebaseVisionImage {
       };
 }
 
+/// Plane attributes to create the image buffer on iOS.
 class FirebaseVisionImagePlaneMetadata {
   const FirebaseVisionImagePlaneMetadata({
     @required this.bytesPerRow,
@@ -130,8 +135,13 @@ class FirebaseVisionImagePlaneMetadata {
         assert(height != null),
         assert(width != null);
 
+  /// The row stride for this color plane, in bytes.
   final int bytesPerRow;
+
+  /// Height of the pixel buffer on iOS.
   final int height;
+
+  /// Width of the pixel buffer on iOS.
   final int width;
 
   Map<String, dynamic> _serialize() => <String, dynamic>{
@@ -147,18 +157,49 @@ class FirebaseVisionImagePlaneMetadata {
 /// Android.
 class FirebaseVisionImageMetadata {
   const FirebaseVisionImageMetadata({
-    @required this.rawFormat,
     @required this.size,
+    @required this.rawFormat,
     @required this.planeData,
     this.rotation = ImageRotation.rotation_0,
-  })  : assert(size != null),
-        assert(rawFormat != null),
-        assert(planeData != null);
+  })  : assert(size != null);
 
+  /// Size of the image in pixels.
   final Size size;
+
+  /// Rotation of the image for Android.
+  ///
+  /// Not currently used on iOS.
   final ImageRotation rotation;
+
+  /// Raw version of the format from the iOS platform.
+  ///
+  /// Since iOS can use any planar format, this format will be used to create
+  /// the image buffer on iOS.
+  ///
+  /// On iOS, this is a `FourCharCode` constant from Pixel Format Identifiers.
+  /// See https://developer.apple.com/documentation/corevideo/1563591-pixel_format_identifiers?language=objc
+  ///
+  /// Not used on Android.
   final dynamic rawFormat;
+
+  /// The plane attributes to create the image buffer on iOS.
+  ///
+  /// Not used on Android.
   final List<FirebaseVisionImagePlaneMetadata> planeData;
+
+  int _imageRotationToInt(ImageRotation rotation) {
+    switch (rotation) {
+      case ImageRotation.rotation_90:
+        return 90;
+      case ImageRotation.rotation_180:
+        return 180;
+      case ImageRotation.rotation_270:
+        return 270;
+      default:
+        assert(rotation == ImageRotation.rotation_0);
+        return 0;
+    }
+  }
 
   Map<String, dynamic> _serialize() => <String, dynamic>{
         'width': size.width,
@@ -175,20 +216,6 @@ class FirebaseVisionImageMetadata {
 abstract class FirebaseVisionDetector {
   /// Uses machine learning model to detect objects of interest in an image.
   Future<dynamic> detectInImage(FirebaseVisionImage visionImage);
-}
-
-int _imageRotationToInt(ImageRotation rotation) {
-  switch (rotation) {
-    case ImageRotation.rotation_90:
-      return 90;
-    case ImageRotation.rotation_180:
-      return 180;
-    case ImageRotation.rotation_270:
-      return 270;
-    default:
-      assert(rotation == ImageRotation.rotation_0);
-      return 0;
-  }
 }
 
 String _enumToString(dynamic enumValue) {
