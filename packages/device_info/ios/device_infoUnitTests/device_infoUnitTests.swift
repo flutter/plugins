698b412024ee7462bfd510c4b555fafa0e03d225
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2019-01-05 03:28:39 +0700

    dummy unnit test

---
+++ packages/device_info/ios/device_infoUnitTests/device_infoUnitTests.swift
@@ -0,0 +1,32 @@
+//
+//  device_infoUnitTests.swift
+//  device_infoUnitTests
+//
+//  Created by TruongSinh Tran-Nguyen on 1/5/19.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+import XCTest
+
+class device_infoUnitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExampleFail() {
+        XCTAssertEqual(1, 3)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    func testExamplePass() {
+        XCTAssertEqual(2, 2)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
