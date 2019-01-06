d6590c168f9cc0257e8a20ce542b0bd5b02bdff5
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2019-01-06 09:12:05 +0700

    full coverage of device_info ios unit test

--- packages/device_info/ios/Tests/unit_test.swift
+++ packages/device_info/ios/Tests/unit_test.swift
@@ -6,6 +6,7 @@
 import XCTest
 
 class device_infoUnitTests: XCTestCase {
+    let r = FLTDeviceInfoPlugin()
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -16,8 +17,25 @@ class device_infoUnitTests: XCTestCase {
     }
 
     func testIsPhysicalDevice() {
-        let r = FLTDeviceInfoPlugin()
         XCTAssertEqual(r.isDevicePhysical(), "false")
+    }
+    
+    func testSuccessCall(){
+        let mCall = FlutterMethodCall.init(methodName: "getIosDeviceInfo", arguments: nil)
+        
+        r.handle(mCall)  { (result) -> () in
+            var jsonResult = result as! Dictionary<String, AnyObject>
+            print(jsonResult)
+            XCTAssertNotNil(jsonResult["utsname"])
+        }
+    }
+
+    func testFlutterMethodNotImplemented(){
+        let mCall = FlutterMethodCall.init(methodName: "some weird method", arguments: nil)
+        
+        r.handle(mCall)  { (result) -> () in
+            XCTAssertEqual(result as! NSObject, FlutterMethodNotImplemented)
+        }
     }
 
 }
