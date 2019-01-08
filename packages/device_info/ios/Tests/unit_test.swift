dba2fc89a2788db7bccdfcd49b386507bd61d701
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2019-01-08 08:01:02 +0700

    resolve https://github.com/flutter/plugins/pull/1036#discussion_r245687152

--- packages/device_info/ios/Tests/unit_test.swift
+++ packages/device_info/ios/Tests/unit_test.swift
@@ -1,7 +1,6 @@
-//
-//  Created by TruongSinh Tran-Nguyen on 2019-01-05.
-//  Copyright © 2019 The Chromium Authors. All rights reserved.
-//
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import XCTest
 
