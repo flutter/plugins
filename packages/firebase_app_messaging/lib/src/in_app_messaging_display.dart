76fb3e50af3f1f3639dfd90a222063f2ac4b4034
Collin Jackson <jackson@google.com>
2019-01-10 12:10:23 -0800

    Remove unused files

--- packages/firebase_app_messaging/lib/src/in_app_messaging_display.dart
+++
@@ -1,9 +0,0 @@
-// Copyright 2019 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-part of firebase_in_app_messaging;
-
-abstract class InAppMessagingDisplay {
-  void displayMessage(InAppMessagingDisplayMessage message, InAppMessagingDisplayDelegate delegate);
-}
