c947992f83df9dd748a135d0f399f0ba404879be
Chris Yang <cyang8954@gmail.com>
2019-01-10 17:15:38 -0800

    init commit

---
+++ packages/in_app_purchase/ios/Classes/FLTSKProductRequestWrapper.h
@@ -0,0 +1,16 @@
+//
+//  FLTSKProductRequestWrapper.h
+//  in_app_purchase
+//
+//  Created by Chris Yang on 1/10/19.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FLTSKProductRequestWrapper : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ packages/in_app_purchase/ios/Classes/FLTSKProductRequestWrapper.m
@@ -0,0 +1,21 @@
+//
+//  FLTSKProductRequestWrapper.m
+//  in_app_purchase
+//
+//  Created by Chris Yang on 1/10/19.
+//
+
+#import "FLTSKProductRequestWrapper.h"
+#import <StoreKit/StoreKit.h>
+
+@interface FLTSKProductRequestWrapper()
+
+@property (strong, nonatomic) SKProductsRequest *request;
+
+@end
+
+@implementation FLTSKProductRequestWrapper
+
+
+
+@end
