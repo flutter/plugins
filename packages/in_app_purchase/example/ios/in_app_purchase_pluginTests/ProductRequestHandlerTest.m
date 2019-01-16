51138679653f0cf03090048b956805b7deadc05c
Chris Yang <cyang8954@gmail.com>
2019-01-15 18:29:26 -0800

    add copyright header

--- packages/in_app_purchase/example/ios/in_app_purchase_pluginTests/ProductRequestHandlerTest.m
+++ packages/in_app_purchase/example/ios/in_app_purchase_pluginTests/ProductRequestHandlerTest.m
@@ -1,10 +1,6 @@
-//
-//  ProductRequestHandlerTest.m
-//  in_app_purchase_pluginTests
-//
-//  Created by Chris Yang on 1/11/19.
-//  Copyright © 2019 The Chromium Authors. All rights reserved.
-//
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #import <XCTest/XCTest.h>
 #import "FIAPProductRequestHandler.h"
