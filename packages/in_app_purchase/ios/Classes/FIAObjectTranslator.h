eca1d7ee412427a1995dbc0fae271f9e350e20b1
Chris Yang <cyang8954@gmail.com>
2019-01-24 11:25:21 -0800

    replacing the lisence header

--- packages/in_app_purchase/ios/Classes/FIAObjectTranslator.h
+++ packages/in_app_purchase/ios/Classes/FIAObjectTranslator.h
@@ -1,9 +1,6 @@
-//
-//  FIAObjectTranslator.h
-//  in_app_purchase
-//
-//  Created by Chris Yang on 1/18/19.
-//
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #import <Foundation/Foundation.h>
 #import <StoreKit/StoreKit.h>
