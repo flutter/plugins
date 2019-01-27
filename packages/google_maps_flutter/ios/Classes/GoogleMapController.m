0ee6a54127014703752d77eb5a0a7d00a44a4242
Jason <jason@gofuseit.com>
2019-01-27 18:42:14 +0200

    styles: Making googlebot happy

--- packages/google_maps_flutter/ios/Classes/GoogleMapController.m
+++ packages/google_maps_flutter/ios/Classes/GoogleMapController.m
@@ -12,7 +12,7 @@ alpha:1.0]
 #define UIColorFromRGB(rgbValue)                                       \
   [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16)) / 255.0 \
                   green:((float)((rgbValue & 0xFF00) >> 8)) / 255.0    \
-                  blue:((float)(rgbValue & 0xFF)) / 255.0              \
+                   blue:((float)(rgbValue & 0xFF)) / 255.0             \
                   alpha:1.0]
 
 #pragma mark - Conversion of JSON-like values sent via platform channels. Forward declarations.
