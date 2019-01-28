f1a590030bc905acbc9bb6ce90a89cd40cf0d710
Aawaz Gyawali <awazgyawali@gmail.com>
2019-01-28 20:14:49 +0545

    IOs changes

---
+++ packages/google_maps_flutter/ios/Classes/GoogleMapCircleController.h
@@ -0,0 +1,16 @@
+//
+//  GoogleMapCircleController.h
+//  google_maps_flutter
+//
+//  Created by Aawaz Gyawali on 1/28/19.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GoogleMapCircleController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ packages/google_maps_flutter/ios/Classes/GoogleMapCircleController.m
@@ -0,0 +1,12 @@
+//
+//  GoogleMapCircleController.m
+//  google_maps_flutter
+//
+//  Created by Aawaz Gyawali on 1/28/19.
+//
+
+#import "GoogleMapCircleController.h"
+
+@implementation GoogleMapCircleController
+
+@end
