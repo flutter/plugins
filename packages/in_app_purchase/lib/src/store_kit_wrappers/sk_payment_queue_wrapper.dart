6fd56112a507d2017dacade5f8ebd23f98f9bad7
Chris Yang <cyang8954@gmail.com>
2019-02-04 15:36:25 -0800

    remove unused import

--- packages/in_app_purchase/lib/src/store_kit_wrappers/sk_payment_queue_wrapper.dart
+++ packages/in_app_purchase/lib/src/store_kit_wrappers/sk_payment_queue_wrapper.dart
@@ -4,7 +4,6 @@
 
 import 'dart:async';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
 import 'package:in_app_purchase/src/channel.dart';
 import 'package:json_annotation/json_annotation.dart';
 
