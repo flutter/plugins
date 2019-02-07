1ab0a4f62e6d22f69da63926c3778bbb50dfbb64
Chris Yang <tyang8954@gmail.com>
2019-02-07 12:36:33 -0800

    Iap add payment objc translators (#1172)

--- packages/in_app_purchase/example/ios/in_app_purchase_pluginTests/TranslatorTest.m
+++ packages/in_app_purchase/example/ios/in_app_purchase_pluginTests/TranslatorTest.m
@@ -12,6 +12,10 @@
 @property(strong, nonatomic) NSDictionary *discountMap;
 @property(strong, nonatomic) NSDictionary *productMap;
 @property(strong, nonatomic) NSDictionary *productResponseMap;
+@property(strong, nonatomic) NSDictionary *downloadMap;
+@property(strong, nonatomic) NSDictionary *paymentMap;
+@property(strong, nonatomic) NSDictionary *transactionMap;
+@property(strong, nonatomic) NSDictionary *errorMap;
 @property(strong, nonatomic) NSDictionary *localeMap;
 
 @end
@@ -42,6 +46,72 @@
   };
   self.productResponseMap =
       @{@"products" : @[ self.productMap ], @"invalidProductIdentifiers" : @[]};
+  self.downloadMap = @{
+    @"state" : @(0),
+    @"contentIdentifier" : [NSNull null],
+    @"contentLength" : @(2),
+    @"contentURL" : @"https://flutter.io",
+    @"contentVersion" : [NSNull null],
+    @"error" : [[NSError errorWithDomain:@"test_stub" code:123 userInfo:@{}] toMap],
+    @"progress" : @(0.5),
+    @"timeRemaining" : @(100),
+    @"downloadTimeUnKnown" : @(NO),
+    @"transactionID" : [NSNull null],
+  };
+  self.paymentMap = @{
+    @"productIdentifier" : @"123",
+    @"requestData" : @"abcdefghabcdefghabcdefghabcdefghabcdefghabcdefghabcdefghabcdefgh",
+    @"quantity" : @(2),
+    @"applicationUsername" : @"app user name",
+    @"simulatesAskToBuyInSandbox" : @(NO)
+  };
+  NSDictionary *originalTransactionMap = @{
+    @"transactionIdentifier" : @"567",
+    @"transactionState" : @(SKPaymentTransactionStatePurchasing),
+    @"payment" : [NSNull null],
+    @"error" : [[NSError errorWithDomain:@"test_stub" code:123 userInfo:@{}] toMap],
+    @"transactionTimeStamp" : @([NSDate date].timeIntervalSince1970),
+    @"originalTransaction" : [NSNull null],
+    @"downloads" : @[ @{
+      @"state" : @(0),
+      @"contentIdentifier" : [NSNull null],
+      @"contentLength" : @(2),
+      @"contentURL" : @"https://flutter.io",
+      @"contentVersion" : [NSNull null],
+      @"error" : [[NSError errorWithDomain:@"test_stub" code:123 userInfo:@{}] toMap],
+      @"progress" : @(0.5),
+      @"timeRemaining" : @(100),
+      @"downloadTimeUnKnown" : @(NO),
+      @"transactionID" : @"567",
+    } ]
+  };
+  self.transactionMap = @{
+    @"transactionIdentifier" : @"567",
+    @"transactionState" : @(SKPaymentTransactionStatePurchasing),
+    @"payment" : [NSNull null],
+    @"error" : [[NSError errorWithDomain:@"test_stub" code:123 userInfo:@{}] toMap],
+    @"transactionTimeStamp" : @([NSDate date].timeIntervalSince1970),
+    @"originalTransaction" : originalTransactionMap,
+    @"downloads" : @[ @{
+      @"state" : @(0),
+      @"contentIdentifier" : [NSNull null],
+      @"contentLength" : @(2),
+      @"contentURL" : @"https://flutter.io",
+      @"contentVersion" : [NSNull null],
+      @"error" : [[NSError errorWithDomain:@"test_stub" code:123 userInfo:@{}] toMap],
+      @"progress" : @(0.5),
+      @"timeRemaining" : @(100),
+      @"downloadTimeUnKnown" : @(NO),
+      @"transactionID" : @"567",
+    } ]
+  };
+  self.errorMap = @{
+    @"code" : @(123),
+    @"domain" : @"test_domain",
+    @"userInfo" : @{
+      @"key" : @"value",
+    }
+  };
 }
 
 - (void)testSKProductSubscriptionPeriodStubToMap {
@@ -70,6 +140,33 @@
   XCTAssertEqualObjects(map, self.productResponseMap);
 }
 
+- (void)testDownloadToMap {
+  // bug with state, downloadTimeUnKnown, transaction and contentIdentifer in KVC, cannot test these
+  // fields.
+  SKDownloadStub *download = [[SKDownloadStub alloc] initWithMap:self.downloadMap];
+  NSDictionary *map = [download toMap];
+  XCTAssertEqualObjects(map, self.downloadMap);
+}
+
+- (void)testPaymentToMap {
+  SKMutablePayment *payment = [[SKMutablePayment alloc] initWithMap:self.paymentMap];
+  NSDictionary *map = [payment toMap];
+  XCTAssertEqualObjects(map, self.paymentMap);
+}
+
+- (void)testPaymentTransactionToMap {
+  // payment is not KVC, cannot test payment field.
+  SKPaymentTransactionStub *paymentTransaction =
+      [[SKPaymentTransactionStub alloc] initWithMap:self.transactionMap];
+  NSDictionary *map = [paymentTransaction toMap];
+  XCTAssertEqualObjects(map, self.transactionMap);
+}
+
+- (void)testError {
+  NSErrorStub *error = [[NSErrorStub alloc] initWithMap:self.errorMap];
+  NSDictionary *map = [error toMap];
+  XCTAssertEqualObjects(map, self.errorMap);
+}
 - (void)testLocaleToMap {
   NSLocale *system = NSLocale.systemLocale;
   NSDictionary *map = [system toMap];
--- packages/in_app_purchase/ios/Classes/FIAObjectTranslator.m
+++ packages/in_app_purchase/ios/Classes/FIAObjectTranslator.m
@@ -80,6 +80,25 @@
 
 @end
 
+@implementation SKPayment (Coder)
+
+- (NSDictionary *)toMap {
+  NSMutableDictionary *map = [[NSMutableDictionary alloc] initWithDictionary:@{
+    @"productIdentifier" : self.productIdentifier ?: [NSNull null],
+    @"requestData" : self.requestData ? [[NSString alloc] initWithData:self.requestData
+                                                              encoding:NSUTF8StringEncoding]
+                                      : [NSNull null],
+    @"quantity" : @(self.quantity),
+    @"applicationUsername" : self.applicationUsername ?: [NSNull null]
+  }];
+  if (@available(iOS 8.3, *)) {
+    [map setObject:@(self.simulatesAskToBuyInSandbox) forKey:@"simulatesAskToBuyInSandbox"];
+  }
+  return map;
+}
+
+@end
+
 @implementation NSLocale (Coder)
 
 - (nullable NSDictionary *)toMap {
@@ -87,6 +106,81 @@
   [map setObject:[self objectForKey:NSLocaleCurrencySymbol] ?: [NSNull null]
           forKey:@"currencySymbol"];
   return map;
+}
+
+@end
+
+@implementation SKMutablePayment (Coder)
+
+- (instancetype)initWithMap:(NSDictionary *)map {
+  self = [self init];
+  if (self) {
+    self.productIdentifier = map[@"productIdentifier"];
+    NSString *utf8String = map[@"requestData"];
+    self.requestData = [utf8String dataUsingEncoding:NSUTF8StringEncoding];
+    self.quantity = [map[@"quantity"] integerValue];
+    self.applicationUsername = map[@"applicationUsername"];
+    if (@available(iOS 8.3, *)) {
+      self.simulatesAskToBuyInSandbox = [map[@"simulatesAskToBuyInSandbox"] boolValue];
+    }
+  }
+  return self;
+}
+
+@end
+
+@implementation SKPaymentTransaction (Coder)
+
+- (NSDictionary *)toMap {
+  NSMutableDictionary *map = [[NSMutableDictionary alloc] initWithDictionary:@{
+    @"error" : [self.error toMap] ?: [NSNull null],
+    @"payment" : self.payment ? [self.payment toMap] : [NSNull null],
+    @"originalTransaction" : self.originalTransaction ? [self.originalTransaction toMap]
+                                                      : [NSNull null],
+    @"transactionTimeStamp" : self.transactionDate ? @(self.transactionDate.timeIntervalSince1970)
+                                                   : [NSNull null],
+    @"transactionIdentifier" : self.transactionIdentifier ?: [NSNull null],
+    @"transactionState" : @(self.transactionState)
+  }];
+  NSMutableArray *downloads = [NSMutableArray new];
+  for (SKDownload *download in self.downloads) {
+    [downloads addObject:[download toMap]];
+  }
+  [map setObject:downloads forKey:@"downloads"];
+  return map;
+}
+
+@end
+
+@implementation SKDownload (Coder)
+
+- (NSDictionary *)toMap {
+  NSMutableDictionary *map = [[NSMutableDictionary alloc] initWithDictionary:@{
+    @"contentLength" : @(self.contentLength),
+    @"contentIdentifier" : self.contentIdentifier ?: [NSNull null],
+    @"contentURL" : self.contentURL.absoluteString ?: [NSNull null],
+    @"contentVersion" : self.contentVersion ?: [NSNull null],
+    @"error" : [self.error toMap] ?: @{},
+    @"progress" : @(self.progress),
+    @"timeRemaining" : @(self.timeRemaining),
+    @"downloadTimeUnKnown" : @(self.timeRemaining == SKDownloadTimeRemainingUnknown),
+    @"transactionID" : self.transaction.transactionIdentifier ?: [NSNull null]
+  }];
+  if (@available(iOS 12.0, *)) {
+    [map setObject:@(self.state) forKey:@"state"];
+  } else {
+    [map setObject:@(self.downloadState) forKey:@"state"];
+  }
+  return map;
+}
+
+@end
+
+@implementation NSError (Coder)
+
+- (NSDictionary *)toMap {
+  return
+      @{@"code" : @(self.code), @"domain" : self.domain ?: @"", @"userInfo" : self.userInfo ?: @{}};
 }
 
 @end
