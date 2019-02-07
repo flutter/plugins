47ac3b506122dbbcd72a6cfe775fe09ba1e402d3
Andre Mourinho <andremourinho@live.com>
2019-02-07 18:30:18 -0500

    fixed formatting

--- packages/webview_flutter/lib/webview_flutter.dart
+++ packages/webview_flutter/lib/webview_flutter.dart
@@ -501,10 +501,10 @@ class CookieManager {
   /// }
   /// This is supported for >= IOS 9.
   Future<void> addCookie(Map<String, String> cookie) async {
-      // TODO(amirh): remove this when the invokeMethod update makes it to stable Flutter.
-      // https://github.com/flutter/flutter/issues/26431
-      // ignore: strong_mode_implicit_dynamic_method
-      return _channel.invokeMethod('addCookie', cookie);
+    // TODO(amirh): remove this when the invokeMethod update makes it to stable Flutter.
+    // https://github.com/flutter/flutter/issues/26431
+    // ignore: strong_mode_implicit_dynamic_method
+    return _channel.invokeMethod('addCookie', cookie);
   }
 }
 
