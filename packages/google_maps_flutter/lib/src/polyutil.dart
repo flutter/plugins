9d2e68b0220dddc3986820544fe46f422e97fb9b
Tolotra <rsamuel15@alustudent.com>
2019-02-10 23:51:38 +0400

    Added PolyUtil class to decode waypoints into List<LatLng>

---
+++ packages/google_maps_flutter/lib/src/polyutil.dart
@@ -0,0 +1,38 @@
+
+part of google_maps_flutter;
+
+class PolyUtil{
+  static List<LatLng> decodePoly(String encoded) {
+
+    List<LatLng> poly = List<LatLng>();
+    int index = 0;
+    int len = encoded.length;
+    int lat = 0, lng = 0;
+
+    while (index < len) {
+      int b, shift = 0, result = 0;
+      do {
+        b = encoded.codeUnitAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+      lat += dlat;
+
+      shift = 0;
+      result = 0;
+      do {
+        b = encoded.codeUnitAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+      lng += dlng;
+
+      LatLng p = LatLng(( lat / 1E5), ( lng / 1E5));
+      poly.add(p);
+    }
+
+    return poly;
+  }
+}
