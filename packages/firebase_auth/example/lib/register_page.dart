edd6ecdba4054c35e8572701f9e4839821c36680
Chris Yang <cyang8954@gmail.com>
2019-02-13 12:53:37 -0800

    first draft

---
+++ packages/firebase_auth/example/lib/register_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class RegisterPage extends StatefulWidget {
+  final String title = 'Registration';
+  @override
+  State<StatefulWidget> createState() => RegisterPageState();
+}
+
+class RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        ]
+      ),
+    );
+  }
+}
