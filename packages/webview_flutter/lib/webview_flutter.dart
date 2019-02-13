f14e45dab6898be96bca93beb2f669ea39010601
Kaushik Iska <kaushikiska@google.com>
2019-02-13 13:11:02 -0800

    await before reload

--- packages/webview_flutter/lib/webview_flutter.dart
+++ packages/webview_flutter/lib/webview_flutter.dart
@@ -408,7 +408,7 @@ class WebViewController {
     // TODO(amirh): remove this on when the invokeMethod update makes it to stable Flutter.
     // https://github.com/flutter/flutter/issues/26431
     // ignore: strong_mode_implicit_dynamic_method
-    _channel.invokeMethod("clearCache");
+    await _channel.invokeMethod("clearCache");
     return reload();
   }
 
