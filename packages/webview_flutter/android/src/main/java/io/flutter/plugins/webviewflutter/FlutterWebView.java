6f4a87e9672595da0748bed769e0dca89c32f066
Rakesh <rockylanjewar@gmail.com>
2019-02-20 13:38:47 +0400

    fixed issue missing method

--- packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
+++ packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
@@ -3,6 +3,7 @@ package io.flutter.plugins.webviewflutter;
 import android.content.Context;
 import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -33,6 +34,18 @@ public class FlutterWebView implements PlatformView, MethodCallHandler {
     if (params.containsKey("initialUrl")) {
       String url = (String) params.get("initialUrl");
       webView.loadUrl(url);
+    }
+  }
+
+    if (params.containsKey("enableJavascriptRedirects")) {
+    boolean enableJavascriptRedirects = (boolean) params.get("enableJavascriptRedirects");
+    if(enableJavascriptRedirects) {
+      webView.setWebViewClient(new WebViewClient() {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+          return false;
+        }
+      });
     }
   }
 
@@ -83,6 +96,12 @@ public class FlutterWebView implements PlatformView, MethodCallHandler {
   }
 
   private void loadUrl(MethodCall methodCall, Result result) {
+    String url = (String) methodCall.arguments;
+    webView.loadUrl(url);
+    result.success(null);
+  }
+
+  private void doJavascriptRedirect(MethodCall methodCall, Result result) {
     String url = (String) methodCall.arguments;
     webView.loadUrl(url);
     result.success(null);
