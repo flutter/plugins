cfd4a55c2c621c35c6e227526a2fedabe7ffbf7a
Tomas Radvansky <radvansky.tomas@gmail.com>
2019-02-23 19:19:09 +1300

    Add SocketTimeOut exception for CloudFunctions

--- packages/cloud_functions/android/src/main/java/io/flutter/plugins/firebase/cloudfunctions/cloudfunctions/CloudFunctionsPlugin.java
+++ packages/cloud_functions/android/src/main/java/io/flutter/plugins/firebase/cloudfunctions/cloudfunctions/CloudFunctionsPlugin.java
@@ -17,6 +17,7 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import java.net.SocketTimeoutException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -60,6 +61,17 @@ public class CloudFunctionsPlugin implements MethodCallHandler {
                         exceptionMap.put("code", exception.getCode().name());
                         exceptionMap.put("message", exception.getMessage());
                         exceptionMap.put("details", exception.getDetails());
+                        result.error(
+                            "functionsError",
+                            "Cloud function failed with exception.",
+                            exceptionMap);
+                      } else if (task.getException() instanceof SocketTimeoutException) {
+                        SocketTimeoutException exception =
+                            (SocketTimeoutException) task.getException();
+                        Map < String, Object > exceptionMap = new HashMap < > ();
+                        exceptionMap.put("code", FirebaseFunctionsException.Code.DEADLINE_EXCEEDED.name());
+                        exceptionMap.put("message", exception.getMessage());
+                        exceptionMap.put("details", "SocketTimeoutException");
                         result.error(
                             "functionsError",
                             "Cloud function failed with exception.",
