942aeac27583061649a74007bda06902448533a6
Kaushik Iska <iska.kaushik@gmail.com>
2019-02-27 07:22:13 -0800

    [google_maps_flutter] Marker APIs are now widget based (Android) (#1229)
    * Marker APIs are now widget based (Android)
    
    note: This commit only deals with Android side of changes.
    
    * Fix format
    
    * Remove MarkerId class

---
+++ packages/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/MarkersController.java
@@ -0,0 +1,115 @@
+package io.flutter.plugins.googlemaps;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import io.flutter.plugin.common.MethodChannel;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class MarkersController {
+
+  private final Map<String, MarkerController> markerIdToController;
+  private final Map<String, String> googleMapsMarkerIdToDartMarkerId;
+  private final MethodChannel methodChannel;
+  private GoogleMap googleMap;
+
+  MarkersController(MethodChannel methodChannel) {
+    this.markerIdToController = new HashMap<>();
+    this.googleMapsMarkerIdToDartMarkerId = new HashMap<>();
+    this.methodChannel = methodChannel;
+  }
+
+  void setGoogleMap(GoogleMap googleMap) {
+    this.googleMap = googleMap;
+  }
+
+  void addMarkers(List<Object> markersToAdd) {
+    if (markersToAdd != null) {
+      for (Object markerToAdd : markersToAdd) {
+        addMarker(markerToAdd);
+      }
+    }
+  }
+
+  void changeMarkers(List<Object> markersToChange) {
+    if (markersToChange != null) {
+      for (Object markerToChange : markersToChange) {
+        changeMarker(markerToChange);
+      }
+    }
+  }
+
+  void removeMarkers(List<Object> markerIdsToRemove) {
+    if (markerIdsToRemove == null) {
+      return;
+    }
+    for (Object rawMarkerId : markerIdsToRemove) {
+      if (rawMarkerId == null) {
+        continue;
+      }
+      String markerId = (String) rawMarkerId;
+      final MarkerController markerController = markerIdToController.remove(markerId);
+      if (markerController != null) {
+        markerController.remove();
+        googleMapsMarkerIdToDartMarkerId.remove(markerController.getGoogleMapsMarkerId());
+      }
+    }
+  }
+
+  boolean onMarkerTap(String googleMarkerId) {
+    String markerId = googleMapsMarkerIdToDartMarkerId.get(googleMarkerId);
+    if (markerId == null) {
+      return false;
+    }
+    methodChannel.invokeMethod("marker#onTap", Convert.toJson(markerId));
+    MarkerController markerController = markerIdToController.get(markerId);
+    if (markerController != null) {
+      return markerController.consumeTapEvents();
+    }
+    return false;
+  }
+
+  void onInfoWindowTap(String googleMarkerId) {
+    String markerId = googleMapsMarkerIdToDartMarkerId.get(googleMarkerId);
+    if (markerId == null) {
+      return;
+    }
+    methodChannel.invokeMethod("infoWindow#onTap", Convert.toJson(markerId));
+  }
+
+  private void addMarker(Object marker) {
+    if (marker == null) {
+      return;
+    }
+    MarkerBuilder markerBuilder = new MarkerBuilder();
+    String markerId = Convert.interpretMarkerOptions(marker, markerBuilder);
+    MarkerOptions options = markerBuilder.build();
+    addMarker(markerId, options, markerBuilder.consumeTapEvents());
+  }
+
+  private void addMarker(String markerId, MarkerOptions markerOptions, boolean consumeTapEvents) {
+    final Marker marker = googleMap.addMarker(markerOptions);
+    MarkerController controller = new MarkerController(marker, consumeTapEvents);
+    markerIdToController.put(markerId, controller);
+    googleMapsMarkerIdToDartMarkerId.put(marker.getId(), markerId);
+  }
+
+  private void changeMarker(Object marker) {
+    if (marker == null) {
+      return;
+    }
+    String markerId = getMarkerId(marker);
+    MarkerController markerController = markerIdToController.get(markerId);
+    if (markerController != null) {
+      Convert.interpretMarkerOptions(marker, markerController);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  private static String getMarkerId(Object marker) {
+    Map<String, Object> markerMap = (Map<String, Object>) marker;
+    return (String) markerMap.get("markerId");
+  }
+}
