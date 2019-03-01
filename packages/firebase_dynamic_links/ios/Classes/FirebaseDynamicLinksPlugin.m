02035d348807604de0da2a1a6c48fdcd58feea87
Maurice Parrish <bmparr@google.com>
2019-03-01 12:28:58 -0800

    Formatting

--- packages/firebase_dynamic_links/ios/Classes/FirebaseDynamicLinksPlugin.m
+++ packages/firebase_dynamic_links/ios/Classes/FirebaseDynamicLinksPlugin.m
@@ -150,8 +150,8 @@
   NSURL *link = [NSURL URLWithString:arguments[@"link"]];
   NSString *domain = arguments[@"domain"];
 
-  FIRDynamicLinkComponents *components =
-      [FIRDynamicLinkComponents componentsWithLink:link domain:domain];
+  FIRDynamicLinkComponents *components = [FIRDynamicLinkComponents componentsWithLink:link
+                                                                               domain:domain];
 
   if (![arguments[@"androidParameters"] isEqual:[NSNull null]]) {
     NSDictionary *params = arguments[@"androidParameters"];
