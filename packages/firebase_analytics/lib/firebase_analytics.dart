e114fb30b0b46b82804504207796f6a98c14cac9
Elliot Hesp <elliot.hesp@gmail.com>
2019-03-05 14:39:33 +0000

    correct formatting

--- packages/firebase_analytics/lib/firebase_analytics.dart
+++ packages/firebase_analytics/lib/firebase_analytics.dart
@@ -162,10 +162,10 @@ class FirebaseAnalytics {
 
   /// Clears all analytics data for this app from the device and resets the app instance id.
   Future<void> resetAnalyticsData() async {
-     // TODO(amirh): remove this on when the invokeMethod update makes it to stable Flutter.
-     // https://github.com/flutter/flutter/issues/26431
-     // ignore: strong_mode_implicit_dynamic_method
-     await _channel.invokeMethod('resetAnalyticsData');
+    // TODO(amirh): remove this on when the invokeMethod update makes it to stable Flutter.
+    // https://github.com/flutter/flutter/issues/26431
+    // ignore: strong_mode_implicit_dynamic_method
+    await _channel.invokeMethod('resetAnalyticsData');
   }
 
   /// Logs the standard `add_payment_info` event.
