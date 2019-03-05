88a49c67c42510b6336b7419fecfa72f6aefb588
Chris Yang <cyang8954@gmail.com>
2019-03-04 16:11:52 -0800

    formatting

--- packages/in_app_purchase/lib/src/store_kit_wrappers/sk_receipt_manager.dart
+++ packages/in_app_purchase/lib/src/store_kit_wrappers/sk_receipt_manager.dart
@@ -5,7 +5,7 @@
 import 'dart:async';
 import 'package:in_app_purchase/src/channel.dart';
 
- ///This class contains static methods to manage StoreKit receipts.
+///This class contains static methods to manage StoreKit receipts.
 class SKReceiptManager {
   /// Retrieve the receipt data from your application's main bundle.
   ///
@@ -14,9 +14,8 @@ class SKReceiptManager {
   /// There are 2 ways to do so. Either validate locally or validate with App Store.
   /// For more details on how to validate the receipt data, you can refer to Apple's document about [`About Receipt Validation`](https://developer.apple.com/library/archive/releasenotes/General/ValidateAppStoreReceipt/Introduction.html#//apple_ref/doc/uid/TP40010573-CH105-SW1).
   /// If the receipt is invalid or missing, you can use [SKRequestMaker.startRefreshReceiptRequest] to request a new receipt.
-  Future<String> retrieveReceiptData(
-      {bool serialized = false}) {
-    return channel.invokeMethod(
-            '-[InAppPurchasePlugin retrieveReceiptData:result:]');
+  Future<String> retrieveReceiptData({bool serialized = false}) {
+    return channel
+        .invokeMethod('-[InAppPurchasePlugin retrieveReceiptData:result:]');
   }
 }
