8d4fe3cc214f6fe8fc6a9d62d215fbfce5d661b2
Amir Hardon <amirha@google.com>
2019-03-07 15:27:19 -0800

    review comments followup

--- packages/webview_flutter/ios/Classes/FLTWKNavigationDelegate.m
+++ packages/webview_flutter/ios/Classes/FLTWKNavigationDelegate.m
@@ -1,4 +1,4 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
+// Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -25,11 +25,31 @@
   }
   NSDictionary* arguments = @{
     @"url" : navigationAction.request.URL.absoluteString,
-    @"isMainFrame" : [NSNumber numberWithBool:navigationAction.targetFrame.isMainFrame]
+    @"isMainFrame" : @(navigationAction.targetFrame.isMainFrame)
   };
   [_methodChannel invokeMethod:@"navigationRequest"
                      arguments:arguments
                         result:^(id _Nullable result) {
+                          if ([result isKindOfClass:[FlutterError class]]) {
+                            NSLog(@"navigationRequest has unexpectedly completed with an error, "
+                                  @"allowing navigation.");
+                            decisionHandler(WKNavigationActionPolicyAllow);
+                            return;
+                          }
+                          if (result == FlutterMethodNotImplemented) {
+                            NSLog(@"navigationRequest was unexepectedly not implemented: %@, "
+                                  @"allowing navigation.",
+                                  result);
+                            decisionHandler(WKNavigationActionPolicyAllow);
+                            return;
+                          }
+                          if (![result isKindOfClass:[NSNumber class]]) {
+                            NSLog(@"navigationRequest unexpectedly returned a non boolean value: "
+                                  @"%@, allowing navigation.",
+                                  result);
+                            decisionHandler(WKNavigationActionPolicyAllow);
+                            return;
+                          }
                           NSNumber* typedResult = result;
                           decisionHandler([typedResult boolValue] ? WKNavigationActionPolicyAllow
                                                                   : WKNavigationActionPolicyCancel);
