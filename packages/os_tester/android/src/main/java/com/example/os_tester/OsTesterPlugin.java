3b05ac67cf66dc9018a6f0663e1d25f6297872ec
Collin Jackson <jackson@google.com>
2019-03-11 09:00:17 -0700

    Add Android example

---
+++ packages/os_tester/android/src/main/java/com/example/os_tester/OsTesterPlugin.java
@@ -0,0 +1,25 @@
+package com.example.os_tester;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** OsTesterPlugin */
+public class OsTesterPlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "os_tester");
+    channel.setMethodCallHandler(new OsTesterPlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+}
