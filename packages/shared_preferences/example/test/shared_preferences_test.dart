61d71a8eb6199d23c9c34d2620ee5940cd787f90
Collin Jackson <jackson@google.com>
2019-03-14 16:19:03 -0700

    Reformat

--- packages/shared_preferences/example/test/shared_preferences_test.dart
+++ packages/shared_preferences/example/test/shared_preferences_test.dart
@@ -3,7 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences_example/main.dart';
 
 void main() {
-  testWidgets('SharedPreferences example widget test', (WidgetTester tester) async {
+  testWidgets('SharedPreferences example widget test',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
     expect(find.text('SharedPreferences Demo'), findsOneWidget);
   });
