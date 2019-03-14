facc429c720e6b8b9ba9469540afd4f4e105d0e8
Collin Jackson <jackson@google.com>
2019-03-14 16:12:13 -0700

    Check in a basic widget test to make the bots happy

---
+++ packages/shared_preferences/example/test/shared_preferences_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:shared_preferences_example/main.dart';
+
+void main() {
+  testWidgets('SharedPreferences example widget test', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    expect(find.text('SharedPreferences Demo'), findsOneWidget);
+  });
+}
