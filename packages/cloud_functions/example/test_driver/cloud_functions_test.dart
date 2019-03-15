f9bced88ba0356e08104e032d2c56a37dcbcdafc
Collin Jackson <jackson@google.com>
2019-03-15 10:40:19 -0700

    Driver test for cloud_functions

---
+++ packages/cloud_functions/example/test_driver/cloud_functions_test.dart
@@ -0,0 +1,14 @@
+// Copyright 2019, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Example', () async {
+    final FlutterDriver driver = await FlutterDriver.connect();
+    await driver.requestData(null, timeout: const Duration(minutes: 1));
+    driver.close();
+  });
+}
