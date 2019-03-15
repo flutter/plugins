8ea4d6f692b8d510b806da514f594dc454e7d104
Collin Jackson <jackson@google.com>
2019-03-15 14:30:00 -0700

    Format

--- packages/firebase_auth/example/test/firebase_auth_test.dart
+++ packages/firebase_auth/example/test/firebase_auth_test.dart
@@ -7,8 +7,7 @@ import 'package:flutter_test/flutter_test.dart';
 import '../lib/main.dart';
 
 void main() {
-  testWidgets('FirebaseAuth example widget test',
-      (WidgetTester tester) async {
+  testWidgets('FirebaseAuth example widget test', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
     await tester.tap(find.text('Test registration'));
     await tester.pumpAndSettle();
