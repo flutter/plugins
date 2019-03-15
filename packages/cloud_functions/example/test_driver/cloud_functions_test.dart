f7528d0c7cdfb21c17e5521bbe605ce8bbc479a0
Collin Jackson <jackson@google.com>
2019-03-15 10:42:36 -0700

    Rename driver test

--- packages/cloud_functions/example/test_driver/cloud_functions_test.dart
+++ packages/cloud_functions/example/test_driver/cloud_functions_test.dart
@@ -6,7 +6,7 @@ import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Example', () async {
+  test('CloudFunctions', () async {
     final FlutterDriver driver = await FlutterDriver.connect();
     await driver.requestData(null, timeout: const Duration(minutes: 1));
     driver.close();
