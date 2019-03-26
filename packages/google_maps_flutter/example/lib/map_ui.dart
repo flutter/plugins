f22eac0d010b7ab575f606eb304a54dbcebb63b2
Kaushik Iska <kaushikiska@google.com>
2019-03-26 11:17:14 -0700

    fix compile error

--- packages/google_maps_flutter/example/lib/map_ui.dart
+++ packages/google_maps_flutter/example/lib/map_ui.dart
@@ -4,8 +4,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_flutter_example/main.dart';
 
+import 'main.dart';
 import 'page.dart';
 
 final LatLngBounds sydneyBounds = LatLngBounds(
