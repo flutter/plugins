dcac87d22f702c2c052518dcb8a2fc8a912920b5
Quentin <quentin@tengio.com>
2019-03-27 10:37:52 +0000

    Use medium resolution preset for the example.

--- packages/camera/example/lib/main.dart
+++ packages/camera/example/lib/main.dart
@@ -203,7 +203,7 @@ class _CameraExampleHomeState extends State<CameraExampleHome> {
     if (controller != null) {
       await controller.dispose();
     }
-    controller = CameraController(cameraDescription, ResolutionPreset.veryLow);
+    controller = CameraController(cameraDescription, ResolutionPreset.medium);
 
     // If the controller is updated then update the UI.
     controller.addListener(() {
