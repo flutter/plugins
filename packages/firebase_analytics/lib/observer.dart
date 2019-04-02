0303a3b49950908dbc56beea777749e1315cd21a
Thomas <thomas@stockxit.com>
2019-04-02 12:57:58 +0200

    Track Navigator.pushReplacement screen changes
    Currently only standard push and pops were automatically logged to Firebase. However, this resulted in missing screen_view events for apps with more complex navigation.

--- packages/firebase_analytics/lib/observer.dart
+++ packages/firebase_analytics/lib/observer.dart
@@ -93,6 +93,14 @@ class FirebaseAnalyticsObserver extends RouteObserver<PageRoute<dynamic>> {
       _sendScreenView(route);
     }
   }
+  
+  @override
+  void didReplace({Route<dynamic> newRoute, Route<dynamic> oldRoute}) {
+    super.didReplace(newRoute: newRoute, oldRoute: oldRoute);
+    if (newRoute is PageRoute) {
+      _sendScreenView(newRoute);
+    }
+  }
 
   @override
   void didPop(Route<dynamic> route, Route<dynamic> previousRoute) {
