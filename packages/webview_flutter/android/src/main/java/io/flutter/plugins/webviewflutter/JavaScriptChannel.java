8b8656c374398d3902cf716ed47b203e2192822f
Amir Hardon <amirha@google.com>
2019-04-02 15:09:41 -0700

    Make sure to post platform channel messages from the platform thread.
    The Android's webview's JavascriptInterface doesn't promise to invoke
    the interface's Java method on the main thread, since postMessage is
    sending a message over the platform channel we need to make sure it's
    doing so from the platform thread.

--- packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/JavaScriptChannel.java
+++ packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/JavaScriptChannel.java
@@ -4,6 +4,8 @@
 
 package io.flutter.plugins.webviewflutter;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.webkit.JavascriptInterface;
 import io.flutter.plugin.common.MethodChannel;
 import java.util.HashMap;
@@ -18,25 +20,39 @@ import java.util.HashMap;
 class JavaScriptChannel {
   private final MethodChannel methodChannel;
   private final String javaScriptChannelName;
+  private final Handler platformThreadHandler;
 
   /**
    * @param methodChannel the Flutter WebView method channel to which JS messages are sent
    * @param javaScriptChannelName the name of the JavaScript channel, this is sent over the method
    *     channel with each message to let the Dart code know which JavaScript channel the message
-   *     was sent through
+   * @param platformThreadHandler
    */
-  JavaScriptChannel(MethodChannel methodChannel, String javaScriptChannelName) {
+  JavaScriptChannel(
+      MethodChannel methodChannel, String javaScriptChannelName, Handler platformThreadHandler) {
     this.methodChannel = methodChannel;
     this.javaScriptChannelName = javaScriptChannelName;
+    this.platformThreadHandler = platformThreadHandler;
   }
 
   // Suppressing unused warning as this is invoked from JavaScript.
   @SuppressWarnings("unused")
   @JavascriptInterface
-  public void postMessage(String message) {
-    HashMap<String, String> arguments = new HashMap<>();
-    arguments.put("channel", javaScriptChannelName);
-    arguments.put("message", message);
-    methodChannel.invokeMethod("javascriptChannelMessage", arguments);
+  public void postMessage(final String message) {
+    Runnable postMessageRunnable =
+        new Runnable() {
+          @Override
+          public void run() {
+            HashMap<String, String> arguments = new HashMap<>();
+            arguments.put("channel", javaScriptChannelName);
+            arguments.put("message", message);
+            methodChannel.invokeMethod("javascriptChannelMessage", arguments);
+          }
+        };
+    if (platformThreadHandler.getLooper() == Looper.getMainLooper()) {
+      postMessageRunnable.run();
+    } else {
+      platformThreadHandler.post(postMessageRunnable);
+    }
   }
 }
