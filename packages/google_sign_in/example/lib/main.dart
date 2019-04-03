fbab80f8ae0f6b531f5431950524e237502e53d3
Chris Yang <ychris@google.com>
2019-04-02 22:00:44 -0700

    fix formatting (#1436)

--- packages/google_sign_in/example/lib/main.dart
+++ packages/google_sign_in/example/lib/main.dart
@@ -54,7 +54,7 @@ class SignInDemoState extends State<SignInDemo> {
     });
     final http.Response response = await http.get(
       'https://people.googleapis.com/v1/people/me/connections'
-          '?requestMask.includeField=person.names',
+      '?requestMask.includeField=person.names',
       headers: await _currentUser.authHeaders,
     );
     if (response.statusCode != 200) {
