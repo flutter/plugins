da9cdd347b04a87ae3f5f9364187d250335ddf46
Chris Yang <ychris@google.com>
2019-04-05 16:58:39 -0700

    remove bad import

--- packages/connectivity/example/test_driver/connectivity_test.dart
+++ packages/connectivity/example/test_driver/connectivity_test.dart
@@ -1,9 +1,8 @@
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
-import 'package:connectivity/connectivity.dart';
 
 void main() {
-  test('$Connectivity', () async {
+  test('connectivity', () async {
     final FlutterDriver driver = await FlutterDriver.connect();
     await driver.requestData(null, timeout: const Duration(minutes: 1));
     driver.close();
