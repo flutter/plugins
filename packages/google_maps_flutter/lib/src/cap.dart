2682866776b8bb1b6940e9852db0d081def7453b
Kaushik Iska <kaushikiska@google.com>
2019-04-09 12:21:13 -0700

    Minor edits to Cap docs

--- packages/google_maps_flutter/lib/src/cap.dart
+++ packages/google_maps_flutter/lib/src/cap.dart
@@ -1,11 +1,11 @@
 part of google_maps_flutter;
 
-/// Immutable cap that can be applied at the start or end vertex of a Polyline.
+/// Cap that can be applied at the start or end vertex of a [Polyline].
 @immutable
 class Cap {
   const Cap._(this._json);
 
-  /// Cap that is squared off exactly at the start or end vertex of a Polyline
+  /// Cap that is squared off exactly at the start or end vertex of a [Polyline]
   /// with solid stroke pattern, equivalent to having no additional cap beyond
   /// the start or end vertex.
   ///
@@ -14,38 +14,32 @@ class Cap {
   static const Cap buttCap = Cap._(<dynamic>['buttCap']);
 
   /// Cap that is a semicircle with radius equal to half the stroke width,
-  /// centered at the start or end vertex of a Polyline with solid stroke
+  /// centered at the start or end vertex of a [Polyline] with solid stroke
   /// pattern.
   static const Cap roundCap = Cap._(<dynamic>['roundCap']);
 
   /// Cap that is squared off after extending half the stroke width beyond the
-  /// start or end vertex of a Polyline with solid stroke pattern.
+  /// start or end vertex of a [Polyline] with solid stroke pattern.
   static const Cap squareCap = Cap._(<dynamic>['squareCap']);
 
   /// Constructs a new CustomCap with a bitmap overlay centered at the start or
-  /// end vertex of a Polyline, orientated according to the direction of the line's
+  /// end vertex of a [Polyline], orientated according to the direction of the line's
   /// first or last edge and scaled with respect to the line's stroke width.
   ///
-  /// CustomCap can be applied to Polyline with any stroke pattern.
+  /// CustomCap can be applied to [Polyline] with any stroke pattern.
   ///
   /// [bitmapDescriptor] must not be null.
+  ///
   /// [refWidth] is the reference stroke width (in pixels) - the stroke width for which
-  /// the cap bitmap at its native dimension is designed. Must be positive.
-  static Cap customCapFromBitmapRefWidth(
-      BitmapDescriptor bitmapDescriptor, double refWidth) {
+  /// the cap bitmap at its native dimension is designed. Must be positive. Default value
+  /// is 10 pixels.
+  static Cap customCapFromBitmap(
+    BitmapDescriptor bitmapDescriptor, {
+    double refWidth = 10,
+  }) {
     assert(bitmapDescriptor != null);
     assert(refWidth > 0.0);
     return Cap._(<dynamic>['customCap', bitmapDescriptor._toJson(), refWidth]);
-  }
-
-  /// Constructs a new CustomCap with a Bitmap overlay.
-  ///
-  /// The default reference stroke width is 10 pixels.
-  ///
-  /// [bitmapDescriptor] must not be null.
-  static Cap customCapFromBitmap(BitmapDescriptor bitmapDescriptor) {
-    assert(bitmapDescriptor != null);
-    return Cap._(<dynamic>['customCap', bitmapDescriptor._toJson()]);
   }
 
   final dynamic _json;
