89b13742672d774b2e116e4081fb08cf4dcc2ae8
Kaushik Iska <kaushikiska@google.com>
2019-04-09 12:13:27 -0700

    rename m_i to p_i for polylines

--- packages/google_maps_flutter/test/polyline_updates_test.dart
+++ packages/google_maps_flutter/test/polyline_updates_test.dart
@@ -9,16 +9,16 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'fake_maps_controllers.dart';
 
-Set<Polyline> _toSet({Polyline m1, Polyline m2, Polyline m3}) {
+Set<Polyline> _toSet({Polyline p1, Polyline p2, Polyline p3}) {
   final Set<Polyline> res = Set<Polyline>.identity();
-  if (m1 != null) {
-    res.add(m1);
+  if (p1 != null) {
+    res.add(p1);
   }
-  if (m2 != null) {
-    res.add(m2);
+  if (p2 != null) {
+    res.add(p2);
   }
-  if (m3 != null) {
-    res.add(m3);
+  if (p3 != null) {
+    res.add(p3);
   }
   return res;
 }
@@ -47,94 +47,94 @@ void main() {
   });
 
   testWidgets('Initializing a polyline', (WidgetTester tester) async {
-    final Polyline m1 = Polyline(polylineId: PolylineId("polyline_1"));
-    await tester.pumpWidget(_mapWithPolylines(_toSet(m1: m1)));
+    final Polyline p1 = Polyline(polylineId: PolylineId("polyline_1"));
+    await tester.pumpWidget(_mapWithPolylines(_toSet(p1: p1)));
 
     final FakePlatformGoogleMap platformGoogleMap =
         fakePlatformViewsController.lastCreatedView;
     expect(platformGoogleMap.polylinesToAdd.length, 1);
 
     final Polyline initializedPolyline = platformGoogleMap.polylinesToAdd.first;
-    expect(initializedPolyline, equals(m1));
+    expect(initializedPolyline, equals(p1));
     expect(platformGoogleMap.polylineIdsToRemove.isEmpty, true);
     expect(platformGoogleMap.polylinesToChange.isEmpty, true);
   });
 
   testWidgets("Adding a polyline", (WidgetTester tester) async {
-    final Polyline m1 = Polyline(polylineId: PolylineId("polyline_1"));
-    final Polyline m2 = Polyline(polylineId: PolylineId("polyline_2"));
+    final Polyline p1 = Polyline(polylineId: PolylineId("polyline_1"));
+    final Polyline p2 = Polyline(polylineId: PolylineId("polyline_2"));
 
-    await tester.pumpWidget(_mapWithPolylines(_toSet(m1: m1)));
-    await tester.pumpWidget(_mapWithPolylines(_toSet(m1: m1, m2: m2)));
+    await tester.pumpWidget(_mapWithPolylines(_toSet(p1: p1)));
+    await tester.pumpWidget(_mapWithPolylines(_toSet(p1: p1, p2: p2)));
 
     final FakePlatformGoogleMap platformGoogleMap =
         fakePlatformViewsController.lastCreatedView;
     expect(platformGoogleMap.polylinesToAdd.length, 1);
 
     final Polyline addedPolyline = platformGoogleMap.polylinesToAdd.first;
-    expect(addedPolyline, equals(m2));
+    expect(addedPolyline, equals(p2));
     expect(platformGoogleMap.polylineIdsToRemove.isEmpty, true);
 
     expect(platformGoogleMap.polylinesToChange.length, 1);
-    expect(platformGoogleMap.polylinesToChange.first, equals(m1));
+    expect(platformGoogleMap.polylinesToChange.first, equals(p1));
   });
 
   testWidgets("Removing a polyline", (WidgetTester tester) async {
-    final Polyline m1 = Polyline(polylineId: PolylineId("polyline_1"));
+    final Polyline p1 = Polyline(polylineId: PolylineId("polyline_1"));
 
-    await tester.pumpWidget(_mapWithPolylines(_toSet(m1: m1)));
+    await tester.pumpWidget(_mapWithPolylines(_toSet(p1: p1)));
     await tester.pumpWidget(_mapWithPolylines(null));
 
     final FakePlatformGoogleMap platformGoogleMap =
         fakePlatformViewsController.lastCreatedView;
     expect(platformGoogleMap.polylineIdsToRemove.length, 1);
-    expect(platformGoogleMap.polylineIdsToRemove.first, equals(m1.polylineId));
+    expect(platformGoogleMap.polylineIdsToRemove.first, equals(p1.polylineId));
 
     expect(platformGoogleMap.polylinesToChange.isEmpty, true);
     expect(platformGoogleMap.polylinesToAdd.isEmpty, true);
   });
 
   testWidgets("Updating a polyline", (WidgetTester tester) async {
-    final Polyline m1 = Polyline(polylineId: PolylineId("polyline_1"));
-    final Polyline m2 =
+    final Polyline p1 = Polyline(polylineId: PolylineId("polyline_1"));
+    final Polyline p2 =
         Polyline(polylineId: PolylineId("polyline_1"), geodesic: true);
 
-    await tester.pumpWidget(_mapWithPolylines(_toSet(m1: m1)));
-    await tester.pumpWidget(_mapWithPolylines(_toSet(m1: m2)));
+    await tester.pumpWidget(_mapWithPolylines(_toSet(p1: p1)));
+    await tester.pumpWidget(_mapWithPolylines(_toSet(p1: p2)));
 
     final FakePlatformGoogleMap platformGoogleMap =
         fakePlatformViewsController.lastCreatedView;
     expect(platformGoogleMap.polylinesToChange.length, 1);
-    expect(platformGoogleMap.polylinesToChange.first, equals(m2));
+    expect(platformGoogleMap.polylinesToChange.first, equals(p2));
 
     expect(platformGoogleMap.polylineIdsToRemove.isEmpty, true);
     expect(platformGoogleMap.polylinesToAdd.isEmpty, true);
   });
 
   testWidgets("Updating a polyline", (WidgetTester tester) async {
-    final Polyline m1 = Polyline(polylineId: PolylineId("polyline_1"));
-    final Polyline m2 =
+    final Polyline p1 = Polyline(polylineId: PolylineId("polyline_1"));
+    final Polyline p2 =
         Polyline(polylineId: PolylineId("polyline_1"), geodesic: true);
 
-    await tester.pumpWidget(_mapWithPolylines(_toSet(m1: m1)));
-    await tester.pumpWidget(_mapWithPolylines(_toSet(m1: m2)));
+    await tester.pumpWidget(_mapWithPolylines(_toSet(p1: p1)));
+    await tester.pumpWidget(_mapWithPolylines(_toSet(p1: p2)));
 
     final FakePlatformGoogleMap platformGoogleMap =
         fakePlatformViewsController.lastCreatedView;
     expect(platformGoogleMap.polylinesToChange.length, 1);
 
     final Polyline update = platformGoogleMap.polylinesToChange.first;
-    expect(update, equals(m2));
+    expect(update, equals(p2));
     expect(update.geodesic, true);
   });
 
   testWidgets("Multi Update", (WidgetTester tester) async {
-    Polyline m1 = Polyline(polylineId: PolylineId("polyline_1"));
-    Polyline m2 = Polyline(polylineId: PolylineId("polyline_2"));
-    final Set<Polyline> prev = _toSet(m1: m1, m2: m2);
-    m1 = Polyline(polylineId: PolylineId("polyline_1"), visible: false);
-    m2 = Polyline(polylineId: PolylineId("polyline_2"), geodesic: true);
-    final Set<Polyline> cur = _toSet(m1: m1, m2: m2);
+    Polyline p1 = Polyline(polylineId: PolylineId("polyline_1"));
+    Polyline p2 = Polyline(polylineId: PolylineId("polyline_2"));
+    final Set<Polyline> prev = _toSet(p1: p1, p2: p2);
+    p1 = Polyline(polylineId: PolylineId("polyline_1"), visible: false);
+    p2 = Polyline(polylineId: PolylineId("polyline_2"), geodesic: true);
+    final Set<Polyline> cur = _toSet(p1: p1, p2: p2);
 
     await tester.pumpWidget(_mapWithPolylines(prev));
     await tester.pumpWidget(_mapWithPolylines(cur));
@@ -148,14 +148,14 @@ void main() {
   });
 
   testWidgets("Multi Update", (WidgetTester tester) async {
-    Polyline m2 = Polyline(polylineId: PolylineId("polyline_2"));
-    final Polyline m3 = Polyline(polylineId: PolylineId("polyline_3"));
-    final Set<Polyline> prev = _toSet(m2: m2, m3: m3);
+    Polyline p2 = Polyline(polylineId: PolylineId("polyline_2"));
+    final Polyline p3 = Polyline(polylineId: PolylineId("polyline_3"));
+    final Set<Polyline> prev = _toSet(p2: p2, p3: p3);
 
-    // m1 is added, m2 is updated, m3 is removed.
-    final Polyline m1 = Polyline(polylineId: PolylineId("polyline_1"));
-    m2 = Polyline(polylineId: PolylineId("polyline_2"), geodesic: true);
-    final Set<Polyline> cur = _toSet(m1: m1, m2: m2);
+    // p1 is added, p2 is updated, p3 is removed.
+    final Polyline p1 = Polyline(polylineId: PolylineId("polyline_1"));
+    p2 = Polyline(polylineId: PolylineId("polyline_2"), geodesic: true);
+    final Set<Polyline> cur = _toSet(p1: p1, p2: p2);
 
     await tester.pumpWidget(_mapWithPolylines(prev));
     await tester.pumpWidget(_mapWithPolylines(cur));
@@ -167,19 +167,19 @@ void main() {
     expect(platformGoogleMap.polylinesToAdd.length, 1);
     expect(platformGoogleMap.polylineIdsToRemove.length, 1);
 
-    expect(platformGoogleMap.polylinesToChange.first, equals(m2));
-    expect(platformGoogleMap.polylinesToAdd.first, equals(m1));
-    expect(platformGoogleMap.polylineIdsToRemove.first, equals(m3.polylineId));
+    expect(platformGoogleMap.polylinesToChange.first, equals(p2));
+    expect(platformGoogleMap.polylinesToAdd.first, equals(p1));
+    expect(platformGoogleMap.polylineIdsToRemove.first, equals(p3.polylineId));
   });
 
   testWidgets(
     "Partial Update",
     (WidgetTester tester) async {
-      final Polyline m1 = Polyline(polylineId: PolylineId("polyline_1"));
-      Polyline m2 = Polyline(polylineId: PolylineId("polyline_2"));
-      final Set<Polyline> prev = _toSet(m1: m1, m2: m2);
-      m2 = Polyline(polylineId: PolylineId("polyline_2"), geodesic: true);
-      final Set<Polyline> cur = _toSet(m1: m1, m2: m2);
+      final Polyline p1 = Polyline(polylineId: PolylineId("polyline_1"));
+      Polyline p2 = Polyline(polylineId: PolylineId("polyline_2"));
+      final Set<Polyline> prev = _toSet(p1: p1, p2: p2);
+      p2 = Polyline(polylineId: PolylineId("polyline_2"), geodesic: true);
+      final Set<Polyline> cur = _toSet(p1: p1, p2: p2);
 
       await tester.pumpWidget(_mapWithPolylines(prev));
       await tester.pumpWidget(_mapWithPolylines(cur));
@@ -187,7 +187,7 @@ void main() {
       final FakePlatformGoogleMap platformGoogleMap =
           fakePlatformViewsController.lastCreatedView;
 
-      expect(platformGoogleMap.polylinesToChange, _toSet(m2: m2));
+      expect(platformGoogleMap.polylinesToChange, _toSet(p2: p2));
       expect(platformGoogleMap.polylineIdsToRemove.isEmpty, true);
       expect(platformGoogleMap.polylinesToAdd.isEmpty, true);
     },
