1d6277f058f8769a5f5b574c6b663aefb388c5b0
Collin Jackson <jackson@google.com>
2019-04-29 04:46:27 -0700

    Fix analyzer warnings and add more documentation

--- packages/cloud_functions/lib/src/https_callable.dart
+++ packages/cloud_functions/lib/src/https_callable.dart
@@ -13,6 +13,20 @@ class HttpsCallable {
   final CloudFunctions _cloudFunctions;
   final String _functionName;
 
+  /// Executes this Callable HTTPS trigger asynchronously.
+  ///
+  /// The data passed into the trigger can be any of the following types:
+  ///
+  /// `null`
+  /// `String`
+  /// `num`
+  /// [List], where the contained objects are also one of these types.
+  /// [Map], where the values are also one of these types.
+  ///
+  /// The request to the Cloud Functions backend made by this method
+  /// automatically includes a Firebase Instance ID token to identify the app
+  /// instance. If a user is logged in with Firebase Auth, an auth ID token for
+  /// the user is also automatically included.
   Future<HttpsCallableResult> call([dynamic parameters]) async {
     try {
       final MethodChannel channel = CloudFunctions.channel;
@@ -39,11 +53,6 @@ class HttpsCallable {
     }
   }
 
-  Duration _timeout;
-
   /// The timeout to use when calling the function. Defaults to 60 seconds.
-  Duration get timeout => _timeout;
-  set timeout(Duration value) {
-    _timeout = value;
-  }
+  Duration timeout;
 }
