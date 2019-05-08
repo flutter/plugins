0dbb6f0aa340cc2650e5ba9955e89995a433c651
Arthur Thompson <arthurthompson@google.com>
2019-05-07 20:03:26 -0700

    Update formatting

--- packages/firebase_core/android/src/main/java/io/flutter/plugins/firebase/core/FlutterFireAppRegistrar.java
+++ packages/firebase_core/android/src/main/java/io/flutter/plugins/firebase/core/FlutterFireAppRegistrar.java
@@ -3,18 +3,13 @@ package io.flutter.plugins.firebase.core;
 import com.google.firebase.components.Component;
 import com.google.firebase.components.ComponentRegistrar;
 import com.google.firebase.platforminfo.LibraryVersionComponent;
-
 import java.util.Collections;
 import java.util.List;
 
 public class FlutterFireAppRegistrar implements ComponentRegistrar {
-    @Override
-    public List<Component<?>> getComponents() {
-        return Collections.<Component<?>>singletonList(
-                LibraryVersionComponent.create(
-                        "flutter-firebase_core",
-                        "0.4.1"
-                )
-        );
-    }
+  @Override
+  public List<Component<?>> getComponents() {
+    return Collections.<Component<?>>singletonList(
+        LibraryVersionComponent.create("flutter-firebase_core", "0.4.1"));
+  }
 }
