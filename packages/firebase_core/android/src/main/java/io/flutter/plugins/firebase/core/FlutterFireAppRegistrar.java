621f0714dfd0a0f29a2d631fe916cf94f07fd8b9
Arthur Thompson <arthurthompson@google.com>
2019-05-07 17:44:27 -0700

    Send Android user agent to Firebase

---
+++ packages/firebase_core/android/src/main/java/io/flutter/plugins/firebase/core/FlutterFireAppRegistrar.java
@@ -0,0 +1,20 @@
+package io.flutter.plugins.firebase.core;
+
+import com.google.firebase.components.Component;
+import com.google.firebase.components.ComponentRegistrar;
+import com.google.firebase.platforminfo.LibraryVersionComponent;
+
+import java.util.Collections;
+import java.util.List;
+
+public class FlutterFireAppRegistrar implements ComponentRegistrar {
+    @Override
+    public List<Component<?>> getComponents() {
+        return Collections.<Component<?>>singletonList(
+                LibraryVersionComponent.create(
+                        "flutter-fire-core",
+                        "0.4.1"
+                )
+        );
+    }
+}
