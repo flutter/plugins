722a74556fa15d28004787f396ca54a4acb9c0ae
Collin Jackson <jackson@google.com>
2019-05-10 07:00:42 -0700

    Update FirebaseCorePlugin.m

--- packages/firebase_core/ios/Classes/FirebaseCorePlugin.m
+++ packages/firebase_core/ios/Classes/FirebaseCorePlugin.m
@@ -7,7 +7,7 @@
 #import <Firebase/Firebase.h>
 
 #define LIBRARY_NAME @"flutter-firebase_core"
-#define LIBRARY_VERSION @"0.4.1"
+#define LIBRARY_VERSION @"0.4.0+1"
 
 static NSDictionary *getDictionaryFromFIROptions(FIROptions *options) {
   if (!options) {
