510861ce12393a3b4580eac3a6eb8bbccd970e95
Collin Jackson <jackson@google.com>
2019-05-14 15:27:32 -0700

    Add compound query to tests

--- packages/cloud_firestore/example/test_driver/cloud_firestore.dart
+++ packages/cloud_firestore/example/test_driver/cloud_firestore.dart
@@ -112,6 +112,7 @@ void main() {
 
       // startAtDocument
       snapshot = await messages
+          .orderBy('created_at')
           .where('test_run', isEqualTo: testRun)
           .startAtDocument(snapshot1)
           .getDocuments();
@@ -122,6 +123,7 @@ void main() {
 
       // startAfterDocument
       snapshot = await messages
+          .orderBy('created_at')
           .where('test_run', isEqualTo: testRun)
           .startAfterDocument(snapshot1)
           .getDocuments();
@@ -131,6 +133,7 @@ void main() {
 
       // endAtDocument
       snapshot = await messages
+          .orderBy('created_at')
           .where('test_run', isEqualTo: testRun)
           .endAtDocument(snapshot2)
           .getDocuments();
@@ -141,6 +144,7 @@ void main() {
 
       // endBeforeDocument
       snapshot = await messages
+          .orderBy('created_at')
           .where('test_run', isEqualTo: testRun)
           .endBeforeDocument(snapshot2)
           .getDocuments();
