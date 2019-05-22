05ac8ceffe0853f48d67cd0ac5e69fa840e2a704
Maurice Parrish <bmparr@google.com>
2019-05-22 15:40:34 -0700

    Android side of adding close method

--- packages/firebase_ml_vision/android/src/main/java/io/flutter/plugins/firebasemlvision/Detector.java
+++ packages/firebase_ml_vision/android/src/main/java/io/flutter/plugins/firebasemlvision/Detector.java
@@ -1,10 +1,10 @@
 package io.flutter.plugins.firebasemlvision;
 
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import java.io.IOException;
 import io.flutter.plugin.common.MethodChannel;
-import java.util.Map;
 
 interface Detector {
-  void handleDetection(
-      FirebaseVisionImage image, Map<String, Object> options, final MethodChannel.Result result);
+  void handleDetection(final FirebaseVisionImage image, final MethodChannel.Result result);
+  void close() throws IOException;
 }
