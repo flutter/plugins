50013e75e6d8427d083a8a46fd02d0d3cb38f54c
Chris Yang <cyang8954@gmail.com>
2019-05-31 15:53:44 -0700

    [firebase_ml_vision] Add missing template type parameter to `invokeMethod` calls. Bump minimum Flutter version to 1.5.0. Replace invokeMethod with invokeMapMethod wherever necessary.

--- packages/firebase_ml_vision/lib/src/barcode_detector.dart
+++ packages/firebase_ml_vision/lib/src/barcode_detector.dart
@@ -190,10 +190,8 @@ class BarcodeDetector {
 
   /// Detects barcodes in the input image.
   Future<List<Barcode>> detectInImage(FirebaseVisionImage visionImage) async {
-    // TODO(amirh): remove this on when the invokeMethod update makes it to stable Flutter.
-    // https://github.com/flutter/flutter/issues/26431
-    // ignore: strong_mode_implicit_dynamic_method
-    final List<dynamic> reply = await FirebaseVision.channel.invokeMethod(
+    final List<dynamic> reply =
+        await FirebaseVision.channel.invokeListMethod<dynamic>(
       'BarcodeDetector#detectInImage',
       <String, dynamic>{
         'options': <String, dynamic>{
