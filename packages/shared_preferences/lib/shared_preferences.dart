1e36f7e3d5844b27a716a5901f9b30fdfed5b6d2
Chris Yang <cyang8954@gmail.com>
2019-06-03 17:31:18 -0700

    [shared_preferences]Add missing template type parameter to `invokeMethod` calls. Bump minimum Flutter version to 1.5.0. Replace invokeMethod with invokeMapMethod wherever necessary.

--- packages/shared_preferences/lib/shared_preferences.dart
+++ packages/shared_preferences/lib/shared_preferences.dart
@@ -114,19 +114,13 @@ class SharedPreferences {
     if (value == null) {
       _preferenceCache.remove(key);
       return _kChannel
-          // TODO(amirh): remove this on when the invokeMethod update makes it to stable Flutter.
-          // https://github.com/flutter/flutter/issues/26431
-          // ignore: strong_mode_implicit_dynamic_method
-          .invokeMethod('remove', params)
+          .invokeMethod<bool>('remove', params)
           .then<bool>((dynamic result) => result);
     } else {
       _preferenceCache[key] = value;
       params['value'] = value;
       return _kChannel
-          // TODO(amirh): remove this on when the invokeMethod update makes it to stable Flutter.
-          // https://github.com/flutter/flutter/issues/26431
-          // ignore: strong_mode_implicit_dynamic_method
-          .invokeMethod('set$valueType', params)
+          .invokeMethod<bool>('set$valueType', params)
           .then<bool>((dynamic result) => result);
     }
   }
@@ -134,18 +128,12 @@ class SharedPreferences {
   /// Always returns true.
   /// On iOS, synchronize is marked deprecated. On Android, we commit every set.
   @deprecated
-  // TODO(amirh): remove this on when the invokeMethod update makes it to stable Flutter.
-  // https://github.com/flutter/flutter/issues/26431
-  // ignore: strong_mode_implicit_dynamic_method
-  Future<bool> commit() async => await _kChannel.invokeMethod('commit');
+  Future<bool> commit() async => await _kChannel.invokeMethod<bool>('commit');
 
   /// Completes with true once the user preferences for the app has been cleared.
   Future<bool> clear() async {
     _preferenceCache.clear();
-    // TODO(amirh): remove this on when the invokeMethod update makes it to stable Flutter.
-    // https://github.com/flutter/flutter/issues/26431
-    // ignore: strong_mode_implicit_dynamic_method
-    return await _kChannel.invokeMethod('clear');
+    return await _kChannel.invokeMethod<bool>('clear');
   }
 
   /// Fetches the latest values from the host platform.
@@ -160,8 +148,8 @@ class SharedPreferences {
   }
 
   static Future<Map<String, Object>> _getSharedPreferencesMap() async {
-    final Map<Object, Object> fromSystem =
-        await _kChannel.invokeMethod('getAll');
+    final Map<String, Object> fromSystem =
+        await _kChannel.invokeMapMethod<String, Object>('getAll');
     assert(fromSystem != null);
     // Strip the flutter. prefix from the returned preferences.
     final Map<String, Object> preferencesMap = <String, Object>{};
