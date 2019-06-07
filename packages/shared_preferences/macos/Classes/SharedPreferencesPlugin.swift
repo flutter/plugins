25f8a63f7e6b29963b00dc50369bb6be7545aa1d
Stuart Morgan <stuartmorgan@google.com>
2019-06-07 12:39:47 -0700

    Rewrite in Swift

--- packages/shared_preferences/macos/Classes/SharedPreferencesPlugin.swift
+++ packages/shared_preferences/macos/Classes/SharedPreferencesPlugin.swift
@@ -7,6 +7,56 @@ import Foundation
 
 public class SharedPreferencesPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    FLTSharedPreferencesPlugin.register(with: registrar)
+    let channel = FlutterMethodChannel(
+      name: "plugins.flutter.io/shared_preferences",
+      binaryMessenger: registrar.messenger)
+    let instance = SharedPreferencesPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
   }
+
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    let method = call.method
+    if method == "getAll" {
+      result(getAllPrefs())
+    } else if method == "setBool" ||
+              method == "setInt" ||
+              method == "setInt" ||
+              method == "setDouble" ||
+              method == "setString" ||
+              method == "setStringList" {
+      let arguments = call.arguments as! [String : Any]
+      let key = arguments["key"] as! String
+      UserDefaults.standard.set(arguments["value"], forKey: key)
+      result(true)
+    } else if (method == "commit") {
+      // UserDefaults does not need to be synchronized.
+      result(true)
+    } else if (method == "remove") {
+      let arguments = call.arguments as! [String : Any]
+      let key = arguments["key"] as! String
+      UserDefaults.standard.removeObject(forKey: key)
+      result(true)
+    } else if (method == "clear") {
+      let defaults = UserDefaults.standard
+      for (key, _) in getAllPrefs() {
+        defaults.removeObject(forKey: key)
+      }
+      result(true)
+    } else {
+      result(FlutterMethodNotImplemented)
+    }
+  }
+}
+
+private func getAllPrefs() -> [String : Any] {
+  var filteredPrefs: [String : Any] = [:]
+  if let appDomain = Bundle.main.bundleIdentifier,
+     let prefs = UserDefaults.standard.persistentDomain(forName: appDomain) {
+    for (key, value) in prefs {
+      if key.hasPrefix("flutter.") {
+        filteredPrefs[key] = value
+      }
+    }
+  }
+  return filteredPrefs
 }
