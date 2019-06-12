40c56d770d3de8e1dbdf98d41ee129cfa3bdedc4
Arthur Thompson <arthurthompson@google.com>
2019-06-12 15:23:44 -0700

    Automate version retrieval

--- packages/firebase_core/android/src/main/java/io/flutter/plugins/firebase/core/FlutterFirebaseAppRegistrar.java
+++ packages/firebase_core/android/src/main/java/io/flutter/plugins/firebase/core/FlutterFirebaseAppRegistrar.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class FlutterFirebaseAppRegistrar implements ComponentRegistrar {
   private static final String LIBRARY_NAME = "flutter-fire-core";
-  private static final String LIBRARY_VERSION = "0.4.0+3";
+  private static final String LIBRARY_VERSION = BuildConfig.FLUTTER_FIRE_VERSION;
 
   @Override
   public List<Component<?>> getComponents() {
