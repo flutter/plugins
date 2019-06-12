ad2c2f47356725927bad69d8e03bad159a84dba6
Maurice Parrish <bmparr@google.com>
2019-06-11 18:16:13 -0700

    RUN CORRECTLY

--- script/build_all_plugins_app.sh
+++ script/build_all_plugins_app.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 # This script builds the app in flutter/plugins/example/all_plugins to make
 # sure all first party plugins can be compiled together.
@@ -10,19 +9,20 @@ REPO_DIR="$(dirname "$SCRIPT_DIR")"
 
 source "$SCRIPT_DIR/common.sh"
 
-dir="$REPO_DIR/examples/all_plugins"
 versions=("debug" "release")
 platforms=("apk" "ios --no-codesign")
 failures=()
 
+cd $REPO_DIR/examples/all_plugins
+
 for platform in "${platforms[@]}"; do
   for version in "${versions[@]}"; do
-    cd "$dir" && flutter build $platform --$version
+    (flutter build $platform --$version) > /dev/null
 
     if [ $? -eq 0 ]; then
       echo "Successfully built all_plugins $version $platform." 
     else
-      error "Failed to build $version $platform."
+      error "Failed to build all_plugins $version $platform."
       failures=("${failures[@]}" "$version $platform")
     fi
   done
