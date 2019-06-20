da30f723e7745719a201f23974cd42a603d350fc
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2019-06-20 14:35:18 +0700

    resolve feedback https://github.com/flutter/plugins/pull/1075#discussion_r294026897

--- packages/firebase_analytics/test/firebase_analytics_test.dart
+++ packages/firebase_analytics/test/firebase_analytics_test.dart
@@ -12,6 +12,19 @@ void main() {
   final FirebaseAnalytics analytics = FirebaseAnalytics();
   const MethodChannel channel =
       MethodChannel('plugins.flutter.io/firebase_analytics');
+  MethodCall methodCall;
+
+  setUp(() async {
+    channel.setMockMethodCallHandler((MethodCall m) async {
+      methodCall = m;
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+    methodCall = null;
+  });
+
   group('filterOutNulls', () {
     test('filters out null values', () {
       final Map<String, dynamic> original = <String, dynamic>{
@@ -27,44 +40,47 @@ void main() {
     });
   });
 
-  group('$FirebaseAnalytics', () {
-    String invokedMethod;
-    dynamic arguments;
-
-    setUp(() {
-      invokedMethod = null;
-      arguments = null;
-
-      channel.setMockMethodCallHandler((MethodCall call) async {
-        invokedMethod = call.method;
-        arguments = call.arguments;
-      });
-    });
-
+  group('FirebaseAnalytics', () {
     test('setUserId', () async {
       await analytics.setUserId('test-user-id');
-      expect(invokedMethod, 'setUserId');
-      expect(arguments, 'test-user-id');
+      expect(
+        methodCall,
+        isMethodCall(
+          'setUserId',
+          arguments: 'test-user-id',
+        ),
+      );
     });
 
     test('setCurrentScreen', () async {
       await analytics.setCurrentScreen(
-          screenName: 'test-screen-name',
-          screenClassOverride: 'test-class-override');
-      expect(invokedMethod, 'setCurrentScreen');
-      expect(arguments, <String, String>{
-        'screenName': 'test-screen-name',
-        'screenClassOverride': 'test-class-override',
-      });
+        screenName: 'test-screen-name',
+        screenClassOverride: 'test-class-override',
+      );
+      expect(
+        methodCall,
+        isMethodCall(
+          'setCurrentScreen',
+          arguments: <String, String>{
+            'screenName': 'test-screen-name',
+            'screenClassOverride': 'test-class-override',
+          },
+        ),
+      );
     });
 
     test('setUserProperty', () async {
       await analytics.setUserProperty(name: 'test_name', value: 'test-value');
-      expect(invokedMethod, 'setUserProperty');
-      expect(arguments, <String, String>{
-        'name': 'test_name',
-        'value': 'test-value',
-      });
+      expect(
+        methodCall,
+        isMethodCall(
+          'setUserProperty',
+          arguments: <String, String>{
+            'name': 'test_name',
+            'value': 'test-value',
+          },
+        ),
+      );
     });
 
     test('setUserProperty rejects invalid names', () async {
@@ -88,57 +104,65 @@ void main() {
 
     test('setAnalyticsCollectionEnabled', () async {
       await analytics.setAnalyticsCollectionEnabled(false);
-      expect(invokedMethod, 'setAnalyticsCollectionEnabled');
-      expect(arguments, false);
+      expect(
+        methodCall,
+        isMethodCall(
+          'setAnalyticsCollectionEnabled',
+          arguments: false,
+        ),
+      );
     });
 
     test('setMinimumSessionDuration', () async {
       await analytics.android.setMinimumSessionDuration(123);
-      expect(invokedMethod, 'setMinimumSessionDuration');
-      expect(arguments, 123);
+      expect(
+        methodCall,
+        isMethodCall(
+          'setMinimumSessionDuration',
+          arguments: 123,
+        ),
+      );
     });
 
     test('setSessionTimeoutDuration', () async {
       await analytics.android.setSessionTimeoutDuration(234);
-      expect(invokedMethod, 'setSessionTimeoutDuration');
-      expect(arguments, 234);
+      expect(
+        methodCall,
+        isMethodCall(
+          'setSessionTimeoutDuration',
+          arguments: 234,
+        ),
+      );
     });
 
     test('resetAnalyticsData', () async {
       await analytics.resetAnalyticsData();
-      expect(invokedMethod, 'resetAnalyticsData');
+      expect(
+        methodCall,
+        isMethodCall(
+          'resetAnalyticsData',
+          arguments: null,
+        ),
+      );
     });
   });
 
-  group('$FirebaseAnalytics analytics events', () {
-    String name;
-    dynamic parameters;
-
-    setUp(() {
-      name = null;
-      parameters = null;
-
-      channel.setMockMethodCallHandler((MethodCall call) async {
-        if (call.method != 'logEvent') {
-          throw ArgumentError('Only logEvent invocations expected');
-        }
-        final dynamic args = call.arguments;
-        name = args['name'];
-        parameters = args['parameters'];
-        expect(args.keys, unorderedEquals(<String>['name', 'parameters']));
-        return Future<void>.value();
-      });
-    });
-
-    tearDown(() {
-      channel.setMockMethodCallHandler(null);
-    });
-
+  group('FirebaseAnalytics analytics events', () {
     test('logEvent log events', () async {
       await analytics.logEvent(
-          name: 'test-event', parameters: <String, dynamic>{'a': 'b'});
-      expect(name, 'test-event');
-      expect(parameters, <String, dynamic>{'a': 'b'});
+        name: 'test-event',
+        parameters: <String, dynamic>{'a': 'b'},
+      );
+      expect(
+        methodCall,
+        isMethodCall(
+          'logEvent',
+          arguments: <String, dynamic>{
+            'name': 'test-event',
+            'parameters': <String, dynamic>{'a': 'b'},
+          },
+        ),
+      );
     });
 
     test('logEvent rejects events with reserved names', () async {
@@ -152,7 +176,7 @@ void main() {
     void smokeTest(String testFunctionName, Future<void> testFunction()) {
       test('$testFunctionName works', () async {
         await testFunction();
-        expect(name, testFunctionName);
+        expect(methodCall.arguments['name'], testFunctionName);
       });
     }
 
