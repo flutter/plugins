6644eb989360deaf6eb0b9b04ce782673400a5b4
Kaushik Iska <kaushikiska@google.com>
2019-06-26 11:04:15 -0700

    need to revert this

--- script/version_checker/lib/main.dart
+++ script/version_checker/lib/main.dart
@@ -1,13 +1,39 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:args/args.dart';
 import 'package:git/git.dart';
-import 'package:path/path.dart' as p;
+import 'package:pub_semver/pub_semver.dart';
 
-Future<void> main() async {
-  print('Current directory: ${p.current}');
+const String kRootDirOption = 'root_dir';
 
-  if (await GitDir.isGitDir(p.current)) {
-    final gitDir = await GitDir.fromExisting(p.current);
-    final commitCount = await gitDir.commitCount();
-    print('Git commit count: $commitCount');
+bool isPubspec(String file) {
+  return file.endsWith('pubspec.yaml');
+}
+
+Future<List<String>> getChangedPubSpecs(GitDir baseGitDir) async {
+  final ProcessResult changedFilesCommand = await baseGitDir
+      .runCommand(<String>['diff', '--name-only', 'master..HEAD']);
+  final List<String> changedFiles =
+      changedFilesCommand.stdout.toString().split('\n');
+  return changedFiles.where(isPubspec).toList();
+}
+
+Future<void> main(List<String> args) async {
+  final ArgParser parser = ArgParser();
+  parser.addOption(kRootDirOption);
+
+  final ArgResults results = parser.parse(args);
+  final String rootDir = results[kRootDirOption];
+
+  if (await GitDir.isGitDir(rootDir)) {
+    final GitDir baseGitDir = await GitDir.fromExisting(rootDir);
+    final List<String> changedPubspecs = await getChangedPubSpecs(baseGitDir);
+
+    print(changedPubspecs);
   } else {
     print('Not a Git directory');
   }
