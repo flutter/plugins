fd2f0bbf39c315dfa522d68d962e0dc4c7a990ba
Maurice Parrish <bmparr@google.com>
2019-06-27 11:42:06 -0700

    Add pod install?

--- script/build_all_plugins_app.sh
+++ script/build_all_plugins_app.sh
@@ -13,7 +13,7 @@ check_changed_packages > /dev/null
 cd $REPO_DIR/examples/all_plugins
 flutter clean > /dev/null
 (cd "$REPO_DIR" && pub global run flutter_plugin_tools gen-pubspec)
-(cd "$REPO_DIR" && pub get)
+(cd $REPO_DIR/examples/all_plugins/ios && pod install)
 
 function error() {
   echo "$@" 1>&2
