35b17638aea4717e2263084950ca4020d7233e2e
Kaushik Iska <iska.kaushik@gmail.com>
2019-07-01 12:37:20 -0700

    Disable version-check until we fix the patch version (#1786)
    * Disable version-check until we fix the patch version
    
    * add reason

--- script/incremental_build.sh
+++ script/incremental_build.sh
@@ -24,7 +24,8 @@ else
     echo "No changes detected in packages."
   else
     (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" --plugins="$CHANGED_PACKAGES" $PLUGIN_SHARDING)
-    echo "Running version check for changed packages"
-    (cd "$REPO_DIR" && pub global run flutter_plugin_tools version-check --base_sha="$(get_branch_base_sha)")
+    # Disabled temporarily see: https://github.com/flutter/flutter/issues/35412
+    # echo "Running version check for changed packages"
+    # (cd "$REPO_DIR" && pub global run flutter_plugin_tools version-check --base_sha="$(get_branch_base_sha)")
   fi
 fi
