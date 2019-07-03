3c46be03acfc8cd05d1297677c23737421333001
Prakhar Srivastav <prsrivastav@google.com>
2019-07-02 19:26:37 -0700

    address comments

--- packages/firebase_inappmessaging/lib/firebase_inappmessaging.dart
+++ packages/firebase_inappmessaging/lib/firebase_inappmessaging.dart
@@ -10,16 +10,11 @@ class FirebaseInAppMessaging {
     await _channel.invokeMethod('triggerEvent', {'eventName': eventName});
   }
 
-  static Future<void> configure({bool suppress, bool dataCollectionEnabled}) async {
-    await _channel.invokeMethod('configure', {
-      "suppress": suppress, "dataCollectionEnabled": dataCollectionEnabled});
-  }
-
   static Future<void> setMessagesSuppressed(bool suppress) async {
     await _channel.invokeMethod('setMessagesSuppressed', {suppress: suppress});
   }
 
-  static Future<void> dataCollectionEnabled(bool dataCollectionEnabled) async {
+  static Future<void> setDataCollectionEnabled(bool dataCollectionEnabled) async {
     await _channel.invokeMethod('dataCollectionEnabled', {dataCollectionEnabled: dataCollectionEnabled});
   }
 }
