95a0d3ccf203d4234517d441b554fa39b1122d8d
Maurice Parrish <bmparr@google.com>
2019-07-08 13:51:26 -0700

    More documentation

--- packages/camera/lib/src/support_android/camera_info.dart
+++ packages/camera/lib/src/support_android/camera_info.dart
@@ -4,8 +4,12 @@
 
 part of support_android_camera;
 
+/// The direction that the camera faces relative to the screen.
 enum Facing { back, front }
 
+/// Information about a camera.
+///
+/// Used with [CameraApi.supportAndroid].
 class CameraInfo implements CameraDescription {
   const CameraInfo._({
     @required this.id,
@@ -27,7 +31,22 @@ class CameraInfo implements CameraDescription {
 
   @override
   final int id;
+
+  /// The direction that the camera faces relative to the screen.
   final Facing facing;
+
+  /// The orientation of the camera image.
+  ///
+  /// The value is the angle that the camera image needs to be rotated clockwise
+  /// so it shows correctly on the display in its natural orientation. It should
+  /// be 0, 90, 180, or 270.
+  ///
+  /// For example, suppose a device has a naturally tall screen. The back-facing
+  /// camera sensor is mounted in landscape. You are looking at the screen.
+  /// If the top side of the camera sensor is aligned with the right edge of the
+  /// screen in natural orientation, the value should be 90. If the top side of
+  /// a front-facing camera sensor is aligned with the right of the screen, the
+  /// value should be 270.
   final int orientation;
 
   @override
--- packages/camera/lib/src/support_android/support_android_camera.dart
+++ packages/camera/lib/src/support_android/support_android_camera.dart
@@ -4,15 +4,40 @@
 
 part of support_android_camera;
 
+/// Class used to control the camera for [CameraApi.supportAndroid].
+///
+/// This class is used to set image capture settings, start/stop preview, snap
+/// pictures, and retrieve frames for encoding for video. This class is a client
+/// for the Camera service, which manages the actual camera hardware.
 class SupportAndroidCamera with NativeMethodCallHandler, CameraClosable {
   SupportAndroidCamera._();
 
+  /// The number of physical cameras available on this device.
+  ///
+  /// The return value of this method might change dynamically if the device
+  /// supports external cameras and an external camera is connected or
+  /// disconnected.
+  ///
+  /// If there is a logical multi-camera in the system, to maintain app backward
+  /// compatibility, this method will only expose one camera per facing for all
+  /// logical camera and physical camera groups. Use [CameraApi.android]
+  /// API to see all cameras.
   static Future<int> getNumberOfCameras() {
     return CameraChannel.channel.invokeMethod<int>(
       '$SupportAndroidCamera#getNumberOfCameras',
     );
   }
 
+  /// Creates a new [SupportAndroidCamera] object to access a hardware camera.
+  ///
+  /// If the same camera is opened by other applications, this will throw a
+  /// [PlatformException].
+  ///
+  /// You must call [release] when you are done using the camera, otherwise it
+  /// will remain locked and be unavailable to other applications.
+  ///
+  /// Your application should only have one Camera object active at a time for a
+  /// particular hardware camera.
   static SupportAndroidCamera open(int cameraId) {
     final SupportAndroidCamera camera = SupportAndroidCamera._();
 
@@ -24,6 +49,9 @@ class SupportAndroidCamera with NativeMethodCallHandler, CameraClosable {
     return camera;
   }
 
+  /// Returns the information about a particular camera.
+  ///
+  /// If [getNumberOfCameras] returns N, the valid id is 0 to N-1.
   static Future<CameraInfo> getCameraInfo(int cameraId) async {
     final Map<String, dynamic> infoMap =
         await CameraChannel.channel.invokeMapMethod<String, dynamic>(
@@ -34,6 +62,9 @@ class SupportAndroidCamera with NativeMethodCallHandler, CameraClosable {
     return CameraInfo._fromMap(infoMap);
   }
 
+  /// Sets the [NativeTexture] to be used for live preview.
+  ///
+  /// This method must be called before [startPreview].
   set previewTexture(NativeTexture texture) {
     assert(!isClosed);
 
@@ -43,6 +74,10 @@ class SupportAndroidCamera with NativeMethodCallHandler, CameraClosable {
     );
   }
 
+  /// Starts capturing and drawing preview frames to the screen.
+  ///
+  /// Preview will not actually start until a [NativeTexture] is supplied with
+  /// [previewTexture].
   Future<void> startPreview() {
     assert(!isClosed);
 
@@ -52,6 +87,7 @@ class SupportAndroidCamera with NativeMethodCallHandler, CameraClosable {
     );
   }
 
+  /// Stops capturing and drawing preview frames to the [previewTexture].
   Future<void> stopPreview() {
     if (isClosed) return Future<void>.value();
 
@@ -61,6 +97,9 @@ class SupportAndroidCamera with NativeMethodCallHandler, CameraClosable {
     );
   }
 
+  /// Disconnects and releases the Camera object resources.
+  ///
+  /// You must call this as soon as you're done with the Camera object.
   Future<void> release() {
     if (isClosed) return Future<void>.value();
 
