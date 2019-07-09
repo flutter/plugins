16854530c5745d69c13685249cd6295b1f5b8fca
Maurice Parrish <bmparr@google.com>
2019-07-09 11:24:53 -0700

    Remove preview

--- packages/camera/lib/camera.dart
+++ packages/camera/lib/camera.dart
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 export 'src/camera_controller.dart';
-export 'src/camera_preview.dart';
 export 'src/camera_testing.dart';
 export 'src/common/camera_interface.dart';
 export 'src/common/native_texture.dart';
