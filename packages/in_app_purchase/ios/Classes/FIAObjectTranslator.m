2e9d9d41f6b2c3dfa28ab3e87f68ab808e4934a2
YukiOya <yuki.oya.1107@gmail.com>
2019-07-11 01:13:41 +0900

    [in_app_purchase] iOS: Support unsupported UserInfo value types in NSError. (#1819)

--- packages/in_app_purchase/ios/Classes/FIAObjectTranslator.m
+++ packages/in_app_purchase/ios/Classes/FIAObjectTranslator.m
@@ -153,11 +153,18 @@
   if (!error) {
     return nil;
   }
-  return @{
-    @"code" : @(error.code),
-    @"domain" : error.domain ?: @"",
-    @"userInfo" : error.userInfo ?: @{}
-  };
+  NSMutableDictionary *userInfo = [NSMutableDictionary new];
+  for (NSErrorUserInfoKey key in error.userInfo) {
+    id value = error.userInfo[key];
+    if ([value isKindOfClass:[NSError class]]) {
+      userInfo[key] = [FIAObjectTranslator getMapFromNSError:value];
+    } else if ([value isKindOfClass:[NSURL class]]) {
+      userInfo[key] = [value absoluteString];
+    } else {
+      userInfo[key] = value;
+    }
+  }
+  return @{@"code" : @(error.code), @"domain" : error.domain ?: @"", @"userInfo" : userInfo};
 }
 
 @end
