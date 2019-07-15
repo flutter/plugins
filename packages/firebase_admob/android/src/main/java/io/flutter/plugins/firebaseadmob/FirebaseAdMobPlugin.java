4c1b60572b8418a434f49b98921841d9b420344c
Collin Jackson <jackson@google.com>
2019-07-15 11:51:57 -0700

    Update FirebaseAdMobPlugin.java

--- packages/firebase_admob/android/src/main/java/io/flutter/plugins/firebaseadmob/FirebaseAdMobPlugin.java
+++ packages/firebase_admob/android/src/main/java/io/flutter/plugins/firebaseadmob/FirebaseAdMobPlugin.java
@@ -26,7 +26,7 @@ public class FirebaseAdMobPlugin implements MethodCallHandler {
   public static void registerWith(Registrar registrar) {
     if (registrar.activity() == null) {
       // If a background flutter view tries to register the plugin, there will be no activity from the registrar,
-      // we stop the registering process immediately because the firebase_admob requires an activity.
+      // We stop the registering process immediately because the firebase_admob requires an activity.
       return;
     }
     final MethodChannel channel =
