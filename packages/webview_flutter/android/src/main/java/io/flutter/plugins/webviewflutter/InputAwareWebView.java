849ec43a72a472bfa77f384535ae54098e7b356d
Michael Klimushyn <mklim@google.com>
2019-07-15 14:57:10 -0700

    [webview_flutter] Support Flutter `TextInput`s (#1852)
    [webview_flutter] Support Flutter `TextInput`s
    
    Adds logic to `InputAwareWebView#clearFocus` to reset input creation
    back to the container view's thread whenever the WebView is unfocused.
    Fixes issue where Flutter UIs could no longer receive InputConnections
    because WebView was causing all connections to happen on WebView's IME
    thread.

--- packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/InputAwareWebView.java
+++ packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/InputAwareWebView.java
@@ -112,4 +112,32 @@ final class InputAwareWebView extends WebView {
         });
     return super.checkInputConnectionProxy(view);
   }
+
+  /**
+   * Ensure that input creation happens back on {@link #containerView}'s thread once this view no
+   * longer has focus.
+   *
+   * <p>The logic in {@link #checkInputConnectionProxy} forces input creation to happen on Webview's
+   * thread for all connections. We undo it here so users will be able to go back to typing in
+   * Flutter UIs as expected.
+   */
+  @Override
+  public void clearFocus() {
+    super.clearFocus();
+    if (proxyAdapterView == null) {
+      // No need to reset the InputConnection to the default thread if we've never changed it.
+      return;
+    }
+    containerView.requestFocus();
+    containerView.post(
+        new Runnable() {
+          @Override
+          public void run() {
+            containerView.onWindowFocusChanged(true);
+            InputMethodManager imm =
+                (InputMethodManager) getContext().getSystemService(INPUT_METHOD_SERVICE);
+            imm.isActive(containerView);
+          }
+        });
+  }
 }
