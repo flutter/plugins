6e19bf12f5780c583469b785e3ebe74727867862
Collin Jackson <jackson@google.com>
2019-07-16 08:30:28 -0700

    Use testWidgets since we don’t support package:test yet

--- packages/package_info/example/test_driver/package_info.dart
+++ packages/package_info/example/test_driver/package_info.dart
@@ -10,7 +10,7 @@ void main() {
   tearDownAll(() => completer.complete(null));
 
   group('package_info test driver', () {
-    test('test package info result', () async {
+    testWidgets('test package info result', (_) async {
       final PackageInfo info = await PackageInfo.fromPlatform();
       // These tests are based on the example app. The tests should be updated if any related info changes.
       if (Platform.isAndroid) {
