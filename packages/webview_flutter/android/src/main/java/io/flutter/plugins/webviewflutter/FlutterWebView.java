bb5eba21025361cefc40fced5454ba09c0b17377
luisdiaz14 <52952792+luisdiaz14@users.noreply.github.com>
2019-07-16 10:51:05 +0200

    Update FlutterWebView.java
    Fix html dropdown

--- packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
+++ packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
@@ -20,6 +20,8 @@ import io.flutter.plugin.platform.PlatformView;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import android.app.Activity;
+import io.flutter.app.FlutterApplication;
 
 public class FlutterWebView implements PlatformView, MethodCallHandler {
   private static final String JS_CHANNEL_NAMES_FIELD = "javascriptChannelNames";
@@ -35,9 +37,19 @@ public class FlutterWebView implements PlatformView, MethodCallHandler {
       int id,
       Map<String, Object> params,
       final View containerView) {
-    webView = new InputAwareWebView(context, containerView);
+    
+    Context activityContext = context;
+    Context appContext = context.getApplicationContext();
+    if (appContext instanceof FlutterApplication) {
+      Activity currentActivity = ((FlutterApplication) appContext).getCurrentActivity();
+      if (currentActivity != null) {
+        activityContext = currentActivity;
+      }
+    }
 
-    platformThreadHandler = new Handler(context.getMainLooper());
+    webView = new InputAwareWebView(activityContext, containerView);
+
+    platformThreadHandler = new Handler(activityContext.getMainLooper());
     // Allow local storage.
     webView.getSettings().setDomStorageEnabled(true);
 
