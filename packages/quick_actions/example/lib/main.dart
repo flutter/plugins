fea7ca102d6151306ef6246844107c155d7a7c46
Júlio Cesar Bueno Cotta <juliocbcotta@gmail.com>
2019-07-16 15:44:46 -0300

    [quick_actions] Fixes Android action forwarding (#1856)
    * - Fixes action forwarding by removing extra Activity.
    - Report shortcut usage to Android framework.
    - Improves example.

--- packages/quick_actions/example/lib/main.dart
+++ packages/quick_actions/example/lib/main.dart
@@ -10,42 +10,53 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Quick Actions Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String shortcut = "no action set";
+
   @override
   void initState() {
     super.initState();
+
     final QuickActions quickActions = QuickActions();
     quickActions.initialize((String shortcutType) {
-      if (shortcutType == 'action_main') {
-        print('The user tapped on the "Main view" action.');
-      }
+      setState(() {
+        if (shortcutType != null) shortcut = shortcutType;
+      });
     });
 
     quickActions.setShortcutItems(<ShortcutItem>[
+      // NOTE: This first action icon will only work on iOS.
+      // In a real world project keep the same file name for both platforms.
       const ShortcutItem(
-          type: 'action_main', localizedTitle: 'Main view', icon: 'AppIcon'),
+        type: 'action_one',
+        localizedTitle: 'Action one',
+        icon: 'AppIcon',
+      ),
+      // NOTE: This second action icon will only work on Android.
+      // In a real world project keep the same file name for both platforms.
+      const ShortcutItem(
+          type: 'action_two',
+          localizedTitle: 'Action two',
+          icon: 'ic_launcher'),
     ]);
   }
 
@@ -53,12 +64,13 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Plugin example app'),
+        title: Text('$shortcut'),
       ),
       body: const Center(
-          child: Text('On home screen, long press the icon to '
-              'get Main view action. Tapping on that action should print '
-              'a message to the log.')),
+        child: Text('On home screen, long press the app icon to '
+            'get Action one or Action two options. Tapping on that action should  '
+            'set the toolbar title.'),
+      ),
     );
   }
 }
