52620ba27a5c4ef23a618f7b28c308c4f5ef0b0c
Lukasz <lukasz@intheloup.io>
2019-07-16 22:45:52 -0400

    Revert “Fix claims type (3)”.

--- packages/firebase_auth/lib/src/id_token_result.dart
+++ packages/firebase_auth/lib/src/id_token_result.dart
@@ -41,8 +41,7 @@ class IdTokenResult {
 
   /// The entire payload claims of the ID token including the standard reserved
   /// claims as well as the custom claims.
-  Map<dynamic, dynamic> get claims =>
-      Map<dynamic, dynamic>.from(_data['claims']);
+  Map<dynamic, dynamic> get claims => _data['claims'];
 
   @override
   String toString() {
