6ab10c13e9e79a75e6ca0654960160dbca71d5a6
Maurice Parrish <bmparr@google.com>
2019-07-22 14:48:34 -0700

    documentation

--- packages/camera/lib/new/src/support_android/camera.dart
+++ packages/camera/lib/new/src/support_android/camera.dart
@@ -1,16 +1,35 @@
 part of support_android_camera;
 
+/// The Camera class used to set image capture settings, start/stop preview, snap pictures, and retrieve frames for encoding for video.
+///
+/// This class is a client for the Camera service, which manages the actual
+/// camera hardware.
+///
+/// This exposes the deprecated Android
+/// [Camera](https://developer.android.com/reference/android/hardware/Camera)
+/// API. This should only be used with Android sdk versions less than 21.
 class Camera with NativeMethodCallHandler {
   Camera._();
 
   bool _isClosed = false;
 
+  /// Retrieves the number of physical cameras available on this device.
   static Future<int> getNumberOfCameras() {
     return CameraChannel.channel.invokeMethod<int>(
       'Camera#getNumberOfCameras',
     );
   }
 
+  /// Creates a new [Camera] object to access a particular hardware camera.
+  ///
+  /// If the same camera is opened by other applications, this will throw a
+  /// [PlatformException].
+  ///
+  /// You must call [release] when you are done using the camera, otherwise it
+  /// will remain locked and be unavailable to other applications.
+  ///
+  /// Your application should only have one [Camera] object active at a time for
+  /// a particular hardware camera.
   static Camera open(int cameraId) {
     final Camera camera = Camera._();
 
@@ -22,6 +41,9 @@ class Camera with NativeMethodCallHandler {
     return camera;
   }
 
+  /// Retrieves information about a particular camera.
+  ///
+  /// If [getNumberOfCameras] returns N, the valid id is 0 to N-1.
   static Future<CameraInfo> getCameraInfo(int cameraId) async {
     final Map<String, dynamic> infoMap =
         await CameraChannel.channel.invokeMapMethod<String, dynamic>(
@@ -32,6 +54,16 @@ class Camera with NativeMethodCallHandler {
     return CameraInfo._fromMap(infoMap);
   }
 
+  /// Sets the [NativeTexture] to be used for live preview.
+  ///
+  /// This method must be called before [startPreview].
+  ///
+  /// The one exception is that if the preview native texture is not set (or
+  /// set to null) before [startPreview] is called, then this method may be
+  /// called once with a non-null parameter to set the preview texture.
+  /// (This allows camera setup and surface creation to happen in parallel,
+  /// saving time.) The preview native texture may not otherwise change while
+  /// preview is running.
   set previewTexture(NativeTexture texture) {
     assert(!_isClosed);
 
@@ -41,6 +73,10 @@ class Camera with NativeMethodCallHandler {
     );
   }
 
+  /// Starts capturing and drawing preview frames to the screen.
+  ///
+  /// Preview will not actually start until a surface is supplied with
+  /// [previewTexture].
   Future<void> startPreview() {
     assert(!_isClosed);
 
@@ -50,8 +86,9 @@ class Camera with NativeMethodCallHandler {
     );
   }
 
+  /// Stops capturing and drawing preview frames to the [previewTexture], and resets the camera for a future call to [startPreview].
   Future<void> stopPreview() {
-    if (_isClosed) return Future<void>.value();
+    assert(!_isClosed);
 
     return CameraChannel.channel.invokeMethod<void>(
       'Camera#stopPreview',
@@ -59,6 +96,9 @@ class Camera with NativeMethodCallHandler {
     );
   }
 
+  /// Disconnects and releases the Camera object resources.
+  ///
+  /// You must call this as soon as you're done with the Camera object.
   Future<void> release() {
     if (_isClosed) return Future<void>.value();
 
--- packages/camera/lib/new/src/support_android/camera_info.dart
+++ packages/camera/lib/new/src/support_android/camera_info.dart
@@ -1,7 +1,11 @@
 part of support_android_camera;
 
+/// The direction that the camera faces.
 enum Facing { back, front }
 
+/// Information about a camera.
+///
+/// Retrieved from [Camera.getCameraInfo].
 class CameraInfo implements CameraDescription {
   const CameraInfo._({
     @required this.id,
@@ -21,8 +25,24 @@ class CameraInfo implements CameraDescription {
     );
   }
 
+  /// Identifier for a particular camera.
   final int id;
+
+  /// The direction that the camera faces.
   final Facing facing;
+
+  /// The orientation of the camera image.
+  ///
+  /// The value is the angle that the camera image needs to be rotated clockwise
+  /// so it shows correctly on the display in its natural orientation.
+  /// It should be 0, 90, 180, or 270.
+  ///
+  /// For example, suppose a device has a naturally tall screen. The back-facing
+  /// camera sensor is mounted in landscape. You are looking at the screen. If
+  /// the top side of the camera sensor is aligned with the right edge of the
+  /// screen in natural orientation, the value should be 90. If the top side of
+  /// a front-facing camera sensor is aligned with the right of the screen, the
+  /// value should be 270.
   final int orientation;
 
   @override
