09ea2ef91e4d28ccd050ca17d1e5e7a0b6011952
Maurice Parrish <bmparr@google.com>
2019-07-24 17:31:28 -0700

    Add licenses

--- packages/cloud_functions/test/cloud_functions_test.dart
+++ packages/cloud_functions/test/cloud_functions_test.dart
@@ -1,6 +1,6 @@
-// Copyright 2018, the Chromium project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_core/firebase_core.dart';
