f3aee1dff02bd086c1833d054ff42722e7a2fbea
creativecreatorormaybenot <19204050+creativecreatorormaybenot@users.noreply.github.com>
2019-07-26 08:17:12 +0000

    minor

--- packages/firebase_crashlytics/ios/Classes/FirebaseCrashlyticsPlugin.m
+++ packages/firebase_crashlytics/ios/Classes/FirebaseCrashlyticsPlugin.m
@@ -62,7 +62,8 @@
     // Add additional information from the Flutter framework to the exception reported in Crashlytics.
     // Using CLSLog instead of CLS_LOG to try to avoid the automatic inclusion of the line number.
     // It also ensures that the log is only written to Crashlytics and not also to the offline log
-    // as explained here: https://support.crashlytics.com/knowledgebase/articles/92519-how-do-i-use-logging
+    // as explained here:
+    // https://support.crashlytics.com/knowledgebase/articles/92519-how-do-i-use-logging
     // Although, that would only happen in debug mode, which this method call is never called in.
     NSString *information = call.arguments[@"information"];
     if ([information length] != 0) {
