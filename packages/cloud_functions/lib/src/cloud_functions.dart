e4cc6004ed8b22f2ef50d1d7dd1ff3ad7e411fa3
Davo <davbr87@gmail.com>
2019-07-28 17:06:29 -0400

    [cloud_functions] Support for cloud functions emulators (#1887)
    * Allowing use emulator functions
    
    Co-Authored-By: Collin Jackson <jackson@google.com>

--- packages/cloud_functions/lib/src/cloud_functions.dart
+++ packages/cloud_functions/lib/src/cloud_functions.dart
@@ -16,9 +16,10 @@ class CloudFunctionsException implements Exception {
 ///
 /// You can get an instance by calling [CloudFunctions.instance].
 class CloudFunctions {
-  CloudFunctions({FirebaseApp app, String region})
+  CloudFunctions({FirebaseApp app, String region, String origin})
       : _app = app ?? FirebaseApp.instance,
-        _region = region;
+        _region = region,
+        _origin = origin;
 
   @visibleForTesting
   static const MethodChannel channel = MethodChannel('cloud_functions');
@@ -31,6 +32,8 @@ class CloudFunctions {
 
   final String _region;
 
+  String _origin;
+
   /// Gets an instance of a Callable HTTPS trigger in Cloud Functions.
   ///
   /// Can then be executed by calling `call()` on it.
@@ -38,5 +41,13 @@ class CloudFunctions {
   /// @param functionName The name of the callable function.
   HttpsCallable getHttpsCallable({@required String functionName}) {
     return HttpsCallable._(this, functionName);
+  }
+
+  /// Changes this instance to point to a Cloud Functions emulator running locally.
+  ///
+  /// @param origin The origin of the local emulator, such as "//10.0.2.2:5005".
+  CloudFunctions useFunctionsEmulator({@required String origin}) {
+    _origin = origin;
+    return this;
   }
 }
