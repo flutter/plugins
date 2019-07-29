0b9be71c8f58b2e47bc2a434c4f194e82be878f8
Lukas Piliszczuk <lukasz@intheloup.io>
2019-07-29 18:17:50 -0400

    [firebase_auth] update getIdTokenResult to IdResultToken (#1871)
    * Implement IdResultToken type and update getIdTokenResult
    
    * Add tests

--- packages/firebase_auth/lib/firebase_auth.dart
+++ packages/firebase_auth/lib/firebase_auth.dart
@@ -22,6 +22,7 @@ part 'src/auth_exception.dart';
 part 'src/auth_result.dart';
 part 'src/firebase_auth.dart';
 part 'src/firebase_user.dart';
+part 'src/id_token_result.dart';
 part 'src/user_info.dart';
 part 'src/user_metadata.dart';
 part 'src/user_update_info.dart';
