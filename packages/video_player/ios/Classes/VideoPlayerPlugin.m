ebd97e9879f1512f1acf64f76f6dd09c4a1cae5b
Rodrigo Castro <recastrodiaz@gmail.com>
2019-08-07 19:08:55 +0100

    - Improve documentation for supported speeds in video_player - Improve comment on why setSpeed is not called when the video is not playing

--- packages/video_player/ios/Classes/VideoPlayerPlugin.m
+++ packages/video_player/ios/Classes/VideoPlayerPlugin.m
@@ -361,7 +361,7 @@ static inline CGFloat radiansToDegrees(CGFloat radians) {
   } else if (speed < 0 || speed > 2.0) {
     result([FlutterError
         errorWithCode:@"unsupported_speed"
-              message:@"Speed must be greater than or equal to 0.0 and less than 2.0"
+              message:@"Speed must be >= 0.0 and <= 2.0"
               details:nil]);
   } else if ((speed > 1.0 && _player.currentItem.canPlayFastForward) ||
              (speed < 1.0 && _player.currentItem.canPlaySlowForward)) {
