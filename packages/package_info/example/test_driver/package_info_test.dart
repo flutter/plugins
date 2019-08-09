542359c137644d8806950d2007acedd355ed564f
Collin Jackson <jackson@google.com>
2019-08-09 14:33:09 -0700

    Fix driver test to not be a test

--- packages/package_info/example/test_driver/package_info_test.dart
+++ packages/package_info/example/test_driver/package_info_test.dart
@@ -1,9 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter_driver/flutter_driver.dart';
 
-void main() {
-  test('package_info', () async {
-    final FlutterDriver driver = await FlutterDriver.connect();
-    await driver.requestData(null, timeout: const Duration(minutes: 1));
-    driver.close();
-  });
+Future<void> main() async {
+  final FlutterDriver driver = await FlutterDriver.connect();
+  await driver.requestData(null, timeout: const Duration(minutes: 1));
+  driver.close();
 }
