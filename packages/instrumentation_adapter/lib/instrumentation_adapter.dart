f0d764c31bd6cf2286e5cbd310fe4955a913e4b6
Collin Jackson <jackson@google.com>
2019-08-21 14:34:54 -0700

    Initial check-in of instrumentation adapter. (Template app only)

---
+++ packages/instrumentation_adapter/lib/instrumentation_adapter.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class InstrumentationAdapter {
+  static const MethodChannel _channel =
+      const MethodChannel('instrumentation_adapter');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+}
