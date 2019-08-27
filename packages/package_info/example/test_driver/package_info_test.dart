a269aff2256ba00259f2e39fbe6ad298fe524015
Collin Jackson <jackson@google.com>
2019-08-27 12:25:01 -0700

    Revert package_info changes for now

--- packages/package_info/example/test_driver/package_info_test.dart
+++ packages/package_info/example/test_driver/package_info_test.dart
@@ -1,9 +1,10 @@
-import 'dart:async';
-
 import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
 
-Future<void> main() async {
-  final FlutterDriver driver = await FlutterDriver.connect();
-  await driver.requestData(null, timeout: const Duration(minutes: 1));
-  driver.close();
+void main() {
+  test('package_info', () async {
+    final FlutterDriver driver = await FlutterDriver.connect();
+    await driver.requestData(null, timeout: const Duration(minutes: 1));
+    driver.close();
+  });
 }
