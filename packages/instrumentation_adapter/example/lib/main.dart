c3a1a800664618efd5aab23eb57e334fbc1eedcc
Collin Jackson <jackson@google.com>
2019-08-27 08:26:49 -0700

    Fix tests

--- packages/instrumentation_adapter/example/lib/main.dart
+++ packages/instrumentation_adapter/example/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:flutter/material.dart';
 
 import 'package:flutter/services.dart';
 import 'package:instrumentation_adapter/instrumentation_adapter.dart';
--- packages/instrumentation_adapter/lib/instrumentation_adapter.dart
+++ packages/instrumentation_adapter/lib/instrumentation_adapter.dart
@@ -3,8 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class InstrumentationAdapter {
-  static const MethodChannel _channel =
-      const MethodChannel('instrumentation_adapter');
+  static const MethodChannel _channel = MethodChannel('instrumentation_adapter');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
