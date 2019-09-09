flutter/plugins

commit 518d3424e58b05262726c55d856cebd82ca6aa3c

[path_provider] add getApplicationLibraryDirectory (#1953)
* [path_provider] add getLibraryDirectory

packages/path_provider/lib/path_provider.dart
@@ -59,6 +59,17 @@ Future<Directory> getApplicationSupportDirectory() async {
   return Directory(path);
 }
 
+/// Path to the directory where application can store files that are persistent,
+/// backed up, and not visible to the user, such as sqlite.db.
+Future<Directory> getLibraryDirectory() async {
+  final String path =
+      await _channel.invokeMethod<String>('getLibraryDirectory');
+  if (path == null) {
+    return null;
+  }
+  return Directory(path);
+}
+
 /// Path to a directory where the application may place data that is
 /// user-generated, or that cannot otherwise be recreated by your application.
 ///

packages/path_provider/test/path_provider_test.dart
@@ -64,12 +64,34 @@ void main() {
     expect(directory, isNull);
   });
 
+  test('getApplicationSupportDirectory test', () async {
+    response = null;
+    final Directory directory = await getApplicationSupportDirectory();
+    expect(
+      log,
+      <Matcher>[
+        isMethodCall('getApplicationSupportDirectory', arguments: null)
+      ],
+    );
+    expect(directory, isNull);
+  });
+
   test('getExternalStorageDirectory test', () async {
     response = null;
     final Directory directory = await getExternalStorageDirectory();
     expect(
       log,
       <Matcher>[isMethodCall('getStorageDirectory', arguments: null)],
+    );
+    expect(directory, isNull);
+  });
+
+  test('getLibraryDirectory test', () async {
+    response = null;
+    final Directory directory = await getLibraryDirectory();
+    expect(
+      log,
+      <Matcher>[isMethodCall('getLibraryDirectory', arguments: null)],
     );
     expect(directory, isNull);
   });
@@ -104,6 +126,20 @@ void main() {
     final String fakePath = "/foo/bar/baz";
     response = fakePath;
     final Directory directory = await getApplicationDocumentsDirectory();
+    expect(directory.path, equals(fakePath));
+  });
+
+  test('ApplicationSupportDirectory path test', () async {
+    final String fakePath = "/foo/bar/baz";
+    response = fakePath;
+    final Directory directory = await getApplicationSupportDirectory();
+    expect(directory.path, equals(fakePath));
+  });
+
+  test('ApplicationLibraryDirectory path test', () async {
+    final String fakePath = "/foo/bar/baz";
+    response = fakePath;
+    final Directory directory = await getLibraryDirectory();
     expect(directory.path, equals(fakePath));
   });
 

packages/path_provider/example/test_driver/path_provider.dart
@@ -45,6 +45,21 @@ void main() {
     file.deleteSync();
   });
 
+  test('getLibraryDirectory', () async {
+    if (Platform.isIOS) {
+      final Directory result = await getLibraryDirectory();
+      final String uuid = Uuid().v1();
+      final File file = File('${result.path}/$uuid.txt');
+      file.writeAsStringSync('Hello world!');
+      expect(file.readAsStringSync(), 'Hello world!');
+      expect(result.listSync(), isNotEmpty);
+      file.deleteSync();
+    } else if (Platform.isAndroid) {
+      final Future<Directory> result = getLibraryDirectory();
+      expect(result, throwsA(isInstanceOf<UnsupportedError>()));
+    }
+  });
+
   test('getExternalStorageDirectory', () async {
     if (Platform.isIOS) {
       final Future<Directory> result = getExternalStorageDirectory();

.gitignore
@@ -22,6 +22,7 @@ Pods/
 ServiceDefinitions.json
 xcuserdata/
 *.xcworkspace
+**/DerivedData/
 
 local.properties
 keystore.properties

packages/path_provider/example/lib/main.dart
@@ -36,6 +36,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   Future<Directory> _tempDirectory;
   Future<Directory> _appSupportDirectory;
+  Future<Directory> _appLibraryDirectory;
   Future<Directory> _appDocumentsDirectory;
   Future<Directory> _externalDocumentsDirectory;
 
@@ -72,6 +73,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _requestAppLibraryDirectory() {
+    setState(() {
+      _appLibraryDirectory = getLibraryDirectory();
+    });
+  }
+
   void _requestExternalStorageDirectory() {
     setState(() {
       _externalDocumentsDirectory = getExternalStorageDirectory();
@@ -85,70 +92,55 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: ListView(
           children: <Widget>[
-            Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: RaisedButton(
-                    child: const Text('Get Temporary Directory'),
-                    onPressed: _requestTempDirectory,
-                  ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: FutureBuilder<Directory>(
-                  future: _tempDirectory, builder: _buildDirectory),
-            ),
-            Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: RaisedButton(
-                    child: const Text('Get Application Documents Directory'),
-                    onPressed: _requestAppDocumentsDirectory,
-                  ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: FutureBuilder<Directory>(
-                  future: _appDocumentsDirectory, builder: _buildDirectory),
-            ),
-            Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: RaisedButton(
-                    child: const Text('Get Application Support Directory'),
-                    onPressed: _requestAppSupportDirectory,
-                  ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: FutureBuilder<Directory>(
-                  future: _appSupportDirectory, builder: _buildDirectory),
-            ),
-            Column(children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: RaisedButton(
-                  child: Text(
-                      '${Platform.isIOS ? "External directories are unavailable " "on iOS" : "Get External Storage Directory"}'),
-                  onPressed:
-                      Platform.isIOS ? null : _requestExternalStorageDirectory,
-                ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: RaisedButton(
+                child: const Text('Get Temporary Directory'),
+                onPressed: _requestTempDirectory,
               ),
-            ]),
-            Expanded(
-              child: FutureBuilder<Directory>(
-                  future: _externalDocumentsDirectory,
-                  builder: _buildDirectory),
             ),
+            FutureBuilder<Directory>(
+                future: _tempDirectory, builder: _buildDirectory),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: RaisedButton(
+                child: const Text('Get Application Documents Directory'),
+                onPressed: _requestAppDocumentsDirectory,
+              ),
+            ),
+            FutureBuilder<Directory>(
+                future: _appDocumentsDirectory, builder: _buildDirectory),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: RaisedButton(
+                child: const Text('Get Application Support Directory'),
+                onPressed: _requestAppSupportDirectory,
+              ),
+            ),
+            FutureBuilder<Directory>(
+                future: _appSupportDirectory, builder: _buildDirectory),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: RaisedButton(
+                child: const Text('Get Application Library Directory'),
+                onPressed: _requestAppLibraryDirectory,
+              ),
+            ),
+            FutureBuilder<Directory>(
+                future: _appLibraryDirectory, builder: _buildDirectory),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: RaisedButton(
+                child: Text(
+                    '${Platform.isIOS ? "External directories are unavailable " "on iOS" : "Get External Storage Directory"}'),
+                onPressed:
+                    Platform.isIOS ? null : _requestExternalStorageDirectory,
+              ),
+            ),
+            FutureBuilder<Directory>(
+                future: _externalDocumentsDirectory, builder: _buildDirectory)
           ],
         ),
       ),

packages/path_provider/ios/Classes/PathProviderPlugin.m
@@ -42,6 +42,8 @@ static FlutterError* getFlutterError(NSError* error) {
       } else {
         result(path);
       }
+    } else if ([@"getLibraryDirectory" isEqualToString:call.method]) {
+      result([self getLibraryDirectory]);
     } else {
       result(FlutterMethodNotImplemented);
     }
@@ -58,6 +60,10 @@ static FlutterError* getFlutterError(NSError* error) {
 
 + (NSString*)getApplicationSupportDirectory {
   return GetDirectoryOfType(NSApplicationSupportDirectory);
+}
+
++ (NSString*)getLibraryDirectory {
+  return GetDirectoryOfType(NSLibraryDirectory);
 }
 
 @end

packages/path_provider/pubspec.yaml
@@ -3,7 +3,7 @@ description: Flutter plugin for getting commonly used locations on the Android &
   iOS file systems, such as the temp and app data directories.
 author: Flutter Team <flutter-dev@googlegroups.com>
 homepage: https://github.com/flutter/plugins/tree/master/packages/path_provider
-version: 1.2.2
+version: 1.3.0
 
 flutter:
   plugin:

commit 1b5f73a0aeabb08b7e73850cb60aad82ff3d5ae9

Resolve test issues introduced in #1953.

packages/path_provider/lib/path_provider.dart
@@ -61,7 +61,12 @@ Future<Directory> getApplicationSupportDirectory() async {
 
 /// Path to the directory where application can store files that are persistent,
 /// backed up, and not visible to the user, such as sqlite.db.
+///
+/// On Android, this function throws an [UnsupportedError] as no equivalent
+/// folder exists.
 Future<Directory> getLibraryDirectory() async {
+  if (_platform.isAndroid)
+    throw UnsupportedError("Functionality not available on Android");
   final String path =
       await _channel.invokeMethod<String>('getLibraryDirectory');
   if (path == null) {
@@ -129,7 +134,7 @@ Future<List<Directory>> getExternalCacheDirectories() async {
 }
 
 /// Shadows directory values from Androids `android.os.Environment` class.
-/// 
+///
 /// https://developer.android.com/reference/android/os/Environment.html#fields_1
 class AndroidEnvironment {
   static const String DIRECTORY_MUSIC = 'Music';
@@ -153,7 +158,7 @@ class AndroidEnvironment {
 ///
 /// On Android this returns Context.getExternalFilesDirs(String type) or
 /// Context.getExternalFilesDir(String type) on API levels below 19.
-/// 
+///
 /// The parameter [type] is optional. If it is set, it *must* be one of the
 /// constants defined in [AndroidEnvironment]. See [AndroidEnvironment] for
 /// more information.

packages/path_provider/test/path_provider_test.dart
@@ -86,8 +86,17 @@ void main() {
     expect(directory, isNull);
   });
 
-  test('getLibraryDirectory test', () async {
-    response = null;
+  test('getLibraryDirectory Android test', () async {
+    try {
+      await getLibraryDirectory();
+      fail('should throw UnsupportedError');
+    } catch (e) {
+      expect(e, isUnsupportedError);
+    }
+  });
+  test('getLibraryDirectory iOS test', () async {
+    setMockPathProviderPlatform(FakePlatform(operatingSystem: 'ios'));
+
     final Directory directory = await getLibraryDirectory();
     expect(
       log,
@@ -136,7 +145,9 @@ void main() {
       <Matcher>[
         isMethodCall(
           'getExternalStorageDirectories',
-          arguments: const <String, String>{'type': AndroidEnvironment.DIRECTORY_MUSIC},
+          arguments: const <String, String>{
+            'type': AndroidEnvironment.DIRECTORY_MUSIC
+          },
         )
       ],
     );
@@ -183,6 +194,8 @@ void main() {
   });
 
   test('ApplicationLibraryDirectory path test', () async {
+    setMockPathProviderPlatform(FakePlatform(operatingSystem: 'ios'));
+
     final String fakePath = "/foo/bar/baz";
     response = fakePath;
     final Directory directory = await getLibraryDirectory();