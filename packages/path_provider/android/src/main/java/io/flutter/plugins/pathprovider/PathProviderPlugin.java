11620f0279cdb28ded3890602171a7de60964a35
Sebastian Roth <sebastian@nextbase.co.uk>
2019-09-09 17:52:56 +0100

    Remove unused import

--- packages/path_provider/android/src/main/java/io/flutter/plugins/pathprovider/PathProviderPlugin.java
+++ packages/path_provider/android/src/main/java/io/flutter/plugins/pathprovider/PathProviderPlugin.java
@@ -6,7 +6,6 @@ package io.flutter.plugins.pathprovider;
 
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
-import android.os.Environment;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
