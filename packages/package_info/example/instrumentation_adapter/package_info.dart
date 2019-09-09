4701e3c4cdecd1ed8053dfbee986fc203c127c50
Collin Jackson <jackson@google.com>
2019-09-09 10:36:52 -0700

    Move test location

--- packages/package_info/example/instrumentation_adapter/package_info.dart
+++ packages/package_info/example/instrumentation_adapter/package_info.dart
@@ -1,8 +1,14 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:instrumentation_adapter/instrumentation_adapter.dart';
 import 'package:package_info/package_info.dart';
 
 void main() {
+  InstrumentationAdapterFlutterBinding.ensureInitialized();
   group('package_info test', () {
     testWidgets('test package info result', (_) async {
       final PackageInfo info = await PackageInfo.fromPlatform();
