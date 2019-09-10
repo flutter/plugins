edb045681c62778f3279d895d9414ccdf8542b83
otopba <otopba@inbox.ru>
2019-09-10 23:50:24 +0300

    Add worker class

--- packages/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/TileProviderController.java
+++ packages/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/TileProviderController.java
@@ -4,6 +4,8 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.google.android.gms.maps.model.Tile;
 import com.google.android.gms.maps.model.TileProvider;
 
@@ -11,16 +13,13 @@ import java.util.concurrent.CountDownLatch;
 
 import io.flutter.plugin.common.MethodChannel;
 
-public class TileProviderController implements TileProvider, MethodChannel.Result {
+public class TileProviderController implements TileProvider {
 
     private static final String TAG = "TileProviderController";
 
     private final String tileOverlayId;
     private final MethodChannel methodChannel;
     private final Handler handler = new Handler(Looper.getMainLooper());
-
-    private CountDownLatch countDownLatch;
-    private Tile tile;
 
     TileProviderController(MethodChannel methodChannel, String tileOverlayId) {
         this.tileOverlayId = tileOverlayId;
@@ -29,44 +28,62 @@ public class TileProviderController implements TileProvider, MethodChannel.Resul
 
     @Override
     public Tile getTile(final int x, final int y, final int zoom) {
-        countDownLatch = new CountDownLatch(1);
-        handler.post(() -> methodChannel.invokeMethod("tileOverlay#getTile",
-                Convert.tileOverlayArgumentsToJson(tileOverlayId, x, y, zoom),
-                TileProviderController.this));
-        try {
-            countDownLatch.await();
-        } catch (InterruptedException e) {
-            Log.e(TAG, String.format("countDownLatch: can't get tile: x = %d, y= %d, zoom = %d", x, y, zoom), e);
-            tile = TileProvider.NO_TILE;
-        }
-        if (tile == null) {
-            tile = TileProvider.NO_TILE;
-        }
-        return tile;
+        Worker worker = new Worker(x, y, zoom);
+        return worker.getTile();
     }
 
-    @Override
-    public void success(Object data) {
-        try {
-            tile = Convert.interpretTile(data);
-        } catch (Exception ex) {
-            Log.e(TAG, "Can't parse tile data", ex);
-            tile = TileProvider.NO_TILE;
+    private final class Worker implements MethodChannel.Result {
+
+        private final CountDownLatch countDownLatch = new CountDownLatch(1);
+        private final int x;
+        private final int y;
+        private final int zoom;
+        private Object result;
+
+        Worker(int x, int y, int zoom) {
+            this.x = x;
+            this.y = y;
+            this.zoom = zoom;
         }
-        countDownLatch.countDown();
+
+        @NonNull
+        Tile getTile() {
+            handler.post(() -> methodChannel.invokeMethod("tileOverlay#getTile",
+                    Convert.tileOverlayArgumentsToJson(tileOverlayId, x, y, zoom),
+                    this));
+            try {
+                countDownLatch.await();
+            } catch (InterruptedException e) {
+                Log.e(TAG, String.format("countDownLatch: can't get tile: x = %d, y= %d, zoom = %d", x, y, zoom), e);
+                return TileProvider.NO_TILE;
+            }
+            try {
+                return Convert.interpretTile(result);
+            } catch (Exception ex) {
+                Log.e(TAG, "Can't parse tile data", ex);
+                return TileProvider.NO_TILE;
+            }
+        }
+
+        @Override
+        public void success(Object data) {
+            result = data;
+            countDownLatch.countDown();
+        }
+
+        @Override
+        public void error(String errorCode, String errorMessage, Object data) {
+            Log.e(TAG, String.format("Can't get tile: errorCode = %s, errorMessage = %s, date = %s", errorCode, errorCode, data));
+            result = null;
+            countDownLatch.countDown();
+        }
+
+        @Override
+        public void notImplemented() {
+            Log.e(TAG, "Can't get tile: notImplemented");
+            result = null;
+            countDownLatch.countDown();
+        }
     }
 
-    @Override
-    public void error(String errorCode, String errorMessage, Object data) {
-        Log.e(TAG, String.format("Can't get tile: errorCode = %s, errorMessage = %s, date = %s", errorCode, errorCode, data));
-        tile = TileProvider.NO_TILE;
-        countDownLatch.countDown();
-    }
-
-    @Override
-    public void notImplemented() {
-        tile = TileProvider.NO_TILE;
-        Log.e(TAG, "Can't get tile: notImplemented");
-        countDownLatch.countDown();
-    }
 }
