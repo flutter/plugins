e5e0cb2ac53cade9bda9c36759f7be358e8cfa34
Felix Angelov <felangelov@gmail.com>
2019-09-19 16:22:32 -0500

    Fix iOS MyLocationButton on iOS (#2068)

--- packages/google_maps_flutter/ios/Classes/GoogleMapController.m
+++ packages/google_maps_flutter/ios/Classes/GoogleMapController.m
@@ -327,7 +327,6 @@ static double ToDouble(NSNumber* data) { return [FLTGoogleMapJsonConversions toD
 
 - (void)setMyLocationEnabled:(BOOL)enabled {
   _mapView.myLocationEnabled = enabled;
-  _mapView.settings.myLocationButton = enabled;
 }
 
 - (void)setMyLocationButtonEnabled:(BOOL)enabled {
