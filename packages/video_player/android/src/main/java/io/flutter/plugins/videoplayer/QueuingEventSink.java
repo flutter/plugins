b9973f20bc0bf28adfd8f578a8a76426adbbf5b9
JarvanMo <jarvan.mo@gmail.com>
2019-09-21 11:39:26 +0800

    reformat code

--- packages/video_player/android/src/main/java/io/flutter/plugins/videoplayer/QueuingEventSink.java
+++ packages/video_player/android/src/main/java/io/flutter/plugins/videoplayer/QueuingEventSink.java
@@ -4,9 +4,8 @@
 
 package io.flutter.plugins.videoplayer;
 
-import java.util.ArrayList;
-
 import io.flutter.plugin.common.EventChannel;
+import java.util.ArrayList;
 
 /**
  * And implementation of {@link EventChannel.EventSink} which can wrap an underlying sink.
@@ -18,70 +17,69 @@ import io.flutter.plugin.common.EventChannel;
  * externally.
  */
 final class QueuingEventSink implements EventChannel.EventSink {
-    private EventChannel.EventSink delegate;
-    private ArrayList<Object> eventQueue = new ArrayList<>();
-    private boolean done = false;
+  private EventChannel.EventSink delegate;
+  private ArrayList<Object> eventQueue = new ArrayList<>();
+  private boolean done = false;
 
-    public void setDelegate(EventChannel.EventSink delegate) {
-        this.delegate = delegate;
-        maybeFlush();
+  public void setDelegate(EventChannel.EventSink delegate) {
+    this.delegate = delegate;
+    maybeFlush();
+  }
+
+  @Override
+  public void endOfStream() {
+    enqueue(new EndOfStreamEvent());
+    maybeFlush();
+    done = true;
+  }
+
+  @Override
+  public void error(String code, String message, Object details) {
+    enqueue(new ErrorEvent(code, message, details));
+    maybeFlush();
+  }
+
+  @Override
+  public void success(Object event) {
+    enqueue(event);
+    maybeFlush();
+  }
+
+  private void enqueue(Object event) {
+    if (done) {
+      return;
     }
+    eventQueue.add(event);
+  }
 
-    @Override
-    public void endOfStream() {
-        enqueue(new EndOfStreamEvent());
-        maybeFlush();
-        done = true;
+  private void maybeFlush() {
+    if (delegate == null) {
+      return;
     }
-
-    @Override
-    public void error(String code, String message, Object details) {
-        enqueue(new ErrorEvent(code, message, details));
-        maybeFlush();
+    for (Object event : eventQueue) {
+      if (event instanceof EndOfStreamEvent) {
+        delegate.endOfStream();
+      } else if (event instanceof ErrorEvent) {
+        ErrorEvent errorEvent = (ErrorEvent) event;
+        delegate.error(errorEvent.code, errorEvent.message, errorEvent.details);
+      } else {
+        delegate.success(event);
+      }
     }
+    eventQueue.clear();
+  }
 
-    @Override
-    public void success(Object event) {
-        enqueue(event);
-        maybeFlush();
+  private static class EndOfStreamEvent {}
+
+  private static class ErrorEvent {
+    String code;
+    String message;
+    Object details;
+
+    ErrorEvent(String code, String message, Object details) {
+      this.code = code;
+      this.message = message;
+      this.details = details;
     }
-
-    private void enqueue(Object event) {
-        if (done) {
-            return;
-        }
-        eventQueue.add(event);
-    }
-
-    private void maybeFlush() {
-        if (delegate == null) {
-            return;
-        }
-        for (Object event : eventQueue) {
-            if (event instanceof EndOfStreamEvent) {
-                delegate.endOfStream();
-            } else if (event instanceof ErrorEvent) {
-                ErrorEvent errorEvent = (ErrorEvent) event;
-                delegate.error(errorEvent.code, errorEvent.message, errorEvent.details);
-            } else {
-                delegate.success(event);
-            }
-        }
-        eventQueue.clear();
-    }
-
-    private static class EndOfStreamEvent {
-    }
-
-    private static class ErrorEvent {
-        String code;
-        String message;
-        Object details;
-
-        ErrorEvent(String code, String message, Object details) {
-            this.code = code;
-            this.message = message;
-            this.details = details;
-        }
-    }
+  }
 }
