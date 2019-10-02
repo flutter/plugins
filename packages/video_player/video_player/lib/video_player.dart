45836b253fbcf667b6f636e5dc322e5379158135
Simon Lightfoot <simon@devangels.london>
2019-10-02 19:52:39 +0100

    [video_player] Fixes issue where initialize() future stalls when failing to load source data and does not throw error.
    Fix formatting.
    Add tests for errors during init.

--- packages/video_player/video_player/lib/video_player.dart
+++ packages/video_player/video_player/lib/video_player.dart
@@ -353,6 +353,9 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
       final PlatformException e = obj;
       value = VideoPlayerValue.erroneous(e.message);
       _timer?.cancel();
+      if (!initializingCompleter.isCompleted) {
+        initializingCompleter.completeError(obj);
+      }
     }
 
     _eventSubscription = _eventChannelFor(_textureId)
