ae42d1240f14055170e08043ed0e0da48cc4a484
Chris Yang <ychris@google.com>
2019-10-02 18:20:42 -0700

    draft

---
+++ packages/connectivity/example/android/app/src/main/java/dev/flutter/plugins/connectivity_exmaple/MainActivity.java
@@ -0,0 +1,12 @@
+package dev.flutter.plugins.connectivity_exmaple;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+
+public class MainActivity extends FlutterActivity {
+
+    @Override
+    public void configureFlutterEngine(FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+    }
+}
