915835ab54d81b4381eaa4ef2b970c5693e0e550
Collin Jackson <jackson@google.com>
2019-10-07 15:32:26 -0700

    e2e test analyzer failure

--- packages/e2e/example/test_driver/example_e2e_test.dart
+++ packages/e2e/example/test_driver/example_e2e_test.dart
@@ -5,7 +5,7 @@ import 'package:flutter_driver/flutter_driver.dart';
 
 Future<void> main() async {
   final FlutterDriver driver = await FlutterDriver.connect();
-  String result =
+  final String result =
       await driver.requestData(null, timeout: const Duration(minutes: 1));
   driver.close();
   exit(result == 'pass' ? 0 : 1);
