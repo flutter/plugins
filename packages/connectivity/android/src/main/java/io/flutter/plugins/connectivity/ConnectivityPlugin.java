43bfaea32653eee478ab4494b4e936280e2b0489
Chris Yang <ychris@google.com>
2019-10-07 16:42:32 -0700

    refactor

--- packages/connectivity/android/src/main/java/io/flutter/plugins/connectivity/ConnectivityPlugin.java
+++ packages/connectivity/android/src/main/java/io/flutter/plugins/connectivity/ConnectivityPlugin.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
@@ -15,66 +16,42 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 /** ConnectivityPlugin */
 public class ConnectivityPlugin implements FlutterPlugin {
 
+  private MethodChannel methodChannel;
+  private EventChannel eventChannel;
+
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
-    final MethodChannel channel =
-        new MethodChannel(registrar.messenger(), "plugins.flutter.io/connectivity");
-    final EventChannel eventChannel =
-        new EventChannel(registrar.messenger(), "plugins.flutter.io/connectivity_status");
 
-    ConnectivityManager connectivityManager =
-        (ConnectivityManager)
-            registrar
-                .context()
-                .getApplicationContext()
-                .getSystemService(Context.CONNECTIVITY_SERVICE);
-    WifiManager wifiManager =
-        (WifiManager)
-            registrar.context().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-
-    Connectivity connectivity = new Connectivity(connectivityManager, wifiManager);
-
-    ConnectivityMethodChannelHandler methodChannelHandler =
-        new ConnectivityMethodChannelHandler(connectivity);
-    ConnectivityBroadcastReceiver receiver =
-        new ConnectivityBroadcastReceiver(registrar.context(), connectivity);
-
-    channel.setMethodCallHandler(methodChannelHandler);
-    eventChannel.setStreamHandler(receiver);
+    ConnectivityPlugin plugin = new ConnectivityPlugin();
+    plugin.setupChannels(registrar.messenger(), registrar.context());
   }
 
   @Override
   public void onAttachedToEngine(FlutterPluginBinding binding) {
-    final MethodChannel channel =
-        new MethodChannel(
-            binding.getFlutterEngine().getDartExecutor(), "plugins.flutter.io/connectivity");
-    final EventChannel eventChannel =
-        new EventChannel(
-            binding.getFlutterEngine().getDartExecutor(), "plugins.flutter.io/connectivity_status");
+    setupChannels(binding.getFlutterEngine().getDartExecutor(), binding.getApplicationContext());
+  }
+
+  @Override
+  public void onDetachedFromEngine(FlutterPluginBinding binding) {
+    methodChannel.setMethodCallHandler(null);
+    eventChannel.setStreamHandler(null);
+  }
+
+  private void setupChannels(BinaryMessenger messenger, Context context) {
+    methodChannel = new MethodChannel(messenger, "plugins.flutter.io/connectivity");
+    eventChannel = new EventChannel(messenger, "plugins.flutter.io/connectivity_status");
     ConnectivityManager connectivityManager =
-        (ConnectivityManager)
-            binding
-                .getApplicationContext()
-                .getApplicationContext()
-                .getSystemService(Context.CONNECTIVITY_SERVICE);
-    WifiManager wifiManager =
-        (WifiManager)
-            binding
-                .getApplicationContext()
-                .getApplicationContext()
-                .getSystemService(Context.WIFI_SERVICE);
+        (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
 
     Connectivity connectivity = new Connectivity(connectivityManager, wifiManager);
 
     ConnectivityMethodChannelHandler methodChannelHandler =
         new ConnectivityMethodChannelHandler(connectivity);
     ConnectivityBroadcastReceiver receiver =
-        new ConnectivityBroadcastReceiver(binding.getApplicationContext(), connectivity);
+        new ConnectivityBroadcastReceiver(context, connectivity);
 
-    channel.setMethodCallHandler(methodChannelHandler);
+    methodChannel.setMethodCallHandler(methodChannelHandler);
     eventChannel.setStreamHandler(receiver);
   }
-
-  @Override
-  public void onDetachedFromEngine(FlutterPluginBinding binding) {}
 }
