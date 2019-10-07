bfe981f7ac61b31e1ce893a2d66180586c44dd42
Matt Carroll <matthew-carroll@users.noreply.github.com>
2019-10-07 14:28:14 -0700

    Changed HiddenLifecycleReference to use reflection to avoid compile-time references.

--- packages/flutter_android_lifecycle/android/src/main/java/io/flutter/embedding/engine/plugins/lifecycle/FlutterLifecycleAdapter.java
+++ packages/flutter_android_lifecycle/android/src/main/java/io/flutter/embedding/engine/plugins/lifecycle/FlutterLifecycleAdapter.java
@@ -7,17 +7,35 @@ package io.flutter.embedding.engine.plugins.lifecycle;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.Lifecycle;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import io.flutter.Log;
+
 public class FlutterLifecycleAdapter {
-  @NonNull private final Lifecycle lifecycle;
+  private static final String TAG = "FlutterLifecycleAdapter";
+
+  @NonNull
+  private Lifecycle lifecycle = null;
 
   public FlutterLifecycleAdapter(@NonNull Object reference) {
-    if (!(reference instanceof HiddenLifecycleReference)) {
-      throw new IllegalArgumentException(
-          "The reference argument must be of type HiddenLifecycleReference. Was actually "
-              + reference);
-    }
+    try {
+      Class hiddenLifecycleClass = Class.forName(
+          "io.flutter.embedding.engine.plugins.lifecycle.HiddenLifecycleReference"
+      );
 
-    this.lifecycle = ((HiddenLifecycleReference) reference).getLifecycle();
+      if (!reference.getClass().equals(hiddenLifecycleClass)) {
+        throw new IllegalArgumentException(
+            "The reference argument must be of type HiddenLifecycleReference. Was actually "
+                + reference);
+      }
+
+      Method getLifecycle = reference.getClass().getMethod("getLifecycle");
+      this.lifecycle = (Lifecycle) getLifecycle.invoke(reference);
+    } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+      Log.w(TAG, "You are attempting to use Flutter plugins that are newer than your"
+          + " version of Flutter. Plugins may not work as expected.");
+    }
   }
 
   @NonNull
