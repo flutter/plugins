ead4e1e197e669cb3dd01e5ef1d2f52b663ea8c9
Chris Yang <ychris@google.com>
2019-10-07 13:53:23 -0700

    migrate device_info

--- packages/device_info/android/src/main/java/io/flutter/plugins/deviceinfo/DeviceInfoPlugin.java
+++ packages/device_info/android/src/main/java/io/flutter/plugins/deviceinfo/DeviceInfoPlugin.java
@@ -4,19 +4,34 @@
 
 package io.flutter.plugins.deviceinfo;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-
 /** DeviceInfoPlugin */
-public class DeviceInfoPlugin {
+public class DeviceInfoPlugin implements FlutterPlugin {
 
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel =
         new MethodChannel(registrar.messenger(), "plugins.flutter.io/device_info");
 
-    final MethodCallHandlerImpl handler = new MethodCallHandlerImpl(registrar.context().getContentResolver());
+    final MethodCallHandlerImpl handler =
+        new MethodCallHandlerImpl(registrar.context().getContentResolver());
     channel.setMethodCallHandler(handler);
   }
+
+  @Override
+  public void onAttachedToEngine(FlutterPlugin.FlutterPluginBinding binding) {
+    final MethodChannel channel =
+        new MethodChannel(
+            binding.getFlutterEngine().getDartExecutor(), "plugins.flutter.io/device_info");
+
+    final MethodCallHandlerImpl handler =
+        new MethodCallHandlerImpl(binding.getApplicationContext().getContentResolver());
+    channel.setMethodCallHandler(handler);
+  }
+
+  @Override
+  public void onDetachedFromEngine(FlutterPlugin.FlutterPluginBinding binding) {}
 }
