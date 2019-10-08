e276af06c3b9f05d7affb5426007049ebf8a77ca
Collin Jackson <jackson@google.com>
2019-10-07 18:51:16 -0700

    Add copyrights

--- packages/package_info/example/test_driver/package_info_e2e_test.dart
+++ packages/package_info/example/test_driver/package_info_e2e_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2019, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:io';
 
 import 'package:flutter_driver/flutter_driver.dart';
@@ -9,3 +13,4 @@ Future<void> main() async {
   driver.close();
   exit(result == 'pass' ? 0 : 1);
 }
+`
