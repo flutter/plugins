ba0bb22364cd34781bc763a9279fc9526a37c032
Chris Yang <ychris@google.com>
2019-10-08 12:50:32 -0700

    rename activities

--- packages/connectivity/example/android/app/src/main/java/io/flutter/plugins/connectivity_example/MainActivity.java
+++ packages/connectivity/example/android/app/src/main/java/io/flutter/plugins/connectivity_example/MainActivity.java
@@ -1,17 +1,18 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 package io.flutter.plugins.connectivityexample;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.connectivity.ConnectivityPlugin;
 
 public class MainActivity extends FlutterActivity {
+
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+  public void configureFlutterEngine(FlutterEngine flutterEngine) {
+    super.configureFlutterEngine(flutterEngine);
+    flutterEngine.getPlugins().add(new ConnectivityPlugin());
   }
 }
