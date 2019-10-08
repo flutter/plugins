ae774b639722edccb1a1af2a241cfc987f6ec372
Chris Yang <ychris@google.com>
2019-10-08 12:59:52 -0700

    rename activities

--- packages/share/example/android/app/src/main/java/io/flutter/plugins/shareexample/MainActivity.java
+++ packages/share/example/android/app/src/main/java/io/flutter/plugins/shareexample/MainActivity.java
@@ -1,18 +1,18 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2019 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 package io.flutter.plugins.shareexample;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.share.SharePlugin;
 
 public class MainActivity extends FlutterActivity {
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+  public void configureFlutterEngine(FlutterEngine flutterEngine) {
+    super.configureFlutterEngine(flutterEngine);
+    flutterEngine.getPlugins().add(new SharePlugin());
   }
 }
