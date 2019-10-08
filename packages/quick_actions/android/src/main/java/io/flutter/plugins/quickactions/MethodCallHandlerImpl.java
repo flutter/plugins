14ad8b1afefa78a4bbca97b4b6197b71149deded
Chris Yang <ychris@google.com>
2019-10-08 16:08:26 -0700

    Update MethodCallHandlerImpl.java

--- packages/quick_actions/android/src/main/java/io/flutter/plugins/quickactions/MethodCallHandlerImpl.java
+++ packages/quick_actions/android/src/main/java/io/flutter/plugins/quickactions/MethodCallHandlerImpl.java
@@ -1,3 +1,7 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.	// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be	// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.	// found in the LICENSE file.
+
 package io.flutter.plugins.quickactions;
 
 import android.annotation.TargetApi;
