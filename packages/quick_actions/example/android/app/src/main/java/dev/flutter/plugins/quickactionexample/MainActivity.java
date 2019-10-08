08319ab195905bd92319dfd37da2ccba28c85648
Chris Yang <ychris@google.com>
2019-10-08 09:34:38 -0700

    migrate

---
+++ packages/quick_actions/example/android/app/src/main/java/dev/flutter/plugins/quickactionexample/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.flutter.plugins.quickactionexample;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.quickactions.QuickActionsPlugin;
+
+public class MainActivity extends FlutterActivity {
+
+    @Override
+    public void configureFlutterEngine(FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        flutterEngine.getPlugins().add(new QuickActionsPlugin());
+    }
+}
