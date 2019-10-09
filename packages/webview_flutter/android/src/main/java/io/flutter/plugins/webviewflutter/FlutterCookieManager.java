88550453b0d321ba6c17542c3c80c558fb0065ec
Michael Klimushyn <mklim@google.com>
2019-10-08 17:51:34 -0700

    Migrate flutter_webview to the new embedding
    This depends on a new bugfix on the engine for text input to work with
    the new embedding (flutter/engine#13015).
    
    This also introduces some interesting and strange new failure cases.
    With the new embedding, a FlutterView can be attached and detached from
    the plugin at any point. It "should never" but technically can happen
    that the plugin gets into a state where there is an active proxy adapter
    view but no valid FlutterView to use for input proxying. For now I've
    mostly added error logs and guards against NPEs detecting whenever we've
    gotten into one of this situations. But I think we should probably
    rigorously test this in an a2a specific situation to truly vet for a2a
    support.
    
    DO NOT MERGE until we can land tests for this.

--- packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterCookieManager.java
+++ packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterCookieManager.java
@@ -15,17 +15,15 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
 class FlutterCookieManager implements MethodCallHandler {
+  private final MethodChannel methodChannel;
 
-  private FlutterCookieManager() {
-    // Do not instantiate.
-    // This class should only be used in context of a BinaryMessenger.
-    // Use FlutterCookieManager#registerWith instead.
+  FlutterCookieManager(BinaryMessenger messenger) {
+    methodChannel = new MethodChannel(messenger, "plugins.flutter.io/cookie_manager");
+    methodChannel.setMethodCallHandler(this);
   }
 
   static void registerWith(BinaryMessenger messenger) {
-    MethodChannel methodChannel = new MethodChannel(messenger, "plugins.flutter.io/cookie_manager");
-    FlutterCookieManager cookieManager = new FlutterCookieManager();
-    methodChannel.setMethodCallHandler(cookieManager);
+    new FlutterCookieManager(messenger);
   }
 
   @Override
@@ -37,6 +35,10 @@ class FlutterCookieManager implements MethodCallHandler {
       default:
         result.notImplemented();
     }
+  }
+
+  void dispose() {
+    methodChannel.setMethodCallHandler(null);
   }
 
   private static void clearCookies(final Result result) {
--- packages/webview_flutter/example/android/app/src/main/java/io/flutter/plugins/webviewflutterexample/MainActivity.java
+++ packages/webview_flutter/example/android/app/src/main/java/io/flutter/plugins/webviewflutterexample/MainActivity.java
@@ -1,17 +1,12 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 package io.flutter.plugins.webviewflutterexample;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.webviewflutter.WebViewFlutterPlugin;
 
 public class MainActivity extends FlutterActivity {
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+  public void configureFlutterEngine(FlutterEngine flutterEngine) {
+    flutterEngine.getPlugins().add(new WebViewFlutterPlugin());
   }
 }
