d119b7b6400dbfa1c38654e2a7870db42d5e45e6
SebastienBtr <sebastien29520@gmail.com>
2019-10-10 11:30:38 +0200

    improve null checker

--- packages/connectivity/lib/connectivity.dart
+++ packages/connectivity/lib/connectivity.dart
@@ -23,10 +23,7 @@ class Connectivity {
   // EventChannel because it is overridden. Forcing the class to be a singleton class can prevent
   // misusage of creating a second instance from a programmer.
   factory Connectivity() {
-    if (_singleton == null) {
-      _singleton = Connectivity._();
-    }
-    return _singleton;
+    return _singleton ??= Connectivity._();
   }
 
   Connectivity._();
