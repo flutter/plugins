b90f434c3352c7e3ba5038842135e1d0c5e9fdfe
Amir Hardon <amirha@google.com>
2019-10-14 13:40:55 -0700

    move the e2e test out of the example app folder and make it test something

--- packages/battery/test/battery_e2e.dart
+++ packages/battery/test/battery_e2e.dart
@@ -3,13 +3,15 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:battery/battery.dart';
 import 'package:e2e/e2e.dart';
-import '../lib/main.dart';
 
 void main() {
   E2EWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('Builds and runs', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
+    final Battery battery = Battery();
+    final int batteryLevel = await battery.batteryLevel;
+    expect(batteryLevel, isNotNull);
   });
 }
