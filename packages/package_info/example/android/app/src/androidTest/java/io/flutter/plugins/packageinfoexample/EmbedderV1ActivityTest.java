2af845757277bbc2d4b3fce0e9cfa5813d82b116
Collin Jackson <jackson@google.com>
2019-10-14 15:28:44 -0700

    Fix package names

--- packages/package_info/example/android/app/src/androidTest/java/io/flutter/plugins/packageinfoexample/EmbedderV1ActivityTest.java
+++ packages/package_info/example/android/app/src/androidTest/java/io/flutter/plugins/packageinfoexample/EmbedderV1ActivityTest.java
@@ -5,11 +5,11 @@
 package io.flutter.plugins.packageinfoexample;
 
 import androidx.test.rule.ActivityTestRule;
-import dev.flutter.plugins.instrumentationadapter.FlutterRunner;
+import dev.flutter.plugins.e2e.FlutterRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 @RunWith(FlutterRunner.class)
-public class MainActivityTest {
-  @Rule public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+public class EmbedderV1ActivityTest {
+  @Rule public ActivityTestRule<EmbedderV1Activity> rule = new ActivityTestRule<>(EmbedderV1Activity.class);
 }
