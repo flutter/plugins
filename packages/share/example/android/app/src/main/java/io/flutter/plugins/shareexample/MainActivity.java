172280328c6b7c5914788f41aadf631d9ba7e2b5
Chris Yang <ychris@google.com>
2019-10-15 16:09:44 -0700

    Update MainActivity.java

--- packages/share/example/android/app/src/main/java/io/flutter/plugins/shareexample/MainActivity.java
+++ packages/share/example/android/app/src/main/java/io/flutter/plugins/shareexample/MainActivity.java
@@ -9,7 +9,8 @@ import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.share.SharePlugin;
 
 public class MainActivity extends FlutterActivity {
-
+  // TODO(cyanglaz): Remove this once v2 of GeneratedPluginRegistrant rolls to stable.
+  // https://github.com/flutter/flutter/issues/42694
   @Override
   public void configureFlutterEngine(FlutterEngine flutterEngine) {
     super.configureFlutterEngine(flutterEngine);
