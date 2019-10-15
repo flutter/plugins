e497d955ce4fb837e2d3b44edd753845bad5b0e8
Collin Jackson <jackson@google.com>
2019-10-15 16:02:01 -0700

    Add comment

--- packages/package_info/example/android/app/src/main/java/io/flutter/plugins/packageinfoexample/MainActivity.java
+++ packages/package_info/example/android/app/src/main/java/io/flutter/plugins/packageinfoexample/MainActivity.java
@@ -10,6 +10,8 @@ import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 
 public class MainActivity extends FlutterActivity {
+  // TODO(jackson): Remove this once v2 of GeneratedPluginRegistrant rolls to stable.
+  // https://github.com/flutter/flutter/issues/42694
   @Override
   public void configureFlutterEngine(FlutterEngine flutterEngine) {
     flutterEngine.getPlugins().add(new PackageInfoPlugin());
