ab9f4795c21fcd3972dca5daa587808fe4c0ab3e
Chris Yang <ychris@google.com>
2019-10-16 12:08:30 -0700

    remove platform check

--- packages/video_player/example/test_driver/video_player_test.dart
+++ packages/video_player/example/test_driver/video_player_test.dart
@@ -14,16 +14,6 @@ Future<void> main() async {
 
   //TODO(cyanglaz): Use TabBar tabs to navigate between pages after https://github.com/flutter/flutter/issues/16991 is fixed.
   test('Push a page contains video and pop back, do not crash.', () async {
-    if (Platform.isIOS) {
-      final SerializableFinder pushTab = find.byValueKey('push_tab');
-      await driver.waitFor(pushTab, timeout: const Duration(seconds: 10));
-      await driver.tap(pushTab, timeout: const Duration(seconds: 10));
-      await driver.waitForAbsent(pushTab, timeout: const Duration(seconds: 10));
-      await driver.waitFor(pushTab, timeout: const Duration(seconds: 30));
-      final Health health =
-          await driver.checkHealth(timeout: const Duration(seconds: 10));
-      expect(health.status, HealthStatus.ok);
-    }
     final SerializableFinder pushTab = find.byValueKey('push_tab');
     await driver.waitFor(pushTab, timeout: const Duration(seconds: 10));
     await driver.tap(pushTab, timeout: const Duration(seconds: 10));
