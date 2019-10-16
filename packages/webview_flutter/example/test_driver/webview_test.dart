727529bf2294159135b2afce8b4e18c3167e8521
Michael Klimushyn <mklim@google.com>
2019-10-16 16:14:41 -0700

    [flutter_webview] Revert v2 embedder support (#2200)
    The flutter_webview plugin needs an API in the v2 embedder that didn't roll into Flutter until `v1.10.9`, and the latest stable is `v.1.9.1`. In addition to this the migration already relied on some bugfixes that weren't really in stable, so the migration was shaky to begin with.

--- packages/webview_flutter/example/test_driver/webview_test.dart
+++ packages/webview_flutter/example/test_driver/webview_test.dart
@@ -3,14 +3,11 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:io';
 
 import 'package:flutter_driver/flutter_driver.dart';
 
 Future<void> main() async {
   final FlutterDriver driver = await FlutterDriver.connect();
-  final String result =
-      await driver.requestData(null, timeout: const Duration(minutes: 1));
+  await driver.requestData(null, timeout: const Duration(minutes: 1));
   driver.close();
-  exit(result == 'pass' ? 0 : 1);
 }
