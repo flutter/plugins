29797cd67d6160ffdd9d7bdc06abb10c094d13ed
Amir Hardon <amirh@users.noreply.github.com>
2019-10-16 16:17:46 -0700

    Add plugin for Android lifecycle in embedding (#2168)

---
+++ packages/flutter_android_lifecycle/CHANGELOG.md
@@ -0,0 +1,4 @@
+## 1.0.0
+
+* Introduces a `FlutterLifecycleAdapter`, which can be used by other plugins to obtain a `Lifecycle`
+  reference from a `FlutterPluginBinding`.
---
+++ packages/flutter_android_lifecycle/example/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Sample flutter_android_lifecycle usage'),
+        ),
+      ),
+    );
+  }
+}
