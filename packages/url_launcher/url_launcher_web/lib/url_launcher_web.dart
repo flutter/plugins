5c35d3f69780134dc3f8414cb800a85a813677a6
Harry Terkelsen <het@google.com>
2019-10-25 15:38:20 -0700

    [url_launcher] Migrate url_launcher_web to the platform interface

--- packages/url_launcher/url_launcher_web/lib/url_launcher_web.dart
+++ packages/url_launcher/url_launcher_web/lib/url_launcher_web.dart
@@ -4,46 +4,40 @@ import 'dart:html' as html;
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:meta/meta.dart';
+import 'package:url_launcher_platform_interface/url_launcher_platform_interface.dart';
 
-class UrlLauncherPlugin {
+class UrlLauncherPlugin extends UrlLauncherPlatform {
   static void registerWith(Registrar registrar) {
-    final MethodChannel channel = MethodChannel(
-        'plugins.flutter.io/url_launcher',
-        const StandardMethodCodec(),
-        registrar.messenger);
-    final UrlLauncherPlugin instance = UrlLauncherPlugin();
-    channel.setMethodCallHandler(instance.handleMethodCall);
-  }
-
-  Future<dynamic> handleMethodCall(MethodCall call) async {
-    switch (call.method) {
-      case 'canLaunch':
-        final String url = call.arguments['url'];
-        return _canLaunch(url);
-      case 'launch':
-        final String url = call.arguments['url'];
-        return _launch(url);
-      default:
-        throw PlatformException(
-            code: 'Unimplemented',
-            details: "The url_launcher plugin for web doesn't implement "
-                "the method '${call.method}'");
-    }
-  }
-
-  bool _canLaunch(String url) {
-    final Uri parsedUrl = Uri.tryParse(url);
-    if (parsedUrl == null) return false;
-
-    return parsedUrl.isScheme('http') || parsedUrl.isScheme('https');
-  }
-
-  bool _launch(String url) {
-    return openNewWindow(url) != null;
+    UrlLauncherPlatform.instance = UrlLauncherPlugin();
   }
 
   @visibleForTesting
   html.WindowBase openNewWindow(String url) {
     return html.window.open(url, '');
+  }
+
+  @override
+  Future<bool> canLaunch(String url) {
+    final Uri parsedUrl = Uri.tryParse(url);
+    if (parsedUrl == null) return Future<bool>.value(false);
+
+    return Future<bool>.value(
+        parsedUrl.isScheme('http') || parsedUrl.isScheme('https'));
+  }
+
+  /// Returns `true` if the given [url] was successfully launched.
+  ///
+  /// For documentation on the other arguments, see the `launch` documentation
+  /// in `package:url_launcher/url_launcher.dart`.
+  Future<bool> launch(
+    String url, {
+    @required bool useSafariVC,
+    @required bool useWebView,
+    @required bool enableJavaScript,
+    @required bool enableDomStorage,
+    @required bool universalLinksOnly,
+    @required Map<String, String> headers,
+  }) {
+    return Future<bool>.value(openNewWindow(url) != null);
   }
 }
