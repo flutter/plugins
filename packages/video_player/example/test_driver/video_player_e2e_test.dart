a0fbd5f7d34d704565dabbbecfd318bdfec41282
Chris Yang <ychris@google.com>
2019-11-20 09:21:00 -0800

    remove extra app.main

--- packages/video_player/example/test_driver/video_player_e2e_test.dart
+++ packages/video_player/example/test_driver/video_player_e2e_test.dart
@@ -4,15 +4,12 @@
 
 import 'dart:async';
 import 'dart:io';
-
 import 'package:flutter_driver/flutter_driver.dart';
-import 'package:video_player_example/main.dart' as app;
 
 Future<void> main() async {
   final FlutterDriver driver = await FlutterDriver.connect();
   final String result =
       await driver.requestData(null, timeout: const Duration(minutes: 1));
-  app.main();
   await driver.close();
   exit(result == 'pass' ? 0 : 1);
 }
