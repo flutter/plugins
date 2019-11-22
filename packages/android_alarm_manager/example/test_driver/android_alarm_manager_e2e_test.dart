59a31acaf75e909eb5d387b6af3efa6c2f6f529c
Ben Konyi <bkonyi@google.com>
2019-11-21 16:32:53 -0800

    Resume background isolate for driver test

--- packages/android_alarm_manager/example/test_driver/android_alarm_manager_e2e_test.dart
+++ packages/android_alarm_manager/example/test_driver/android_alarm_manager_e2e_test.dart
@@ -2,14 +2,38 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter_driver/flutter_driver.dart';
 
+Future<StreamSubscription> resumeIsolatesOnPause(FlutterDriver driver) async {
+    final vm = await driver.serviceClient.getVM();
+    // // unpause any paused isolated
+    for (final isolateRef in vm.isolates) {
+      final isolate = await isolateRef.load();
+      if (isolate.isPaused) {
+        isolate.resume();
+      }
+    }
+    return driver.serviceClient.onIsolateRunnable
+        .asBroadcastStream()
+        .listen((isolateRef) async {
+      final isolate = await isolateRef.load();
+      if (isolate.isPaused) {
+        isolate.resume();
+      }
+    });
+}
+
 Future<void> main() async {
   final FlutterDriver driver = await FlutterDriver.connect();
+  // flutter drive causes isolates to be paused on spawn. The background isolate
+  // for this plugin will need to be resumed for the test to pass.
+  final subscription = await resumeIsolatesOnPause(driver);
   final String result =
-      await driver.requestData(null, timeout: const Duration(minutes: 1));
+      await driver.requestData(null, timeout: const Duration(minutes: 2));
   driver.close();
+  subscription.cancel();
   exit(result == 'pass' ? 0 : 1);
 }
