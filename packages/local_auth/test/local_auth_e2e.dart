4ce7a695d7f54651aa5e45f9e9b2dd2f57c06453
Maurice Parrish <bmparr@google.com>
2019-11-22 15:59:29 -0800

    Add e2e test

--- packages/local_auth/test/local_auth_e2e.dart
+++ packages/local_auth/test/local_auth_e2e.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:e2e/e2e.dart';
 
+import '../lib/local_auth.dart';
+
 void main() {
   E2EWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('e', (WidgetTester tester) async {
-    expect(true, isTrue);
+  testWidgets('canCheckBiometrics', (WidgetTester tester) async {
+    expect(LocalAuthentication().canCheckBiometrics, completion(isTrue));
   });
 }
