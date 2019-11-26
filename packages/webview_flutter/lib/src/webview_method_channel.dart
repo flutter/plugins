d249f9e968be7d221868898a8bdf31b8083a2001
Michael Klimushyn <mklim@google.com>
2019-11-26 15:50:01 -0800

    [webview_flutter] Fix pedantic lint errors

--- packages/webview_flutter/lib/src/webview_method_channel.dart
+++ packages/webview_flutter/lib/src/webview_method_channel.dart
@@ -10,6 +10,8 @@ import '../platform_interface.dart';
 
 /// A [WebViewPlatformController] that uses a method channel to control the webview.
 class MethodChannelWebViewPlatform implements WebViewPlatformController {
+  /// Constructs an instance that will listen for webviews broadcasting to the
+  /// given [id], using the given [WebViewPlatformCallbacksHandler].
   MethodChannelWebViewPlatform(int id, this._platformCallbacksHandler)
       : assert(_platformCallbacksHandler != null),
         _channel = MethodChannel('plugins.flutter.io/webview_$id') {
