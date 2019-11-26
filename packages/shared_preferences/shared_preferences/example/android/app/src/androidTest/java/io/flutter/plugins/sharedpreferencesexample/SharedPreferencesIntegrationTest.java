02adfc8d80d5920b77f2ab4e5bc30d3ace25ee4d
Collin Jackson <jackson@google.com>
2019-11-26 14:41:58 -0800

    Tests pass for shared prefs plugin

--- packages/shared_preferences/shared_preferences/example/android/app/src/androidTest/java/io/flutter/plugins/sharedpreferencesexample/SharedPreferencesIntegrationTest.java
+++ packages/shared_preferences/shared_preferences/example/android/app/src/androidTest/java/io/flutter/plugins/sharedpreferencesexample/SharedPreferencesIntegrationTest.java
@@ -8,6 +8,7 @@ import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.flutter.action.FlutterActions;
 import androidx.test.espresso.flutter.assertion.FlutterAssertions;
 import androidx.test.espresso.flutter.matcher.FlutterMatchers;
+import androidx.test.espresso.NoActivityResumedException;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 import org.junit.Before;
@@ -18,8 +19,12 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public final class SharedPreferencesIntegrationTest {
 
+    @Rule
+    public ActivityTestRule<MainActivity> myActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class, true, false);
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         ActivityScenario.launch(MainActivity.class);
     }
 
@@ -32,14 +37,9 @@ public final class SharedPreferencesIntegrationTest {
                         FlutterAssertions.matches(
                                 FlutterMatchers.withText(
                                         "Button tapped 1 time.\n\nThis should persist across restarts.")));
-        // go back to main page
-        pressBack();
-        // close the application
         pressBackUnconditionally();
         // reopen the application
-        ActivityScenario.launch(MainActivity.class);
-        onFlutterWidget(FlutterMatchers.withText("Shared Preferences Sample"))
-                .perform(FlutterActions.click());
+        myActivityTestRule.launchActivity(null);
         onFlutterWidget(FlutterMatchers.withValueKey("ResultText"))
                 .check(
                         FlutterAssertions.matches(
