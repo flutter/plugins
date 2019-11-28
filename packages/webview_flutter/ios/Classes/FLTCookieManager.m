c30bce0d8409be62a2dd76ac792cc3c3e532fad3
Simon Lightfoot <simon@devangels.london>
2019-11-28 14:49:22 +0000

    Add iOS implementation of CookieManager.

--- packages/webview_flutter/ios/Classes/FLTCookieManager.m
+++ packages/webview_flutter/ios/Classes/FLTCookieManager.m
@@ -8,46 +8,89 @@
 }
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
-  FLTCookieManager *instance = [[FLTCookieManager alloc] init];
-
-  FlutterMethodChannel *channel =
-      [FlutterMethodChannel methodChannelWithName:@"plugins.flutter.io/cookie_manager"
-                                  binaryMessenger:[registrar messenger]];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FLTCookieManager *instance = [[FLTCookieManager alloc] init];
+    
+    FlutterMethodChannel *channel =
+    [FlutterMethodChannel methodChannelWithName:@"plugins.flutter.io/cookie_manager"
+                                binaryMessenger:[registrar messenger]];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
-  if ([[call method] isEqualToString:@"clearCookies"]) {
-    [self clearCookies:result];
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
-}
-
-- (void)getCookies:(FlutterResult)result API_AVAILABLE(ios(11.0)) {
-  [cookieStore getAllCookies:^(NSArray<NSHTTPCookie *> *cookies) {
-    NSArray *serialized = [CookieDto manyToDictionary:[CookieDto manyFromNSHTTPCookies:cookies]];
-    result(serialized);
-  }];
-}
-
-- (void)setCookies:(FlutterMethodCall *)call result:(FlutterResult)result API_AVAILABLE(ios(11.0)) {
-  NSArray<CookieDto *> *cookieDtos = [CookieDto manyFromDictionaries:[call arguments]];
-  for (CookieDto *cookieDto in cookieDtos) {
-    [cookieStore setCookie:[cookieDto toNSHTTPCookie]
-         completionHandler:^(){
-         }];
-  }
-}
-
-- (void)clearCookies:(FlutterResult)result API_AVAILABLE(ios(11.0)) {
-  [cookieStore getAllCookies:^(NSArray<NSHTTPCookie *> *allCookies) {
-    for (NSHTTPCookie *cookie in allCookies) {
-      [cookieStore deleteCookie:cookie
-              completionHandler:^(){
-              }];
+    if ([[call method] isEqualToString:@"getCookies"]) {
+        [self getCookies:call result:result];
+    } else if ([[call method] isEqualToString:@"setCookies"]) {
+        [self setCookies:call result:result];
+    } else if ([[call method] isEqualToString:@"clearCookies"]) {
+        [self clearCookies:result];
+    } else {
+        result(FlutterMethodNotImplemented);
     }
-  }];
+}
+
+- (NSURL *)getUrlArgument:(FlutterMethodCall *)call result:(FlutterResult)result {
+    if(![call.arguments isKindOfClass:[NSDictionary class]]) {
+        result([FlutterError errorWithCode:[NSString stringWithFormat:@"Invalid argument. Expected NSDictionary, received %@", [call.arguments class]] message:nil details:nil]);
+        return nil;
+    }
+    NSString *url = call.arguments[@"url"];
+    if(url == nil) {
+        result([FlutterError errorWithCode:@"Missing url argument" message:nil details:nil]);
+        return nil;
+    }
+    return [[NSURL alloc] initWithString:url];
+}
+
+- (void)getCookies:(FlutterMethodCall *)call result:(FlutterResult)result {
+    NSURL *url = [self getUrlArgument:call result:result];
+    if(url == nil){
+        return;
+    }
+    
+    WKWebsiteDataStore *dataStore = [WKWebsiteDataStore defaultDataStore];
+    WKHTTPCookieStore *cookieStore = [dataStore httpCookieStore];
+    [cookieStore getAllCookies:^(NSArray<NSHTTPCookie *> *cookies) {
+        NSArray *filtered = [cookies filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(id object, NSDictionary * bindings) {
+            return [((NSHTTPCookie *)object).domain isEqualToString:url.host];
+        }]];
+        NSDictionary<NSString *, NSString *> *headers = [NSHTTPCookie requestHeaderFieldsWithCookies:filtered];
+        result(headers[@"Cookie"]);
+    }];
+}
+
+- (void)setCookies:(FlutterMethodCall *)call result:(FlutterResult)result {
+    NSURL *url = [self getUrlArgument:call result:result];
+    if(url == nil){
+        return;
+    }
+    
+    NSArray<NSString *> *headers = call.arguments[@"cookies"];
+    NSMutableArray<NSHTTPCookie *> *cookies = [[NSMutableArray alloc] init];
+    for(int i = 0; i < headers.count; i++) {
+        [cookies addObjectsFromArray:[NSHTTPCookie cookiesWithResponseHeaderFields:@{@"Set-Cookie": headers[i]} forURL:url]];
+    }
+    
+    WKWebsiteDataStore *dataStore = [WKWebsiteDataStore defaultDataStore];
+    WKHTTPCookieStore *cookieStore = [dataStore httpCookieStore];
+    for(int i = 0; i < cookies.count; i++) {
+        [cookieStore setCookie:cookies[i] completionHandler:nil];
+    }
+    result(nil);
+}
+
+- (void)clearCookies:(FlutterResult)result {
+    WKWebsiteDataStore *dataStore = [WKWebsiteDataStore defaultDataStore];
+    NSSet<NSString *> *dataTypes = [NSSet setWithObject:WKWebsiteDataTypeCookies];
+    [dataStore fetchDataRecordsOfTypes:dataTypes
+                     completionHandler:^(NSArray<WKWebsiteDataRecord *> *cookies) {
+        if(cookies.count > 0){
+            [dataStore removeDataOfTypes:dataTypes forDataRecords:cookies completionHandler:^{
+                result(@(YES));
+            }];
+        } else {
+            result(@(NO));
+        }
+    }];
 }
 
 @end
