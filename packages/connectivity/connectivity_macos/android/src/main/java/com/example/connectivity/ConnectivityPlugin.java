77830a51c42b508b51d5e82955bf7b9d6f922072
Francisco Magdaleno Arceo <franciscojma@google.com>
2019-12-05 14:59:16 -0800

    Format again

--- packages/connectivity/connectivity_macos/android/src/main/java/com/example/connectivity/ConnectivityPlugin.java
+++ packages/connectivity/connectivity_macos/android/src/main/java/com/example/connectivity/ConnectivityPlugin.java
@@ -3,7 +3,6 @@ package com.example.connectivity;
 import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
