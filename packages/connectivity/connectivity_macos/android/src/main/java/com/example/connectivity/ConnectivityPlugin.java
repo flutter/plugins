f1cd0a4b1ce87608f46e296920293de75a90cf3d
Francisco Magdaleno Arceo <franciscojma@google.com>
2019-12-05 14:23:28 -0800

    Add lint and erase code

--- packages/connectivity/connectivity_macos/android/src/main/java/com/example/connectivity/ConnectivityPlugin.java
+++ packages/connectivity/connectivity_macos/android/src/main/java/com/example/connectivity/ConnectivityPlugin.java
@@ -11,35 +11,12 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 /** ConnectivityPlugin */
 public class ConnectivityPlugin implements FlutterPlugin, MethodCallHandler {
   @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    final MethodChannel channel =
-        new MethodChannel(
-            flutterPluginBinding.getFlutterEngine().getDartExecutor(), "connectivity");
-    channel.setMethodCallHandler(new ConnectivityPlugin());
-  }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "connectivity");
-    channel.setMethodCallHandler(new ConnectivityPlugin());
-  }
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {}
+  
+  public static void registerWith(Registrar registrar) {}
 
   @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
-    }
-  }
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {}
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {}
