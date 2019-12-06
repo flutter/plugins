0fd410012c4e32046ed06f184c999eff8c3ea9ea
Michael Klimushyn <mklim@google.com>
2019-12-06 08:59:02 -0800

    Review feedback, more documentation

--- packages/google_sign_in/google_sign_in_platform_interface/lib/src/types.dart
+++ packages/google_sign_in/google_sign_in_platform_interface/lib/src/types.dart
@@ -4,33 +4,58 @@
 
 import 'package:quiver/core.dart';
 
-/// Holds default configuration options to use when signing in.
+/// Default configuration options to use when signing in.
+///
+/// See also https://developers.google.com/android/reference/com/google/android/gms/auth/api/signin/GoogleSignInOptions
 enum SignInOption {
   /// Default configuration. Provides stable user ID and basic profile information.
+  ///
+  /// See also https://developers.google.com/android/reference/com/google/android/gms/auth/api/signin/GoogleSignInOptions.html#DEFAULT_SIGN_IN.
   standard,
 
   /// Recommended configuration for Games sign in.
+  ///
+  /// This is currently only supported on Android and will throw an error if used
+  /// on other platforms.
+  ///
+  /// See also https://developers.google.com/android/reference/com/google/android/gms/auth/api/signin/GoogleSignInOptions.html#public-static-final-googlesigninoptions-default_games_sign_in.
   games
 }
 
-/// Data struct representing information about the signed in user.
+/// Holds information about the signed in user.
 class GoogleSignInUserData {
   /// Uses the given data to construct an instance. Any of these parameters
   /// could be null.
   GoogleSignInUserData(
       {this.displayName, this.email, this.id, this.photoUrl, this.idToken});
 
-  /// The human readable display name of the signed in user.
+  /// The display name of the signed in user.
+  ///
+  /// Not guaranteed to be present for all users, even when configured.
   String displayName;
 
-  /// The email address of the signed in user. This may change over time and is
-  /// not a unique identifier.
+  /// The email address of the signed in user.
+  ///
+  /// Applications should not key users by email address since a Google account's
+  /// email address can change. Use [id] as a key instead.
+  ///
+  /// _Important_: Do not use this returned email address to communicate the
+  /// currently signed in user to your backend server. Instead, send an ID token
+  /// which can be securely validated on the server. See [idToken].
   String email;
 
-  /// A token uniquely identifying the account.
+  /// The unique ID for the Google account.
+  ///
+  /// This is the preferred unique key to use for a user record.
+  ///
+  /// _Important_: Do not use this returned Google ID to communicate the
+  /// currently signed in user to your backend server. Instead, send an ID token
+  /// which can be securely validated on the server. See [idToken].
   String id;
 
-  /// A URL to the profile picture of the user.
+  /// The photo url of the signed in user if the user has a profile picture.
+  ///
+  /// Not guaranteed to be present for all users, even when configured.
   String photoUrl;
 
   /// An token that can be sent to your own server to verify the authentication
