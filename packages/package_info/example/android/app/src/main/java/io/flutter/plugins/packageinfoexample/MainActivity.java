0a2b5ab29d809111b03a6296f87cb99c4aa60c62
Collin Jackson <jackson@google.com>
2019-12-09 13:45:46 -0800

    fix file that wasn't meant to be checked in

--- packages/package_info/example/android/app/src/main/java/io/flutter/plugins/packageinfoexample/MainActivity.java
+++ packages/package_info/example/android/app/src/main/java/io/flutter/plugins/packageinfoexample/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends FlutterActivity {
   @Override
   public void configureFlutterEngine(FlutterEngine flutterEngine) {
     flutterEngine.getPlugins().add(new PackageInfoPlugin());
+    flutterEngine.getPlugins().add(new E2EPlugin());
   }
 }
