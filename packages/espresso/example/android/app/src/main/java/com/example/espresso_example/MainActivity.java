5377d27f95886fec30ad1c5d36ed75def685e3bc
Collin Jackson <jackson@google.com>
2019-12-09 16:48:43 -0800

    Fix build failure

--- packages/espresso/example/android/app/src/main/java/com/example/espresso_example/MainActivity.java
+++ packages/espresso/example/android/app/src/main/java/com/example/espresso_example/MainActivity.java
@@ -8,6 +8,5 @@ import io.flutter.plugins.GeneratedPluginRegistrant;
 public class MainActivity extends FlutterActivity {
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
   }
 }
