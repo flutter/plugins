51dd1402a19662ce851953b9cf1f52843541a67d
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2019-12-11 00:06:18 +0300

    revert spaces

--- packages/video_player/video_player/android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayer.java
+++ packages/video_player/video_player/android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayer.java
@@ -87,12 +87,12 @@ final class VideoPlayer {
     DataSource.Factory dataSourceFactory;
     if (isHTTP(uri)) {
       dataSourceFactory =
-         new DefaultHttpDataSourceFactory(
-             "ExoPlayer",
-             null,
-             DefaultHttpDataSource.DEFAULT_CONNECT_TIMEOUT_MILLIS,
-             DefaultHttpDataSource.DEFAULT_READ_TIMEOUT_MILLIS,
-             true);
+          new DefaultHttpDataSourceFactory(
+              "ExoPlayer",
+              null,
+              DefaultHttpDataSource.DEFAULT_CONNECT_TIMEOUT_MILLIS,
+              DefaultHttpDataSource.DEFAULT_READ_TIMEOUT_MILLIS,
+              true);
     } else {
       dataSourceFactory = new DefaultDataSourceFactory(context, "ExoPlayer");
     }
