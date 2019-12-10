9027dc606929ebea00d51be32c2e66a8437233bc
Francisco Magdaleno Arceo <franciscojma@google.com>
2019-12-10 11:22:39 -0800

    Example registrant

--- packages/shared_preferences/shared_preferences/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/shared_preferences/shared_preferences/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import connectivity_macos
+import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
