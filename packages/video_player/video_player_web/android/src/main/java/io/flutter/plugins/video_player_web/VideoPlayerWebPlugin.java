46b98589bc5d3300e889a9d61429cc8c91ea977f
Amir Hardon <amirh@users.noreply.github.com>
2019-12-12 12:37:17 -0800

    Remove AndroidX references from no-op android implementations (#2411)
    Adding an android/ folder as a temporary mitigation for: flutter/flutter#46898

--- packages/video_player/video_player_web/android/src/main/java/io/flutter/plugins/video_player_web/VideoPlayerWebPlugin.java
+++ packages/video_player/video_player_web/android/src/main/java/io/flutter/plugins/video_player_web/VideoPlayerWebPlugin.java
@@ -1,21 +1,15 @@
 package io.flutter.plugins.video_player_web;
 
-import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** VideoPlayerWebPlugin */
 public class VideoPlayerWebPlugin implements FlutterPlugin {
   @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {}
+  public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {}
 
   public static void registerWith(Registrar registrar) {}
 
   @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {}
-
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {}
+  public void onDetachedFromEngine(FlutterPluginBinding binding) {}
 }
