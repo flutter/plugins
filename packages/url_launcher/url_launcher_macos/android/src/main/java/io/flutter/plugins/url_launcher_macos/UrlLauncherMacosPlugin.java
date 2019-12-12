5dd2d8639af6bf4b3296a6f893e84611d1e9d330
Amir Hardon <amirha@google.com>
2019-12-12 12:00:01 -0800

    remove androidx references from no-op android implementations

--- packages/url_launcher/url_launcher_macos/android/src/main/java/io/flutter/plugins/url_launcher_macos/UrlLauncherMacosPlugin.java
+++ packages/url_launcher/url_launcher_macos/android/src/main/java/io/flutter/plugins/url_launcher_macos/UrlLauncherMacosPlugin.java
@@ -1,15 +1,14 @@
 package io.flutter.plugins.url_launcher_macos;
 
-import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 public class UrlLauncherMacosPlugin implements FlutterPlugin {
   @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {}
+  public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {}
 
   public static void registerWith(Registrar registrar) {}
 
   @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {}
+  public void onDetachedFromEngine(FlutterPluginBinding binding) {}
 }
