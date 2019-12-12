71f01c495ad4685f59adb94671cf44b986889d58
Michael Klimushyn <mklim@google.com>
2019-12-12 14:12:01 -0500

    [google_sign_in_web] Add a no-op Android implementation

---
+++ packages/google_sign_in/google_sign_in_web/android/src/main/java/io/flutter/plugins/google_sign_in_web/GoogleSignInWebPlugin.java
@@ -0,0 +1,44 @@
+package io.flutter.plugins.google_sign_in_web;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** GoogleSignInWebPlugin */
+public class GoogleSignInWebPlugin implements FlutterPlugin, MethodCallHandler {
+  @Override
+  public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {
+    final MethodChannel channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "google_sign_in_web");
+    channel.setMethodCallHandler(new GoogleSignInWebPlugin());
+  }
+
+  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
+  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
+  // plugin registration via this function while apps migrate to use the new Android APIs
+  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
+  //
+  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
+  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
+  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
+  // in the same class.
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "google_sign_in_web");
+    channel.setMethodCallHandler(new GoogleSignInWebPlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+
+  @Override
+  public void onDetachedFromEngine(FlutterPluginBinding binding) {
+  }
+}
