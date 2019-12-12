375545568fc2cd0c3e418b2d37924bfdcbf06614
Amir Hardon <amirh@users.noreply.github.com>
2019-12-11 16:08:38 -0800

    [url_launcher] remove temporary dependency override (#2401)
    Follow-up to #2399 removing the dependency override.

--- script/build_all_plugins_app.sh
+++ script/build_all_plugins_app.sh
@@ -11,14 +11,18 @@ source "$SCRIPT_DIR/common.sh"
 check_changed_packages > /dev/null
 
 readonly EXCLUDED_PLUGINS_LIST=(
+  "google_sign_in_platform_interface"
+  "google_sign_in_web"
   "instrumentation_adapter"
+  "plugin_platform_interface"
+  "shared_preferences_macos"
+  "shared_preferences_platform_interface"
+  "shared_preferences_web"
+  "url_launcher_macos"
   "url_launcher_platform_interface"
   "url_launcher_web"
-  "url_launcher_macos"
-  "google_sign_in_platform_interface"
   "video_player_platform_interface"
-  "shared_preferences_platform_interface"
-  "shared_preferences_macos"
+  "video_player_web"
 )
 # Comma-separated string of the list above
 readonly EXCLUDED=$(IFS=, ; echo "${EXCLUDED_PLUGINS_LIST[*]}")
