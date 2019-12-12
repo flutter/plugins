30e1e3d164fb3c44b609ac40c7b85f9b0e08a8b7
Amir Hardon <amirh@users.noreply.github.com>
2019-12-12 12:36:50 -0800

    [shared_preferences] add a no-op android folder to web/macos packages (#2409)
    Adding an android/ folder as a temporary mitigation for: flutter/flutter#46898

---
+++ packages/shared_preferences/shared_preferences_web/android/src/main/java/io/flutter/plugins/shared_preferences_web/SharedPreferencesWebPlugin.java
@@ -0,0 +1,20 @@
+package io.flutter.plugins.shared_preferences_web;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** SharedPreferencesWebPlugin */
+public class SharedPreferencesWebPlugin implements FlutterPlugin {
+  @Override
+  public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {}
+
+  public static void registerWith(Registrar registrar) {}
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {}
+
+  @Override
+  public void onDetachedFromEngine(FlutterPluginBinding binding) {}
+}
