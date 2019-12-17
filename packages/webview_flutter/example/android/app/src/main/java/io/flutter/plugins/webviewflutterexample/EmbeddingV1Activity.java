8fcf99d8153807c8783ca478f12bfe83479672ed
Michael Klimushyn <mklim@google.com>
2019-12-17 14:52:01 -0800

    Final changes

--- packages/webview_flutter/example/android/app/src/main/java/io/flutter/plugins/webviewflutterexample/EmbeddingV1Activity.java
+++ packages/webview_flutter/example/android/app/src/main/java/io/flutter/plugins/webviewflutterexample/EmbeddingV1Activity.java
@@ -6,12 +6,14 @@ package io.flutter.plugins.webviewflutterexample;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import dev.flutter.plugins.e2e.E2EPlugin;
+import io.flutter.plugins.webviewflutter.WebViewFlutterPlugin;
 
 public class EmbeddingV1Activity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    E2EPlugin.registerWith(registrarFor("dev.flutter.plugins.e2e.E2EPlugin"));
+    WebViewFlutterPlugin.registerWith(registrarFor("io.flutter.plugins.webviewflutter.WebViewFlutterPlugin"));
   }
 }
