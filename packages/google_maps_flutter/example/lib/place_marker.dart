6b34432c59d0035615da3408f0298968642b388a
Tim Sneath <timsneath@google.com>
2020-01-02 17:40:36 -0800

    Add typed_data import

--- packages/google_maps_flutter/example/lib/place_marker.dart
+++ packages/google_maps_flutter/example/lib/place_marker.dart
@@ -7,6 +7,7 @@
 import 'dart:async';
 import 'dart:math';
 import 'dart:ui';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
