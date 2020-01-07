1230ea442829f4a7f244f0126190714b0d73df37
Chris Yang <ychris@google.com>
2020-01-07 10:03:33 -0800

    Update FLTWebViewTests.m

--- packages/webview_flutter/ios/Tests/FLTWebViewTests.m
+++ packages/webview_flutter/ios/Tests/FLTWebViewTests.m
@@ -2,10 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <Flutter/Flutter.h>
-#import <OCMock/OCMock.h>
-#import <XCTest/XCTest.h>
-
+@import Flutter;
+@import OCMock;
+@import XCTest;
 @import webview_flutter;
 
 @interface FLTWebViewTests : XCTestCase
