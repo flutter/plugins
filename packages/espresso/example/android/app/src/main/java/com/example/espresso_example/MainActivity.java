d69e4073c9603578ae5c0be2df1ff3a7ab0fe69f
Collin Jackson <jackson@google.com>
2020-01-09 13:54:41 -0800

    reformat

--- packages/espresso/example/android/app/src/main/java/com/example/espresso_example/MainActivity.java
+++ packages/espresso/example/android/app/src/main/java/com/example/espresso_example/MainActivity.java
@@ -3,10 +3,8 @@ package com.example.espresso_example;
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
   @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-  }
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {}
 }
