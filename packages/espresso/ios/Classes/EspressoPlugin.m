81294818e56899e2e87136dc4507e7c0ad76b870
Collin Jackson <jackson@google.com>
2020-01-09 15:20:59 -0800

    reformat

--- packages/espresso/ios/Classes/EspressoPlugin.m
+++ packages/espresso/ios/Classes/EspressoPlugin.m
@@ -2,9 +2,9 @@
 
 @implementation EspressoPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"espresso"
-            binaryMessenger:[registrar messenger]];
+  FlutterMethodChannel* channel =
+      [FlutterMethodChannel methodChannelWithName:@"espresso"
+                                  binaryMessenger:[registrar messenger]];
   EspressoPlugin* instance = [[EspressoPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
