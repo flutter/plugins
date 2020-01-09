8b6542bed8e69687eacf8132c7b2fedd674f0689
Collin Jackson <jackson@google.com>
2020-01-09 14:49:27 -0800

    fix ios build

--- packages/espresso/ios/Classes/EspressoPlugin.m
+++ packages/espresso/ios/Classes/EspressoPlugin.m
@@ -1,15 +1,15 @@
 #import "EspressoPlugin.h"
-#if __has_include(<espresso/espresso-Swift.h>)
-#import <espresso/espresso-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "espresso-Swift.h"
-#endif
 
 @implementation EspressoPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftEspressoPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"espresso"
+            binaryMessenger:[registrar messenger]];
+  EspressoPlugin* instance = [[EspressoPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  result(FlutterMethodNotImplemented);
 }
 @end
