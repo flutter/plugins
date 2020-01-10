66152f6bac1e1cf309167a49933a8ffcc01afb97
David Iglesias <ditman@gmail.com>
2020-01-09 17:12:31 -0800

    [google_sign_in_web] Fix race condition on init. (#2455)
    * [google_sign_in_web] Wait for Auth2 to be really ready.
    
    The Auth object that `.init()` returns may not be fully ready by the time
    we start calling methods on it; however it has a `.then()` method that
    gets called when it is *really* ready to go.
    
    * Add a completer to signal when Auth2 is ready.
    * Throw StateError synchronously if .initialized is checked before calling .init()
    * Move auth2-dependent init to the `then` method.
      * onSuccess, complete the auth2 ready future.
      * onError, throw a PlatformException. This is normally triggered when
    the domain:port of the web app hasn't been whitelisted on the google
    sign in console, and things like that. Useful for debugging.
    * Update README with latest setup instructions.

--- packages/google_sign_in/google_sign_in_web/test/gapi_load_test.dart
+++ packages/google_sign_in/google_sign_in_web/test/gapi_load_test.dart
@@ -7,14 +7,16 @@
 import 'dart:html' as html;
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:google_sign_in_platform_interface/google_sign_in_platform_interface.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'gapi_mocks/gapi_mocks.dart' as gapi_mocks;
 import 'utils.dart';
 
 void main() {
-  gapiUrl = toBase64Url(gapi_mocks.gapiInitSuccess());
+  gapiUrl = toBase64Url(gapi_mocks.auth2InitSuccess(GoogleSignInUserData()));
 
-  test('Plugin is initialized after GAPI fully loads', () async {
+  test('Plugin is initialized after GAPI fully loads and init is called',
+      () async {
     expect(
       html.querySelector('script[src^="data:"]'),
       isNull,
@@ -26,6 +28,12 @@ void main() {
       isNotNull,
       reason: 'Mock script should be injected',
     );
+    expect(() {
+      plugin.initialized;
+    }, throwsStateError,
+        reason:
+            'The plugin should throw if checking for `initialized` before calling .init');
+    await plugin.init(hostedDomain: '', clientId: '');
     await plugin.initialized;
     expect(
       plugin.initialized,
