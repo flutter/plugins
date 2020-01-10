33b060c43e9986f97d3dc359755b106d94f9a1c1
Collin Jackson <jackson@google.com>
2020-01-10 15:53:48 -0800

    reformat

--- packages/shared_preferences/shared_preferences/example/android/app/src/androidTest/java/io/flutter/plugins/sharedpreferencesexample/SharedPreferencesIntegrationTest.java
+++ packages/shared_preferences/shared_preferences/example/android/app/src/androidTest/java/io/flutter/plugins/sharedpreferencesexample/SharedPreferencesIntegrationTest.java
@@ -1,20 +1,14 @@
 package io.flutter.plugins.sharedpreferencesexample;
 
-import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.Espresso.pressBackUnconditionally;
-import static androidx.test.espresso.flutter.action.FlutterActions.click;
-import static androidx.test.espresso.flutter.action.FlutterActions.syntheticClick;
-import static androidx.test.espresso.flutter.assertion.FlutterAssertions.matches;
 import static androidx.test.espresso.flutter.EspressoFlutter.onFlutterWidget;
+import static androidx.test.espresso.flutter.action.FlutterActions.click;
+import static androidx.test.espresso.flutter.assertion.FlutterAssertions.matches;
+import static androidx.test.espresso.flutter.matcher.FlutterMatchers.withText;
 import static androidx.test.espresso.flutter.matcher.FlutterMatchers.withTooltip;
 import static androidx.test.espresso.flutter.matcher.FlutterMatchers.withValueKey;
-import static androidx.test.espresso.flutter.matcher.FlutterMatchers.withText;
 
 import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.flutter.action.FlutterActions;
-import androidx.test.espresso.flutter.assertion.FlutterAssertions;
-import androidx.test.espresso.flutter.matcher.FlutterMatchers;
-import androidx.test.espresso.NoActivityResumedException;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 import org.junit.Before;
@@ -25,31 +19,25 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public final class SharedPreferencesIntegrationTest {
 
-    @Rule
-    public ActivityTestRule<MainActivity> myActivityTestRule =
-            new ActivityTestRule<>(MainActivity.class, true, false);
+  @Rule
+  public ActivityTestRule<MainActivity> myActivityTestRule =
+      new ActivityTestRule<>(MainActivity.class, true, false);
 
-    @Before
-    public void setUp() {
-        ActivityScenario.launch(MainActivity.class);
-    }
+  @Before
+  public void setUp() {
+    ActivityScenario.launch(MainActivity.class);
+  }
 
-    @Test
-    public void tapToCheckPersistentData() throws Exception {
-        onFlutterWidget(withTooltip("Clear")).perform(click());
-        onFlutterWidget(withTooltip("Increment")).perform(click());
-        onFlutterWidget(withValueKey("ResultText"))
-                .check(
-                        matches(
-                                withText(
-                                        "Button tapped 1 time.\n\nThis should persist across restarts.")));
-        pressBackUnconditionally();
-        // reopen the application
-        myActivityTestRule.launchActivity(null);
-        onFlutterWidget(withValueKey("ResultText"))
-                .check(
-                        matches(
-                                withText(
-                                        "Button tapped 1 time.\n\nThis should persist across restarts.")));
-    }
+  @Test
+  public void tapToCheckPersistentData() throws Exception {
+    onFlutterWidget(withTooltip("Clear")).perform(click());
+    onFlutterWidget(withTooltip("Increment")).perform(click());
+    onFlutterWidget(withValueKey("ResultText"))
+        .check(matches(withText("Button tapped 1 time.\n\nThis should persist across restarts.")));
+    pressBackUnconditionally();
+    // reopen the application
+    myActivityTestRule.launchActivity(null);
+    onFlutterWidget(withValueKey("ResultText"))
+        .check(matches(withText("Button tapped 1 time.\n\nThis should persist across restarts.")));
+  }
 }
--- packages/shared_preferences/shared_preferences/example/test_driver/shared_preferences.dart
+++ packages/shared_preferences/shared_preferences/example/test_driver/shared_preferences.dart
@@ -1,7 +1,7 @@
-import 'package:flutter_driver/driver_extension.dart';	
-import 'package:shared_preferences_example/main.dart' as app;	
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:shared_preferences_example/main.dart' as app;
 
-void main() {	
-  enableFlutterDriverExtension();	
-  app.main();	
+void main() {
+  enableFlutterDriverExtension();
+  app.main();
 }
