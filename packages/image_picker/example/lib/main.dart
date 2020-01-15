57253e3e6dd0339d9658ab2922757db1952ab143
Lucas Alves <lcsjalves@gmail.com>
2020-01-15 10:45:13 -0300

    Removing unnecessary imports

--- packages/image_picker/example/lib/main.dart
+++ packages/image_picker/example/lib/main.dart
@@ -8,8 +8,6 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/basic.dart';
-import 'package:flutter/src/widgets/container.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
 
