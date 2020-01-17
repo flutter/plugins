3bf4b57a522aadd4ea2e8fdfad76867a32d96159
MH Johnson <johnsonmh@google.com>
2020-01-16 20:37:28 -0500

    remove unused import

--- packages/video_player/video_player/test/sub_rip_file_test.dart
+++ packages/video_player/video_player/test/sub_rip_file_test.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:video_player/src/closed_caption_file.dart';
 import 'package:video_player/video_player.dart';
