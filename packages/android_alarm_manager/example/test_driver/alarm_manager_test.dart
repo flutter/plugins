20edffa441a15bffd68df31b71fb388382ad9f44
Ben Konyi <bkonyi@google.com>
2020-01-23 13:23:02 -0800

    Fixed typo

--- packages/android_alarm_manager/example/test_driver/alarm_manager_test.dart
+++ packages/android_alarm_manager/example/test_driver/alarm_manager_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_driver/driver_extension.dart';
-// ignore: avoid_relative_lib_import
+// ignore: avoid_relative_lib_imports
 import '../lib/main.dart' as app;
 
 Future<void> main() async {
