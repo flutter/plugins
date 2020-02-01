9c5810c467969c0dd86b48e91e107da0066de8ca
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2020-02-01 20:16:56 +0300

    merge fixes

--- packages/video_player/video_player/lib/video_player.dart
+++ packages/video_player/video_player/lib/video_player.dart
@@ -16,11 +16,6 @@ export 'package:video_player_platform_interface/video_player_platform_interface.
 
 export 'src/closed_caption_file.dart';
 
-final VideoPlayerPlatform _videoPlayerPlatform = VideoPlayerPlatform.instance
-  // This will clear all open videos on the platform when a full restart is
-  // performed.
-  ..init();
-
 /// The duration, current position, buffering state, error state and settings
 /// of a [VideoPlayerController].
 class VideoPlayerValue {
