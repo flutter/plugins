ebb6f6edecea69217cdb33f408e11e32449f44dd
Nurhan Turgut <nurhan@google.com>
2020-02-05 13:29:26 -0800

    changes by format tool

--- packages/e2e/lib/_extension_web.dart
+++ packages/e2e/lib/_extension_web.dart
@@ -18,13 +18,15 @@ import 'dart:js_util' as js_util;
 /// See also:
 ///
 ///  * [_extension_io.dart], which has the dart:io implementation
-void registerWebServiceExtension(Future<Map<String, dynamic>> Function(Map<String, String>) call) {
-  js_util.setProperty(html.window, '\$flutterDriver', allowInterop((dynamic message) async {
+void registerWebServiceExtension(
+    Future<Map<String, dynamic>> Function(Map<String, String>) call) {
+  js_util.setProperty(html.window, '\$flutterDriver',
+      allowInterop((dynamic message) async {
     // ignore: undefined_function, undefined_identifier
     final Map<String, String> params = Map<String, String>.from(
         jsonDecode(message as String) as Map<String, dynamic>);
-    final Map<String, dynamic> result = Map<String, dynamic>.from(
-        await call(params));
+    final Map<String, dynamic> result =
+        Map<String, dynamic>.from(await call(params));
     context['\$flutterDriverResult'] = json.encode(result);
   }));
 }
