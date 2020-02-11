3351487bc79d3cbd911ee91d6b5db8c73d855ca4
Francisco Madgaleno <franciscojma86@gmail.com>
2020-02-11 08:10:03 -0800

    Comments

--- packages/connectivity/connectivity_platform_interface/lib/method_channel_connectivity.dart
+++ packages/connectivity/connectivity_platform_interface/lib/method_channel_connectivity.dart
@@ -1,4 +1,4 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,6 +9,7 @@ import 'package:meta/meta.dart';
 
 import 'connectivity_platform_interface.dart';
 
+/// The method channel used to interact with the native platform.
 @visibleForTesting
 const MethodChannel method_channel = MethodChannel('plugins.flutter.io/connectivity');
 
