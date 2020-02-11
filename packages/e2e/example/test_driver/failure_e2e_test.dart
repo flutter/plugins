c083e11af17e18ecf4c313b11f9d126912170413
Collin Jackson <jackson@google.com>
2020-02-10 16:17:48 -0800

    Test failure behavior

---
+++ packages/e2e/example/test_driver/failure_e2e_test.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+Future<void> main() async {
+  test('fails gracefully', () async {
+    final FlutterDriver driver = await FlutterDriver.connect();
+    final String result =
+        await driver.requestData(null, timeout: const Duration(minutes: 1));
+    await driver.close();
+    expect(
+      result,
+      'failure',
+      skip: true, // https://github.com/flutter/flutter/issues/48601
+    );
+  });
+}
