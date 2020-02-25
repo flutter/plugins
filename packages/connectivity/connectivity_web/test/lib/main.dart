6720cc93bc1d13fa4adb6be8a0f137c74a74cb4c
David Iglesias Teixeira <ditman@gmail.com>
2020-02-24 19:48:44 -0800

    Another go at e2e tests.

--- packages/connectivity/connectivity_web/test/lib/main.dart
+++ packages/connectivity/connectivity_web/test/lib/main.dart
@@ -1,24 +1,92 @@
-import 'dart:async';
-
+import 'package:connectivity_web_example/src/connectivity_mocks.dart';
+import 'package:e2e/e2e.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_driver/driver_extension.dart';
+import 'package:connectivity_platform_interface/connectivity_platform_interface.dart';
+import 'package:connectivity_web/connectivity_web.dart';
 
 void main() {
-  final Completer<String> completer = Completer<String>();
-  enableFlutterDriverExtension(handler: (_) => completer.future);
-  tearDownAll(() => completer.complete(null));
+  E2EWidgetsFlutterBinding.ensureInitialized();
 
-  setUp(() {
-    print('Hello, testing!');
+  group('checkConnectivity', () {
+    void testCheckConnectivity({
+      String type,
+      String effectiveType,
+      num downlink = 10,
+      num rtt = 50,
+      ConnectivityResult expected,
+    }) {
+      MockNetworkInformation connection = MockNetworkInformation(
+          type: type,
+          effectiveType: effectiveType,
+          downlink: downlink,
+          rtt: rtt);
+      ConnectivityPlugin plugin = ConnectivityPlugin.withConnection(connection);
+      expect(plugin.checkConnectivity(), completion(equals(expected)));
+    }
+
+    group('in Chrome', () {
+      test('0 downlink and rtt -> none', () {
+        testCheckConnectivity(
+            effectiveType: '4g',
+            downlink: 0,
+            rtt: 0,
+            expected: ConnectivityResult.none);
+      });
+      test('slow-2g -> mobile', () {
+        testCheckConnectivity(
+            effectiveType: 'slow-2g', expected: ConnectivityResult.mobile);
+      });
+      test('2g -> mobile', () {
+        testCheckConnectivity(
+            effectiveType: '2g', expected: ConnectivityResult.mobile);
+      });
+      test('3g -> mobile', () {
+        testCheckConnectivity(
+            effectiveType: '3g', expected: ConnectivityResult.mobile);
+      });
+      test('4g -> wifi', () {
+        testCheckConnectivity(
+            effectiveType: '4g', expected: ConnectivityResult.wifi);
+      });
+    });
+
+    group('unsupported browsers', () {
+      test('null connection -> none', () {
+        ConnectivityPlugin plugin = ConnectivityPlugin.withConnection(null);
+        expect(plugin.checkConnectivity(),
+            completion(equals(ConnectivityResult.none)));
+      });
+    });
   });
 
-  tearDown(() {
-    print('Bye, testing!');
-  });
+  group('get onConnectivityChanged', () {
+    group('in Chrome', () {
+      test('puts change events in a Stream', () async {
+        MockNetworkInformation connection =
+            MockNetworkInformation(effectiveType: '4g', downlink: 10, rtt: 50);
+        ConnectivityPlugin plugin =
+            ConnectivityPlugin.withConnection(connection);
 
-  test('getPlatformVersion', () async {
+        Stream<ConnectivityResult> results = plugin.onConnectivityChanged;
 
-    print('Testing some!');
-  
+        // Fake a disconnect-reconnect
+        connection.mockChangeValue(downlink: 0, rtt: 0);
+        connection.mockChangeValue(downlink: 10, rtt: 50);
+
+        // The stream of results is infinite, so we need to .take(2) for this test to complete.
+        expect(
+            results.take(2).toList(),
+            completion(
+                equals([ConnectivityResult.none, ConnectivityResult.wifi])));
+      });
+    });
+
+    group('unsupported browsers', () {
+      test('null connection -> none', () {
+        ConnectivityPlugin plugin = ConnectivityPlugin.withConnection(null);
+        expect(plugin.onConnectivityChanged.last,
+            completion(equals(ConnectivityResult.none)));
+      });
+    });
   });
 }
