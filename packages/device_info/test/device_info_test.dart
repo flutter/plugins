986bff7479c9d097293d9567e16b7cf198f50486
Jaideep Prasad <jddeep003@gmail.com>
2020-02-25 14:30:54 +0530

    [device_info] Tests: Added device_info Test.
    [device_info] Tests: Added device_info Test. Tests were missing for the package.

---
+++ packages/device_info/test/device_info_test.dart
@@ -0,0 +1,17 @@
+import 'package:device_info/device_info.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+
+  test('Testing the Device Info', (){
+
+    DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+
+    deviceInfoPlugin.androidInfo.then((AndroidDeviceInfo andinfo){
+      expect(andinfo, isNotNull);
+    });
+    deviceInfoPlugin.iosInfo.then((IosDeviceInfo iosinfo){
+      expect(iosinfo, isNotNull);
+    });
+  });
+}
