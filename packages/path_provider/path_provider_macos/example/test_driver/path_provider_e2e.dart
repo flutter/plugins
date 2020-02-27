b55dd6c188b5ca56dccae36a2c98ad409a66c1fa
Francisco Magdaleno Arceo <franciscojma@google.com>
2020-02-26 20:34:19 -0800

    Import

--- packages/path_provider/path_provider_macos/example/test_driver/path_provider_e2e.dart
+++ packages/path_provider/path_provider_macos/example/test_driver/path_provider_e2e.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
-
 import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:path_provider/path_provider.dart';
