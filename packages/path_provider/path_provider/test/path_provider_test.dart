d0afe4ddad0a0f6ef745b9be957fda09ba0582ff
Francisco Magdaleno Arceo <franciscojma@google.com>
2020-02-28 10:47:29 -0800

    Change interface version

--- packages/path_provider/path_provider/test/path_provider_test.dart
+++ packages/path_provider/path_provider/test/path_provider_test.dart
@@ -11,7 +11,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-const String kTemproraryPath = 'temproraryPath';
+const String kTemproraryPath = 'temporaryPath';
 const String kApplicationSupportPath = 'applicationSupportPath';
 const String kDownloadsPath = 'downloadsPath';
 const String kLibraryPath = 'libraryPath';
