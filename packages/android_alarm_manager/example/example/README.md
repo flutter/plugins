a8bb31e498d88f5f25c7e105e309971a301920b5
Collin Jackson <jackson@google.com>
2020-03-02 10:36:38 -0800

    Split espresso and e2e example apps

---
+++ packages/android_alarm_manager/example/example/README.md
@@ -0,0 +1,8 @@
+# android_alarm_manager_example
+
+Demonstrates how to use the android_alarm_manager plugin.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](http://flutter.io/).
--- packages/android_alarm_manager/example/test_driver/alarm_manager_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_driver/driver_extension.dart';
-// ignore: avoid_relative_lib_imports
-import '../lib/main.dart' as app;
-
-Future<void> main() async {
-  enableFlutterDriverExtension();
-  await app.main();
-}
