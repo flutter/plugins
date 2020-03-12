8f665d245d4a25c56a320a97f494d7e6b272f8a6
garyqian <garyq@google.com>
2020-03-12 16:54:59 -0700

    formatting

--- packages/quick_actions/example/android/app/src/main/java/io/flutter/plugins/quickactionsexample/MainActivityTest.java
+++ packages/quick_actions/example/android/app/src/main/java/io/flutter/plugins/quickactionsexample/MainActivityTest.java
@@ -1,8 +1,8 @@
-// Copyright 2019 The Chromium Authors. All rights reserved.	
-// Use of this source code is governed by a BSD-style license that can be	
-// found in the LICENSE file.	
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-package io.flutter.plugins.quickactionsexample;	
+package io.flutter.plugins.quickactionsexample;
 
 import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.e2e.FlutterRunner;
@@ -12,5 +12,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(FlutterRunner.class)
 public class MainActivityTest {
-  @Rule public ActivityTestRule<FlutterActivity> rule = new ActivityTestRule<>(FlutterActivity.class);	
+  @Rule
+  public ActivityTestRule<FlutterActivity> rule = new ActivityTestRule<>(FlutterActivity.class);
 }
