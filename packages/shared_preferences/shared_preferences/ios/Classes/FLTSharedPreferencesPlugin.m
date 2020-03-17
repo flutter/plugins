3f3464223a04c14e6cdab57aaa0a574d267f35da
Luis Freire <luis.freire@hypnotic.pt>
2020-03-17 19:08:34 +0000

    Update shared preferences to support domains

--- packages/shared_preferences/shared_preferences/ios/Classes/FLTSharedPreferencesPlugin.m
+++ packages/shared_preferences/shared_preferences/ios/Classes/FLTSharedPreferencesPlugin.m
@@ -8,6 +8,8 @@ static NSString *const CHANNEL_NAME = @"plugins.flutter.io/shared_preferences";
 
 @implementation FLTSharedPreferencesPlugin
 
+NSString *customDomain;
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
   FlutterMethodChannel *channel = [FlutterMethodChannel methodChannelWithName:CHANNEL_NAME
                                                               binaryMessenger:registrar.messenger];
@@ -20,7 +22,8 @@ static NSString *const CHANNEL_NAME = @"plugins.flutter.io/shared_preferences";
     } else if ([method isEqualToString:@"setBool"]) {
       NSString *key = arguments[@"key"];
       NSNumber *value = arguments[@"value"];
-      [[NSUserDefaults standardUserDefaults] setBool:value.boolValue forKey:key];
+      [domainUserDefaults() setBool:value.boolValue forKey:key];
+      [domainUserDefaults() synchronize];
       result(@YES);
     } else if ([method isEqualToString:@"setInt"]) {
       NSString *key = arguments[@"key"];
@@ -28,35 +31,41 @@ static NSString *const CHANNEL_NAME = @"plugins.flutter.io/shared_preferences";
       // int type in Dart can come to native side in a variety of forms
       // It is best to store it as is and send it back when needed.
       // Platform channel will handle the conversion.
-      [[NSUserDefaults standardUserDefaults] setValue:value forKey:key];
+      [domainUserDefaults() setValue:value forKey:key];
+      [domainUserDefaults() synchronize];
       result(@YES);
     } else if ([method isEqualToString:@"setDouble"]) {
       NSString *key = arguments[@"key"];
       NSNumber *value = arguments[@"value"];
-      [[NSUserDefaults standardUserDefaults] setDouble:value.doubleValue forKey:key];
+      [domainUserDefaults() setDouble:value.doubleValue forKey:key];
+      [domainUserDefaults() synchronize];
       result(@YES);
     } else if ([method isEqualToString:@"setString"]) {
       NSString *key = arguments[@"key"];
       NSString *value = arguments[@"value"];
-      [[NSUserDefaults standardUserDefaults] setValue:value forKey:key];
+      [domainUserDefaults() setValue:value forKey:key];
+      [domainUserDefaults() synchronize];
       result(@YES);
     } else if ([method isEqualToString:@"setStringList"]) {
       NSString *key = arguments[@"key"];
       NSArray *value = arguments[@"value"];
-      [[NSUserDefaults standardUserDefaults] setValue:value forKey:key];
+      [domainUserDefaults() setValue:value forKey:key];
+      [domainUserDefaults() synchronize];
       result(@YES);
     } else if ([method isEqualToString:@"commit"]) {
       // synchronize is deprecated.
       // "this method is unnecessary and shouldn't be used."
       result(@YES);
     } else if ([method isEqualToString:@"remove"]) {
-      [[NSUserDefaults standardUserDefaults] removeObjectForKey:arguments[@"key"]];
+      [domainUserDefaults() removeObjectForKey:arguments[@"key"]];
+      [domainUserDefaults() synchronize];
       result(@YES);
     } else if ([method isEqualToString:@"clear"]) {
-      NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+      NSUserDefaults *defaults = domainUserDefaults();
       for (NSString *key in getAllPrefs()) {
         [defaults removeObjectForKey:key];
       }
+      [domainUserDefaults() synchronize];
       result(@YES);
     } else {
       result(FlutterMethodNotImplemented);
@@ -66,9 +75,12 @@ static NSString *const CHANNEL_NAME = @"plugins.flutter.io/shared_preferences";
 
 #pragma mark - Private
 
+static id domainUserDefaults() {
+    return customDomain != nil ? [[NSUserDefaults alloc] initWithSuiteName: customDomain] : [NSUserDefaults standardUserDefaults];
+}
+
 static NSMutableDictionary *getAllPrefs() {
-  NSString *appDomain = [[NSBundle mainBundle] bundleIdentifier];
-  NSDictionary *prefs = [[NSUserDefaults standardUserDefaults] persistentDomainForName:appDomain];
+  NSDictionary *prefs = [domainUserDefaults() dictionaryRepresentation];
   NSMutableDictionary *filteredPrefs = [NSMutableDictionary dictionary];
   if (prefs != nil) {
     for (NSString *candidateKey in prefs) {
