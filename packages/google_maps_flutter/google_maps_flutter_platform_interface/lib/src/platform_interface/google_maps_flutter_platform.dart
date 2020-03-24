eb89c8d7feb90ede6d6061f14398ea109e59af1e
David Iglesias Teixeira <ditman@gmail.com>
2020-03-24 11:12:49 -0700

    Adjust imports in platform interface

--- packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/platform_interface/google_maps_flutter_platform.dart
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/platform_interface/google_maps_flutter_platform.dart
@@ -8,9 +8,9 @@ import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-import 'src/method_channel/method_channel_google_maps_flutter.dart';
+import 'package:google_maps_flutter_platform_interface/src/method_channel/method_channel_google_maps_flutter.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 /// Exception when a map style is invalid or was unable to be set.
 ///
