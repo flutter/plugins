bf964047af738f10803e45f1631c1e37d3cdf17d
miyakeryo <ryo@miyake.id>
2020-03-24 09:13:44 +0900

    [google_maps_flutter] Remove extra line

--- packages/google_maps_flutter/google_maps_flutter/example/test_driver/google_map_inspector.dart
+++ packages/google_maps_flutter/google_maps_flutter/example/test_driver/google_map_inspector.dart
@@ -3,7 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:typed_data';
-
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
