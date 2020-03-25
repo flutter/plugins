e3babd79c76841a5edfbcb27a9bd9a0a67a04a17
albertbow <albertbow@google.com>
2020-03-25 15:00:00 -0700

    Split native test cases aligning to the Dart test cases.

--- packages/e2e/ios/Classes/E2EIosTest.h
+++ packages/e2e/ios/Classes/E2EIosTest.h
@@ -1,22 +1,39 @@
 #import <Foundation/Foundation.h>
+#include <objc/runtime.h>
 
-@interface E2EIosTest : NSObject
+@interface E2EIosTestResult : NSObject
 
-- (BOOL)testE2E:(NSString **)testResult;
+@property(nonatomic, readonly) NSString *testCase;
+
+@property(nonatomic, readonly) NSString *result;
+
+- (instancetype)init NS_UNAVAILABLE;
+
+- (instancetype)initWithTestCase:(NSString *)testCase
+                      withResult:(NSString *)result;
 
 @end
 
-#define E2E_IOS_RUNNER(__test_class)                    \
-  @interface __test_class : XCTestCase                  \
-  @end                                                  \
-                                                        \
-  @implementation __test_class                          \
-                                                        \
-  -(void)testE2E {                                      \
-    NSString *testResult;                               \
-    E2EIosTest *e2eIosTest = [[E2EIosTest alloc] init]; \
-    BOOL testPass = [e2eIosTest testE2E:&testResult];   \
-    XCTAssertTrue(testPass, @"%@", testResult);         \
-  }                                                     \
-                                                        \
-  @end
+NSArray<NSInvocation *> *E2EMakeTestInvocations(Class xcTestCase);
+
+#define E2E_IOS_RUNNER(__test_class)                                           \
+@interface __test_class : XCTestCase                                           \
+@end                                                                           \
+\
+@implementation __test_class                                                   \
+\
++ (NSArray<NSInvocation *> *)testInvocations {                                 \
+  return E2EMakeTestInvocations([self class]);                                 \
+}                                                                              \
+\
+- (void)printTestResult {                                                      \
+  NSDictionary<NSString *, E2EIosTestResult *> *results =                      \
+      objc_getAssociatedObject([self class], @selector(printTestResult));      \
+  E2EIosTestResult *testResult = results[NSStringFromSelector(_cmd)];          \
+  if (![testResult.result isEqualToString:@"success"]) {                       \
+    XCTFail("'%@' failed with message: %@",                                    \
+            testResult.testCase, testResult.result);                           \
+  }                                                                            \
+}                                                                              \
+\
+@end
--- packages/e2e/ios/Classes/E2EIosTest.m
+++ packages/e2e/ios/Classes/E2EIosTest.m
@@ -1,43 +1,59 @@
 #import "E2EIosTest.h"
 #import "E2EPlugin.h"
 
-@implementation E2EIosTest
+@implementation E2EIosTestResult
 
-- (BOOL)testE2E:(NSString **)testResult {
+- (instancetype)initWithTestCase:(NSString *)testCase
+                      withResult:(NSString *)result {
+  self = [super init];
+  if (self) {
+    _testCase = testCase;
+    _result = result;
+  }
+  return self;
+}
+
+@end
+
+static SEL AddTestCase(Class xcTestCase, NSString *name) {
+  SEL methodSelector = NSSelectorFromString(name);
+  Method realMethod = class_getInstanceMethod(xcTestCase, @selector(printTestResult));
+  IMP realImp = method_getImplementation(realMethod);
+  struct objc_method_description *desc = method_getDescription(realMethod);
+  class_addMethod(xcTestCase, methodSelector, realImp, desc->types);
+  return methodSelector;
+}
+
+NSArray<NSInvocation *> *E2EMakeTestInvocations(Class xcTestCase) {
+  NSMutableDictionary<NSString *, E2EIosTestResult *> *results = [@{} mutableCopy];
   E2EPlugin *e2ePlugin = [E2EPlugin instance];
   UIViewController *rootViewController =
       [[[[UIApplication sharedApplication] delegate] window] rootViewController];
   if (![rootViewController isKindOfClass:[FlutterViewController class]]) {
     NSLog(@"expected FlutterViewController as rootViewController.");
-    return NO;
+    return @[];
   }
   FlutterViewController *flutterViewController = (FlutterViewController *)rootViewController;
   [e2ePlugin setupChannels:flutterViewController.engine.binaryMessenger];
   while (!e2ePlugin.testResults) {
     CFRunLoopRunInMode(kCFRunLoopDefaultMode, 1.f, NO);
   }
-  NSDictionary<NSString *, NSString *> *testResults = e2ePlugin.testResults;
-  NSMutableArray<NSString *> *passedTests = [NSMutableArray array];
-  NSMutableArray<NSString *> *failedTests = [NSMutableArray array];
-  NSLog(@"==================== Test Results =====================");
-  for (NSString *test in testResults.allKeys) {
-    NSString *result = testResults[test];
-    if ([result isEqualToString:@"success"]) {
-      NSLog(@"%@ passed.", test);
-      [passedTests addObject:test];
-    } else {
-      NSLog(@"%@ failed.", test);
-      [failedTests addObject:test];
-    }
+  NSCharacterSet *whiteSpaceSet = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+  NSMutableArray<NSInvocation *> *invocations = [@[] mutableCopy];
+  for (NSString *testCase in e2ePlugin.testResults.allKeys) {
+    NSString *formattedTestCase =
+        [[testCase stringByTrimmingCharactersInSet:whiteSpaceSet]
+            stringByReplacingOccurrencesOfString:@" " withString:@"_"];
+    results[formattedTestCase] =
+        [[E2EIosTestResult alloc] initWithTestCase:testCase
+                                        withResult:e2ePlugin.testResults[testCase]];
+    SEL testSelector = AddTestCase(xcTestCase, formattedTestCase);
+    NSMethodSignature *signature = [xcTestCase instanceMethodSignatureForSelector:testSelector];
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+    invocation.selector = testSelector;
+    [invocations addObject:invocation];
   }
-  NSLog(@"================== Test Results End ====================");
-  BOOL testPass = failedTests.count == 0;
-  if (!testPass && testResult) {
-    *testResult =
-        [NSString stringWithFormat:@"Detected failed E2E test(s) %@ among %@",
-                                   failedTests.description, testResults.allKeys.description];
-  }
-  return testPass;
+  objc_setAssociatedObject(xcTestCase, @selector(printTestResult),
+                           results, OBJC_ASSOCIATION_RETAIN);
+  return invocations;
 }
-
-@end
