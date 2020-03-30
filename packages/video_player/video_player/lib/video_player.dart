944bfa4231492f8c0eeba03f20ae54d1b04f15c2
hahai96 <hasonhai1996@gmail.com>
2020-03-30 14:46:20 +0700

    Update video_player.dart

--- packages/video_player/video_player/lib/video_player.dart
+++ packages/video_player/video_player/lib/video_player.dart
@@ -478,7 +478,7 @@ class _VideoAppLifeCycleObserver extends Object with WidgetsBindingObserver {
     switch (state) {
       case AppLifecycleState.paused:
         _wasPlayingBeforePause = _controller.value.isPlaying;
-        _controller.pause();
+        _controller.play();
         break;
       case AppLifecycleState.resumed:
         if (_wasPlayingBeforePause) {
