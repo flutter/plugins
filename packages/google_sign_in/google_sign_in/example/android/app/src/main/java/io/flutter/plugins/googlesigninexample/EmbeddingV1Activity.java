1c2371676103ada742034cf49f8b9f22a8d55c34
Chris Yang <cyang8954@gmail.com>
2020-04-01 11:14:12 -0700

    fix v1 activity

--- packages/google_sign_in/google_sign_in/example/android/app/src/main/java/io/flutter/plugins/googlesigninexample/EmbeddingV1Activity.java
+++ packages/google_sign_in/google_sign_in/example/android/app/src/main/java/io/flutter/plugins/googlesigninexample/EmbeddingV1Activity.java
@@ -7,10 +7,12 @@ package io.flutter.plugins.googlesigninexample;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.googlesignin.GoogleSignInPlugin;
+import io.flutter.view.FlutterMain;
 
 public class EmbeddingV1Activity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    FlutterMain.startInitialization(this);
     super.onCreate(savedInstanceState);
     GoogleSignInPlugin.registerWith(registrarFor("io.flutter.plugins.googlesignin"));
   }
