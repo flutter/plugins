38a84400fb7af8cde65a90f12338c0c8dcbb9b5b
David Iglesias Teixeira <ditman@gmail.com>
2020-04-13 20:11:13 -0700

    Add fallback on dart:html for other browsers.

--- packages/connectivity/experimental_connectivity_web/lib/experimental_connectivity_web.dart
+++ packages/connectivity/experimental_connectivity_web/lib/experimental_connectivity_web.dart
@@ -1,64 +1,25 @@
 import 'dart:async';
 
 import 'package:connectivity_platform_interface/connectivity_platform_interface.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-import 'package:js/js.dart';
 
-import 'src/generated/network_information_types.dart' as dom;
-import 'src/utils/connectivity_result.dart';
+import 'src/network_information_api_connectivity_plugin.dart';
+import 'src/dart_html_connectivity_plugin.dart';
 
 /// The web implementation of the ConnectivityPlatform of the Connectivity plugin.
 class ConnectivityPlugin extends ConnectivityPlatform {
   /// Factory method that initializes the connectivity plugin platform with an instance
   /// of the plugin for the web.
   static void registerWith(Registrar registrar) {
-    ConnectivityPlatform.instance = ConnectivityPlugin();
-  }
-
-  final dom.NetworkInformation _networkInformation;
-  final bool _networkInformationApiSupported;
-
-  /// The constructor of the plugin.
-  ConnectivityPlugin() : this.withConnection(dom.navigator?.connection);
-
-  /// Creates the plugin, with an override of the NetworkInformation object.
-  @visibleForTesting
-  ConnectivityPlugin.withConnection(dom.NetworkInformation connection)
-      : _networkInformationApiSupported = connection != null,
-        _networkInformation = connection;
-
-  /// Checks the connection status of the device.
-  @override
-  Future<ConnectivityResult> checkConnectivity() async {
-    if (!_networkInformationApiSupported) {
-      return null;
+    if (NetworkInformationApiConnectivityPlugin.isSupported()) {
+      ConnectivityPlatform.instance = NetworkInformationApiConnectivityPlugin();
+    } else {
+      ConnectivityPlatform.instance = DartHtmlConnectivityPlugin();
     }
-    return networkInformationToConnectivityResult(_networkInformation);
   }
 
-  Stream<ConnectivityResult> get _noopStream async* {
-    yield null;
-  }
-
-  StreamController<ConnectivityResult> _connectivityResult;
-
-  /// Returns a Stream of ConnectivityResults changes.
-  @override
-  Stream<ConnectivityResult> get onConnectivityChanged {
-    if (!_networkInformationApiSupported) {
-      return _noopStream;
-    }
-    if (_connectivityResult == null) {
-      _connectivityResult = StreamController<ConnectivityResult>();
-      _networkInformation.onchange = allowInterop((_) {
-        _connectivityResult
-            .add(networkInformationToConnectivityResult(_networkInformation));
-      });
-    }
-    return _connectivityResult.stream;
-  }
+  // The following are completely unsupported methods on the web platform.
 
   /// Obtains the wifi name (SSID) of the connected network
   @override
--- packages/connectivity/experimental_connectivity_web/test/lib/main.dart
+++ packages/connectivity/experimental_connectivity_web/test/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:e2e/e2e.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:connectivity_platform_interface/connectivity_platform_interface.dart';
-import 'package:experimental_connectivity_web/experimental_connectivity_web.dart';
+import 'package:experimental_connectivity_web/src/network_information_api_connectivity_plugin.dart';
 
 import 'src/connectivity_mocks.dart';
 
@@ -21,71 +21,54 @@ void main() {
           effectiveType: effectiveType,
           downlink: downlink,
           rtt: rtt);
-      ConnectivityPlugin plugin = ConnectivityPlugin.withConnection(connection);
+      NetworkInformationApiConnectivityPlugin plugin =
+          NetworkInformationApiConnectivityPlugin.withConnection(connection);
       expect(plugin.checkConnectivity(), completion(equals(expected)));
     }
 
-    group('in Chrome', () {
-      test('0 downlink and rtt -> none', () {
-        testCheckConnectivity(
-            effectiveType: '4g',
-            downlink: 0,
-            rtt: 0,
-            expected: ConnectivityResult.none);
-      });
-      test('slow-2g -> mobile', () {
-        testCheckConnectivity(
-            effectiveType: 'slow-2g', expected: ConnectivityResult.mobile);
-      });
-      test('2g -> mobile', () {
-        testCheckConnectivity(
-            effectiveType: '2g', expected: ConnectivityResult.mobile);
-      });
-      test('3g -> mobile', () {
-        testCheckConnectivity(
-            effectiveType: '3g', expected: ConnectivityResult.mobile);
-      });
-      test('4g -> wifi', () {
-        testCheckConnectivity(
-            effectiveType: '4g', expected: ConnectivityResult.wifi);
-      });
+    test('0 downlink and rtt -> none', () {
+      testCheckConnectivity(
+          effectiveType: '4g',
+          downlink: 0,
+          rtt: 0,
+          expected: ConnectivityResult.none);
     });
-
-    group('unsupported browsers', () {
-      test('null connection -> null', () {
-        ConnectivityPlugin plugin = ConnectivityPlugin.withConnection(null);
-        expect(plugin.checkConnectivity(), completion(null));
-      });
+    test('slow-2g -> mobile', () {
+      testCheckConnectivity(
+          effectiveType: 'slow-2g', expected: ConnectivityResult.mobile);
+    });
+    test('2g -> mobile', () {
+      testCheckConnectivity(
+          effectiveType: '2g', expected: ConnectivityResult.mobile);
+    });
+    test('3g -> mobile', () {
+      testCheckConnectivity(
+          effectiveType: '3g', expected: ConnectivityResult.mobile);
+    });
+    test('4g -> wifi', () {
+      testCheckConnectivity(
+          effectiveType: '4g', expected: ConnectivityResult.wifi);
     });
   });
 
   group('get onConnectivityChanged', () {
-    group('in Chrome', () {
-      test('puts change events in a Stream', () async {
-        MockNetworkInformation connection =
-            MockNetworkInformation(effectiveType: '4g', downlink: 10, rtt: 50);
-        ConnectivityPlugin plugin =
-            ConnectivityPlugin.withConnection(connection);
+    test('puts change events in a Stream', () async {
+      MockNetworkInformation connection =
+          MockNetworkInformation(effectiveType: '4g', downlink: 10, rtt: 50);
+      NetworkInformationApiConnectivityPlugin plugin =
+          NetworkInformationApiConnectivityPlugin.withConnection(connection);
 
-        Stream<ConnectivityResult> results = plugin.onConnectivityChanged;
+      Stream<ConnectivityResult> results = plugin.onConnectivityChanged;
 
-        // Fake a disconnect-reconnect
-        connection.mockChangeValue(downlink: 0, rtt: 0);
-        connection.mockChangeValue(downlink: 10, rtt: 50);
+      // Fake a disconnect-reconnect
+      connection.mockChangeValue(downlink: 0, rtt: 0);
+      connection.mockChangeValue(downlink: 10, rtt: 50);
 
-        // The stream of results is infinite, so we need to .take(2) for this test to complete.
-        expect(
-            results.take(2).toList(),
-            completion(
-                equals([ConnectivityResult.none, ConnectivityResult.wifi])));
-      });
-    });
-
-    group('unsupported browsers', () {
-      test('null connection -> null', () {
-        ConnectivityPlugin plugin = ConnectivityPlugin.withConnection(null);
-        expect(plugin.onConnectivityChanged.last, completion(null));
-      });
+      // The stream of results is infinite, so we need to .take(2) for this test to complete.
+      expect(
+          results.take(2).toList(),
+          completion(
+              equals([ConnectivityResult.none, ConnectivityResult.wifi])));
     });
   });
 }
