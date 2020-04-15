4ae292668479de7bf8122fb60bd5a0fa37840909
Francisco Magdaleno <franciscojma86@gmail.com>
2020-04-15 14:09:52 -0700

    License

--- packages/shared_preferences/shared_preferences_windows/example/test_driver/shared_preferences_e2e.dart
+++ packages/shared_preferences/shared_preferences_windows/example/test_driver/shared_preferences_e2e.dart
@@ -1,3 +1,7 @@
+// Copyright 2017, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:async';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
