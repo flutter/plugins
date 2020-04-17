c495513ec6286abe690b1a3f8304455dbe88018c
Jenn Magder <magder@google.com>
2020-04-17 16:36:45 -0700

    Add shard to lint task

--- script/incremental_build.sh
+++ script/incremental_build.sh
@@ -29,21 +29,21 @@ elif [[ "${ACTIONS[@]}" == "analyze" ]]; then
 fi
 
 BRANCH_NAME="${BRANCH_NAME:-"$(git rev-parse --abbrev-ref HEAD)"}"
-if [[ "${BRANCH_NAME}" == "master" ]]; then
+# if [[ "${BRANCH_NAME}" == "master" ]]; then
   echo "Running for all packages"
   (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" $PLUGIN_SHARDING)
-else
-  # Sets CHANGED_PACKAGES
-  check_changed_packages
+# else
+#   # Sets CHANGED_PACKAGES
+#   check_changed_packages
 
-  if [[ "$CHANGED_PACKAGES" == "" ]]; then
-    echo "No changes detected in packages."
-    echo "Running for all packages"
-    (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" $PLUGIN_SHARDING)
-  else
-    echo running "${ACTIONS[@]}"
-    (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" --plugins="$CHANGED_PACKAGES" $PLUGIN_SHARDING)
-    echo "Running version check for changed packages"
-    (cd "$REPO_DIR" && pub global run flutter_plugin_tools version-check --base_sha="$(get_branch_base_sha)")
-  fi
+#   if [[ "$CHANGED_PACKAGES" == "" ]]; then
+#     echo "No changes detected in packages."
+#     echo "Running for all packages"
+#     (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" $PLUGIN_SHARDING)
+#   else
+#     echo running "${ACTIONS[@]}"
+#     (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" --plugins="$CHANGED_PACKAGES" $PLUGIN_SHARDING)
+#     echo "Running version check for changed packages"
+#     (cd "$REPO_DIR" && pub global run flutter_plugin_tools version-check --base_sha="$(get_branch_base_sha)")
+#   fi
 fi
