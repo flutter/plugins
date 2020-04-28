d7898d565ca977aced12ad673a3ea4d97fa3b845
Aswin.K.V <aswin.k@bounceshare.com>
2020-04-28 13:24:14 +0530

    moved getDistance to platform_interface

--- packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/platform_interface/google_maps_flutter_platform.dart
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/platform_interface/google_maps_flutter_platform.dart
@@ -239,6 +239,17 @@ abstract class GoogleMapsFlutterPlatform extends PlatformInterface {
     throw UnimplementedError('getZoomLevel() has not been implemented.');
   }
 
+  /// Returns [double] corresponding to the distance in meters of the [LatLng] in the current map view.
+  ///
+  /// Return NaN (Not-a-Number) if unable to get distance in meters.
+  /// This is only accurate for small Earth distances.
+  Future<double> getDistance(double meters, LatLng latLng) async {
+    final double distance = await channel.invokeMethod<double>(
+        'map#getDistance',
+        <String, dynamic>{"meter": meters, "location": latLng._toJson()});
+    return distance;
+  }
+
   /// Returns the image bytes of the map
   Future<Uint8List> takeSnapshot({
     @required int mapId,
