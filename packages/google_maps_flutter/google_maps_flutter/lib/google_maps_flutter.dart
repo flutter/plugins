2a8837d16d5e0c198e0a13831bf7e9180f90997c
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2020-05-03 10:36:06 +0300

    cleanup

--- packages/google_maps_flutter/google_maps_flutter/lib/google_maps_flutter.dart
+++ packages/google_maps_flutter/google_maps_flutter/lib/google_maps_flutter.dart
@@ -12,8 +12,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:collection/collection.dart';
-
 import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 import 'package:google_maps_flutter_platform_interface/src/method_channel/method_channel_google_maps_flutter.dart';
 
@@ -46,4 +44,5 @@ export 'package:google_maps_flutter_platform_interface/google_maps_flutter_platf
         ScreenCoordinate;
 
 part 'src/controller.dart';
+
 part 'src/google_map.dart';
