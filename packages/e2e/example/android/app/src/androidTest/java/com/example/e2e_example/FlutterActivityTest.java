691cd9bdebd8010603d10918d391e72c7a75816d
xster <xster@google.com>
2020-05-21 12:23:50 -0700

    Clean up plugins post v2 Android embedding (#2778)

--- packages/e2e/example/android/app/src/androidTest/java/com/example/e2e_example/FlutterActivityTest.java
+++ packages/e2e/example/android/app/src/androidTest/java/com/example/e2e_example/FlutterActivityTest.java
@@ -2,12 +2,13 @@ package com.example.e2e_example;
 
 import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.e2e.FlutterTestRunner;
+import io.flutter.embedding.android.FlutterActivity;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 @RunWith(FlutterTestRunner.class)
-public class MainActivityTest {
+public class FlutterActivityTest {
   @Rule
-  public ActivityTestRule<MainActivity> rule =
-      new ActivityTestRule<>(MainActivity.class, true, false);
+  public ActivityTestRule<FlutterActivity> rule =
+      new ActivityTestRule<>(FlutterActivity.class, true, false);
 }
