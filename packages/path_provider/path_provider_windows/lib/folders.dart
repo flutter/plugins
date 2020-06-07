383ef8fcbc1b9e030d10f1db1661f23ab8360d0c
Tim Sneath <timsneath@google.com>
2020-06-06 18:41:31 -0700

    Add missing copyright comment

--- packages/path_provider/path_provider_windows/lib/folders.dart
+++ packages/path_provider/path_provider_windows/lib/folders.dart
@@ -1,3 +1,7 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:win32/win32.dart';
 
 /// A class containing the GUID references for each of the documented Windows
