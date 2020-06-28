f890c46f0c55c276869b2db9c7ba0b76ef7a997a
Gonçalo Palma <solid.goncalo@gmail.com>
2020-06-28 17:10:37 +0100

    Requested Changes

--- packages/url_launcher/url_launcher_web/lib/url_launcher_web.dart
+++ packages/url_launcher/url_launcher_web/lib/url_launcher_web.dart
@@ -7,8 +7,10 @@ import 'package:url_launcher_platform_interface/url_launcher_platform_interface.
 
 import 'package:platform_detect/platform_detect.dart' show browser;
 
-const _mailtoScheme = 'mailto';
-const _phoneScheme = 'tel';
+const _safariTargetTopSchemes = {
+  'mailto',
+  'tel',
+};
 
 /// The web implementation of [UrlLauncherPlatform].
 ///
@@ -17,7 +19,10 @@ class UrlLauncherPlugin extends UrlLauncherPlatform {
   html.Window _window;
 
   // The set of schemes that can be handled by the plugin
-  static final _supportedSchemes = {'http', 'https', _mailtoScheme, _phoneScheme};
+  static final _supportedSchemes = {
+    'http',
+    'https',
+  }.union(_safariTargetTopSchemes);
 
   /// A constructor that allows tests to override the window object used by the plugin.
   UrlLauncherPlugin({@visibleForTesting html.Window window})
@@ -30,7 +35,8 @@ class UrlLauncherPlugin extends UrlLauncherPlatform {
 
   String _getUrlScheme(String url) => Uri.tryParse(url)?.scheme;
 
-  bool _isMailtoScheme(String url) => _getUrlScheme(url) == _mailtoScheme;
+  bool _isSafariTargetTopScheme(String url) =>
+      _safariTargetTopSchemes.contains(_getUrlScheme(url));
 
   /// Opens the given [url] in a new window.
   ///
@@ -39,7 +45,8 @@ class UrlLauncherPlugin extends UrlLauncherPlatform {
   html.WindowBase openNewWindow(String url) {
     // We need to open mailto urls on the _top window context on safari browsers.
     // See https://github.com/flutter/flutter/issues/51461 for reference.
-    final target = browser.isSafari && _isMailtoScheme(url) ? '_top' : '';
+    final target =
+        browser.isSafari && _isSafariTargetTopScheme(url) ? '_top' : '';
     return _window.open(url, target);
   }
 
