de1bdccda1ec65ce16db4f19b5b5b2a3a61d6b7a
Stuart Morgan <stuartmorgan@google.com>
2020-07-06 19:28:24 -0400

    Fix compilation

--- packages/url_launcher/url_launcher_linux/linux/url_launcher_plugin.cc
+++ packages/url_launcher/url_launcher_linux/linux/url_launcher_plugin.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "include/url_launcher/url_launcher_plugin.h"
+#include "include/url_launcher_linux/url_launcher_plugin.h"
 
 #include <flutter_linux/flutter_linux.h>
 #include <gtk/gtk.h>
