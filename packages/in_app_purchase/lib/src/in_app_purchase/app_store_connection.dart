b863b7e043afda301e3e5e1e3ccf9cd1e534cecd
Yijie Xu <yijiexu@google.com>
2020-07-21 12:13:21 -0700

    Handle receipt exceptions

--- packages/in_app_purchase/lib/src/in_app_purchase/app_store_connection.dart
+++ packages/in_app_purchase/lib/src/in_app_purchase/app_store_connection.dart
@@ -82,7 +82,7 @@ class AppStoreConnection implements InAppPurchaseConnection {
     List<PurchaseDetails> pastPurchases = [];
 
     try {
-      String receiptData = await _observer.getReceiptData();
+      String receiptData = await getReceiptData();
       final List<SKPaymentTransactionWrapper> restoredTransactions =
           await _observer.getRestoredTransactions(
               queue: _skPaymentQueueWrapper,
@@ -124,7 +124,7 @@ class AppStoreConnection implements InAppPurchaseConnection {
   @override
   Future<PurchaseVerificationData> refreshPurchaseVerificationData() async {
     await SKRequestMaker().startRefreshReceiptRequest();
-    String receipt = await SKReceiptManager.retrieveReceiptData();
+    String receipt = await getReceiptData();
     return PurchaseVerificationData(
         localVerificationData: receipt,
         serverVerificationData: receipt,
