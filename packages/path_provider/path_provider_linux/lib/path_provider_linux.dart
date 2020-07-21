621bafe1fdb3b51c3c7bf8e16ce09c5e323d54ba
Robert Ancell <robert.ancell@canonical.com>
2020-07-21 16:58:32 +1200

    [path_provider] Use the application ID in the application support path (#2845)

--- packages/path_provider/path_provider_linux/lib/path_provider_linux.dart
+++ packages/path_provider/path_provider_linux/lib/path_provider_linux.dart
@@ -3,10 +3,20 @@
 // found in the LICENSE file.
 import 'dart:io';
 import 'dart:async';
+import 'dart:ffi';
+import 'package:ffi/ffi.dart';
 
 import 'package:xdg_directories/xdg_directories.dart' as xdg;
 import 'package:path/path.dart' as path;
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+
+// GApplication* g_application_get_default();
+typedef g_application_get_default_c = IntPtr Function();
+typedef g_application_get_default_dart = int Function();
+
+// const gchar* g_application_get_application_id(GApplication* application);
+typedef g_application_get_application_id_c = Pointer<Utf8> Function(IntPtr);
+typedef g_application_get_application_id_dart = Pointer<Utf8> Function(int);
 
 /// The linux implementation of [PathProviderPlatform]
 ///
@@ -22,11 +32,43 @@ class PathProviderLinux extends PathProviderPlatform {
     return Future.value("/tmp");
   }
 
+  // Gets the application ID set in GApplication.
+  String _getApplicationId() {
+    DynamicLibrary gio;
+    try {
+      gio = DynamicLibrary.open('libgio-2.0.so');
+    } on ArgumentError {
+      return null;
+    }
+    var g_application_get_default = gio.lookupFunction<
+        g_application_get_default_c,
+        g_application_get_default_dart>('g_application_get_default');
+    var app = g_application_get_default();
+    if (app == 0) return null;
+
+    var g_application_get_application_id = gio.lookupFunction<
+            g_application_get_application_id_c,
+            g_application_get_application_id_dart>(
+        'g_application_get_application_id');
+    var app_id = g_application_get_application_id(app);
+    if (app_id == null) return null;
+
+    return Utf8.fromUtf8(app_id);
+  }
+
+  // Gets the unique ID for this application.
+  Future<String> _getId() async {
+    var appId = _getApplicationId();
+    if (appId != null) return appId;
+
+    // Fall back to using the executable name.
+    return path.basenameWithoutExtension(
+        await File('/proc/self/exe').resolveSymbolicLinks());
+  }
+
   @override
   Future<String> getApplicationSupportPath() async {
-    final processName = path.basenameWithoutExtension(
-        await File('/proc/self/exe').resolveSymbolicLinks());
-    final directory = Directory(path.join(xdg.dataHome.path, processName));
+    final directory = Directory(path.join(xdg.dataHome.path, await _getId()));
     // Creating the directory if it doesn't exist, because mobile implementations assume the directory exists
     if (!await directory.exists()) {
       await directory.create(recursive: true);
