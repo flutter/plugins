3cab622b54f6265d41542b6274eb4ad86952ab32
Dan Field <dnfield@google.com>
2020-07-21 20:32:25 -0700

    fix up for latest driver style test

--- packages/e2e/lib/e2e.dart
+++ packages/e2e/lib/e2e.dart
@@ -37,6 +37,7 @@ class E2EWidgetsFlutterBinding extends LiveTestWidgetsFlutterBinding {
       } on MissingPluginException {
         print('Warning: E2E test plugin was not detected.');
       }
+      print(_results);
       if (!_allTestsPassed.isCompleted) _allTestsPassed.complete(true);
     });
   }
