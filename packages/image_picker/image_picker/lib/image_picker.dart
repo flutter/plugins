89e8610b475549d627224b7fc96479f32f6643d9
jarrod courtland colburn <jcourtlandcolburn@gmail.com>
2020-07-26 16:51:00 -0400

    deprecate imagePicker.retrieveLostData

--- packages/image_picker/image_picker/lib/image_picker.dart
+++ packages/image_picker/image_picker/lib/image_picker.dart
@@ -178,6 +178,7 @@ class ImagePicker {
   /// See also:
   /// * [LostDataResponse], for what's included in the response.
   /// * [Android Activity Lifecycle](https://developer.android.com/reference/android/app/Activity.html), for more information on MainActivity destruction.
+  @Deprecated('Use imagePicker.getLostData() method instead.')
   static Future<LostDataResponse> retrieveLostData() {
     return platform.retrieveLostDataAsDartIoFile();
   }
