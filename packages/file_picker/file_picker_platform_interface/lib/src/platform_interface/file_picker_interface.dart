f1383ea12342bd6b28b8e1de90de76541f1c1d74
Jason Panelli <jasonpanelli@google.com>
2020-08-04 15:22:40 -0700

    Add XPath class and New API to interface

--- packages/file_picker/file_picker_platform_interface/lib/src/platform_interface/file_picker_interface.dart
+++ packages/file_picker/file_picker_platform_interface/lib/src/platform_interface/file_picker_interface.dart
@@ -10,6 +10,20 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 import '../method_channel/method_channel_file_picker.dart';
 
+
+// TODO: move to its own file
+/// Cross platform path class
+class XPath {
+  /// XPath constructor
+  XPath(this._path, {int modified, int created}):
+      _modified = modified,
+      _created = created;
+
+  final String _path;
+  final int _modified;
+  final int _created;
+}
+
 /// The interface that implementations of file_picker must implement.
 ///
 /// Platform implementations should extend this class rather than implement it as `file_picker`
@@ -36,6 +50,24 @@ abstract class FilePickerPlatform extends PlatformInterface {
     PlatformInterface.verifyToken(instance, _token);
     _instance = instance;
   }
+
+  /// Open file dialog for loading files and return a file path
+  XPath getReadPath() {
+    throw UnimplementedError('getReadPath() has not been implemented.');
+  }
+
+  /// Open file dialog for loading files and return a list of file paths
+  List<XPath> getReadPaths() {
+    throw UnimplementedError('getReadPaths() has not been implemented.');
+  }
+
+  /// Open file dialog for saving files and return a file path at which to save
+  XPath getSavePath() {
+    throw UnimplementedError('loadFile() has not been implemented.');
+  }
+
+
+  /// OLD API:
 
   /// Load file from user's computer and return it as an XFile
   Future<List<XFile>> loadFile({List<FileTypeFilterGroup> acceptedTypes}) {
