9fcb61d78910f8b3028762f38f4ee8800cfb5044
David Iglesias Teixeira <ditman@gmail.com>
2020-08-05 12:50:36 -0700

    Check if the size has actually been passed from the plugin.

--- packages/google_maps_flutter/google_maps_flutter_web/lib/src/convert.dart
+++ packages/google_maps_flutter/google_maps_flutter_web/lib/src/convert.dart
@@ -342,20 +342,20 @@ gmaps.MarkerOptions _markerOptionsFromMarker(
   Marker marker,
   gmaps.Marker currentMarker,
 ) {
-  final iconConfig = marker.icon.toJson();
+  final iconConfig = marker.icon.toJson() as List;
   gmaps.Icon icon;
 
   if (iconConfig[0] == 'fromAssetImage') {
     // iconConfig[2] contains the DPIs of the screen, but that information is
     // already encoded in the iconConfig[1]
 
-    // iconConfig[3] contain the [width, height] of the image, if available
     icon = gmaps.Icon()
       // TODO: Move the comment below to analysis-options.yaml
       // ignore:undefined_prefixed_name
       ..url = ui.webOnlyAssetManager.getAssetUrl(iconConfig[1]);
 
-    if (iconConfig[3] != null) {
+    // iconConfig[3] may contain the [width, height] of the image, if passed!
+    if (iconConfig.length >= 4 && iconConfig[3] != null) {
       final size = gmaps.Size(iconConfig[3][0], iconConfig[3][1]);
       icon
         ..size = size
