6e54f0341417d4af0d85bb8570df0b1b867389e1
Karzan Kamal <kurdappdev@gmail.com>
2020-08-09 13:19:43 +0300

    playerItemWithAsset
    AVURLAssetPreferPreciseDurationAndTimingKey

--- packages/video_player/video_player/ios/Classes/FLTVideoPlayerPlugin.m
+++ packages/video_player/video_player/ios/Classes/FLTVideoPlayerPlugin.m
@@ -163,7 +163,8 @@ static inline CGFloat radiansToDegrees(CGFloat radians) {
 }
 
 - (instancetype)initWithURL:(NSURL*)url frameUpdater:(FLTFrameUpdater*)frameUpdater {
-  AVPlayerItem* item = [AVPlayerItem playerItemWithURL:url];
+  AVAsset *asset = [AVURLAsset URLAssetWithURL:wav options:@{AVURLAssetPreferPreciseDurationAndTimingKey: @YES}];
+  AVPlayerItem* item = [AVPlayerItem playerItemWithAsset:asset];
   return [self initWithPlayerItem:item frameUpdater:frameUpdater];
 }
 
