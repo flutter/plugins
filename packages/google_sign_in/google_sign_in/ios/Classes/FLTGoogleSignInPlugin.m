87bc053559853b4e73ee50c3538dec731ba48c1c
Ryan Heise <ryan@ryanheise.com>
2020-08-11 01:38:35 +1000

    Move hostedDomain null check into objective c

--- packages/google_sign_in/google_sign_in/ios/Classes/FLTGoogleSignInPlugin.m
+++ packages/google_sign_in/google_sign_in/ios/Classes/FLTGoogleSignInPlugin.m
@@ -80,7 +80,8 @@ static FlutterError *getFlutterError(NSError *error) {
         [GIDSignIn sharedInstance].clientID = plist[kClientIdKey];
         [GIDSignIn sharedInstance].serverClientID = plist[kServerClientIdKey];
         [GIDSignIn sharedInstance].scopes = call.arguments[@"scopes"];
-        [GIDSignIn sharedInstance].hostedDomain = call.arguments[@"hostedDomain"];
+	NSString *hostedDomain = call.arguments[@"hostedDomain"];
+	[GIDSignIn sharedInstance].hostedDomain = hostedDomain != (id)[NSNull null] ? hostedDomain : @"";
         result(nil);
       } else {
         result([FlutterError errorWithCode:@"missing-config"
