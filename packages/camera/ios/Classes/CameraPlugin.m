a8710a2690138439deed0d70e3019af526225ef2
Dan Field <dnfield@google.com>
2020-08-12 17:05:09 -0700

    weakSelf

--- packages/camera/ios/Classes/CameraPlugin.m
+++ packages/camera/ios/Classes/CameraPlugin.m
@@ -837,8 +837,9 @@ FourCharCode const videoFormat = kCVPixelFormatType_32BGRA;
       }
       int64_t textureId = [_registry registerTexture:cam];
       _camera = cam;
+      __weak CameraPlugin* weakSelf = self;
       cam.onFrameAvailable = ^{
-        [self.registry textureFrameAvailable:textureId];
+        [weakSelf.registry textureFrameAvailable:textureId];
       };
       FlutterEventChannel *eventChannel = [FlutterEventChannel
           eventChannelWithName:[NSString
