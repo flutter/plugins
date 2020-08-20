bcdc562b8746be24e0a48ddc0f203d1bf9b042c9
Jason Panelli <jasonpanelli@google.com>
2020-08-20 14:09:55 -0700

    Update Example App - Separate load and save

--- packages/file_selector/file_selector/example/lib/main.dart
+++ packages/file_selector/file_selector/example/lib/main.dart
@@ -12,31 +12,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'File Selector Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'File Selector Demo Home Page'),
+      routes: {
+        '/save' : (context) => SaveTest(title: "Save Example"),
+        '/load' : (context) => LoadTest(title: "Load Example"),
+      },
     );
   }
 }
 
-/// Home Page of the application
-class MyHomePage extends StatefulWidget {
-  
-  /// Constructor for MyHomePage
-  MyHomePage({Key key, this.title}) : super(key: key);
+
+class SaveTest extends StatefulWidget {
+  SaveTest({Key key, this.title}) : super(key: key);
 
   /// Title of Home Page
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _SaveTestState createState() => _SaveTestState();
 }
 
-/// State of Home Page
-class _MyHomePageState extends State<MyHomePage> {
+class _SaveTestState extends State<SaveTest> {
   final TextEditingController _fileController = TextEditingController();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _extensionController = TextEditingController();
@@ -61,29 +62,6 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     new_file.saveTo('');
-  }
-
-  void _loadFile() async {
-    XFile file;
-    if (_extensionController.text.isNotEmpty) {
-      List<XTypeGroup> typeGroups = List();
-
-      List<XType> types = List();
-      _extensionController.text.split(',').forEach((type) => types.add(XType(extension: type)));
-
-      typeGroups.add(XTypeGroup(label: 'Example Files', fileTypes: types));
-      file = await loadFile(acceptedTypeGroups: typeGroups);
-    } else {
-      file = await loadFile();
-    }
-
-    String text = await file.readAsString();
-
-    _fileController.text = text;
-
-    if (file.name.isNotEmpty) {
-      _nameController.text = file.name;
-    }
   }
 
   @override
@@ -124,6 +102,86 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Text('Press to save file'),
               onPressed: () => { _saveFile() },
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoadTest extends StatefulWidget {
+  LoadTest({Key key, this.title}) : super(key: key);
+
+  /// Title of Home Page
+  final String title;
+
+
+  @override
+  _LoadTestState createState() => _LoadTestState();
+}
+
+class _LoadTestState extends State<LoadTest> {
+  final TextEditingController _fileController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _extensionController = TextEditingController();
+
+  void _loadFile() async {
+    XFile file;
+    if (_extensionController.text.isNotEmpty) {
+      List<XTypeGroup> typeGroups = List();
+
+      List<XType> types = List();
+      _extensionController.text.split(',').forEach((type) => types.add(XType(extension: type)));
+
+      typeGroups.add(XTypeGroup(label: 'Example Files', fileTypes: types));
+      file = await loadFile(acceptedTypeGroups: typeGroups);
+    } else {
+      file = await loadFile();
+    }
+
+    String text = await file.readAsString();
+
+    _fileController.text = text;
+
+    if (file.name.isNotEmpty) {
+      _nameController.text = file.name;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 300,
+              child: TextField(
+                enabled: false,
+                minLines: 1,
+                maxLines: 12,
+                controller: _nameController,
+                decoration: InputDecoration(
+                  hintText: 'File Name Will Appear Here',
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              child: TextField(
+                enabled: false,
+                minLines: 1,
+                maxLines: 12,
+                controller: _fileController,
+                decoration: InputDecoration(
+                  hintText: 'File Contents Will Appear Here',
+                ),
+              ),
+            ),
             Container(
               width: 300,
               child: TextField(
@@ -142,4 +200,50 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+}
+
+
+
+/// Home Page of the application
+class MyHomePage extends StatefulWidget {
+  
+  /// Constructor for MyHomePage
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  /// Title of Home Page
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+/// State of Home Page
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            SizedBox(height: 10),
+            RaisedButton(
+              child: Text('Press to try saving a file'),
+              onPressed: () => Navigator.pushNamed(context, '/save'),
+            ),
+            RaisedButton(
+              child: Text('Press to try loading a file'),
+              onPressed: () => Navigator.pushNamed(context, '/load'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
 }
