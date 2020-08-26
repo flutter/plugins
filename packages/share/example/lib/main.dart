271cf5e24e4619994966851a8fe36af663fea447
Patrick Boos <pboos@carecon.com>
2020-08-26 08:31:34 +0200

    [share] example: wrap within SingleChildScrollView

--- packages/share/example/lib/main.dart
+++ packages/share/example/lib/main.dart
@@ -32,85 +32,87 @@ class DemoAppState extends State<DemoApp> {
           appBar: AppBar(
             title: const Text('Share Plugin Demo'),
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Share text:',
-                    hintText: 'Enter some text and/or link to share',
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextField(
+                    decoration: const InputDecoration(
+                      labelText: 'Share text:',
+                      hintText: 'Enter some text and/or link to share',
+                    ),
+                    maxLines: 2,
+                    onChanged: (String value) => setState(() {
+                      text = value;
+                    }),
                   ),
-                  maxLines: 2,
-                  onChanged: (String value) => setState(() {
-                    text = value;
-                  }),
-                ),
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Share subject:',
-                    hintText: 'Enter subject to share (optional)',
+                  TextField(
+                    decoration: const InputDecoration(
+                      labelText: 'Share subject:',
+                      hintText: 'Enter subject to share (optional)',
+                    ),
+                    maxLines: 2,
+                    onChanged: (String value) => setState(() {
+                      subject = value;
+                    }),
                   ),
-                  maxLines: 2,
-                  onChanged: (String value) => setState(() {
-                    subject = value;
-                  }),
-                ),
-                const Padding(padding: EdgeInsets.only(top: 12.0)),
-                _buildImagePreviews(),
-                ListTile(
-                  leading: Icon(Icons.add),
-                  title: Text("Add image"),
-                  onTap: () async {
-                    final imagePicker = ImagePicker();
-                    final pickedFile = await imagePicker.getImage(
-                      source: ImageSource.gallery,
-                    );
-                    if (pickedFile != null) {
-                      setState(() {
-                        imagePaths.add(pickedFile.path);
-                      });
-                    }
-                  },
-                ),
-                const Padding(padding: EdgeInsets.only(top: 12.0)),
-                Builder(
-                  builder: (BuildContext context) {
-                    return RaisedButton(
-                      child: const Text('Share'),
-                      onPressed: text.isEmpty
-                          ? null
-                          : () async {
-                              // A builder is used to retrieve the context immediately
-                              // surrounding the RaisedButton.
-                              //
-                              // The context's `findRenderObject` returns the first
-                              // RenderObject in its descendent tree when it's not
-                              // a RenderObjectWidget. The RaisedButton's RenderObject
-                              // has its position and size after it's built.
-                              final RenderBox box = context.findRenderObject();
+                  const Padding(padding: EdgeInsets.only(top: 12.0)),
+                  _buildImagePreviews(),
+                  ListTile(
+                    leading: Icon(Icons.add),
+                    title: Text("Add image"),
+                    onTap: () async {
+                      final imagePicker = ImagePicker();
+                      final pickedFile = await imagePicker.getImage(
+                        source: ImageSource.gallery,
+                      );
+                      if (pickedFile != null) {
+                        setState(() {
+                          imagePaths.add(pickedFile.path);
+                        });
+                      }
+                    },
+                  ),
+                  const Padding(padding: EdgeInsets.only(top: 12.0)),
+                  Builder(
+                    builder: (BuildContext context) {
+                      return RaisedButton(
+                        child: const Text('Share'),
+                        onPressed: text.isEmpty
+                            ? null
+                            : () async {
+                                // A builder is used to retrieve the context immediately
+                                // surrounding the RaisedButton.
+                                //
+                                // The context's `findRenderObject` returns the first
+                                // RenderObject in its descendent tree when it's not
+                                // a RenderObjectWidget. The RaisedButton's RenderObject
+                                // has its position and size after it's built.
+                                final RenderBox box =
+                                    context.findRenderObject();
 
-                              if (imagePaths.isNotEmpty) {
-                                await Share.shareFiles(imagePaths,
-                                    text: text,
-                                    subject: subject,
-                                    sharePositionOrigin:
-                                        box.localToGlobal(Offset.zero) &
-                                            box.size);
-                              } else {
-                                await Share.share(text,
-                                    subject: subject,
-                                    sharePositionOrigin:
-                                        box.localToGlobal(Offset.zero) &
-                                            box.size);
-                              }
-                            },
-                    );
-                  },
-                ),
-              ],
+                                if (imagePaths.isNotEmpty) {
+                                  await Share.shareFiles(imagePaths,
+                                      text: text,
+                                      subject: subject,
+                                      sharePositionOrigin:
+                                          box.localToGlobal(Offset.zero) &
+                                              box.size);
+                                } else {
+                                  await Share.share(text,
+                                      subject: subject,
+                                      sharePositionOrigin:
+                                          box.localToGlobal(Offset.zero) &
+                                              box.size);
+                                }
+                              },
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           )),
     );
