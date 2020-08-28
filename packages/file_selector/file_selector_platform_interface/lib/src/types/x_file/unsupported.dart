7677ccdc34142976f4811b310d187c0139ab533f
Jason Panelli <jasonpanelli@google.com>
2020-08-28 13:28:20 -0700

    Update unsupported.dart

--- packages/file_selector/file_selector_platform_interface/lib/src/types/x_file/unsupported.dart
+++ packages/file_selector/file_selector_platform_interface/lib/src/types/x_file/unsupported.dart
@@ -1,4 +1,5 @@
 import 'dart:typed_data';
+import 'package:meta/meta.dart';
 
 import './base.dart';
 
@@ -15,23 +16,38 @@ class XFile extends XFileBase {
   /// `path` of the file doesn't match what the user sees when selecting it
   /// (like in web)
   XFile(
-    String path, {
-    String name,
-    int length,
-    Uint8List bytes,
-  }) : super(path) {
+      String path, {
+        String mimeType,
+        String name,
+        int length,
+        Uint8List bytes,
+        DateTime lastModified,
+        @visibleForTesting XFileTestOverrides overrides,
+      }) : super(path) {
     throw UnimplementedError(
         'XFile is not available in your current platform.');
   }
 
   /// Construct a XFile object from its data
   XFile.fromData(
-      Uint8List data, {
-        String path,
+      Uint8List bytes, {
+        String mimeType,
         String name,
         int length,
+        DateTime lastModified,
+        String path,
+        @visibleForTesting XFileTestOverrides overrides,
       }) : super(path) {
     throw UnimplementedError(
         'XFile is not available in your current platform.');
   }
+}
+
+/// Overrides some functions of XFile for testing purposes
+@visibleForTesting class XFileTestOverrides {
+  /// For overriding the creation of the file input element.
+  dynamic Function(String href, String suggestedName) createAnchorElement;
+
+  /// Default constructor for overrides
+  XFileTestOverrides({this.createAnchorElement});
 }
