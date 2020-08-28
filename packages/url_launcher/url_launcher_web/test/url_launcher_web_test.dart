ad6a8c1af863f0fdfdf5f5a5c0fe07066b38cb90
balvinderz <balvindersi2@gmail.com>
2020-08-29 01:21:03 +0530

    added webOnlyWindowName parameter to launch()

--- packages/url_launcher/url_launcher_web/test/url_launcher_web_test.dart
+++ packages/url_launcher/url_launcher_web/test/url_launcher_web_test.dart
@@ -145,6 +145,19 @@ void main() {
 
         verify(mockWindow.open('sms:+19725551212?body=hello%20there', ''));
       });
+      test('setting oOnlyLinkTarget as _self opens the url in the same tab',
+          () {
+        plugin.openNewWindow("https://www.google.com",
+            webOnlyWindowName: "_self");
+        verify(mockWindow.open('https://www.google.com', '_self'));
+      });
+
+      test('setting webOnlyLinkTarget as _blank opens the url in a new tab',
+          () {
+        plugin.openNewWindow("https://www.google.com",
+            webOnlyWindowName: "_blank");
+        verify(mockWindow.open('https://www.google.com', '_blank'));
+      });
 
       group('Safari', () {
         setUp(() {
@@ -180,6 +193,13 @@ void main() {
 
           verify(
               mockWindow.open('sms:+19725551212?body=hello%20there', '_top'));
+        });
+        test(
+            'mailto urls should use _blank if webOnlyWindowName is set as _blank',
+            () {
+          plugin.openNewWindow("mailto:name@mydomain.com",
+              webOnlyWindowName: "_blank");
+          verify(mockWindow.open("mailto:name@mydomain.com", "_blank"));
         });
       });
     });
