0c1d3b3f7af5903b1ceaed84e13c39ba64b423b9
nturgut <nurhan@google.com>
2020-08-31 12:11:15 -0700

    remove example screenshot saving since it's failing on android. examples in integration_test package is common

--- packages/integration_test/example/test_driver/example_integration_extended_test.dart
+++ packages/integration_test/example/test_driver/example_integration_extended_test.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:integration_test/integration_test_driver_extended.dart';
@@ -9,22 +8,7 @@ Future<void> main() async {
   await integrationDriver(
     driver: driver,
     onScreenshot: (String screenshotName, List<int> screenshotBytes) async {
-      // The screenshot is saved as png. Later it can be used for golden testing
-      // with library of choice, such as skia_client.dart.
-      final String screenshotPath =
-          await _saveScreenshot(screenshotBytes, screenshotName);
-      print('INFO: screenshot recorded $screenshotPath');
       return true;
     },
   );
-}
-
-/// Example method for saving the screenshot taken by the Webdriver to a `png`
-/// file.
-Future<String> _saveScreenshot(List<int> screenshot, String path) async {
-  final File file = File('$path.png');
-  if (!file.existsSync()) {
-    await file.writeAsBytes(screenshot);
-  }
-  return '$path.png';
 }
