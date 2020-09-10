7a2122217ae4c1551b7741d265bff93a4727bcdf
Jonas Simoes <Jonas.AlvesSimoes@engelvoelkers.com>
2020-09-10 11:42:36 +0200

    Fix android

--- packages/google_sign_in/google_sign_in_platform_interface/lib/src/types.dart
+++ packages/google_sign_in/google_sign_in_platform_interface/lib/src/types.dart
@@ -27,7 +27,12 @@ class GoogleSignInUserData {
   /// Uses the given data to construct an instance. Any of these parameters
   /// could be null.
   GoogleSignInUserData(
-      {this.displayName, this.email, this.id, this.photoUrl, this.idToken});
+      {this.displayName,
+      this.email,
+      this.id,
+      this.photoUrl,
+      this.idToken,
+      this.serverAuthCode});
 
   /// The display name of the signed in user.
   ///
@@ -62,9 +67,13 @@ class GoogleSignInUserData {
   /// data.
   String idToken;
 
+  /// Authorization code required to make API calls from the server.
+  /// Read more on <https://developers.google.com/identity/sign-in/android/offline-access>
+  String serverAuthCode;
+
   @override
-  int get hashCode =>
-      hashObjects(<String>[displayName, email, id, photoUrl, idToken]);
+  int get hashCode => hashObjects(
+      <String>[displayName, email, id, photoUrl, idToken, serverAuthCode]);
 
   @override
   bool operator ==(dynamic other) {
@@ -75,7 +84,8 @@ class GoogleSignInUserData {
         otherUserData.email == email &&
         otherUserData.id == id &&
         otherUserData.photoUrl == photoUrl &&
-        otherUserData.idToken == idToken;
+        otherUserData.idToken == idToken &&
+        otherUserData.serverAuthCode == serverAuthCode;
   }
 }
 
