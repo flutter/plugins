c68a55d53c2e6da0a4d6fb89de058579f68e9250
Hamdi Kahloun <hamdikahlounacil@gmail.com>
2020-09-10 21:12:31 +0100

    [share] MethodCallHandler.java uses unchecked or unsafe operations

--- packages/share/android/src/main/java/io/flutter/plugins/share/MethodCallHandler.java
+++ packages/share/android/src/main/java/io/flutter/plugins/share/MethodCallHandler.java
@@ -4,18 +4,17 @@
 
 package io.flutter.plugins.share;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import java.io.*;
-import java.util.Map;
 
 /** Handles the method calls for the plugin. */
 class MethodCallHandler implements MethodChannel.MethodCallHandler {
 
   private Share share;
-  private Gson gson = new Gson();
 
   MethodCallHandler(Share share) {
     this.share = share;
@@ -23,34 +22,30 @@ class MethodCallHandler implements MethodChannel.MethodCallHandler {
 
   @Override
   public void onMethodCall(MethodCall call, MethodChannel.Result result) {
-    ShareMap shareMap;
+    String text, subject;
+    List<String> paths, mimeTypes;
     switch (call.method) {
       case "share":
         expectMapArguments(call);
         // Android does not support showing the share sheet at a particular point on screen.
-        // Avoiding uses unchecked or unsafe Casting by converting Map to Object using Gson.
-        try {
-          shareMap = getShareMap(call);
-          share.share(shareMap.getText(), shareMap.getSubject());
-          result.success(null);
-        } catch (JsonSyntaxException e) {
-          result.error(e.getMessage(), null, null);
-        }
+        text = call.argument("text");
+        subject = call.argument("subject");
+        share.share(text, subject);
+        result.success(null);
         break;
       case "shareFiles":
         expectMapArguments(call);
 
         // Android does not support showing the share sheet at a particular point on screen.
-        // Avoiding uses unchecked or unsafe Casting by converting Map to Object using Gson.
         try {
-          shareMap = getShareMap(call);
-          share.shareFiles(
-              shareMap.getPaths(),
-              shareMap.getMimeTypes(),
-              shareMap.getText(),
-              shareMap.getSubject());
+          paths = call.argument("paths");
+          mimeTypes = call.argument("mimeTypes");
+          text = call.argument("text");
+          subject = call.argument("subject");
+          share.shareFiles(paths, mimeTypes, text, subject);
+          share.shareFiles(paths, mimeTypes, text, subject);
           result.success(null);
-        } catch (IOException | JsonSyntaxException e) {
+        } catch (IOException e) {
           result.error(e.getMessage(), null, null);
         }
         break;
@@ -64,10 +59,5 @@ class MethodCallHandler implements MethodChannel.MethodCallHandler {
     if (!(call.arguments instanceof Map)) {
       throw new IllegalArgumentException("Map argument expected");
     }
-  }
-
-  private ShareMap getShareMap(MethodCall call) {
-    String jsonData = gson.toJson(call.arguments);
-    return gson.fromJson(jsonData, ShareMap.class);
   }
 }
