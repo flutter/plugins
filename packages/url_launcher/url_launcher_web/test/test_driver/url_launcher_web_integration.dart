83cb622cd672b391ab416b795de371554ddc3eb4
David Iglesias Teixeira <ditman@gmail.com>
2020-09-22 15:37:45 -0700

    Remove TestOn notice.

--- packages/url_launcher/url_launcher_web/test/test_driver/url_launcher_web_integration.dart
+++ packages/url_launcher/url_launcher_web/test/test_driver/url_launcher_web_integration.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-@TestOn('chrome') // Uses web-only Flutter SDK
-
 import 'dart:html' as html;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
