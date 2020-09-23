c59ea1692581847d655f1f32d636effd74b4a7be
David Iglesias <ditman@gmail.com>
2020-09-23 11:11:27 -0700

    [url_launcher_web] Remove package:platform_detect (#3071)
    This change removes the dependency that url_launcher_web has in package:platform_detect to achieve a null-safe gallery app.
    
    It also migrates the unit tests to run with flutter drive, instead of flutter test with --platform.

--- packages/url_launcher/url_launcher_web/lib/url_launcher_web.dart
+++ packages/url_launcher/url_launcher_web/lib/url_launcher_web.dart
@@ -5,19 +5,33 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:meta/meta.dart';
 import 'package:url_launcher_platform_interface/url_launcher_platform_interface.dart';
 
-import 'package:platform_detect/platform_detect.dart' show browser;
-
 const _safariTargetTopSchemes = {
   'mailto',
   'tel',
   'sms',
 };
+String _getUrlScheme(String url) => Uri.tryParse(url)?.scheme;
+
+bool _isSafariTargetTopScheme(String url) =>
+    _safariTargetTopSchemes.contains(_getUrlScheme(url));
+
+// Taken from package:platform_detect.
+bool _navigatorIsSafari(html.Navigator navigator) {
+  // An web view running in an iOS app does not have a 'Version/X.X.X' string in the appVersion
+  final vendor = navigator.vendor;
+  final appVersion = navigator.appVersion;
+  return vendor != null &&
+      vendor.contains('Apple') &&
+      appVersion != null &&
+      appVersion.contains('Version');
+}
 
 /// The web implementation of [UrlLauncherPlatform].
 ///
 /// This class implements the `package:url_launcher` functionality for the web.
 class UrlLauncherPlugin extends UrlLauncherPlatform {
   html.Window _window;
+  bool _isSafari = false;
 
   // The set of schemes that can be handled by the plugin
   static final _supportedSchemes = {
@@ -26,18 +40,15 @@ class UrlLauncherPlugin extends UrlLauncherPlatform {
   }.union(_safariTargetTopSchemes);
 
   /// A constructor that allows tests to override the window object used by the plugin.
-  UrlLauncherPlugin({@visibleForTesting html.Window window})
-      : _window = window ?? html.window;
+  UrlLauncherPlugin({@visibleForTesting html.Window debugWindow})
+      : _window = debugWindow ?? html.window {
+    _isSafari = _navigatorIsSafari(_window.navigator);
+  }
 
   /// Registers this class as the default instance of [UrlLauncherPlatform].
   static void registerWith(Registrar registrar) {
     UrlLauncherPlatform.instance = UrlLauncherPlugin();
   }
-
-  String _getUrlScheme(String url) => Uri.tryParse(url)?.scheme;
-
-  bool _isSafariTargetTopScheme(String url) =>
-      _safariTargetTopSchemes.contains(_getUrlScheme(url));
 
   /// Opens the given [url] in the specified [webOnlyWindowName].
   ///
@@ -47,7 +58,7 @@ class UrlLauncherPlugin extends UrlLauncherPlatform {
     // We need to open mailto, tel and sms urls on the _top window context on safari browsers.
     // See https://github.com/flutter/flutter/issues/51461 for reference.
     final target = webOnlyWindowName ??
-        ((browser.isSafari && _isSafariTargetTopScheme(url)) ? '_top' : '');
+        ((_isSafari && _isSafariTargetTopScheme(url)) ? '_top' : '');
     return _window.open(url, target);
   }
 
--- packages/url_launcher/url_launcher_web/test/test_driver/url_launcher_web_integration.dart
+++ packages/url_launcher/url_launcher_web/test/test_driver/url_launcher_web_integration.dart
@@ -2,45 +2,53 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-@TestOn('chrome') // Uses web-only Flutter SDK
-
 import 'dart:html' as html;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:mockito/mockito.dart';
+import 'package:integration_test/integration_test.dart';
 
-import 'package:platform_detect/test_utils.dart' as platform;
+class _MockWindow extends Mock implements html.Window {}
 
-class MockWindow extends Mock implements html.Window {}
+class _MockNavigator extends Mock implements html.Navigator {}
 
 void main() {
-  group('$UrlLauncherPlugin', () {
-    MockWindow mockWindow = MockWindow();
-    UrlLauncherPlugin plugin = UrlLauncherPlugin(window: mockWindow);
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('UrlLauncherPlugin', () {
+    _MockWindow mockWindow;
+    _MockNavigator mockNavigator;
+
+    UrlLauncherPlugin plugin;
 
     setUp(() {
-      platform.configurePlatformForTesting(browser: platform.chrome);
+      mockWindow = _MockWindow();
+      mockNavigator = _MockNavigator();
+      when(mockWindow.navigator).thenReturn(mockNavigator);
+
+      plugin = UrlLauncherPlugin(debugWindow: mockWindow);
     });
 
     group('canLaunch', () {
-      test('"http" URLs -> true', () {
+      testWidgets('"http" URLs -> true', (WidgetTester _) async {
         expect(plugin.canLaunch('http://google.com'), completion(isTrue));
       });
 
-      test('"https" URLs -> true', () {
-        expect(plugin.canLaunch('https://google.com'), completion(isTrue));
+      testWidgets('"https" URLs -> true', (WidgetTester _) async {
+        expect(
+            plugin.canLaunch('https://go, (Widogle.com'), completion(isTrue));
       });
 
-      test('"mailto" URLs -> true', () {
+      testWidgets('"mailto" URLs -> true', (WidgetTester _) async {
         expect(
             plugin.canLaunch('mailto:name@mydomain.com'), completion(isTrue));
       });
 
-      test('"tel" URLs -> true', () {
+      testWidgets('"tel" URLs -> true', (WidgetTester _) async {
         expect(plugin.canLaunch('tel:5551234567'), completion(isTrue));
       });
 
-      test('"sms" URLs -> true', () {
+      testWidgets('"sms" URLs -> true', (WidgetTester _) async {
         expect(plugin.canLaunch('sms:+19725551212?body=hello%20there'),
             completion(isTrue));
       });
@@ -50,15 +58,15 @@ void main() {
       setUp(() {
         // Simulate that window.open does something.
         when(mockWindow.open('https://www.google.com', ''))
-            .thenReturn(MockWindow());
+            .thenReturn(_MockWindow());
         when(mockWindow.open('mailto:name@mydomain.com', ''))
-            .thenReturn(MockWindow());
-        when(mockWindow.open('tel:5551234567', '')).thenReturn(MockWindow());
+            .thenReturn(_MockWindow());
+        when(mockWindow.open('tel:5551234567', '')).thenReturn(_MockWindow());
         when(mockWindow.open('sms:+19725551212?body=hello%20there', ''))
-            .thenReturn(MockWindow());
+            .thenReturn(_MockWindow());
       });
 
-      test('launching a URL returns true', () {
+      testWidgets('launching a URL returns true', (WidgetTester _) async {
         expect(
             plugin.launch(
               'https://www.google.com',
@@ -72,7 +80,7 @@ void main() {
             completion(isTrue));
       });
 
-      test('launching a "mailto" returns true', () {
+      testWidgets('launching a "mailto" returns true', (WidgetTester _) async {
         expect(
             plugin.launch(
               'mailto:name@mydomain.com',
@@ -86,7 +94,7 @@ void main() {
             completion(isTrue));
       });
 
-      test('launching a "tel" returns true', () {
+      testWidgets('launching a "tel" returns true', (WidgetTester _) async {
         expect(
             plugin.launch(
               'tel:5551234567',
@@ -100,7 +108,7 @@ void main() {
             completion(isTrue));
       });
 
-      test('launching a "sms" returns true', () {
+      testWidgets('launching a "sms" returns true', (WidgetTester _) async {
         expect(
             plugin.launch(
               'sms:+19725551212?body=hello%20there',
@@ -116,44 +124,51 @@ void main() {
     });
 
     group('openNewWindow', () {
-      test('http urls should be launched in a new window', () {
+      testWidgets('http urls should be launched in a new window',
+          (WidgetTester _) async {
         plugin.openNewWindow('http://www.google.com');
 
         verify(mockWindow.open('http://www.google.com', ''));
       });
 
-      test('https urls should be launched in a new window', () {
+      testWidgets('https urls should be launched in a new window',
+          (WidgetTester _) async {
         plugin.openNewWindow('https://www.google.com');
 
         verify(mockWindow.open('https://www.google.com', ''));
       });
 
-      test('mailto urls should be launched on a new window', () {
+      testWidgets('mailto urls should be launched on a new window',
+          (WidgetTester _) async {
         plugin.openNewWindow('mailto:name@mydomain.com');
 
         verify(mockWindow.open('mailto:name@mydomain.com', ''));
       });
 
-      test('tel urls should be launched on a new window', () {
+      testWidgets('tel urls should be launched on a new window',
+          (WidgetTester _) async {
         plugin.openNewWindow('tel:5551234567');
 
         verify(mockWindow.open('tel:5551234567', ''));
       });
 
-      test('sms urls should be launched on a new window', () {
+      testWidgets('sms urls should be launched on a new window',
+          (WidgetTester _) async {
         plugin.openNewWindow('sms:+19725551212?body=hello%20there');
 
         verify(mockWindow.open('sms:+19725551212?body=hello%20there', ''));
       });
-      test('setting webOnlyLinkTarget as _self opens the url in the same tab',
-          () {
+      testWidgets(
+          'setting webOnlyLinkTarget as _self opens the url in the same tab',
+          (WidgetTester _) async {
         plugin.openNewWindow('https://www.google.com',
             webOnlyWindowName: '_self');
         verify(mockWindow.open('https://www.google.com', '_self'));
       });
 
-      test('setting webOnlyLinkTarget as _blank opens the url in a new tab',
-          () {
+      testWidgets(
+          'setting webOnlyLinkTarget as _blank opens the url in a new tab',
+          (WidgetTester _) async {
         plugin.openNewWindow('https://www.google.com',
             webOnlyWindowName: '_blank');
         verify(mockWindow.open('https://www.google.com', '_blank'));
@@ -161,42 +176,51 @@ void main() {
 
       group('Safari', () {
         setUp(() {
-          platform.configurePlatformForTesting(browser: platform.safari);
+          when(mockNavigator.vendor).thenReturn('Apple Computer, Inc.');
+          when(mockNavigator.appVersion).thenReturn(
+              '5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15');
+          // Recreate the plugin, so it grabs the overrides from this group
+          plugin = UrlLauncherPlugin(debugWindow: mockWindow);
         });
 
-        test('http urls should be launched in a new window', () {
+        testWidgets('http urls should be launched in a new window',
+            (WidgetTester _) async {
           plugin.openNewWindow('http://www.google.com');
 
           verify(mockWindow.open('http://www.google.com', ''));
         });
 
-        test('https urls should be launched in a new window', () {
+        testWidgets('https urls should be launched in a new window',
+            (WidgetTester _) async {
           plugin.openNewWindow('https://www.google.com');
 
           verify(mockWindow.open('https://www.google.com', ''));
         });
 
-        test('mailto urls should be launched on the same window', () {
+        testWidgets('mailto urls should be launched on the same window',
+            (WidgetTester _) async {
           plugin.openNewWindow('mailto:name@mydomain.com');
 
           verify(mockWindow.open('mailto:name@mydomain.com', '_top'));
         });
 
-        test('tel urls should be launched on the same window', () {
+        testWidgets('tel urls should be launched on the same window',
+            (WidgetTester _) async {
           plugin.openNewWindow('tel:5551234567');
 
           verify(mockWindow.open('tel:5551234567', '_top'));
         });
 
-        test('sms urls should be launched on the same window', () {
+        testWidgets('sms urls should be launched on the same window',
+            (WidgetTester _) async {
           plugin.openNewWindow('sms:+19725551212?body=hello%20there');
 
           verify(
               mockWindow.open('sms:+19725551212?body=hello%20there', '_top'));
         });
-        test(
+        testWidgets(
             'mailto urls should use _blank if webOnlyWindowName is set as _blank',
-            () {
+            (WidgetTester _) async {
           plugin.openNewWindow('mailto:name@mydomain.com',
               webOnlyWindowName: '_blank');
           verify(mockWindow.open('mailto:name@mydomain.com', '_blank'));
