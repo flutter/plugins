a42986714bd3a409e88814e4ba2f5e3314efde3c
Quynh Vu <quynh.vdq@gmail.com>
2020-09-30 23:12:49 +0700

    close capture session before stop recording

--- packages/camera/android/src/main/java/io/flutter/plugins/camera/Camera.java
+++ packages/camera/android/src/main/java/io/flutter/plugins/camera/Camera.java
@@ -368,6 +368,7 @@ public class Camera {
 
     try {
       recordingVideo = false;
+      closeCaptureSession();
       mediaRecorder.stop();
       mediaRecorder.reset();
 //      startPreview();
