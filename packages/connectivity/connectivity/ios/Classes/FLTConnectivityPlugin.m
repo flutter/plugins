77333b583a9cdadb1f2b8b9086ad393f98ffbf20
Kevin Gray <KevinTheGray8@gmail.com>
2020-09-30 22:42:44 -0400

    move the rest of the ios code back

--- packages/connectivity/connectivity/ios/Classes/FLTConnectivityPlugin.m
+++ packages/connectivity/connectivity/ios/Classes/FLTConnectivityPlugin.m
@@ -62,7 +62,40 @@
 - (NSString*)getBSSID {
   return [self findNetworkInfo:@"BSSID"];
 }
+#endif
 
+- (NSString*)getWifiIP {
+  NSString* address = @"error";
+  struct ifaddrs* interfaces = NULL;
+  struct ifaddrs* temp_addr = NULL;
+  int success = 0;
+
+  // retrieve the current interfaces - returns 0 on success
+  success = getifaddrs(&interfaces);
+  if (success == 0) {
+    // Loop through linked list of interfaces
+    temp_addr = interfaces;
+    while (temp_addr != NULL) {
+      if (temp_addr->ifa_addr->sa_family == AF_INET) {
+        // Check if interface is en0 which is the wifi connection on the iPhone
+        if ([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+          // Get NSString from C String
+          address = [NSString
+              stringWithUTF8String:inet_ntoa(((struct sockaddr_in*)temp_addr->ifa_addr)->sin_addr)];
+        }
+      }
+
+      temp_addr = temp_addr->ifa_next;
+    }
+  }
+
+  // Free memory
+  freeifaddrs(interfaces);
+
+  return address;
+}
+
+#ifndef NO_LOCATION_PERMISSION_CONNECTIVITY
 - (NSString*)convertCLAuthorizationStatusToString:(CLAuthorizationStatus)status {
   switch (status) {
     case kCLAuthorizationStatusNotDetermined: {
@@ -142,37 +175,6 @@
 - (void)onReachabilityDidChange:(NSNotification*)notification {
   Reachability* curReach = [notification object];
   _eventSink([self statusFromReachability:curReach]);
-}
-
-- (NSString*)getWifiIP {
-  NSString* address = @"error";
-  struct ifaddrs* interfaces = NULL;
-  struct ifaddrs* temp_addr = NULL;
-  int success = 0;
-
-  // retrieve the current interfaces - returns 0 on success
-  success = getifaddrs(&interfaces);
-  if (success == 0) {
-    // Loop through linked list of interfaces
-    temp_addr = interfaces;
-    while (temp_addr != NULL) {
-      if (temp_addr->ifa_addr->sa_family == AF_INET) {
-        // Check if interface is en0 which is the wifi connection on the iPhone
-        if ([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
-          // Get NSString from C String
-          address = [NSString
-              stringWithUTF8String:inet_ntoa(((struct sockaddr_in*)temp_addr->ifa_addr)->sin_addr)];
-        }
-      }
-
-      temp_addr = temp_addr->ifa_next;
-    }
-  }
-
-  // Free memory
-  freeifaddrs(interfaces);
-
-  return address;
 }
 
 #pragma mark FlutterStreamHandler impl
