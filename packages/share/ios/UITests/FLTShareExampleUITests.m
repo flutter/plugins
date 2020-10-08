0f1017e2a7113a66a5b67b6ca8cc4c4310080bdb
Chris Yang <ychris@google.com>
2020-10-08 13:43:14 -0700

    foprmat

--- packages/share/ios/UITests/FLTShareExampleUITests.m
+++ packages/share/ios/UITests/FLTShareExampleUITests.m
@@ -11,32 +11,33 @@
 @implementation FLTShareExampleUITests
 
 - (void)setUp {
-    self.continueAfterFailure = NO;
+  self.continueAfterFailure = NO;
 }
 
-
 - (void)testShareWithEmptyOrigin {
-    XCUIApplication *app = [[XCUIApplication alloc] init];
-    [app launch];
+  XCUIApplication* app = [[XCUIApplication alloc] init];
+  [app launch];
 
-    XCUIElement* shareWithEmptyOriginButton =
-        [app.buttons elementMatchingPredicate:[NSPredicate predicateWithFormat:@"label == %@", @"Share With Empty Origin"]];
-    if (![shareWithEmptyOriginButton waitForExistenceWithTimeout:30]) {
-      NSLog(@"%@", app.debugDescription);
-      XCTFail(@"Failed due to not able to find shareWithEmptyOriginButton with %@ seconds", @(30));
-    }
+  XCUIElement* shareWithEmptyOriginButton = [app.buttons
+      elementMatchingPredicate:[NSPredicate
+                                   predicateWithFormat:@"label == %@", @"Share With Empty Origin"]];
+  if (![shareWithEmptyOriginButton waitForExistenceWithTimeout:30]) {
+    NSLog(@"%@", app.debugDescription);
+    XCTFail(@"Failed due to not able to find shareWithEmptyOriginButton with %@ seconds", @(30));
+  }
 
-    XCTAssertNotNil(shareWithEmptyOriginButton);
-    [shareWithEmptyOriginButton tap];
+  XCTAssertNotNil(shareWithEmptyOriginButton);
+  [shareWithEmptyOriginButton tap];
 
-    // Find the share popup.
-    XCUIElement* activityListView =
-        [app.otherElements elementMatchingPredicate:[NSPredicate predicateWithFormat:@"identifier == %@", @"ActivityListView"]];
-    if (![activityListView waitForExistenceWithTimeout:30]) {
-      NSLog(@"%@", app.debugDescription);
-      XCTFail(@"Failed due to not able to find activityListView with %@ seconds", @(30));
-    }
-    XCTAssertNotNil(activityListView);
+  // Find the share popup.
+  XCUIElement* activityListView = [app.otherElements
+      elementMatchingPredicate:[NSPredicate
+                                   predicateWithFormat:@"identifier == %@", @"ActivityListView"]];
+  if (![activityListView waitForExistenceWithTimeout:30]) {
+    NSLog(@"%@", app.debugDescription);
+    XCTFail(@"Failed due to not able to find activityListView with %@ seconds", @(30));
+  }
+  XCTAssertNotNil(activityListView);
 }
 
 @end
