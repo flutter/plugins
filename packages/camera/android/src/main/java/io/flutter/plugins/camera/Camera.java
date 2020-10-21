87dcf5a00ac682c6715aa4a2f19446a491d9bbab
Sempakonka <47318358+Sempakonka@users.noreply.github.com>
2020-10-21 18:11:23 +0200

    fix formatting

--- packages/camera/android/src/main/java/io/flutter/plugins/camera/Camera.java
+++ packages/camera/android/src/main/java/io/flutter/plugins/camera/Camera.java
@@ -215,12 +215,12 @@ public class Camera {
   public void takePicture(String filePath, @NonNull final Result result) {
     final File file = new File(filePath);
 
-        FilePathValidator validator = new FilePathValidator(filePath);
-        validator.validate();
-        if (!validator.isValid()) {
-            result.error("filePathInvalid", validator.getErrorMessage(), null);
-            return;
-        }
+    FilePathValidator validator = new FilePathValidator(filePath);
+    validator.validate();
+    if (!validator.isValid()) {
+        result.error("filePathInvalid", validator.getErrorMessage(), null);
+        return;
+    }
 
     pictureImageReader.setOnImageAvailableListener(
         reader -> {
@@ -334,23 +334,24 @@ public class Camera {
     cameraDevice.createCaptureSession(surfaceList, callback, null);
   }
 
-    public void startVideoRecording(String filePath, Result result) {
-        FilePathValidator validator = new FilePathValidator(filePath);
-        validator.validate();
-        if (!validator.isValid()) {
-            result.error("filePathInvalid", validator.getErrorMessage(), null);
-            return;
-        }
-        try {
-            prepareMediaRecorder(filePath);
-            recordingVideo = true;
-            createCaptureSession(
-                    CameraDevice.TEMPLATE_RECORD, () -> mediaRecorder.start(), mediaRecorder.getSurface());
-            result.success(null);
-        } catch (CameraAccessException | IOException e) {
-            result.error("videoRecordingFailed", e.getMessage(), null);
-        }
+  public void startVideoRecording(String filePath, Result result) {
+    FilePathValidator validator = new FilePathValidator(filePath);
+    validator.validate();
+    if (!validator.isValid()) {
+        result.error("filePathInvalid", validator.getErrorMessage(), null);
+        return;
     }
+
+    try {
+      prepareMediaRecorder(filePath);
+      recordingVideo = true;
+      createCaptureSession(
+          CameraDevice.TEMPLATE_RECORD, () -> mediaRecorder.start(), mediaRecorder.getSurface());
+      result.success(null);
+    } catch (CameraAccessException | IOException e) {
+      result.error("videoRecordingFailed", e.getMessage(), null);
+    }
+  }
 
   public void stopVideoRecording(@NonNull final Result result) {
     if (!recordingVideo) {
