e5f868749a57f112f276fe233b6a41634c4532ba
Chris Yang <ychris@google.com>
2020-10-21 12:24:58 -0700

    1.0

--- packages/connectivity/connectivity/lib/connectivity.dart
+++ packages/connectivity/connectivity/lib/connectivity.dart
@@ -53,6 +53,8 @@ class Connectivity {
   ///
   /// From android 8.0 onwards the GPS must be ON (high accuracy)
   /// in order to be able to obtain the SSID.
+  @Deprecated(
+      'This method is deprecated. Please use wifi_info_flutter instead. See https://github.com/flutter/plugins/blob/master/packages/wifi_info_flutter/wifi_info_flutter/README.md')
   Future<String> getWifiName() {
     return _platform.getWifiName();
   }
@@ -63,11 +65,15 @@ class Connectivity {
   ///
   /// From Android 8.0 onwards the GPS must be ON (high accuracy)
   /// in order to be able to obtain the BSSID.
+  @Deprecated(
+      'This method is deprecated. Please use wifi_info_flutter instead. See https://github.com/flutter/plugins/blob/master/packages/wifi_info_flutter/wifi_info_flutter/README.md')
   Future<String> getWifiBSSID() {
     return _platform.getWifiBSSID();
   }
 
   /// Obtains the IP address of the connected wifi network
+  @Deprecated(
+      'This method is deprecated. Please use wifi_info_flutter instead. See https://github.com/flutter/plugins/blob/master/packages/wifi_info_flutter/wifi_info_flutter/README.md')
   Future<String> getWifiIP() {
     return _platform.getWifiIP();
   }
@@ -98,7 +104,7 @@ class Connectivity {
   /// * The app uses the NEHotspotConfiguration API to configure the current Wi-Fi network.
   /// * The app has active VPN configurations installed.
   ///
-  /// If the app falls into the first category, call this method before calling `getWifiBSSID` or `getWifiIP`.
+  /// If the app falls into the first category, c1all this method before calling `getWifiBSSID` or `getWifiIP`.
   /// For example,
   /// ```dart
   /// if (Platform.isIOS) {
@@ -120,6 +126,8 @@ class Connectivity {
   /// Ideally, a location service authorization should only be requested if the current authorization status is not determined.
   ///
   /// See also [getLocationServiceAuthorization] to obtain current location service status.
+  @Deprecated(
+      'This method is deprecated. Please use wifi_info_flutter instead. See https://github.com/flutter/plugins/blob/master/packages/wifi_info_flutter/wifi_info_flutter/README.md')
   Future<LocationAuthorizationStatus> requestLocationServiceAuthorization({
     bool requestAlwaysLocationUsage = false,
   }) {
@@ -164,6 +172,8 @@ class Connectivity {
   /// ```
   ///
   /// See also [requestLocationServiceAuthorization] for requesting a location service authorization.
+  @Deprecated(
+      'This method is deprecated. Please use wifi_info_flutter instead. See https://github.com/flutter/plugins/blob/master/packages/wifi_info_flutter/wifi_info_flutter/README.md')
   Future<LocationAuthorizationStatus> getLocationServiceAuthorization() {
     return _platform.getLocationServiceAuthorization();
   }
