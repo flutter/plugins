37c24759e1bb92d7fc2faff227a33d342c4d51b6
David Iglesias Teixeira <ditman@gmail.com>
2020-10-30 16:00:57 -0700

    Remove analysis_options.yaml from url_launcher_web.

---
+++ packages/url_launcher/url_launcher_web/lib/src/shims/dart_ui.dart
@@ -0,0 +1,8 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// This file shims dart:ui in web-only scenarios, getting rid of the need to
+/// suppress analyzer warnings.
+
+export 'dart_ui_fake.dart' if (dart.library.html) 'dart:ui';
