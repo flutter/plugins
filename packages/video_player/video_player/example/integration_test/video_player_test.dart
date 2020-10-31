66588380869000671e83013b720b5882540148d7
Chris Yang <ychris@google.com>
2020-10-30 19:55:29 -0700

    remove unused imports

--- packages/video_player/video_player/example/integration_test/video_player_test.dart
+++ packages/video_player/video_player/example/integration_test/video_player_test.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:flutter_test/flutter_test.dart';
