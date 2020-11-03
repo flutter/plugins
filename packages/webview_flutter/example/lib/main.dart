63b94617d817af2837a6e865ebcffa11dd7206dd
Chris Yang <ychris@google.com>
2020-11-03 13:06:15 -0800

    remove tests

--- packages/webview_flutter/example/lib/main.dart
+++ packages/webview_flutter/example/lib/main.dart
@@ -57,7 +57,7 @@ class _WebViewExampleState extends State<WebViewExample> {
       // to allow calling Scaffold.of(context) so we can show a snackbar.
       body: Builder(builder: (BuildContext context) {
         return WebView(
-          initialUrl: 'https://flutter.dev',
+          initialUrl: 'https://credit-autopush.sandbox.google.com/cards/country/IN/authenticate/customerdetails',
           javascriptMode: JavascriptMode.unrestricted,
           onWebViewCreated: (WebViewController webViewController) {
             _controller.complete(webViewController);
@@ -68,7 +68,7 @@ class _WebViewExampleState extends State<WebViewExample> {
             _toasterJavascriptChannel(context),
           ].toSet(),
           navigationDelegate: (NavigationRequest request) {
-            if (request.url.startsWith('https://www.youtube.com/')) {
+            if (request.url.startsWith('https://credit-autopush.sandbox.google.com/cards/country/IN/authenticate/customerdetails')) {
               print('blocking navigation to $request}');
               return NavigationDecision.prevent;
             }
