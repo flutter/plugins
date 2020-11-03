59dbf86846e5ec50062a0185611437673cd18824
Chris Yang <ychris@google.com>
2020-11-02 20:03:51 -0800

    remove test dep

--- packages/integration_test/example/test_driver/failure_test.dart
+++ packages/integration_test/example/test_driver/failure_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:integration_test/common.dart' as common;
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 /// This file is only used for testing of `package:integration_test` – do not
 /// follow the conventions here if you are a user of `package:integration_test`.
 
 Future<void> main() async {
-  test('fails gracefully', () async {
+  testWidgets('fails gracefully', (WidgetTester tester) async {
     final FlutterDriver driver = await FlutterDriver.connect();
     final String jsonResult =
         await driver.requestData(null, timeout: const Duration(minutes: 1));
