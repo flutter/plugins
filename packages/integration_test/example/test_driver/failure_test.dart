92cdce11ab301f0395fc60d7fc2a74048d7ec18d
Chris Yang <ychris@google.com>
2020-11-03 13:21:57 -0800

    comment out tests and add todo

---
+++ packages/integration_test/example/test_driver/failure_test.dart
@@ -0,0 +1,21 @@
+// TODO(cyanglaz): Re-enable tests when https://github.com/flutter/flutter/issues/69439 is fixed.
+
+// import 'package:flutter_driver/flutter_driver.dart';
+// import 'package:integration_test/common.dart' as common;
+// import 'package:test/test.dart';
+
+// /// This file is only used for testing of `package:integration_test` – do not
+// /// follow the conventions here if you are a user of `package:integration_test`.
+// Future<void> main() async {
+//   test('fails gracefully', () async {
+//     final FlutterDriver driver = await FlutterDriver.connect();
+//     final String jsonResult =
+//         await driver.requestData(null, timeout: const Duration(minutes: 1));
+//     common.Response response = common.Response.fromJson(jsonResult);
+//     await driver.close();
+//     expect(
+//       response.allTestsPassed,
+//       false,
+//     );
+//   });
+// }
