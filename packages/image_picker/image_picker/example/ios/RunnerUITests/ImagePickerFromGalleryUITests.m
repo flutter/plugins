f0ab4d5ed1fc7b8049e815f58d93b3a9a0356c65
Chris Yang <ychris@google.com>
2020-11-06 15:01:39 -0800

    format

--- packages/image_picker/image_picker/example/ios/RunnerUITests/ImagePickerFromGalleryUITests.m
+++ packages/image_picker/image_picker/example/ios/RunnerUITests/ImagePickerFromGalleryUITests.m
@@ -7,7 +7,7 @@
 
 @interface ImagePickerFromGalleryUITests : XCTestCase
 
-@property (nonatomic, strong) XCUIApplication *app;
+@property(nonatomic, strong) XCUIApplication* app;
 
 @end
 
@@ -18,19 +18,22 @@
 
   self.continueAfterFailure = NO;
   self.app = [[XCUIApplication alloc] init];
-  [self.app  launch];
-  [self addUIInterruptionMonitorWithDescription:@"Permission popups" handler:^BOOL(XCUIElement * _Nonnull interruptingElement) {
-    XCUIElement *ok = interruptingElement.buttons[@"OK"];
-    if (ok.exists) {
-      [ok tap];
-    }
-    // iOS 14.
-    XCUIElement *allPhotoPermission = interruptingElement.buttons[@"Allow Access to All Photos"];
-    if (allPhotoPermission.exists) {
-      [allPhotoPermission tap];
-    }
-    return YES;
-  }];
+  [self.app launch];
+  [self addUIInterruptionMonitorWithDescription:@"Permission popups"
+                                        handler:^BOOL(XCUIElement* _Nonnull interruptingElement) {
+                                          XCUIElement* ok = interruptingElement.buttons[@"OK"];
+                                          if (ok.exists) {
+                                            [ok tap];
+                                          }
+                                          // iOS 14.
+                                          XCUIElement* allPhotoPermission =
+                                              interruptingElement
+                                                  .buttons[@"Allow Access to All Photos"];
+                                          if (allPhotoPermission.exists) {
+                                            [allPhotoPermission tap];
+                                          }
+                                          return YES;
+                                        }];
 }
 
 - (void)testPickingFromGallery {
@@ -66,14 +69,16 @@
   XCTAssertTrue(pickButton.exists);
   [pickButton tap];
 
-  // There is a known bug where the permission popups interruption won't get fired until a tap happened in the app. We expect a permission popup so we do a tap here.
+  // There is a known bug where the permission popups interruption won't get fired until a tap
+  // happened in the app. We expect a permission popup so we do a tap here.
   [self.app tap];
 
   // Find and tap on the `Cancel` button.
   NSPredicate* predicateToFindCancelButton =
       [NSPredicate predicateWithFormat:@"label == %@", @"Cancel"];
 
-  XCUIElement* cancelButton = [self.app.buttons elementMatchingPredicate:predicateToFindCancelButton];
+  XCUIElement* cancelButton =
+      [self.app.buttons elementMatchingPredicate:predicateToFindCancelButton];
   if (![cancelButton waitForExistenceWithTimeout:30]) {
     NSLog(@"%@", self.app.debugDescription);
     XCTFail(@"Failed due to not able to find Cancel button with %@ seconds", @(30));
@@ -96,13 +101,12 @@
 }
 
 - (void)launchPickerAndPick {
-
   // Find and tap on the pick from gallery button.
   NSPredicate* predicateToFindImageFromGalleryButton =
       [NSPredicate predicateWithFormat:@"label == %@", @"image_picker_example_from_gallery"];
 
   XCUIElement* imageFromGalleryButton =
-  [self.app.otherElements elementMatchingPredicate:predicateToFindImageFromGalleryButton];
+      [self.app.otherElements elementMatchingPredicate:predicateToFindImageFromGalleryButton];
   if (![imageFromGalleryButton waitForExistenceWithTimeout:30]) {
     NSLog(@"%@", self.app.debugDescription);
     XCTFail(@"Failed due to not able to find image from gallery button with %@ seconds", @(30));
@@ -126,14 +130,18 @@
 
   // Find an image and tap on it. (IOS 14 UI, images are showing direclty)
   XCUIElement* aImage = self.app.scrollViews.firstMatch.images.firstMatch;
-  XCUIElement* allPhotosCell = [self.app.cells elementMatchingPredicate:[NSPredicate predicateWithFormat:@"label == %@", @"All Photos"]];
-  XCUIElement *firstExist = [RunnerUITestUtils waitForFirstExistence:@[aImage, allPhotosCell] timeout:30];
+  XCUIElement* allPhotosCell = [self.app.cells
+      elementMatchingPredicate:[NSPredicate predicateWithFormat:@"label == %@", @"All Photos"]];
+  XCUIElement* firstExist = [RunnerUITestUtils waitForFirstExistence:@[ aImage, allPhotosCell ]
+                                                             timeout:30];
   if (firstExist) {
     // For iOS 14 and below, we need to tap on the "All Photos" cell to get to the images page.
     // The image a11y info is also different.
     if (allPhotosCell.exists) {
       [allPhotosCell tap];
-      aImage = [self.app.collectionViews elementMatchingType:XCUIElementTypeCollectionView identifier:@"PhotosGridView"].cells.firstMatch;
+      aImage = [self.app.collectionViews elementMatchingType:XCUIElementTypeCollectionView
+                                                  identifier:@"PhotosGridView"]
+                   .cells.firstMatch;
     }
     if (![aImage waitForExistenceWithTimeout:30]) {
       NSLog(@"%@", self.app.debugDescription);
