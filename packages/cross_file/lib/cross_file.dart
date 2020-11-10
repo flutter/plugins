01fe65e49f09869690155bedeb67f03c8ec22941
Maurits van Beusekom <maurits@baseflow.com>
2020-11-10 21:21:40 +0100

    Revert "Added missing copyright"
    This reverts commit cf7e8d5f3810ae646669f584738502a8cc3c5ca1.

--- packages/cross_file/lib/cross_file.dart
+++ packages/cross_file/lib/cross_file.dart
@@ -1,5 +1,1 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 export 'src/cross_file.dart';
