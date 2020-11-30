8ee04427ba3eba2d1a8f3cb81085d99a5bb07004
Maurits van Beusekom <maurits@baseflow.com>
2020-11-30 09:37:44 +0100

    Added missing license to Dart files

--- packages/camera/camera_platform_interface/lib/src/events/camera_event.dart
+++ packages/camera/camera_platform_interface/lib/src/events/camera_event.dart
@@ -1,3 +1,7 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /// Generic Event coming from the native side of Camera.
 ///
 /// All [CameraEvent]s contain the `cameraId` that originated the event. This
--- packages/camera/camera_platform_interface/lib/src/types/resolution_preset.dart
+++ packages/camera/camera_platform_interface/lib/src/types/resolution_preset.dart
@@ -1,3 +1,7 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /// Affect the quality of video recording and image capture:
 ///
 /// If a preset is not available on the camera being used a preset of lower quality will be selected automatically.
