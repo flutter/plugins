ca13bd5867a682bfbf4371c881a5fbf781850c9a
Maurits van Beusekom <maurits@baseflow.com>
2020-12-03 11:39:00 +0100

    Use import/export instead of part implementation

--- packages/camera/camera/lib/src/camera_image.dart
+++ packages/camera/camera/lib/src/camera_image.dart
@@ -2,7 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-part of camera;
+import 'dart:typed_data';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 /// A single color plane of image data.
 ///
