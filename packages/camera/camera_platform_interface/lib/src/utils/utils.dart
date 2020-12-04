9fef1c7f289caa81d683cdb5c7a5fb006fc840e5
Maurits van Beusekom <maurits@baseflow.com>
2020-12-04 08:50:27 +0100

    [camera] Add `camera_platform_interface` package (#3253)
    * Make sure only camera_platform_interface has updates
    
    * add dev dependency to async package
    
    * refactored import to package import
    
    * Fix formatting issues

---
+++ packages/camera/camera_platform_interface/lib/src/utils/utils.dart
@@ -0,0 +1,14 @@
+import 'package:camera_platform_interface/camera_platform_interface.dart';
+
+/// Parses a string into a corresponding CameraLensDirection.
+CameraLensDirection parseCameraLensDirection(String string) {
+  switch (string) {
+    case 'front':
+      return CameraLensDirection.front;
+    case 'back':
+      return CameraLensDirection.back;
+    case 'external':
+      return CameraLensDirection.external;
+  }
+  throw ArgumentError('Unknown CameraLensDirection value');
+}
