ad50416de30920190bf28bf9aa5c864b6aaecffe
Mehmet Fidanboylu <mehmetf@google.com>
2020-12-14 15:23:55 -0800

    Add webview_flutter to list of nnbd plugins

--- script/nnbd_plugins.sh
+++ script/nnbd_plugins.sh
@@ -5,6 +5,7 @@
 # null-safe is available on stable.
 
 readonly NNBD_PLUGINS_LIST=(
+  "android_intent"
   "connectivity"
   "device_info"
   "flutter_plugin_android_lifecycle"
@@ -15,6 +16,7 @@ readonly NNBD_PLUGINS_LIST=(
   "plugin_platform_interface"
   "url_launcher"
   "video_player"
+  "webview_flutter"
 )
 
 export EXCLUDED_PLUGINS_FROM_STABLE=$(IFS=, ; echo "${NNBD_PLUGINS_LIST[*]}")
