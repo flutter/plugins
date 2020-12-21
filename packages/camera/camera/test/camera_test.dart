e94b474030fd93dac40cda1c8c33c6f54b81f144
daniel.roek <daniel.roek@gmail.com>
2020-12-21 13:39:52 +0100

    Removed imageStreamImageFormat from CameraValue

--- packages/camera/camera/test/camera_test.dart
+++ packages/camera/camera/test/camera_test.dart
@@ -311,21 +311,6 @@ void main() {
           )));
     });
   });
-
-  test('imageFormat can be set', () async {
-    CameraController cameraController = CameraController(
-      CameraDescription(
-          name: 'cam',
-          lensDirection: CameraLensDirection.back,
-          sensorOrientation: 90),
-      ResolutionPreset.max,
-      imageStreamImageFormat: ImageFormatGroup.jpeg,
-    );
-
-    await cameraController.initialize();
-    expect(cameraController.value.imageStreamImageFormat, ImageFormatGroup.jpeg);
-  });
-
 }
 
 class MockCameraPlatform extends Mock
