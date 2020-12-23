ab15e89a7a0dc6ab0a4db0ab690eb089eabc3447
daniel.roek <daniel.roek@gmail.com>
2020-12-23 15:35:31 +0100

    Added ImageFormatGroup test

---
+++ packages/camera/camera_platform_interface/test/types/image_group_test.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:camera_platform_interface/src/types/types.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('$ImageFormatGroup tests', () {
+    test('ImageFormatGroupName extension returns correct values', () {
+      debugDefaultTargetPlatformOverride = TargetPlatform.android;
+      expect(ImageFormatGroup.jpeg.name(), 'jpeg');
+      expect(ImageFormatGroup.yuv420.name(), 'yuv420');
+      expect(ImageFormatGroup.bgra8888.name(), 'unknown');
+      expect(ImageFormatGroup.unknown.name(), 'unknown');
+      debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+      expect(ImageFormatGroup.bgra8888.name(), 'bgra8888');
+      expect(ImageFormatGroup.yuv420.name(), 'yuv420');
+      expect(ImageFormatGroup.jpeg.name(), 'unknown');
+      expect(ImageFormatGroup.unknown.name(), 'unknown');
+    });
+  });
+}
