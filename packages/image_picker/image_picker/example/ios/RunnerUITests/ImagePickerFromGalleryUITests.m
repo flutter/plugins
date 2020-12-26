27ec9fabba01ef053dd417dde95d33651b61d160
Martin Geidobler <>
2020-12-26 10:07:16 +0100

    Removed all implementations of OtherElement

--- packages/image_picker/image_picker/example/ios/RunnerUITests/ImagePickerFromGalleryUITests.m
+++ packages/image_picker/image_picker/example/ios/RunnerUITests/ImagePickerFromGalleryUITests.m
@@ -63,14 +63,6 @@ const int kElementWaitingTime = 30;
   NSPredicate* predicateToFindImageFromGalleryButton =
       [NSPredicate predicateWithFormat:@"label == %@", @"image_picker_example_from_gallery"];
 
-  XCUIElement* imageFromGalleryButton =
-      [self.app.otherElements elementMatchingPredicate:predicateToFindImageFromGalleryButton];
-  if (![imageFromGalleryButton waitForExistenceWithTimeout:kElementWaitingTime]) {
-    os_log_error(OS_LOG_DEFAULT, "%@", self.app.debugDescription);
-    XCTFail(@"Failed due to not able to find image from gallery button with %@ seconds",
-            @(kElementWaitingTime));
-  }
-
   XCTAssertTrue(imageFromGalleryButton.exists);
   [imageFromGalleryButton tap];
 
@@ -111,17 +103,7 @@ const int kElementWaitingTime = 30;
       elementMatchingPredicate:[NSPredicate
                                    predicateWithFormat:@"label == %@",
                                                        @"You have not yet picked an image."]];
-  if (![imageNotPickedText waitForExistenceWithTimeout:kElementWaitingTime]) {
-    // Before https://github.com/flutter/engine/pull/22811 the label's a11y type was otherElements.
-    // TODO(cyanglaz): Remove this after
-    // https://github.com/flutter/flutter/commit/057e8230743ec96f33b73948ccd6b80081e3615e rolled to
-    // stable.
-    // https://github.com/flutter/flutter/issues/71927
-    imageNotPickedText = [self.app.otherElements
-        elementMatchingPredicate:[NSPredicate
-                                     predicateWithFormat:@"label == %@",
-                                                         @"You have not yet picked an image."]];
-  }
+
   if (![imageNotPickedText waitForExistenceWithTimeout:kElementWaitingTime]) {
     os_log_error(OS_LOG_DEFAULT, "%@", self.app.debugDescription);
     XCTFail(@"Failed due to not able to find imageNotPickedText with %@ seconds",
@@ -135,14 +117,6 @@ const int kElementWaitingTime = 30;
   // Find and tap on the pick from gallery button.
   NSPredicate* predicateToFindImageFromGalleryButton =
       [NSPredicate predicateWithFormat:@"label == %@", @"image_picker_example_from_gallery"];
-
-  XCUIElement* imageFromGalleryButton =
-      [self.app.otherElements elementMatchingPredicate:predicateToFindImageFromGalleryButton];
-  if (![imageFromGalleryButton waitForExistenceWithTimeout:kElementWaitingTime]) {
-    os_log_error(OS_LOG_DEFAULT, "%@", self.app.debugDescription);
-    XCTFail(@"Failed due to not able to find image from gallery button with %@ seconds",
-            @(kElementWaitingTime));
-  }
 
   XCTAssertTrue(imageFromGalleryButton.exists);
   [imageFromGalleryButton tap];
