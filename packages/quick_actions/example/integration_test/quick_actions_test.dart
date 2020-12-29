d3dd34dbdb7efe23383fc10ee846544591328248
Maurice Parrish <bmparr@google.com>
2020-12-29 13:25:34 -0500

    migrate to null safety

--- packages/quick_actions/example/integration_test/quick_actions_test.dart
+++ packages/quick_actions/example/integration_test/quick_actions_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// @dart = 2.9
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:quick_actions/quick_actions.dart';
