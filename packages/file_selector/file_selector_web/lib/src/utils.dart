7206b0eb497c30a2afd11ea14bf1dc25cfc17a41
Juanjo Tugores <tugorez@gmail.com>
2021-01-12 12:37:13 -0600

    Add missing license comment to files

--- packages/file_selector/file_selector_web/lib/src/utils.dart
+++ packages/file_selector/file_selector_web/lib/src/utils.dart
@@ -1,3 +1,7 @@
+// Copyright 2020 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:file_selector_platform_interface/file_selector_platform_interface.dart';
 
 /// Convert list of XTypeGroups to a comma-separated string
