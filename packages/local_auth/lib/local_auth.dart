0c199f61d1e5b717ad76d1f25a86ecfdbbb81d0d
Kenneth Jones <kendjone@iu.edu>
2021-01-12 19:48:43 -0600

    Add deprecated annotation to authenticateWithBiometrics

--- packages/local_auth/lib/local_auth.dart
+++ packages/local_auth/lib/local_auth.dart
@@ -30,42 +30,7 @@ void setMockPathProviderPlatform(Platform platform) {
 
 /// A Flutter plugin for authenticating the user identity locally.
 class LocalAuthentication {
-  /// Authenticates the user with biometrics available on the device.
-  ///
-  /// Returns a [Future] holding true, if the user successfully authenticated,
-  /// false otherwise.
-  ///
-  /// [localizedReason] is the message to show to user while prompting them
-  /// for authentication. This is typically along the lines of: 'Please scan
-  /// your finger to access MyApp.'
-  ///
-  /// [useErrorDialogs] = true means the system will attempt to handle user
-  /// fixable issues encountered while authenticating. For instance, if
-  /// fingerprint reader exists on the phone but there's no fingerprint
-  /// registered, the plugin will attempt to take the user to settings to add
-  /// one. Anything that is not user fixable, such as no biometric sensor on
-  /// device, will be returned as a [PlatformException].
-  ///
-  /// [stickyAuth] is used when the application goes into background for any
-  /// reason while the authentication is in progress. Due to security reasons,
-  /// the authentication has to be stopped at that time. If stickyAuth is set
-  /// to true, authentication resumes when the app is resumed. If it is set to
-  /// false (default), then as soon as app is paused a failure message is sent
-  /// back to Dart and it is up to the client app to restart authentication or
-  /// do something else.
-  ///
-  /// Construct [AndroidAuthStrings] and [IOSAuthStrings] if you want to
-  /// customize messages in the dialogs.
-  ///
-  /// Setting [sensitiveTransaction] to true enables platform specific
-  /// precautions. For instance, on face unlock, Android opens a confirmation
-  /// dialog after the face is recognized to make sure the user meant to unlock
-  /// their phone.
-  ///
-  /// Throws an [PlatformException] if there were technical problems with local
-  /// authentication (e.g. lack of relevant hardware). This might throw
-  /// [PlatformException] with error code [otherOperatingSystem] on the iOS
-  /// simulator.
+  @Deprecated("Use `authenticate` with `biometricOnly: true` instead")
   Future<bool> authenticateWithBiometrics({
     required String localizedReason,
     bool useErrorDialogs = true,
