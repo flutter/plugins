196e296317b82e05d3e93f36b7059c2551bf2e44
Ruby Hartono <58564005+rh-id@users.noreply.github.com>
2021-01-14 11:17:04 +0700

    Use ServiceLoader for ExecutorService injection

--- packages/shared_preferences/shared_preferences/android/src/main/java/io/flutter/plugins/sharedpreferences/AsyncHandler.java
+++ packages/shared_preferences/shared_preferences/android/src/main/java/io/flutter/plugins/sharedpreferences/AsyncHandler.java
@@ -2,6 +2,8 @@ package io.flutter.plugins.sharedpreferences;
 
 import android.os.Handler;
 import android.os.Looper;
+import java.util.Iterator;
+import java.util.ServiceLoader;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
@@ -13,18 +15,22 @@ class AsyncHandler {
   private final Handler handler;
 
   public AsyncHandler() {
-    // Replace this executor when there is somehow a way in the future
-    // to inject from plugin engine or application
-    executorService =
-        Executors.newSingleThreadExecutor(
-            new ThreadFactory() {
-              @Override
-              public Thread newThread(Runnable r) {
-                Thread thread = new Thread(r, "SharedPreferencesAsync");
-                thread.setDaemon(true);
-                return null;
-              }
-            });
+    Iterator<ExecutorService> executorServiceIterator =
+        ServiceLoader.load(ExecutorService.class).iterator();
+    if (executorServiceIterator.hasNext()) {
+      executorService = executorServiceIterator.next();
+    } else {
+      executorService =
+          Executors.newSingleThreadExecutor(
+              new ThreadFactory() {
+                @Override
+                public Thread newThread(Runnable r) {
+                  Thread thread = new Thread(r, "SharedPreferencesAsync");
+                  thread.setDaemon(true);
+                  return null;
+                }
+              });
+    }
     handler = new Handler(Looper.getMainLooper());
   }
 
