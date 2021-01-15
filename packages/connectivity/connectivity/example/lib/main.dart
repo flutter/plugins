c5cb077f77f861620146afb31a2c8151aa41d0fa
Mrinal <jain.mrinal140@gmail.com>
2021-01-15 16:41:54 +0530

    Update main.dart
    Update main.dart and removed unwanted type mentioned  for  StreamSubscription due to which example war not working.

--- packages/connectivity/connectivity/example/lib/main.dart
+++ packages/connectivity/connectivity/example/lib/main.dart
@@ -51,7 +51,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String _connectionStatus = 'Unknown';
   final Connectivity _connectivity = Connectivity();
-  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+  StreamSubscription<ConnectivityResult> _connectivitySubscription;
 
   @override
   void initState() {
