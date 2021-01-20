1e093db942f101926f1ce7c906fcf0cdc146ddcd
Vishnu Agarwal <53317018+vishnuiiit@users.noreply.github.com>
2021-01-20 19:38:43 +0530

    Removed wrapped gesture detector from Android view
    Since the above mentioned issues https://github.com/flutter/flutter/issues/24585 and https://github.com/flutter/flutter/issues/24584 are fixed.

--- packages/webview_flutter/lib/src/webview_android.dart
+++ packages/webview_flutter/lib/src/webview_android.dart
@@ -27,34 +27,23 @@ class AndroidWebView implements WebViewPlatform {
     Set<Factory<OneSequenceGestureRecognizer>>? gestureRecognizers,
   }) {
     assert(webViewPlatformCallbacksHandler != null);
-    return GestureDetector(
-      // We prevent text selection by intercepting the long press event.
-      // This is a temporary stop gap due to issues with text selection on Android:
-      // https://github.com/flutter/flutter/issues/24585 - the text selection
-      // dialog is not responding to touch events.
-      // https://github.com/flutter/flutter/issues/24584 - the text selection
-      // handles are not showing.
-      // TODO(amirh): remove this when the issues above are fixed.
-      onLongPress: () {},
-      excludeFromSemantics: true,
-      child: AndroidView(
-        viewType: 'plugins.flutter.io/webview',
-        onPlatformViewCreated: (int id) {
-          if (onWebViewPlatformCreated == null) {
-            return;
-          }
-          onWebViewPlatformCreated(MethodChannelWebViewPlatform(
-              id, webViewPlatformCallbacksHandler));
-        },
-        gestureRecognizers: gestureRecognizers,
-        // WebView content is not affected by the Android view's layout direction,
-        // we explicitly set it here so that the widget doesn't require an ambient
-        // directionality.
-        layoutDirection: TextDirection.rtl,
-        creationParams:
-            MethodChannelWebViewPlatform.creationParamsToMap(creationParams),
-        creationParamsCodec: const StandardMessageCodec(),
-      ),
+    return AndroidView(
+      viewType: 'plugins.flutter.io/webview',
+      onPlatformViewCreated: (int id) {
+        if (onWebViewPlatformCreated == null) {
+          return;
+        }
+        onWebViewPlatformCreated(MethodChannelWebViewPlatform(
+            id, webViewPlatformCallbacksHandler));
+      },
+      gestureRecognizers: gestureRecognizers,
+      // WebView content is not affected by the Android view's layout direction,
+      // we explicitly set it here so that the widget doesn't require an ambient
+      // directionality.
+      layoutDirection: TextDirection.rtl,
+      creationParams:
+          MethodChannelWebViewPlatform.creationParamsToMap(creationParams),
+      creationParamsCodec: const StandardMessageCodec(),
     );
   }
 
