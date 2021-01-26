241bca9443982114e62c03506b95a2a640bbb947
李阳 <kiritobeater.summer@gmail.com>
2021-01-26 19:30:54 +0800

    [video_player] set AudioSessionCategory to PlayAndRecord

--- packages/video_player/video_player/ios/Classes/FLTVideoPlayerPlugin.m
+++ packages/video_player/video_player/ios/Classes/FLTVideoPlayerPlugin.m
@@ -498,8 +498,8 @@ static inline CGFloat radiansToDegrees(CGFloat radians) {
 }
 
 - (void)initialize:(FlutterError* __autoreleasing*)error {
-  // Allow audio playback when the Ring/Silent switch is set to silent
-  [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+  [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord
+                                         error:nil];
 
   for (NSNumber* textureId in _players) {
     [_registry unregisterTexture:[textureId unsignedIntegerValue]];
@@ -592,11 +592,12 @@ static inline CGFloat radiansToDegrees(CGFloat radians) {
 - (void)setMixWithOthers:(FLTMixWithOthersMessage*)input
                    error:(FlutterError* _Nullable __autoreleasing*)error {
   if ([input.mixWithOthers boolValue]) {
-    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord
                                      withOptions:AVAudioSessionCategoryOptionMixWithOthers
                                            error:nil];
   } else {
-    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord
+                                           error:nil];
   }
 }
 
