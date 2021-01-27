16317ef9745f9f82b4ad142e93432d14918a3e23
Nathanael <nathanaelneveux@me.com>
2021-01-27 15:09:41 -0500

    refactor FLTCMTimeToMillis

--- packages/video_player/video_player/ios/Classes/FLTVideoPlayerPlugin.m
+++ packages/video_player/video_player/ios/Classes/FLTVideoPlayerPlugin.m
@@ -11,13 +11,6 @@
 #error Code Requires ARC.
 #endif
 
-const int64_t TIME_UNSET = -9223372036854775807;
-
-int64_t FLTCMTimeToMillis(CMTime time) {
-  if (time.timescale == 0) return 0;
-  return time.value * 1000 / time.timescale;
-}
-
 @interface FLTFrameUpdater : NSObject
 @property(nonatomic) int64_t textureId;
 @property(nonatomic, weak, readonly) NSObject<FlutterTextureRegistry>* registry;
@@ -105,6 +98,16 @@ static void* playbackBufferFullContext = &playbackBufferFullContext;
       _eventSink(@{@"event" : @"completed"});
     }
   }
+}
+
+const int64_t TIME_UNSET = -9223372036854775807;
+
+static inline int64_t FLTCMTimeToMillis(CMTime time) {
+  // When CMTIME_IS_INDEFINITE return a value that matches TIME_UNSET from ExoPlayer2 on Android.
+  // Fixes https://github.com/flutter/flutter/issues/48670
+  if (CMTIME_IS_INDEFINITE(time)) return TIME_UNSET;
+  if (time.timescale == 0) return 0;
+  return time.value * 1000 / time.timescale;
 }
 
 static inline CGFloat radiansToDegrees(CGFloat radians) {
@@ -343,14 +346,7 @@ static inline CGFloat radiansToDegrees(CGFloat radians) {
   return FLTCMTimeToMillis([_player currentTime]);
 }
 
-- (bool)isDurationIndefinite {
-  return CMTIME_IS_INDEFINITE([[_player currentItem] duration]);
-}
-
 - (int64_t)duration {
-  // When CMTIME_IS_INDEFINITE return a value that matches TIME_UNSET from ExoPlayer2 on Android.
-  // Fixes https://github.com/flutter/flutter/issues/48670
-  if ([self isDurationIndefinite]) return TIME_UNSET;
   return FLTCMTimeToMillis([[_player currentItem] duration]);
 }
 
