abfa9a1c4cc1d9c0a6cc50f02c35066850364d80
Maurits van Beusekom <maurits@baseflow.com>
2021-02-17 15:56:28 +0100

    Added timeout for waiting pre-capture state Android

--- packages/camera/camera/android/src/main/java/io/flutter/plugins/camera/Camera.java
+++ packages/camera/camera/android/src/main/java/io/flutter/plugins/camera/Camera.java
@@ -36,6 +36,7 @@ import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.SystemClock;
 import android.util.Log;
 import android.util.Range;
 import android.util.Rational;
@@ -73,6 +74,11 @@ interface ErrorCallback {
 public class Camera {
   private static final String TAG = "Camera";
 
+  /**
+   * Timeout for the pre-capture sequence.
+   */
+  private static final long PRECAPTURE_TIMEOUT_MS = 1000;
+
   private final SurfaceTextureEntry flutterTexture;
   private final CameraManager cameraManager;
   private final DeviceOrientationManager deviceOrientationListener;
@@ -105,6 +111,7 @@ public class Camera {
   private boolean useAutoFocus = true;
   private Range<Integer> fpsRange;
   private PlatformChannel.DeviceOrientation lockedCaptureOrientation;
+  private long preCaptureStartTime;
 
   private static final HashMap<String, Integer> supportedImageFormats;
   // Current supported outputs
@@ -503,11 +510,16 @@ public class Camera {
                   || aeState == CaptureRequest.CONTROL_AE_STATE_FLASH_REQUIRED
                   || aeState == CaptureRequest.CONTROL_AE_STATE_CONVERGED) {
                 pictureCaptureRequest.setState(State.waitingPreCaptureReady);
+                setPreCaptureStartTime();
               }
               break;
             case waitingPreCaptureReady:
               if (aeState == null || aeState != CaptureRequest.CONTROL_AE_STATE_PRECAPTURE) {
                 runPictureCapture();
+              } else {
+                if (hitPreCaptureTimeout()) {
+                  unlockAutoFocus();
+                }
               }
           }
         }
@@ -1140,6 +1152,22 @@ public class Camera {
       imageStreamReader.setOnImageAvailableListener(null, null);
     }
     startPreview();
+  }
+
+  /**
+   * Sets the time the pre-capture sequence started.
+   */
+  private void setPreCaptureStartTime() {
+    preCaptureStartTime = SystemClock.elapsedRealtime();
+  }
+
+  /**
+   * Check if the timeout for the pre-capture sequence has been reached.
+   *
+   * @return true if the timeout is reached; otherwise false is returned.
+   */
+  private boolean hitPreCaptureTimeout() {
+    return (SystemClock.elapsedRealtime() - preCaptureStartTime) > PRECAPTURE_TIMEOUT_MS;
   }
 
   private void closeCaptureSession() {
