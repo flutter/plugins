245662fae72ddb81634bad97f38bd5cb5d60426e
Stuart Morgan <stuartmorgan@google.com>
2021-02-17 11:33:31 -0500

    Rewrite and migrate macOS example

--- packages/path_provider/path_provider_macos/example/integration_test/path_provider_test.dart
+++ packages/path_provider/path_provider_macos/example/integration_test/path_provider_test.dart
@@ -2,42 +2,46 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// @dart=2.9
+
 import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('getTemporaryDirectory', (WidgetTester tester) async {
-    final Directory result = await getTemporaryDirectory();
+    final PathProviderPlatform provider = PathProviderPlatform.instance;
+    final String result = await provider.getTemporaryPath();
     _verifySampleFile(result, 'temporaryDirectory');
   });
 
   testWidgets('getApplicationDocumentsDirectory', (WidgetTester tester) async {
-    final Directory result = await getApplicationDocumentsDirectory();
+    final PathProviderPlatform provider = PathProviderPlatform.instance;
+    final String result = await provider.getApplicationDocumentsPath();
     _verifySampleFile(result, 'applicationDocuments');
   });
 
   testWidgets('getApplicationSupportDirectory', (WidgetTester tester) async {
-    final Directory result = await getApplicationSupportDirectory();
+    final PathProviderPlatform provider = PathProviderPlatform.instance;
+    final String result = await provider.getApplicationSupportPath();
     _verifySampleFile(result, 'applicationSupport');
   });
 
-  testWidgets('getLibraryDirectory', (WidgetTester tester) async {
-    if (!Platform.isMacOS) {
-      return;
-    }
-    final Directory result = await getLibraryDirectory();
-    _verifySampleFile(result, 'library');
+  testWidgets('getDownloadsDirectory', (WidgetTester tester) async {
+    final PathProviderPlatform provider = PathProviderPlatform.instance;
+    final String result = await provider.getDownloadsPath();
+    _verifySampleFile(result, 'downloads');
   });
 }
 
-/// Verify a file called [name] in [directory] by recreating it with test
+/// Verify a file called [name] in [directoryPath] by recreating it with test
 /// contents when necessary.
-void _verifySampleFile(Directory directory, String name) {
-  final File file = File('${directory.path}/$name');
+void _verifySampleFile(String directoryPath, String name) {
+  final Directory directory = Directory(directoryPath);
+  final File file = File('${directory.path}${Platform.pathSeparator}$name');
 
   if (file.existsSync()) {
     file.deleteSync();
