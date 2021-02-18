3d015a14ae7815f5dafaf9076ad7e6e1b04946f3
Chris Yang <ychris@google.com>
2021-02-18 15:00:57 -0800

    dart 2.9

--- packages/in_app_purchase/integration_test/in_app_purchase_test.dart
+++ packages/in_app_purchase/integration_test/in_app_purchase_test.dart
@@ -1,8 +1,8 @@
 // Copyright 2019, the Chromium project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-// @dart = 2.10
 
+// @dart = 2.9
 import 'package:flutter_test/flutter_test.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:integration_test/integration_test.dart';
