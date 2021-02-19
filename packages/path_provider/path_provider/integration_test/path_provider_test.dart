83ab5cc388e4364481fe8ca5e38728759c16754b
Stuart Morgan <stuartmorgan@google.com>
2021-02-19 14:49:51 -0500

    Update all integration tests to NNBD

--- packages/path_provider/path_provider/integration_test/path_provider_test.dart
+++ packages/path_provider/path_provider/integration_test/path_provider_test.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @dart=2.9
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:integration_test/integration_test.dart';
@@ -12,7 +10,7 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('Can get temporary directory', (WidgetTester tester) async {
-    final String tempPath = (await getTemporaryDirectory()).path;
+    final String? tempPath = (await getTemporaryDirectory())?.path;
     expect(tempPath, isNotEmpty);
   });
 }
--- packages/path_provider/path_provider_macos/example/integration_test/path_provider_test.dart
+++ packages/path_provider/path_provider_macos/example/integration_test/path_provider_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart=2.9
-
 import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
@@ -14,31 +12,31 @@ void main() {
 
   testWidgets('getTemporaryDirectory', (WidgetTester tester) async {
     final PathProviderPlatform provider = PathProviderPlatform.instance;
-    final String result = await provider.getTemporaryPath();
+    final String? result = await provider.getTemporaryPath();
     _verifySampleFile(result, 'temporaryDirectory');
   });
 
   testWidgets('getApplicationDocumentsDirectory', (WidgetTester tester) async {
     final PathProviderPlatform provider = PathProviderPlatform.instance;
-    final String result = await provider.getApplicationDocumentsPath();
+    final String? result = await provider.getApplicationDocumentsPath();
     _verifySampleFile(result, 'applicationDocuments');
   });
 
   testWidgets('getApplicationSupportDirectory', (WidgetTester tester) async {
     final PathProviderPlatform provider = PathProviderPlatform.instance;
-    final String result = await provider.getApplicationSupportPath();
+    final String? result = await provider.getApplicationSupportPath();
     _verifySampleFile(result, 'applicationSupport');
   });
 
   testWidgets('getLibraryDirectory', (WidgetTester tester) async {
     final PathProviderPlatform provider = PathProviderPlatform.instance;
-    final String result = await provider.getLibraryPath();
+    final String? result = await provider.getLibraryPath();
     _verifySampleFile(result, 'library');
   });
 
   testWidgets('getDownloadsDirectory', (WidgetTester tester) async {
     final PathProviderPlatform provider = PathProviderPlatform.instance;
-    final String result = await provider.getDownloadsPath();
+    final String? result = await provider.getDownloadsPath();
     // _verifySampleFile causes hangs in driver for some reason, so just
     // validate that a non-empty path was returned.
     expect(result, isNotEmpty);
@@ -49,7 +47,9 @@ void main() {
 /// contents when necessary.
 ///
 /// If [createDirectory] is true, the directory will be created if missing.
-void _verifySampleFile(String directoryPath, String name) {
+void _verifySampleFile(String? directoryPath, String name) {
+  expect(directoryPath, isNotNull);
+  if (directoryPath == null) return;
   final Directory directory = Directory(directoryPath);
   final File file = File('${directory.path}${Platform.pathSeparator}$name');
 
