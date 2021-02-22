54057f89e7c3d9e4add4830a8a7fdfbb91fcb0b9
Chris Yang <ychris@google.com>
2021-02-22 10:19:16 -0800

    stable release

--- packages/connectivity/connectivity/example/test_driver/test/integration_test.dart
+++ packages/connectivity/connectivity/example/test_driver/test/integration_test.dart
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(amirh): Remove this once flutter_driver supports null safety.
-// https://github.com/flutter/flutter/issues/71379
-// @dart = 2.9
-
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter_driver/flutter_driver.dart';
