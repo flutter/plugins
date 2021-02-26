42d4d241beed94ee0ad8e4be09eddb4c516fce9d
Chris Yang <ychris@google.com>
2021-02-26 14:44:25 -0800

    in app purchase

--- packages/in_app_purchase/example/test_driver/integration_test/in_app_purchase_test.dart
+++ packages/in_app_purchase/example/test_driver/integration_test/in_app_purchase_test.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart = 2.9
 import 'package:flutter_test/flutter_test.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:integration_test/integration_test.dart';
