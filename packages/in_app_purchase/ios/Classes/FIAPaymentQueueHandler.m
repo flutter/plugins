b76aeabbabf030b1dc9043c40dc1dae39e56089b
Anton Borries <anton.borries@gmail.com>
2021-03-02 19:09:08 +0100

    Use respondToSelector to check for xcode version
    Also add Log message if functionality is unavaible

--- packages/in_app_purchase/ios/Classes/FIAPaymentQueueHandler.m
+++ packages/in_app_purchase/ios/Classes/FIAPaymentQueueHandler.m
@@ -67,8 +67,10 @@
 }
 
 - (void)presentCodeRedemptionSheet {
-  if (@available(iOS 14.0, *)) {
+  if (@available(iOS 14, *) && [self.queue respondsToSelector:@selector(presentCodeRedemptionSheet)]) {
     [self.queue presentCodeRedemptionSheet];
+  } else {
+      NSLog(@"presentCodeRedemptionSheet is only available on iOS 14 and Xcode 12 or newer");
   }
 }
 
