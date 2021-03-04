65cbd920d911c627837cea809b46231df63f64d7
Peter Stojanowski <peter.stojanowski.external@xain.io>
2021-03-04 16:41:27 +0000

    Remove WebViewData class

--- packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/WebViewManager.java
+++ packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/WebViewManager.java
@@ -18,39 +18,27 @@ public enum WebViewManager {
     }
 
     public InputAwareWebView webViewForId(String webViewId) {
-        return cache.get(webViewId).webView;
+        return cache.get(webViewId);
     }
 
     public void cacheWebView(InputAwareWebView webView, String webViewId) {
-        WebViewData data = new WebViewData(webView);
-        cache.put(webViewId, data);
+        cache.put(webViewId, webView);
     }
 
     public void clearAll() {
         cache.evictAll();
     }
 
-    private class WebViewData {
-        private InputAwareWebView webView;
-
-        WebViewData(InputAwareWebView webView) {
-            this.webView = webView;
-        }
-    }
-
-    private class WebViewCache extends LruCache<java.lang.String, WebViewManager.WebViewData> {
+    private class WebViewCache extends LruCache<java.lang.String, InputAwareWebView> {
 
         WebViewCache() {
             super(10);
         }
 
         @Override
-        protected void entryRemoved(boolean evicted, @NonNull String key, @NonNull WebViewData oldValue, @Nullable WebViewData newValue) {
-            super.entryRemoved(evicted, key, oldValue, newValue);
-
-            oldValue.webView.dispose();
-            oldValue.webView.destroy();
-            oldValue.webView = null;
+        protected void entryRemoved(boolean evicted, @NonNull String key, @NonNull InputAwareWebView oldValue, @Nullable InputAwareWebView newValue) {
+            oldValue.dispose();
+            oldValue.destroy();
         }
     }
 }
