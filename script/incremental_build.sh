98dd1622bd7d9d63f46e35d90c82a3ab65cf447e
Chris Yang <ychris@google.com>
2021-03-04 13:06:57 -0800

    version check for all plugins

--- script/incremental_build.sh
+++ script/incremental_build.sh
@@ -52,6 +52,7 @@ else
     echo "No changes detected in packages."
     echo "Running for all packages"
     (cd "$REPO_DIR" && plugin_tools "${ACTIONS[@]}" --exclude="$ALL_EXCLUDED" ${PLUGIN_SHARDING[@]})
+    (cd "$REPO_DIR" && plugin_tools version-check --base_sha="$(get_branch_base_sha)")
   else
     echo running "${ACTIONS[@]}"
     (cd "$REPO_DIR" && plugin_tools "${ACTIONS[@]}" --plugins="$CHANGED_PACKAGES" --exclude="$ALL_EXCLUDED" ${PLUGIN_SHARDING[@]})
