c7a02d46003ebd4e8a5e06d97bfac6952e084b75
daniel.roek <daniel.roek@gmail.com>
2021-03-05 15:42:55 +0100

    remove unused imports

--- packages/image_picker/image_picker/example/android/app/src/test/java/io/flutter/plugins/imagepicker/FileUtilTest.java
+++ packages/image_picker/image_picker/example/android/app/src/test/java/io/flutter/plugins/imagepicker/FileUtilTest.java
@@ -16,8 +16,6 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Objects;
 
 import org.junit.Before;
 import org.junit.Test;
