26879dbd2f75f8f4f67ac8183aeefc394e6a1efa
Ian Hickson <ian@hixie.ch>
2021-03-05 17:54:03 -0800

    Import flutter_test for future compatibility (#3665)

--- packages/in_app_purchase/test/in_app_purchase_connection/google_play_connection_test.dart
+++ packages/in_app_purchase/test/in_app_purchase_connection/google_play_connection_test.dart
@@ -5,9 +5,8 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart' show TestWidgetsFlutterBinding;
+import 'package:flutter_test/flutter_test.dart';
 import 'package:in_app_purchase/src/in_app_purchase/purchase_details.dart';
-import 'package:test/test.dart';
 
 import 'package:flutter/widgets.dart' as widgets;
 import 'package:in_app_purchase/billing_client_wrappers.dart';
