92029e345964607c59266ee79e53ff63ecb1a4bd
Andrew Coutts <andrewjohncoutts@gmail.com>
2021-03-10 06:24:59 -0500

    Ensure the camera UI can handle when there's too many camera toggle options to fit horizontally on screen

--- packages/camera/camera/example/lib/main.dart
+++ packages/camera/camera/example/lib/main.dart
@@ -155,11 +155,17 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
           _modeControlRowWidget(),
           Padding(
             padding: const EdgeInsets.all(5.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+            child: Stack(
               children: <Widget>[
-                _cameraTogglesRowWidget(),
-                _thumbnailWidget(),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  padding: EdgeInsets.only(right: 90),
+                  child: _cameraTogglesRowWidget(),
+                ),
+                Positioned(
+                  right: 0,
+                  child: _thumbnailWidget(),
+                ),
               ],
             ),
           ),
@@ -221,35 +227,32 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
   Widget _thumbnailWidget() {
     final VideoPlayerController? localVideoController = videoController;
 
-    return Expanded(
-      child: Align(
-        alignment: Alignment.centerRight,
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            localVideoController == null && imageFile == null
-                ? Container()
-                : SizedBox(
-                    child: (localVideoController == null)
-                        ? Image.file(File(imageFile!.path))
-                        : Container(
-                            child: Center(
-                              child: AspectRatio(
-                                  aspectRatio:
-                                      localVideoController.value.size != null
-                                          ? localVideoController
-                                              .value.aspectRatio
-                                          : 1.0,
-                                  child: VideoPlayer(localVideoController)),
-                            ),
-                            decoration: BoxDecoration(
-                                border: Border.all(color: Colors.pink)),
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          localVideoController == null && imageFile == null
+              ? Container()
+              : SizedBox(
+                  child: (localVideoController == null)
+                      ? Image.file(File(imageFile!.path))
+                      : Container(
+                          child: Center(
+                            child: AspectRatio(
+                                aspectRatio:
+                                    localVideoController.value.size != null
+                                        ? localVideoController.value.aspectRatio
+                                        : 1.0,
+                                child: VideoPlayer(localVideoController)),
                           ),
-                    width: 64.0,
-                    height: 64.0,
-                  ),
-          ],
-        ),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.pink)),
+                        ),
+                  width: 64.0,
+                  height: 64.0,
+                ),
+        ],
       ),
     );
   }
