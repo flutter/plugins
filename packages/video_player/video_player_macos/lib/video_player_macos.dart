13e55fbef29f8239b5a3b3222634069b7153fc6f
Ben Hagen <c.ben.hagen@gmail.com>
2021-03-10 07:37:48 +0100

    [video_player] Add macOS implementation

---
+++ packages/video_player/video_player_macos/lib/video_player_macos.dart
@@ -0,0 +1,3 @@
+// The video_player_platform_interface defaults to MethodChannelVideoPlayer
+// as its instance, which is all the macOS implementation needs. This file
+// is here to silence warnings when publishing to pub.
