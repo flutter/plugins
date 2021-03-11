eb847e807042493d057b7ed4f2272a15b2324416
Pavel <p97p@ya.ru>
2021-03-11 04:23:11 +0300

    headers implementation

--- packages/video_player/video_player_platform_interface/lib/method_channel_video_player.dart
+++ packages/video_player/video_player_platform_interface/lib/method_channel_video_player.dart
@@ -37,6 +37,7 @@ class MethodChannelVideoPlayer extends VideoPlayerPlatform {
       case DataSourceType.network:
         message.uri = dataSource.uri;
         message.formatHint = _videoFormatStringMap[dataSource.formatHint];
+        message.httpHeaders = dataSource.httpHeaders;
         break;
       case DataSourceType.file:
         message.uri = dataSource.uri;
