9cf211317f54022cffa69249fac0f43262bc4805
Guilherme Girotto <guiga741@gmail.com>
2021-03-12 07:58:52 -0300

    Fixes FLTGoogleSignIn formatting

--- packages/google_sign_in/google_sign_in/ios/Classes/FLTGoogleSignInPlugin.m
+++ packages/google_sign_in/google_sign_in/ios/Classes/FLTGoogleSignInPlugin.m
@@ -77,14 +77,15 @@ static FlutterError *getFlutterError(NSError *error) {
                                                        ofType:@"plist"];
       if (path) {
         NSMutableDictionary *plist = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
-        BOOL hasDynamicClientId = [[call.arguments valueForKey:@"clientId"] isKindOfClass:[NSString class]];
+        BOOL hasDynamicClientId =
+            [[call.arguments valueForKey:@"clientId"] isKindOfClass:[NSString class]];
 
         if (hasDynamicClientId) {
           [GIDSignIn sharedInstance].clientID = [call.arguments valueForKey:@"clientId"];
         } else {
           [GIDSignIn sharedInstance].clientID = plist[kClientIdKey];
         }
-        
+
         [GIDSignIn sharedInstance].serverClientID = plist[kServerClientIdKey];
         [GIDSignIn sharedInstance].scopes = call.arguments[@"scopes"];
         [GIDSignIn sharedInstance].hostedDomain = call.arguments[@"hostedDomain"];
