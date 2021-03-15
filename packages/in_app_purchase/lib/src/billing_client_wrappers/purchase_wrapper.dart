1255402eebaed90621b1a525fc2c9b60da4dc8a7
Stuart Morgan <stuartmorgan@google.com>
2021-03-15 12:59:36 -0400

    Standardize more non-standard license block formatting

--- packages/in_app_purchase/lib/src/billing_client_wrappers/purchase_wrapper.dart
+++ packages/in_app_purchase/lib/src/billing_client_wrappers/purchase_wrapper.dart
@@ -1,6 +1,6 @@
 // Copyright 2019 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'dart:ui' show hashValues;
 import 'package:flutter/foundation.dart';
