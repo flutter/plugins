d460834f2796cefefa855c554c9ac1fef65a8e82
Stuart Morgan <stuartmorgan@google.com>
2021-03-15 16:09:54 -0400

    Add missing header blocks to image_picker

--- packages/image_picker/image_picker_platform_interface/lib/src/types/picked_file/picked_file.dart
+++ packages/image_picker/image_picker_platform_interface/lib/src/types/picked_file/picked_file.dart
@@ -1,3 +1,7 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 export 'lost_data.dart';
 export 'unsupported.dart'
     if (dart.library.html) 'html.dart'
--- packages/image_picker/image_picker_platform_interface/lib/src/types/types.dart
+++ packages/image_picker/image_picker_platform_interface/lib/src/types/types.dart
@@ -1,3 +1,7 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 export 'camera_device.dart';
 export 'image_source.dart';
 export 'retrieve_type.dart';
