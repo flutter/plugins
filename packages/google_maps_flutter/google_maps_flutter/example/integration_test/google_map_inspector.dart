c70c6eca5b17780faa9f4153c4d675e83a3a0c8b
stuartmorgan <stuartmorgan@google.com>
2021-03-16 17:50:51 -0400

    Add missing licenses, and add a check (#3720)
    Adds a new CI check that all code files have a copyright+license block (and that it's one we are expecting to see).
    
    Fixes the ~350 files (!) that did not have them. This includes all of the files in the .../example/ directories, following the example of flutter/flutter. (This does mean some manual intervention will be needed when generating new example directories in the future, but it's one-time per example.)
    
    Also standardized some variants that used different line breaks than most of the rest of the repo (likely added since I standardized them all a while ago, but didn't add a check for at the time to enforce going forward), to simplify the checks.
    
    Fixes flutter/flutter#77114

--- packages/google_maps_flutter/google_maps_flutter/example/integration_test/google_map_inspector.dart
+++ packages/google_maps_flutter/google_maps_flutter/example/integration_test/google_map_inspector.dart
@@ -1,6 +1,6 @@
-// Copyright 2019, the Chromium project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// Copyright 2019, the Chromium project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 // @dart=2.9
 
 import 'dart:typed_data';
--- packages/google_sign_in/google_sign_in_web/test/tests_exist_elsewhere_test.dart
+++ packages/google_sign_in/google_sign_in_web/test/tests_exist_elsewhere_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2019, the Flutter project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
