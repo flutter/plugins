a8551330e9939da054b24db03f15337cae9af753
Guilherme Girotto <guiga741@gmail.com>
2021-03-18 13:06:03 -0300

    [google_sign_in] Adds support to send `clientId` as a parameter (#3640)

--- packages/google_sign_in/google_sign_in/example/lib/main.dart
+++ packages/google_sign_in/google_sign_in/example/lib/main.dart
@@ -12,6 +12,8 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 GoogleSignIn _googleSignIn = GoogleSignIn(
+  // Optional clientId
+  // clientId: '479882132969-9i9aqik3jfjd7qhci1nqf0bm2g71rm1u.apps.googleusercontent.com',
   scopes: <String>[
     'email',
     'https://www.googleapis.com/auth/contacts.readonly',
