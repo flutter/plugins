c9fbf5a54108632d4c6bde6ddf6fa413331e9f0e
daniel.roek <daniel.roek@gmail.com>
2021-03-19 15:25:15 +0100

    Removed accidental \\\

--- packages/quick_actions/quick_actions_platform_interface/lib/types/types.dart
+++ packages/quick_actions/quick_actions_platform_interface/lib/types/types.dart
@@ -1,6 +1,6 @@
 // Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.\\\
+// found in the LICENSE file.
 
 export 'quick_action_handler.dart';
 export 'shortcut_item.dart';
