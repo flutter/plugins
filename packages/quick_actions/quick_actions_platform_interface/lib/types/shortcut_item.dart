f73a1cb6bd747652efe63cc32475a5c2c2a88103
daniel.roek <daniel.roek@gmail.com>
2021-03-19 15:14:30 +0100

    Added license in files

--- packages/quick_actions/quick_actions_platform_interface/lib/types/shortcut_item.dart
+++ packages/quick_actions/quick_actions_platform_interface/lib/types/shortcut_item.dart
@@ -1,3 +1,7 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.\\\
+
 /// Home screen quick-action shortcut item.
 class ShortcutItem {
   /// Constructs an instance with the given [type], [localizedTitle], and
