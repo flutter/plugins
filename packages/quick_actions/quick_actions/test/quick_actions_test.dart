c4d0cf4449de496e5baa4ed733667b1c0359c677
daniel.roek <daniel.roek@gmail.com>
2021-03-24 11:37:51 +0100

    Removed unused import

--- packages/quick_actions/quick_actions/test/quick_actions_test.dart
+++ packages/quick_actions/quick_actions/test/quick_actions_test.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
