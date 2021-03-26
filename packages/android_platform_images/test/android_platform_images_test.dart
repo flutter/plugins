f9e69a52d76315721c718481939ce961b61b0ab1
vimerzhao <vimerzhao@gmail.com>
2021-03-26 12:55:37 +0800

    delete unused unit test

--- packages/android_platform_images/test/android_platform_images_test.dart
+++ packages/android_platform_images/test/android_platform_images_test.dart
@@ -1,23 +1,2 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:android_platform_images/android_platform_images.dart';
-
 void main() {
-  const MethodChannel channel = MethodChannel('android_platform_images');
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await AndroidPlatformImages.platformVersion, '42');
-  });
 }
