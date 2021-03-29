65d14b038d7c31d2cf37a8c0d5b81eac267b03b3
math1man <aweiland@google.com>
2021-03-29 15:27:13 -0700

    Remove explicit layoutDirection from webview_android.dart. Despite the comment here, this layout direction affects the rendering of the scroll bar.

--- packages/webview_flutter/lib/src/webview_android.dart
+++ packages/webview_flutter/lib/src/webview_android.dart
@@ -47,10 +47,6 @@ class AndroidWebView implements WebViewPlatform {
               id, webViewPlatformCallbacksHandler));
         },
         gestureRecognizers: gestureRecognizers,
-        // WebView content is not affected by the Android view's layout direction,
-        // we explicitly set it here so that the widget doesn't require an ambient
-        // directionality.
-        layoutDirection: TextDirection.rtl,
         creationParams:
             MethodChannelWebViewPlatform.creationParamsToMap(creationParams),
         creationParamsCodec: const StandardMessageCodec(),
