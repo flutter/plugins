be5aa2eb02e23eee99b0ec61e4dec75aeb53c808
Robert Ancell <robert.ancell@canonical.com>
2021-03-30 16:07:33 +1300

    Move just the FFI code into a conditional import

---
+++ packages/path_provider/path_provider_linux/lib/src/get_application_id_real.dart
@@ -0,0 +1,38 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:ffi';
+import 'package:ffi/ffi.dart';
+
+// GApplication* g_application_get_default();
+typedef _g_application_get_default_c = IntPtr Function();
+typedef _g_application_get_default_dart = int Function();
+
+// const gchar* g_application_get_application_id(GApplication* application);
+typedef _g_application_get_application_id_c = Pointer<Utf8> Function(IntPtr);
+typedef _g_application_get_application_id_dart = Pointer<Utf8> Function(int);
+
+// Gets the application ID set in GApplication.
+String? getApplicationId() {
+  DynamicLibrary gio;
+  try {
+    gio = DynamicLibrary.open('libgio-2.0.so');
+  } on ArgumentError {
+    return null;
+  }
+  var _g_application_get_default = gio.lookupFunction<
+      _g_application_get_default_c,
+      _g_application_get_default_dart>('g_application_get_default');
+  var app = _g_application_get_default();
+  if (app == 0) return null;
+
+  var _g_application_get_application_id = gio.lookupFunction<
+          _g_application_get_application_id_c,
+          _g_application_get_application_id_dart>(
+      'g_application_get_application_id');
+  var app_id = _g_application_get_application_id(app);
+  if (app_id == null) return null;
+
+  return app_id.toDartString();
+}
--- packages/path_provider/path_provider_linux/lib/src/path_provider_linux.dart
+++ packages/path_provider/path_provider_linux/lib/src/path_provider_linux.dart
@@ -3,20 +3,12 @@
 // found in the LICENSE file.
 import 'dart:io';
 import 'dart:async';
-import 'dart:ffi';
-import 'package:ffi/ffi.dart';
 
 import 'package:xdg_directories/xdg_directories.dart' as xdg;
 import 'package:path/path.dart' as path;
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 
-// GApplication* g_application_get_default();
-typedef _g_application_get_default_c = IntPtr Function();
-typedef _g_application_get_default_dart = int Function();
-
-// const gchar* g_application_get_application_id(GApplication* application);
-typedef _g_application_get_application_id_c = Pointer<Utf8> Function(IntPtr);
-typedef _g_application_get_application_id_dart = Pointer<Utf8> Function(int);
+import 'get_application_id.dart';
 
 /// The linux implementation of [PathProviderPlatform]
 ///
@@ -32,30 +24,6 @@ class PathProviderLinux extends PathProviderPlatform {
     return Future.value("/tmp");
   }
 
-  // Gets the application ID set in GApplication.
-  String? _getApplicationId() {
-    DynamicLibrary gio;
-    try {
-      gio = DynamicLibrary.open('libgio-2.0.so');
-    } on ArgumentError {
-      return null;
-    }
-    var _g_application_get_default = gio.lookupFunction<
-        _g_application_get_default_c,
-        _g_application_get_default_dart>('g_application_get_default');
-    var app = _g_application_get_default();
-    if (app == 0) return null;
-
-    var _g_application_get_application_id = gio.lookupFunction<
-            _g_application_get_application_id_c,
-            _g_application_get_application_id_dart>(
-        'g_application_get_application_id');
-    var app_id = _g_application_get_application_id(app);
-    if (app_id == null) return null;
-
-    return app_id.toDartString();
-  }
-
   // Gets the name of this executable.
   Future<String> _getExecutableName() async {
     return path.basenameWithoutExtension(
@@ -64,7 +32,7 @@ class PathProviderLinux extends PathProviderPlatform {
 
   // Gets the unique ID for this application.
   Future<String> _getId() async {
-    var appId = _getApplicationId();
+    var appId = getApplicationId();
     if (appId != null) return appId;
 
     // Fall back to using the executable name.
