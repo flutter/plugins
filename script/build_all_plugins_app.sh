8c537533bfd462df103baa0b5c5a20e5469dd765
Stuart Morgan <stuartmorgan@google.com>
2021-03-30 15:46:45 -0400

    Ensure BRANCH_NAME is set

--- script/build_all_plugins_app.sh
+++ script/build_all_plugins_app.sh
@@ -41,6 +41,7 @@ echo "Excluding the following plugins: $ALL_EXCLUDED"
 
 # Master now creates null-safe app code by default; migrate stable so both
 # branches are building in the same mode.
+BRANCH_NAME="${BRANCH_NAME:-"$(git rev-parse --abbrev-ref HEAD)"}"
 if [[ "${BRANCH_NAME}" == "stable" ]]; then
   (cd $REPO_DIR/all_plugins && dart migrate --apply-changes)
 fi
