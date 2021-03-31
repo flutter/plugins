b222b25861fab2e032c877c2cb941d76f449a905
Robert Ancell <robert.ancell@canonical.com>
2021-03-31 13:55:55 +1300

    [path_provider] Update the Linux example template

--- packages/path_provider/path_provider_linux/example/linux/flutter/generated_plugin_registrant.cc
+++ packages/path_provider/path_provider_linux/example/linux/flutter/generated_plugin_registrant.cc
@@ -2,6 +2,10 @@
 //  Generated file. Do not edit.
 //
 
+// clang-format off
+
 #include "generated_plugin_registrant.h"
 
-void fl_register_plugins(FlPluginRegistry* registry) {}
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+}
