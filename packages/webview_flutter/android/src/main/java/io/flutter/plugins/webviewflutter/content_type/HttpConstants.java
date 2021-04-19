436f7809efaef6a203360b9ee6d6581fe6830216
Simon Joecks <simon.joecks@gmail.com>
2021-04-19 16:47:56 +0200

    Add ContentType detection from libadblockpluck-android [1]
    Normally the ContentType is passed down from the browser to
    the blocking mechanisms in the form of Mojom::ResourceType (see chromium).
    But this is unfortunatly not avialabe in the android webview
    thus we use the adblocker ContentType detection to guess the ContentType.
    
    Also this commit is wirering all the mecanisms in the shouldInterceptRequest
    method following the adblockeplus implemenation.
    
    [1] https://github.com/adblockplus/libadblockplus-android

---
+++ packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/content_type/HttpConstants.java
@@ -0,0 +1,35 @@
+package io.flutter.plugins.webviewflutter.content_type;
+
+public class HttpConstants {
+    public static final String HEADER_REFERRER = "Referer";
+    public static final String HEADER_REQUESTED_WITH = "X-Requested-With";
+    public static final String HEADER_REQUESTED_WITH_XMLHTTPREQUEST = "XMLHttpRequest";
+    public static final String HEADER_REQUESTED_RANGE = "Range";
+    public static final String HEADER_LOCATION = "Location";
+    public static final String HEADER_COOKIE = "Cookie";
+    public static final String HEADER_USER_AGENT = "User-Agent";
+    public static final String HEADER_ACCEPT = "Accept";
+    public static final String HEADER_REFRESH = "Refresh";
+    // use low-case strings as in WebResponse all header keys are lowered-case
+    public static final String HEADER_SET_COOKIE = "set-cookie";
+    public static final String HEADER_WWW_AUTHENTICATE = "www-authenticate";
+    public static final String HEADER_PROXY_AUTHENTICATE = "proxy-authenticate";
+    public static final String HEADER_EXPIRES = "expires";
+    public static final String HEADER_DATE = "date";
+    public static final String HEADER_RETRY_AFTER = "retry-after";
+    public static final String HEADER_LAST_MODIFIED = "last-modified";
+    public static final String HEADER_CONTENT_LENGTH = "content-length";
+
+    /**
+     * Possible values for request method argument (see `request(..)` method)
+     */
+    public static final String REQUEST_METHOD_GET = "GET";
+    public static final String REQUEST_METHOD_POST = "POST";
+    public static final String REQUEST_METHOD_HEAD = "HEAD";
+    public static final String REQUEST_METHOD_OPTIONS = "OPTIONS";
+    public static final String REQUEST_METHOD_PUT = "PUT";
+    public static final String REQUEST_METHOD_DELETE = "DELETE";
+    public static final String REQUEST_METHOD_TRACE = "TRACE";
+
+    public static final String MIME_TYPE_TEXT_HTML = "text/html";
+}
