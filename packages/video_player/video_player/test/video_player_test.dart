3c8d958ed653fdad2c6b709b42869ebab6ec15ac
KyleFin <5882840+KyleFin@users.noreply.github.com>
2021-04-21 23:21:31 -0600

    Restoring `main` fork branch to fetch from upstream (will create a new video_player_pause_on_complete remote branch in my fork).

--- packages/video_player/video_player/test/video_player_test.dart
+++ packages/video_player/video_player/test/video_player_test.dart
@@ -277,23 +277,6 @@ void main() {
       expect(fakeVideoPlayerPlatform.calls.last, 'setPlaybackSpeed');
     });
 
-    test('play restarts from beginning if video is at end', () async {
-      final VideoPlayerController controller = VideoPlayerController.network(
-        'https://127.0.0.1',
-      );
-      await controller.initialize();
-      const Duration nonzeroDuration = Duration(milliseconds: 100);
-      controller.value = controller.value.copyWith(duration: nonzeroDuration);
-      await controller.seekTo(nonzeroDuration);
-      expect(controller.value.isPlaying, isFalse);
-      expect(controller.value.position, nonzeroDuration);
-
-      await controller.play();
-
-      expect(controller.value.isPlaying, isTrue);
-      expect(controller.value.position, Duration.zero);
-    });
-
     test('setLooping', () async {
       final VideoPlayerController controller = VideoPlayerController.network(
         'https://127.0.0.1',
@@ -435,8 +418,6 @@ void main() {
           'https://127.0.0.1',
         );
         await controller.initialize();
-        const Duration nonzeroDuration = Duration(milliseconds: 100);
-        controller.value = controller.value.copyWith(duration: nonzeroDuration);
         expect(controller.value.isPlaying, isFalse);
         await controller.play();
         expect(controller.value.isPlaying, isTrue);
@@ -448,7 +429,7 @@ void main() {
         await tester.pumpAndSettle();
 
         expect(controller.value.isPlaying, isFalse);
-        expect(controller.value.position, nonzeroDuration);
+        expect(controller.value.position, controller.value.duration);
       });
 
       testWidgets('buffering status', (WidgetTester tester) async {
