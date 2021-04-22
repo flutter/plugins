49cc2393c9c922e2aa49303fb137f77afdd1c784
Robert Ancell <robert.ancell@canonical.com>
2021-04-23 11:23:08 +1200

    Local variables don't need private names

--- packages/path_provider/path_provider_linux/lib/src/get_application_id_real.dart
+++ packages/path_provider/path_provider_linux/lib/src/get_application_id_real.dart
@@ -6,12 +6,12 @@ import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 
 // GApplication* g_application_get_default();
-typedef _g_application_get_default_c = IntPtr Function();
-typedef _g_application_get_default_dart = int Function();
+typedef g_application_get_default_c = IntPtr Function();
+typedef g_application_get_default_dart = int Function();
 
 // const gchar* g_application_get_application_id(GApplication* application);
-typedef _g_application_get_application_id_c = Pointer<Utf8> Function(IntPtr);
-typedef _g_application_get_application_id_dart = Pointer<Utf8> Function(int);
+typedef g_application_get_application_id_c = Pointer<Utf8> Function(IntPtr);
+typedef g_application_get_application_id_dart = Pointer<Utf8> Function(int);
 
 /// Gets the application ID for this app.
 String? getApplicationId() {
@@ -21,17 +21,17 @@ String? getApplicationId() {
   } on ArgumentError {
     return null;
   }
-  var _g_application_get_default = gio.lookupFunction<
-      _g_application_get_default_c,
-      _g_application_get_default_dart>('g_application_get_default');
-  var app = _g_application_get_default();
+  var g_application_get_default = gio.lookupFunction<
+      g_application_get_default_c,
+      g_application_get_default_dart>('g_application_get_default');
+  var app = g_application_get_default();
   if (app == 0) return null;
 
-  var _g_application_get_application_id = gio.lookupFunction<
-          _g_application_get_application_id_c,
-          _g_application_get_application_id_dart>(
+  var g_application_get_application_id = gio.lookupFunction<
+          g_application_get_application_id_c,
+          g_application_get_application_id_dart>(
       'g_application_get_application_id');
-  var app_id = _g_application_get_application_id(app);
+  var app_id = g_application_get_application_id(app);
   if (app_id == null) return null;
 
   return app_id.toDartString();
