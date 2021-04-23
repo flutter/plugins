2402e1c59530898711fbbd1ed2f1a05a6ec98761
Robert Ancell <robert.ancell@canonical.com>
2021-04-23 12:18:24 +1200

    Add back code dropped in merge

--- packages/path_provider/path_provider_linux/test/path_provider_linux_test.dart
+++ packages/path_provider/path_provider_linux/test/path_provider_linux_test.dart
@@ -3,8 +3,16 @@
 // found in the LICENSE file.
 import 'package:flutter_test/flutter_test.dart';
 import 'package:path_provider_linux/path_provider_linux.dart';
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  PathProviderLinux.register();
+
+  setUp(() {});
+
+  tearDown(() {});
+
   test('getTemporaryPath', () async {
     final PathProviderPlatform plugin = PathProviderPlatform.instance;
     expect(await plugin.getTemporaryPath(), '/tmp');
