b4e6989f22bb2dab7dc5b632f44084c4f47a9592
Phil Quitslund <pq@users.noreply.github.com>
2021-04-23 10:10:48 -0700

    [path_provider_*] code cleanup: sort directives (#3823)
    * sort directives
    
    * fmt

--- packages/path_provider/path_provider/example/integration_test/path_provider_test.dart
+++ packages/path_provider/path_provider/example/integration_test/path_provider_test.dart
@@ -6,8 +6,8 @@
 
 import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
