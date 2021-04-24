0a4c3afb11a9201b0a4437c0503ee09d2474cc73
najBaby <40722453+najBaby@users.noreply.github.com>
2021-04-24 22:44:25 +0200

    Enable TLS 1.2 for android api 19 and older
    Add TLSSocketFactory to CustomSSLSocketFactory

--- packages/video_player/video_player/android/src/main/java/io/flutter/plugins/videoplayer/CustomSSLSocketFactory.java
+++ packages/video_player/video_player/android/src/main/java/io/flutter/plugins/videoplayer/CustomSSLSocketFactory.java
@@ -6,7 +6,9 @@ package io.flutter.plugins.videoplayer;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import android.os.Build;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import javax.net.ssl.SSLContext;
@@ -20,6 +22,10 @@ public class CustomSSLSocketFactory extends SSLSocketFactory {
     SSLContext context = SSLContext.getInstance("TLS");
     context.init(null, null, null);
     sslSocketFactory = context.getSocketFactory();
+    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+      sslSocketFactory = new TLSSocketFactory(context.getSocketFactory());
+    }
+    connection.setSSLSocketFactory(sslSocketFactory);
   }
 
   @Override
@@ -71,4 +77,70 @@ public class CustomSSLSocketFactory extends SSLSocketFactory {
     }
     return socket;
   }
+}
+
+public static class TLSSocketFactory extends SSLSocketFactory {
+
+    private SSLSocketFactory internalSSLSocketFactory;
+
+    public TLSSocketFactory(SSLSocketFactory delegate) throws KeyManagementException, NoSuchAlgorithmException {
+        internalSSLSocketFactory = delegate;
+    }
+
+    @Override
+    public String[] getDefaultCipherSuites() {
+        return internalSSLSocketFactory.getDefaultCipherSuites();
+    }
+
+    @Override
+    public String[] getSupportedCipherSuites() {
+        return internalSSLSocketFactory.getSupportedCipherSuites();
+    }
+
+    @Override
+    public Socket createSocket(Socket s, String host, int port, boolean autoClose) throws IOException {
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket(s, host, port, autoClose));
+    }
+
+    @Override
+    public Socket createSocket(String host, int port) throws IOException, UnknownHostException {
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket(host, port));
+    }
+
+    @Override
+    public Socket createSocket(String host, int port, InetAddress localHost, int localPort) throws IOException, UnknownHostException {
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket(host, port, localHost, localPort));
+    }
+
+    @Override
+    public Socket createSocket(InetAddress host, int port) throws IOException {
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket(host, port));
+    }
+
+    @Override
+    public Socket createSocket(InetAddress address, int port, InetAddress localAddress, int localPort) throws IOException {
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket(address, port, localAddress, localPort));
+    }
+
+    /*
+     * Utility methods
+     */
+
+    private static Socket enableTLSOnSocket(Socket socket) {
+        if (socket != null && (socket instanceof SSLSocket)
+                && isTLSServerEnabled((SSLSocket) socket)) { // skip the fix if server doesn't provide there TLS version
+            ((SSLSocket) socket).setEnabledProtocols(new String[]{"TLS_v1_1", "TLS_v1_2"});
+        }
+        return socket;
+    }
+
+    private static boolean isTLSServerEnabled(SSLSocket sslSocket) {
+        System.out.println("__prova__ :: " + sslSocket.getSupportedProtocols().toString());
+        for (String protocol : sslSocket.getSupportedProtocols()) {
+            if (protocol.equals("TLS_v1_1") || protocol.equals("TLS_v1_2")) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
