467a0550da895d470a67e5d41b7fbb56db455177
James Clarke <james@clarkezone.net>
2021-04-27 20:09:43 -0700

    dum di dum

--- packages/url_launcher/url_launcher_windows/windows/include/url_launcher_windows/url_launcher_plugin.h
+++ packages/url_launcher/url_launcher_windows/windows/include/url_launcher_windows/url_launcher_plugin.h
@@ -1,7 +1,6 @@
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 #ifndef PACKAGES_URL_LAUNCHER_URL_LAUNCHER_WINDOWS_WINDOWS_INCLUDE_URL_LAUNCHER_WINDOWS_URL_LAUNCHER_PLUGIN_H_
 #define PACKAGES_URL_LAUNCHER_URL_LAUNCHER_WINDOWS_WINDOWS_INCLUDE_URL_LAUNCHER_WINDOWS_URL_LAUNCHER_PLUGIN_H_
 
