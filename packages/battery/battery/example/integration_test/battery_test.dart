963679cbf57c83c6a6d872676d0ddbfcfb397f38
Stuart Morgan <stuartmorgan@google.com>
2021-05-06 12:45:49 -0400

    Fix iOS battery integration test to pass on simulator

--- packages/battery/battery/example/integration_test/battery_test.dart
+++ packages/battery/battery/example/integration_test/battery_test.dart
@@ -4,6 +4,7 @@
 
 // @dart = 2.9
 
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:battery/battery.dart';
 import 'package:integration_test/integration_test.dart';
@@ -13,7 +14,18 @@ void main() {
 
   testWidgets('Can get battery level', (WidgetTester tester) async {
     final Battery battery = Battery();
-    final int batteryLevel = await battery.batteryLevel;
+    int batteryLevel;
+    try {
+      batteryLevel = await battery.batteryLevel;
+    } on PlatformException catch (e) {
+      // The "UNAVAIBLE" error just means that the system reported the battery
+      // level as unknown (e.g., the test is running on simulator); it still
+      // indicates that the plugin itself is working as expected, so consider it
+      // as passing.
+      if (e.code == 'UNAVAILABLE') {
+        batteryLevel = 1;
+      }
+    }
     expect(batteryLevel, isNotNull);
   });
 }
