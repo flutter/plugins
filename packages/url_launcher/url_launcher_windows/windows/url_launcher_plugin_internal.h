7c34c0d5368021f1db8dd210e81db9f99487b31b
James Clarke <james@clarkezone.net>
2021-05-14 06:58:02 -0700

    Fix the build

--- packages/url_launcher/url_launcher_windows/windows/url_launcher_plugin_internal.h
+++ packages/url_launcher/url_launcher_windows/windows/url_launcher_plugin_internal.h
@@ -4,6 +4,8 @@
 #ifndef PACKAGES_URL_LAUNCHER_URL_LAUNCHER_WINDOWS_WINDOWS_URL_LAUNCHER_PLUGIN_INTERNAL_H_
 #define PACKAGES_URL_LAUNCHER_URL_LAUNCHER_WINDOWS_WINDOWS_URL_LAUNCHER_PLUGIN_INTERNAL_H_
 
+#include "include/url_launcher_windows/url_launcher_plugin.h"
+
 void OpenLink(std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> value, std::string url_string);
 
 void CanLaunch(std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> value, std::string url_string);
