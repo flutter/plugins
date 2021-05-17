4f6db34236b773178b966a0412d129b589b37413
Emmanuel Garcia <egarciad@google.com>
2021-05-17 12:13:59 -0700

    Bump min Android SDK to the version required at runtime (#3894)

--- packages/webview_flutter/README.md
+++ packages/webview_flutter/README.md
@@ -26,7 +26,19 @@ implementation. Note that on Android versions prior to Android 10 Hybrid Composi
 
 ### Using Hybrid Composition
 
-To enable hybrid composition, set `WebView.platform = SurfaceAndroidWebView();` in `initState()`.
+1. Set the `minSdkVersion` in `android/app/build.gradle`:
+
+```groovy
+android {
+    defaultConfig {
+        minSdkVersion 19
+    }
+}
+```
+
+This means that app will only be available for users that run Android SDK 19 or higher.
+
+2. To enable hybrid composition, set `WebView.platform = SurfaceAndroidWebView();` in `initState()`.
 For example:
 
 ```dart
--- script/tool/lib/src/create_all_plugins_app_command.dart
+++ script/tool/lib/src/create_all_plugins_app_command.dart
@@ -81,7 +81,13 @@ class CreateAllPluginsAppCommand extends PluginCommand {
 
     final StringBuffer newGradle = StringBuffer();
     for (final String line in gradleFile.readAsLinesSync()) {
-      newGradle.writeln(line);
+      if (line.contains('minSdkVersion 16')) {
+        // Android SDK 20 is required by Google maps.
+        // Android SDK 19 is required by WebView.
+        newGradle.writeln('minSdkVersion 20');
+      } else {
+        newGradle.writeln(line);
+      }
       if (line.contains('defaultConfig {')) {
         newGradle.writeln('        multiDexEnabled true');
       } else if (line.contains('dependencies {')) {
