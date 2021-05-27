ba2d0ae10c1399cebd0878dd340f8f8fb3b93829
Maurits van Beusekom <maurits@baseflow.com>
2021-05-27 19:26:25 +0200

    Added support to request list of purchases (#3944)

--- packages/in_app_purchase/in_app_purchase_android/CHANGELOG.md
+++ packages/in_app_purchase/in_app_purchase_android/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1
+
+* Added support to request a list of active subscriptions and non-consumed one-time purchases on Android, through the `InAppPurchaseAndroidPlatformAddition.queryPastPurchases` method.
+
 ## 0.1.0+1
 
 * Migrate maven repository from jcenter to mavenCentral.
---
+++ packages/in_app_purchase/in_app_purchase_android/lib/src/types/query_purchase_details_response.dart
@@ -0,0 +1,27 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:in_app_purchase_platform_interface/in_app_purchase_platform_interface.dart';
+
+import 'types.dart';
+
+/// The response object for fetching the past purchases.
+///
+/// An instance of this class is returned in [InAppPurchaseConnection.queryPastPurchases].
+class QueryPurchaseDetailsResponse {
+  /// Creates a new [QueryPurchaseDetailsResponse] object with the provider information.
+  QueryPurchaseDetailsResponse({required this.pastPurchases, this.error});
+
+  /// A list of successfully fetched past purchases.
+  ///
+  /// If there are no past purchases, or there is an [error] fetching past purchases,
+  /// this variable is an empty List.
+  /// You should verify the purchase data using [PurchaseDetails.verificationData] before using the [PurchaseDetails] object.
+  final List<GooglePlayPurchaseDetails> pastPurchases;
+
+  /// The error when fetching past purchases.
+  ///
+  /// If the fetch is successful, the value is `null`.
+  final IAPError? error;
+}
