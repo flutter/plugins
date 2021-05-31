1cfae5933ff439ccceaef420f26cd54507d680de
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2021-05-31 10:34:50 +0200

    use transparent bg

--- packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
+++ packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
@@ -6,6 +6,7 @@ package io.flutter.plugins.webviewflutter;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.display.DisplayManager;
 import android.os.Build;
 import android.os.Handler;
@@ -136,7 +137,10 @@ public class FlutterWebView implements PlatformView, MethodCallHandler {
     if (shouldLoad) {
       // allow app cache
       webView.getSettings().setAppCacheEnabled(true);
-      
+
+      // use transparent bg
+      webView.setBackgroundColor(Color.TRANSPARENT);
+
       // Allow local storage.
       webView.getSettings().setDomStorageEnabled(true);
       webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
