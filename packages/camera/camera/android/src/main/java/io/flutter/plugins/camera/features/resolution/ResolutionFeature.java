24af3676d7ff62f61fde5d382a268e976e43b3c4
Maurits van Beusekom <maurits@baseflow.com>
2021-06-16 17:06:52 +0200

    Format

--- packages/camera/camera/android/src/main/java/io/flutter/plugins/camera/features/resolution/ResolutionFeature.java
+++ packages/camera/camera/android/src/main/java/io/flutter/plugins/camera/features/resolution/ResolutionFeature.java
@@ -64,7 +64,7 @@ public class ResolutionFeature extends CameraFeature<ResolutionPreset> {
     }
 
     switch (preset) {
-      // All of these cases deliberately fall through to get the best available profile.
+        // All of these cases deliberately fall through to get the best available profile.
       case max:
         if (CamcorderProfile.hasProfile(cameraId, CamcorderProfile.QUALITY_HIGH)) {
           return CamcorderProfile.get(cameraId, CamcorderProfile.QUALITY_HIGH);
