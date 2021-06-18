af0a29b68eaaa866cc5c0baf0f89452f465e7d10
Ferry Spiering <ferry.spiering@gmail.com>
2021-06-18 10:31:29 +0200

    Remove redundant conversion from context to activity

--- packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FileChooserLauncher.java
+++ packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FileChooserLauncher.java
@@ -12,7 +12,6 @@ import static io.flutter.plugins.webviewflutter.Constants.EXTRA_TITLE;
 import static io.flutter.plugins.webviewflutter.Constants.EXTRA_TYPE;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.ContextWrapper;
@@ -25,40 +24,28 @@ import androidx.core.content.ContextCompat;
 
 public class FileChooserLauncher extends BroadcastReceiver {
 
-  private Activity activity;
+  private Context context;
   private String title;
   private String type;
   private boolean showCameraOption;
   private ValueCallback<Uri[]> filePathCallback;
 
   public FileChooserLauncher(
-      Context context,
-      String title,
-      String type,
-      boolean showCameraOption,
-      ValueCallback<Uri[]> filePathCallback) {
-    this.activity = getActivityByContext(context);
+          Context context,
+          String title,
+          String type,
+          boolean showCameraOption,
+          ValueCallback<Uri[]> filePathCallback) {
+    this.context = context;
     this.title = title;
     this.type = type;
     this.showCameraOption = showCameraOption;
     this.filePathCallback = filePathCallback;
   }
 
-  private Activity getActivityByContext(Context context) {
-    if (context == null) {
-      return null;
-    } else if (context instanceof Activity) {
-      return (Activity) context;
-    } else if (context instanceof ContextWrapper) {
-      return getActivityByContext(((ContextWrapper) context).getBaseContext());
-    }
-
-    return null;
-  }
-
   private boolean hasCameraPermission() {
-    return ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA)
-        == PackageManager.PERMISSION_GRANTED;
+    return ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+            == PackageManager.PERMISSION_GRANTED;
   }
 
   public void start() {
@@ -67,33 +54,33 @@ public class FileChooserLauncher extends BroadcastReceiver {
     } else {
       IntentFilter intentFilter = new IntentFilter();
       intentFilter.addAction(ACTION_REQUEST_CAMERA_PERMISSION_FINISHED);
-      activity.registerReceiver(this, intentFilter);
+      context.registerReceiver(this, intentFilter);
 
-      activity.startActivity(new Intent(activity, RequestCameraPermissionActivity.class));
+      context.startActivity(new Intent(context, RequestCameraPermissionActivity.class));
     }
   }
 
   private void showFileChooser() {
     IntentFilter intentFilter = new IntentFilter(ACTION_FILE_CHOOSER_FINISHED);
-    activity.registerReceiver(this, intentFilter);
+    context.registerReceiver(this, intentFilter);
 
-    Intent intent = new Intent(activity, FileChooserActivity.class);
+    Intent intent = new Intent(context, FileChooserActivity.class);
     intent.putExtra(EXTRA_TITLE, title);
     intent.putExtra(EXTRA_TYPE, type);
     intent.putExtra(EXTRA_SHOW_CAMERA_OPTION, showCameraOption && hasCameraPermission());
-    activity.startActivity(intent);
+    context.startActivity(intent);
   }
 
   @Override
   public void onReceive(Context context, Intent intent) {
     if (intent.getAction().equals(ACTION_REQUEST_CAMERA_PERMISSION_FINISHED)) {
-      activity.unregisterReceiver(this);
+      context.unregisterReceiver(this);
       showFileChooser();
     } else if (intent.getAction().equals(ACTION_FILE_CHOOSER_FINISHED)) {
       String uriString = intent.getStringExtra(EXTRA_FILE_URI);
       Uri[] result = uriString != null ? new Uri[] {Uri.parse(uriString)} : null;
       filePathCallback.onReceiveValue(result);
-      activity.unregisterReceiver(this);
+      context.unregisterReceiver(this);
       filePathCallback = null;
     }
   }
