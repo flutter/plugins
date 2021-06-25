24a33b061e99b5829af262d647fef9200b58237e
Maurits van Beusekom <maurits@baseflow.com>
2021-06-25 09:43:17 +0200

    Added showPriceConsent feature to example app

---
+++ packages/in_app_purchase/in_app_purchase_ios/example/lib/example_payment_queue_delegate.dart
@@ -0,0 +1,19 @@
+import 'package:in_app_purchase_ios/store_kit_wrappers.dart';
+
+/// Example implementation of the
+/// [`SKPaymentQueueDelegate`](https://developer.apple.com/documentation/storekit/skpaymentqueuedelegate?language=objc).
+///
+/// The payment queue delegate can be implementated to provide information
+/// needed to complete transactions.
+class ExamplePaymentQueueDelegate implements SKPaymentQueueDelegateWrapper {
+  @override
+  bool shouldContinueTransaction(
+      SKPaymentTransactionWrapper transaction, SKStorefrontWrapper storefront) {
+    return true;
+  }
+
+  @override
+  bool shouldShowPriceConsent() {
+    return false;
+  }
+}
