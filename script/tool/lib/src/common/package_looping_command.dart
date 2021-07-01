0f3dfb94ef5b298b54efc6163da1719c36ba83fa
stuartmorgan <stuartmorgan@google.com>
2021-07-01 16:25:21 -0700

    [flutter_plugin_tools] Add a summary for successful runs (#4118)
    Add a summary to the end of successful runs for everything using the new looping base command, similar to what we do for summarizing failures. This will make it easy to manually check results for PRs that we know should be changing the set of run packages (adding a new package, adding a new test type to a package, adding a new test type to the tool), as well as spot-checking when we see unexpected results (e.g., looking back and why a PR didn't fail CI when we discover that it should have).
    
    To support better surfacing skips, this restructures the return value of `runForPackage` to have "skip" as one of the options. As a result of it being a return value, packages that used `printSkip` to indicate that *parts* of the command were being skipped have been changed to no longer do that.
    
    Fixes https://github.com/flutter/flutter/issues/85626

--- script/tool/lib/src/common/package_looping_command.dart
+++ script/tool/lib/src/common/package_looping_command.dart
@@ -11,6 +11,48 @@ import 'core.dart';
 import 'plugin_command.dart';
 import 'process_runner.dart';
 
+/// Possible outcomes of a command run for a package.
+enum RunState {
+  /// The command succeeded for the package.
+  succeeded,
+
+  /// The command was skipped for the package.
+  skipped,
+
+  /// The command failed for the package.
+  failed,
+}
+
+/// The result of a [runForPackage] call.
+class PackageResult {
+  /// A successful result.
+  PackageResult.success() : this._(RunState.succeeded);
+
+  /// A run that was skipped as explained in [reason].
+  PackageResult.skip(String reason)
+      : this._(RunState.skipped, <String>[reason]);
+
+  /// A run that failed.
+  ///
+  /// If [errors] are provided, they will be listed in the summary, otherwise
+  /// the summary will simply show that the package failed.
+  PackageResult.fail([List<String> errors = const <String>[]])
+      : this._(RunState.failed, errors);
+
+  const PackageResult._(this.state, [this.details = const <String>[]]);
+
+  /// The state the package run completed with.
+  final RunState state;
+
+  /// Information about the result:
+  /// - For `succeeded`, this is empty.
+  /// - For `skipped`, it contains a single entry describing why the run was
+  ///   skipped.
+  /// - For `failed`, it contains zero or more specific error details to be
+  ///   shown in the summary.
+  final List<String> details;
+}
+
 /// An abstract base class for a command that iterates over a set of packages
 /// controlled by a standard set of flags, running some actions on each package,
 /// and collecting and reporting the success/failure of those actions.
@@ -21,6 +63,15 @@ abstract class PackageLoopingCommand extends PluginCommand {
     ProcessRunner processRunner = const ProcessRunner(),
     GitDir? gitDir,
   }) : super(packagesDir, processRunner: processRunner, gitDir: gitDir);
+
+  /// Packages that had at least one [logWarning] call.
+  final Set<Directory> _packagesWithWarnings = <Directory>{};
+
+  /// Number of warnings that happened outside of a [runForPackage] call.
+  int _otherWarningCount = 0;
+
+  /// The package currently being run by [runForPackage].
+  Directory? _currentPackage;
 
   /// Called during [run] before any calls to [runForPackage]. This provides an
   /// opportunity to fail early if the command can't be run (e.g., because the
@@ -33,7 +84,7 @@ abstract class PackageLoopingCommand extends PluginCommand {
   /// be included in the final error summary (e.g., a command that only has a
   /// single failure mode), or strings that should be listed for that package
   /// in the final summary. An empty list indicates success.
-  Future<List<String>> runForPackage(Directory package);
+  Future<PackageResult> runForPackage(Directory package);
 
   /// Called during [run] after all calls to [runForPackage]. This provides an
   /// opportunity to do any cleanup of run-level state.
@@ -71,18 +122,19 @@ abstract class PackageLoopingCommand extends PluginCommand {
 
   // ----------------------------------------
 
-  /// A convenience constant for [runForPackage] success that's more
-  /// self-documenting than the value.
-  static const List<String> success = <String>[];
-
-  /// A convenience constant for [runForPackage] failure without additional
-  /// context that's more self-documenting than the value.
-  static const List<String> failure = <String>[''];
-
-  /// Prints a message using a standard format indicating that the package was
-  /// skipped, with an explanation of why.
-  void printSkip(String reason) {
-    print(Colorize('SKIPPING: $reason')..darkGray());
+  /// Logs that a warning occurred, and prints `warningMessage` in yellow.
+  ///
+  /// Warnings are not surfaced in CI summaries, so this is only useful for
+  /// highlighting something when someone is already looking though the log
+  /// messages. DO NOT RELY on someone noticing a warning; instead, use it for
+  /// things that might be useful to someone debugging an unexpected result.
+  void logWarning(String warningMessage) {
+    print(Colorize(warningMessage)..yellow());
+    if (_currentPackage != null) {
+      _packagesWithWarnings.add(_currentPackage!);
+    } else {
+      ++_otherWarningCount;
+    }
   }
 
   /// Returns the identifying name to use for [package].
@@ -110,41 +162,44 @@ abstract class PackageLoopingCommand extends PluginCommand {
 
   @override
   Future<void> run() async {
+    _packagesWithWarnings.clear();
+    _otherWarningCount = 0;
+    _currentPackage = null;
+
     await initializeRun();
 
     final List<Directory> packages = includeSubpackages
         ? await getPackages().toList()
         : await getPlugins().toList();
 
-    final Map<Directory, List<String>> results = <Directory, List<String>>{};
+    final Map<Directory, PackageResult> results = <Directory, PackageResult>{};
     for (final Directory package in packages) {
+      _currentPackage = package;
       _printPackageHeading(package);
-      results[package] = await runForPackage(package);
+      final PackageResult result = await runForPackage(package);
+      if (result.state == RunState.skipped) {
+        print(Colorize('${indentation}SKIPPING: ${result.details.first}')
+          ..darkGray());
+      }
+      results[package] = result;
     }
+    _currentPackage = null;
 
     completeRun();
 
     // If there were any errors reported, summarize them and exit.
-    if (results.values.any((List<String> failures) => failures.isNotEmpty)) {
-      const String indentation = '  ';
-      printError(failureListHeader);
-      for (final Directory package in packages) {
-        final List<String> errors = results[package]!;
-        if (errors.isNotEmpty) {
-          final String errorIndentation = indentation * 2;
-          String errorDetails = errors.join('\n$errorIndentation');
-          if (errorDetails.isNotEmpty) {
-            errorDetails = ':\n$errorIndentation$errorDetails';
-          }
-          printError(
-              '$indentation${getPackageDescription(package)}$errorDetails');
-        }
-      }
-      printError(failureListFooter);
+    if (results.values
+        .any((PackageResult result) => result.state == RunState.failed)) {
+      _printFailureSummary(packages, results);
       throw ToolExit(exitCommandFoundErrors);
     }
 
-    printSuccess('\n\nNo issues found!');
+    // Otherwise, print a summary of what ran for ease of auditing that all the
+    // expected tests ran.
+    _printRunSummary(packages, results);
+
+    print('\n');
+    printSuccess('No issues found!');
   }
 
   /// Prints the status message indicating that the command is being run for
@@ -166,5 +221,87 @@ abstract class PackageLoopingCommand extends PluginCommand {
       heading = '$heading...';
     }
     print(Colorize(heading)..cyan());
+  }
+
+  /// Prints a summary of packges run, packages skipped, and warnings.
+  void _printRunSummary(
+      List<Directory> packages, Map<Directory, PackageResult> results) {
+    final Set<Directory> skippedPackages = results.entries
+        .where((MapEntry<Directory, PackageResult> entry) =>
+            entry.value.state == RunState.skipped)
+        .map((MapEntry<Directory, PackageResult> entry) => entry.key)
+        .toSet();
+    final int skipCount = skippedPackages.length;
+    // Split the warnings into those from packages that ran, and those that
+    // were skipped.
+    final Set<Directory> _skippedPackagesWithWarnings =
+        _packagesWithWarnings.intersection(skippedPackages);
+    final int skippedWarningCount = _skippedPackagesWithWarnings.length;
+    final int runWarningCount =
+        _packagesWithWarnings.length - skippedWarningCount;
+
+    final String runWarningSummary =
+        runWarningCount > 0 ? ' ($runWarningCount with warnings)' : '';
+    final String skippedWarningSummary =
+        runWarningCount > 0 ? ' ($skippedWarningCount with warnings)' : '';
+    print('------------------------------------------------------------');
+    if (hasLongOutput) {
+      _printPerPackageRunOverview(packages, skipped: skippedPackages);
+    }
+    print(
+        'Ran for ${packages.length - skipCount} package(s)$runWarningSummary');
+    if (skipCount > 0) {
+      print('Skipped $skipCount package(s)$skippedWarningSummary');
+    }
+    if (_otherWarningCount > 0) {
+      print('$_otherWarningCount warnings not associated with a package');
+    }
+  }
+
+  /// Prints a one-line-per-package overview of the run results for each
+  /// package.
+  void _printPerPackageRunOverview(List<Directory> packages,
+      {required Set<Directory> skipped}) {
+    print('Run overview:');
+    for (final Directory package in packages) {
+      final bool hadWarning = _packagesWithWarnings.contains(package);
+      Colorize summary;
+      if (skipped.contains(package)) {
+        if (hadWarning) {
+          summary = Colorize('skipped (with warning)')..lightYellow();
+        } else {
+          summary = Colorize('skipped')..darkGray();
+        }
+      } else {
+        if (hadWarning) {
+          summary = Colorize('ran (with warning)')..yellow();
+        } else {
+          summary = Colorize('ran')..green();
+        }
+      }
+      print('  ${getPackageDescription(package)} - $summary');
+    }
+    print('');
+  }
+
+  /// Prints a summary of all of the failures from [results].
+  void _printFailureSummary(
+      List<Directory> packages, Map<Directory, PackageResult> results) {
+    const String indentation = '  ';
+    printError(failureListHeader);
+    for (final Directory package in packages) {
+      final PackageResult result = results[package]!;
+      if (result.state == RunState.failed) {
+        final String errorIndentation = indentation * 2;
+        String errorDetails = '';
+        if (result.details.isNotEmpty) {
+          errorDetails =
+              ':\n$errorIndentation${result.details.join('\n$errorIndentation')}';
+        }
+        printError(
+            '$indentation${getPackageDescription(package)}$errorDetails');
+      }
+    }
+    printError(failureListFooter);
   }
 }
