7aa4d09ffc110310e3bbdc97dae1df0363f7b7fb
Rene Floor <rene@baseflow.com>
2021-07-01 12:02:28 +0200

    Fixed test

--- packages/in_app_purchase/in_app_purchase_ios/example/ios/RunnerTests/TranslatorTests.m
+++ packages/in_app_purchase/in_app_purchase_ios/example/ios/RunnerTests/TranslatorTests.m
@@ -149,7 +149,7 @@
 
 - (void)testLocaleToMap {
   if (@available(iOS 10.0, *)) {
-    NSLocale *system = NSLocale.systemLocale;
+    NSLocale *system = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
     NSDictionary *map = [FIAObjectTranslator getMapFromNSLocale:system];
     XCTAssertEqualObjects(map[@"currencySymbol"], system.currencySymbol);
     XCTAssertEqualObjects(map[@"countryCode"], system.countryCode);
