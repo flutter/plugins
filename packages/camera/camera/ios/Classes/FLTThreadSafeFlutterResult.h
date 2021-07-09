bd7d8ebe4341e7e19eb97cd5b9bacdf43fd45a5a
Rene Floor <rene@baseflow.com>
2021-07-09 15:54:31 +0200

    copyright

--- packages/camera/camera/ios/Classes/FLTThreadSafeFlutterResult.h
+++ packages/camera/camera/ios/Classes/FLTThreadSafeFlutterResult.h
@@ -1,9 +1,6 @@
-//
-//  Header.h
-//  camera
-//
-//  Created by Rene Floor on 07/07/2021.
-//
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #import <Flutter/Flutter.h>
 
