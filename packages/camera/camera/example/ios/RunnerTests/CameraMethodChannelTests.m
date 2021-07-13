71e321e5dca8634129f414d9ba3923146f76eaaa
Rene Floor <rene@baseflow.com>
2021-07-13 11:14:34 +0200

    Extend unit test

--- packages/camera/camera/example/ios/RunnerTests/CameraMethodChannelTests.m
+++ packages/camera/camera/example/ios/RunnerTests/CameraMethodChannelTests.m
@@ -5,6 +5,7 @@
 @import camera;
 @import XCTest;
 @import AVFoundation;
+#import <OCMock/OCMock.h>
 
 @interface FLTThreadSafeFlutterResult : NSObject
 @property(readonly, nonatomic) FlutterResult flutterResult;
@@ -46,9 +47,21 @@
 }
 
 - (void)testCreate_ShouldCallResultOnMainThread {
+  // Setup mocks for initWithCameraName method
+  id avCaptureDeviceInputMock = OCMClassMock([AVCaptureDeviceInput class]);
+  OCMStub([avCaptureDeviceInputMock deviceInputWithDevice:[OCMArg any] error:[OCMArg anyObjectRef]])
+      .andReturn([AVCaptureInput alloc]);
+
+  id avCaptureSessionMock = OCMClassMock([AVCaptureSession class]);
+  OCMStub([avCaptureSessionMock alloc]).andReturn(avCaptureSessionMock);
+  OCMStub([avCaptureSessionMock canSetSessionPreset:[OCMArg any]]).andReturn(YES);
+
+  // Setup method call
   dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
 
-  FlutterMethodCall *call = [FlutterMethodCall methodCallWithMethodName:@"create" arguments:nil];
+  FlutterMethodCall *call = [FlutterMethodCall
+      methodCallWithMethodName:@"create"
+                     arguments:@{@"resolutionPreset" : @"medium", @"enableAudio" : @(1)}];
   __block id result = nil;
   MockFLTThreadSafeFlutterResult *resultObject =
       [[MockFLTThreadSafeFlutterResult alloc] initWithResultCallback:^(id actualResult) {
@@ -56,13 +69,23 @@
         dispatch_semaphore_signal(semaphore);
       }];
 
+  // Call handleMethodCall
   [_camera handleMethodCallWithThreadSafeResult:call result:resultObject];
+
+  // Don't expect a result yet
+  XCTAssertNil(result);
 
   while (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW)) {
     [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode
                              beforeDate:[NSDate dateWithTimeIntervalSinceNow:0]];
   }
+  // Expect a result after waiting for thread to switch
   XCTAssertNotNil(result);
+
+  // Verify the result
+  NSDictionary *dictionaryResult = (NSDictionary *)result;
+  XCTAssertNotNil(dictionaryResult);
+  XCTAssert([[dictionaryResult allKeys] containsObject:@"cameraId"]);
 }
 
 @end
