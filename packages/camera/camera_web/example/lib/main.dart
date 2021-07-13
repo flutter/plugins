22417958dd8e180dcc2bf2dec45931e4eee08bad
Bartosz Selwesiuk <bselwesiuk@gmail.com>
2021-07-13 12:22:10 +0200

    refactor: make testing MyApp a stateless widget

--- packages/camera/camera_web/example/lib/main.dart
+++ packages/camera/camera_web/example/lib/main.dart
@@ -4,17 +4,10 @@
 
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 /// App for testing
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Directionality(
