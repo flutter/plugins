6cc702faed1981aea46a63ee1fad6e6ef1e23b20
Jenn Magder <magder@google.com>
2021-07-15 15:33:03 -0700

    Angle imports

--- packages/google_sign_in/google_sign_in/ios/Classes/FLTGoogleSignInPlugin_Private.h
+++ packages/google_sign_in/google_sign_in/ios/Classes/FLTGoogleSignInPlugin_Private.h
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "FLTGoogleSignInPlugin.h"
+#import <google_sign_in/FLTGoogleSignInPlugin.h>
 
 @class GIDSignIn;
 
