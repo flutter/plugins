ea0a31a9e84bda865f5de91498aa82361c139c1e
Bartosz Selwesiuk <bselwesiuk@gmail.com>
2021-07-16 12:38:37 +0200

    test: add availableCameras tests

--- packages/camera/camera_web/example/integration_test/helpers/mocks.dart
+++ packages/camera/camera_web/example/integration_test/helpers/mocks.dart
@@ -4,6 +4,7 @@
 
 import 'dart:html';
 
+import 'package:camera_web/src/camera_settings.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockWindow extends Mock implements Window {}
@@ -12,7 +13,37 @@ class MockNavigator extends Mock implements Navigator {}
 
 class MockMediaDevices extends Mock implements MediaDevices {}
 
+class MockCameraSettings extends Mock implements CameraSettings {}
+
 class MockMediaStreamTrack extends Mock implements MediaStreamTrack {}
+
+/// A fake [MediaStream] that returns the provided [_videoTracks].
+class FakeMediaStream extends Fake implements MediaStream {
+  FakeMediaStream(this._videoTracks);
+
+  final List<MediaStreamTrack> _videoTracks;
+
+  @override
+  List<MediaStreamTrack> getVideoTracks() => _videoTracks;
+}
+
+/// A fake [MediaDeviceInfo] that returns the provided [_deviceId], [_label] and [_kind].
+class FakeMediaDeviceInfo extends Fake implements MediaDeviceInfo {
+  FakeMediaDeviceInfo(this._deviceId, this._label, this._kind);
+
+  final String _deviceId;
+  final String _label;
+  final String _kind;
+
+  @override
+  String? get deviceId => _deviceId;
+
+  @override
+  String? get label => _label;
+
+  @override
+  String? get kind => _kind;
+}
 
 /// A fake [DomException] that returns the provided error [_name].
 class FakeDomException extends Fake implements DomException {
