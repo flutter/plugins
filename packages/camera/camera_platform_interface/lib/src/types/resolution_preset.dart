b24f6f0e436ff88a52ca238ee523a9b80a0a23f8
Bartosz Selwesiuk <bselwesiuk@gmail.com>
2021-07-19 18:11:57 +0200

    feat: add createCamera implementation

--- packages/camera/camera_platform_interface/lib/src/types/resolution_preset.dart
+++ packages/camera/camera_platform_interface/lib/src/types/resolution_preset.dart
@@ -6,10 +6,10 @@
 ///
 /// If a preset is not available on the camera being used a preset of lower quality will be selected automatically.
 enum ResolutionPreset {
-  /// 352x288 on iOS, 240p (320x240) on Android
+  /// 352x288 on iOS, 240p (320x240) on Android and Web
   low,
 
-  /// 480p (640x480 on iOS, 720x480 on Android)
+  /// 480p (640x480 on iOS, 720x480 on Android and Web)
   medium,
 
   /// 720p (1280x720)
