337dc68d7b67eee25ac6ca7ebf071751a9d760c9
Bartosz Selwesiuk <bselwesiuk@gmail.com>
2021-07-20 21:30:50 +0200

    [camera_web] Add Camera class to control video and take pictures (#4168)

--- packages/camera/camera_web/lib/src/shims/dart_ui_real.dart
+++ packages/camera/camera_web/lib/src/shims/dart_ui_real.dart
@@ -2,5 +2,4 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export 'camera_error_codes.dart';
-export 'camera_options.dart';
+export 'dart:ui';
