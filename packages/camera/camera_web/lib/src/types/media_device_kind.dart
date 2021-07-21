132dddd4dd0bee91764c196fb6f5631f8f95c758
Bartosz Selwesiuk <bselwesiuk@gmail.com>
2021-07-21 11:51:55 +0200

    docs: add copyright

--- packages/camera/camera_web/lib/src/types/media_device_kind.dart
+++ packages/camera/camera_web/lib/src/types/media_device_kind.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /// A kind of a media device.
 ///
 /// See: https://developer.mozilla.org/en-US/docs/Web/API/MediaDeviceInfo/kind
