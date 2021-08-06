8bb8ce2f8b47387e5f7f9ca26b41325bde3912c7
Bartosz Selwesiuk <bselwesiuk@gmail.com>
2021-08-06 13:57:38 +0200

    feat: add screen orientation type

--- packages/camera/camera_web/lib/src/types/types.dart
+++ packages/camera/camera_web/lib/src/types/types.dart
@@ -1,9 +1,9 @@
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 export 'camera_error_code.dart';
 export 'camera_metadata.dart';
 export 'camera_options.dart';
 export 'camera_web_exception.dart';
 export 'media_device_kind.dart';
+export 'orientation_type.dart';
