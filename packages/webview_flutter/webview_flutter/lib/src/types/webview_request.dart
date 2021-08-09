91f152b9709d8648f1f01b98137510d3080ae612
yusufdag <yusuf@baseflow.com>
2021-08-09 11:13:50 +0200

    Add licence comment

--- packages/webview_flutter/webview_flutter/lib/src/types/webview_request.dart
+++ packages/webview_flutter/webview_flutter/lib/src/types/webview_request.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'dart:typed_data';
 
 /// Defines the supported HTTP methods for loading a page in the [WebView].
