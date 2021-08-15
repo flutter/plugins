ed78bb5e2b78532f6b9bdc1b2aea57dd1c7a99ca
Maurice Parrish <bmparr@google.com>
2021-08-15 13:04:02 -0700

    add unit tests

---
+++ packages/quick_actions/quick_actions/android/src/test/java/io/flutter/plugins/quickactions/QuickActionsTest.java
@@ -0,0 +1,50 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugins.quickactions;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import org.junit.Test;
+import java.nio.ByteBuffer;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.StandardMethodCodec;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class QuickActionsTest {
+  private static class TestBinaryMessenger implements BinaryMessenger {
+    public MethodCall lastMethodCall;
+
+    @Override
+    public void send(@NonNull String channel, @Nullable ByteBuffer message) {
+      send(channel, message, null);
+    }
+
+    @Override
+    public void send(@NonNull String channel, @Nullable ByteBuffer message, @Nullable final BinaryReply callback) {
+      if (channel.equals("plugins.flutter.io/quick_actions")) {
+        lastMethodCall = StandardMethodCodec.INSTANCE.decodeMethodCall((ByteBuffer) message.position(0));
+      }
+    }
+
+    @Override
+    public void setMessageHandler(@NonNull String channel, @Nullable BinaryMessageHandler handler) {
+      // Do nothing.
+    }
+  }
+
+  @Test
+  public void canAttachToEngine() {
+    final TestBinaryMessenger testBinaryMessenger = new TestBinaryMessenger();
+    final FlutterPluginBinding mockPluginBinding = mock(FlutterPluginBinding.class);
+    when(mockPluginBinding.getBinaryMessenger()).thenReturn(testBinaryMessenger);
+
+    final QuickActionsPlugin plugin = new QuickActionsPlugin();
+    plugin.onAttachedToEngine(mockPluginBinding);
+  }
+}
