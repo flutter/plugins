73f5bda982272a67dde25caedecaad9720a33cf2
Dan Gillespie <daniel.gillespie0949@gmail.com>
2021-08-25 17:35:21 -0700

    added self sign ssl for exoplayer

--- packages/video_player/video_player/android/src/main/java/io/flutter/plugins/videoplayer/CustomSSLSocketFactory.java
+++ packages/video_player/video_player/android/src/main/java/io/flutter/plugins/videoplayer/CustomSSLSocketFactory.java
@@ -9,16 +9,54 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class CustomSSLSocketFactory extends SSLSocketFactory {
   private SSLSocketFactory sslSocketFactory;
 
+  private static TrustManager[] disableSslCertificateVerify() {
+    return new TrustManager[] {
+            new X509TrustManager() {
+              public X509Certificate[] getAcceptedIssuers() {
+                X509Certificate[] myTrustedAnchors = new X509Certificate[0];
+                return myTrustedAnchors;
+              }
+
+              @Override
+              public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+
+              @Override
+              public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+            }
+    };
+  }
+
+  private static HostnameVerifier trustAllSslHostnameVerifier() {
+    return new HostnameVerifier() {
+      @Override
+      public boolean verify(String s, SSLSession sslSession) {
+        return true;
+      }
+    };
+  }
+
   public CustomSSLSocketFactory() throws KeyManagementException, NoSuchAlgorithmException {
     SSLContext context = SSLContext.getInstance("TLS");
-    context.init(null, null, null);
+    context.init(null, disableSslCertificateVerify(), new SecureRandom());
+
+    HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
+    HttpsURLConnection.setDefaultHostnameVerifier(trustAllSslHostnameVerifier());
+
     sslSocketFactory = context.getSocketFactory();
   }
 
