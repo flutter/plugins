e11863d77d20c9f92fd2024e55630053d010727d
Uday Bidkar <udaybidkar@google.com>
2021-09-03 17:39:32 -0700

    Adds a class for GroundOverlay

---
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/ground_overlay.dart
@@ -0,0 +1,214 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:ui' show hashValues;
+
+import 'package:flutter/foundation.dart';
+import 'package:meta/meta.dart' show immutable;
+
+import 'types.dart';
+
+/// Uniquely identifies a [GroundOverlay] among [GoogleMap] ground overlays.
+@immutable
+class GroundOverlayId extends MapsObjectId<GroundOverlay> {
+  /// Creates an immutable identifier for a [GroundOverlay].
+  const GroundOverlayId(String value) : super(value);
+}
+
+/// An image overlays that is tied to latitude/longitude coordinates, so it
+/// moves when you drag or zoom the map.
+///
+/// A ground overlay is an image that is fixed to a map. Unlike markers, ground
+/// overlays are oriented against the Earth's surface rather than the screen,
+/// so rotating, tilting or zooming the map will change the orientation of the
+/// image. Ground overlays are useful when you wish to fix a single image at one
+/// area on the map. If you want to add extensive imagery that covers a large
+/// portion of the map, you should consider a Tile overlay.
+///
+class GroundOverlay implements MapsObject {
+  /// Creates an immutable representation of a [GroundOverlay] to draw on
+  /// [GoogleMap].
+  const GroundOverlay({
+    required this.groundOverlayId,
+    required this.image,
+    this.anchorU,
+    this.anchorV,
+    this.bearing = 0,
+    this.isClickable = false,
+    this.position,
+    this.width,
+    this.height,
+    this.positionFromBounds,
+    this.transparency = 0,
+    this.isVisible = true,
+    this.zIndex = 0,
+  }) : assert(((height == null &&
+                    width == null &&
+                    position == null &&
+                    positionFromBounds != null) ||
+                (height != null &&
+                    width != null &&
+                    position != null &&
+                    positionFromBounds == null) ||
+                (height == null &&
+                    width != null &&
+                    position != null &&
+                    positionFromBounds == null)) &&
+            (transparency >= 0.0 && transparency <= 1.0) &&
+            (anchorU == null || (anchorU >= 0.0 && anchorU <= 1.0)) &&
+            (anchorV == null || (anchorV >= 0.0 && anchorV <= 1.0)));
+
+  @override
+  MapsObjectId get mapsId => groundOverlayId;
+
+  /// Uniquely identifies a [GroundOverlay].
+  final GroundOverlayId groundOverlayId;
+
+  /// Specifies the image for this ground overlay.
+  final BitmapDescriptor image;
+
+  /// Specifies u-coordinate of the anchor.
+  final double? anchorU;
+
+  /// Specifies v-coordinate of the anchor.
+  final double? anchorV;
+
+  /// Specifies the bearing of the ground overlay in degrees clockwise from
+  /// north.
+  final double bearing;
+
+  /// Specifies whether the ground overlay is clickable.
+  final bool isClickable;
+
+  /// The location on the map LatLng to which the anchor point in the given
+  /// image will remain fixed. The anchor will remain fixed to the position on
+  /// the ground when transformations are applied (e.g., setDimensions,
+  /// setBearing, etc.). At least [width] must be specified along with this
+  /// parameter. The image will be scaled to fit the dimensions specified.
+  final LatLng? position;
+
+  /// The width of the overlay (in meters). Must be provided along with the
+  /// [position]. The [height], if not specified, will be determined
+  /// automatically based on the image aspect ratio if not specified.
+  final double? width;
+
+  /// The height of the overlay (in meters). May be provided along with the
+  /// [position]. If not specified, it'll will be determined automatically based
+  /// on the image aspect ratio.
+  final double? height;
+
+  /// Specifies the position for this ground overlay.
+  final LatLngBounds? positionFromBounds;
+
+  /// Specifies the transparency of the ground overlay.
+  final double transparency;
+
+  /// Specifies the visibility for the ground overlay.
+  final bool isVisible;
+
+  /// Specifies the ground overlay's zIndex, i.e., the order in which it will be
+  /// drawn.
+  final double zIndex;
+
+  /// Creates a new [GroundOverlay] object whose values are the same as this
+  /// instance, unless overwritten by the specified parameters.
+  GroundOverlay copyWith({
+    GroundOverlayId? groundOverlayIdParam,
+    BitmapDescriptor? imageParam,
+    double? anchorUParam,
+    double? anchorVParam,
+    double? bearingParam,
+    bool? isClickableParam,
+    LatLng? positionParam,
+    double? widthParam,
+    double? heightParam,
+    LatLngBounds? positionFromBoundsParam,
+    double? transparencyParam,
+    bool? isVisibleParam,
+    double? zIndexParam,
+  }) {
+    return GroundOverlay(
+      groundOverlayId: groundOverlayIdParam ?? groundOverlayId,
+      image: imageParam ?? image,
+      anchorU: anchorUParam ?? anchorU,
+      anchorV: anchorVParam ?? anchorV,
+      bearing: bearingParam ?? bearing,
+      isClickable: isClickableParam ?? isClickable,
+      position: positionParam ?? position,
+      width: widthParam ?? width,
+      height: heightParam ?? height,
+      positionFromBounds: positionFromBoundsParam ?? positionFromBounds,
+      transparency: transparencyParam ?? transparency,
+      isVisible: isVisibleParam ?? isVisible,
+      zIndex: zIndexParam ?? zIndex,
+    );
+  }
+
+  @override
+  clone() => copyWith();
+
+  @override
+  Object toJson() {
+    final Map<String, Object> json = <String, Object>{};
+
+    void addIfPresent(String fieldName, Object? value) {
+      if (value != null) {
+        json[fieldName] = value;
+      }
+    }
+
+    addIfPresent("groundOverlayId", groundOverlayId.value);
+    addIfPresent("image", image);
+    addIfPresent("anchorU", anchorU);
+    addIfPresent("anchorV", anchorV);
+    addIfPresent("bearing", bearing);
+    addIfPresent("isClickable", isClickable);
+    addIfPresent("position", position);
+    addIfPresent("width", width);
+    addIfPresent("height", height);
+    addIfPresent("positionFromBounds", positionFromBounds);
+    addIfPresent("transparency", transparency);
+    addIfPresent("isVisible", isVisible);
+    addIfPresent("zIndex", zIndex);
+
+    return json;
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    return other is GroundOverlay &&
+        groundOverlayId == other.groundOverlayId &&
+        image == other.image &&
+        anchorU == other.anchorU &&
+        anchorV == other.anchorV &&
+        bearing == other.bearing &&
+        isClickable == other.isClickable &&
+        position == other.position &&
+        width == other.width &&
+        height == other.height &&
+        positionFromBounds == other.positionFromBounds &&
+        transparency == other.transparency &&
+        isVisible == other.isVisible &&
+        zIndex == other.zIndex;
+  }
+
+  @override
+  int get hashCode => hashValues(
+      groundOverlayId,
+      image,
+      anchorU,
+      anchorV,
+      bearing,
+      isClickable,
+      position,
+      width,
+      height,
+      positionFromBounds,
+      transparency,
+      isVisible,
+      zIndex);
+}
---
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/test/types/ground_overlay_test.dart
@@ -0,0 +1,165 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:ui' show hashValues;
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
+import 'package:google_maps_flutter_platform_interface/src/types/ground_overlay.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('ground overlay id tests', () {
+    test('equality', () async {
+      const GroundOverlayId id1 = GroundOverlayId('1');
+      const GroundOverlayId id2 = GroundOverlayId('1');
+      const GroundOverlayId id3 = GroundOverlayId('2');
+      expect(id1, id2);
+      expect(id1, isNot(id3));
+    });
+
+    test('toString', () async {
+      const GroundOverlayId id1 = GroundOverlayId('1');
+      expect(id1.toString(), 'GroundOverlayId(1)');
+    });
+  });
+
+  group('ground overlay tests', () {
+    test('toJson returns correct format', () async {
+      const GroundOverlay groundOverlay = GroundOverlay(
+          groundOverlayId: GroundOverlayId('id'),
+          image: BitmapDescriptor.defaultMarker,
+          anchorU: 0.75,
+          anchorV: 0.5,
+          bearing: 2,
+          isClickable: true,
+          position: LatLng(30, 50),
+          width: 2,
+          transparency: 0.7,
+          isVisible: true,
+          zIndex: 5);
+      final Object json = groundOverlay.toJson();
+
+      expect(json, <String, Object>{
+        'groundOverlayId': 'id',
+        'image': BitmapDescriptor.defaultMarker,
+        'anchorU': moreOrLessEquals(0.75),
+        'anchorV': moreOrLessEquals(0.5),
+        'bearing': 2,
+        'isClickable': true,
+        'position': LatLng(30, 50),
+        'width': 2,
+        'transparency': moreOrLessEquals(0.7),
+        'isVisible': true,
+        'zIndex': 5,
+      });
+    });
+
+    test('invalid transparency throws', () async {
+      expect(
+          () => GroundOverlay(
+              groundOverlayId: GroundOverlayId('id'),
+              image: BitmapDescriptor.defaultMarker,
+              transparency: -0.1),
+          throwsAssertionError);
+      expect(
+          () => GroundOverlay(
+              groundOverlayId: GroundOverlayId('id2'),
+              image: BitmapDescriptor.defaultMarker,
+              transparency: 1.2),
+          throwsAssertionError);
+    });
+
+    test('equality', () async {
+      const GroundOverlay groundOverlay1 = GroundOverlay(
+          groundOverlayId: GroundOverlayId('id'),
+          image: BitmapDescriptor.defaultMarker,
+          anchorU: 0.75,
+          anchorV: 0.5,
+          bearing: 2,
+          isClickable: true,
+          position: LatLng(30, 50),
+          width: 2,
+          transparency: 0.7,
+          isVisible: true,
+          zIndex: 5);
+      const GroundOverlay groundOverlaySameValues = GroundOverlay(
+          groundOverlayId: GroundOverlayId('id'),
+          image: BitmapDescriptor.defaultMarker,
+          anchorU: 0.75,
+          anchorV: 0.5,
+          bearing: 2,
+          isClickable: true,
+          position: LatLng(30, 50),
+          width: 2,
+          transparency: 0.7,
+          isVisible: true,
+          zIndex: 5);
+      const GroundOverlay groundOverlayDifferentId = GroundOverlay(
+          groundOverlayId: GroundOverlayId('id2'),
+          image: BitmapDescriptor.defaultMarker,
+          anchorU: 0.75,
+          anchorV: 0.5,
+          bearing: 2,
+          isClickable: true,
+          position: LatLng(30, 50),
+          width: 2,
+          transparency: 0.7,
+          isVisible: true,
+          zIndex: 5);
+      expect(groundOverlay1, groundOverlaySameValues);
+      expect(groundOverlay1, isNot(groundOverlayDifferentId));
+    });
+
+    test('clone', () async {
+      // Set non-default values for every parameter.
+      const GroundOverlay groundOverlay = GroundOverlay(
+          groundOverlayId: GroundOverlayId('id'),
+          image: BitmapDescriptor.defaultMarker,
+          anchorU: 0.75,
+          anchorV: 0.5,
+          bearing: 2,
+          isClickable: true,
+          position: LatLng(30, 50),
+          width: 2,
+          transparency: 0.7,
+          isVisible: true,
+          zIndex: 5);
+      expect(groundOverlay, groundOverlay.clone());
+    });
+
+    test('hashCode', () async {
+      const GroundOverlay groundOverlay = GroundOverlay(
+          groundOverlayId: GroundOverlayId('id'),
+          image: BitmapDescriptor.defaultMarker,
+          anchorU: 0.75,
+          anchorV: 0.5,
+          bearing: 2,
+          isClickable: true,
+          position: LatLng(30, 50),
+          width: 2,
+          transparency: 0.7,
+          isVisible: true,
+          zIndex: 5);
+
+      expect(
+          groundOverlay.hashCode,
+          hashValues(
+              groundOverlay.groundOverlayId,
+              groundOverlay.image,
+              groundOverlay.anchorU,
+              groundOverlay.anchorV,
+              groundOverlay.bearing,
+              groundOverlay.isClickable,
+              groundOverlay.position,
+              groundOverlay.width,
+              groundOverlay.height,
+              groundOverlay.positionFromBounds,
+              groundOverlay.transparency,
+              groundOverlay.isVisible,
+              groundOverlay.zIndex));
+    });
+  });
+}
