b0e89c3df64442f8ff0b030a3e6c6b058b9ceb83
Rebar Ahmad <rebar.ahmad@gmail.com>
2021-09-05 15:09:16 +0200

    Add macOS Support

---
+++ packages/in_app_purchase/in_app_purchase_macos/lib/store_kit_wrappers.dart
@@ -0,0 +1,11 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+export 'src/store_kit_wrappers/sk_payment_queue_delegate_wrapper.dart';
+export 'src/store_kit_wrappers/sk_payment_queue_wrapper.dart';
+export 'src/store_kit_wrappers/sk_payment_transaction_wrappers.dart';
+export 'src/store_kit_wrappers/sk_product_wrapper.dart';
+export 'src/store_kit_wrappers/sk_receipt_manager.dart';
+export 'src/store_kit_wrappers/sk_request_maker.dart';
+export 'src/store_kit_wrappers/sk_storefront_wrapper.dart';
