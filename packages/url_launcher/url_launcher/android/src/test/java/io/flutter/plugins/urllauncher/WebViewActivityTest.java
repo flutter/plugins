758698c5921cea3540e39e1090031c31d5c71a5d
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2021-09-27 21:30:37 +0800

    format

--- packages/url_launcher/url_launcher/android/src/test/java/io/flutter/plugins/urllauncher/WebViewActivityTest.java
+++ packages/url_launcher/url_launcher/android/src/test/java/io/flutter/plugins/urllauncher/WebViewActivityTest.java
@@ -4,26 +4,10 @@
 
 package io.flutter.plugins.urllauncher;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import android.os.Bundle;
-import androidx.test.core.app.ApplicationProvider;
-import io.flutter.plugin.common.BinaryMessenger;
-import io.flutter.plugin.common.BinaryMessenger.BinaryMessageHandler;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel.Result;
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.Assert.assertEquals;
 import org.robolectric.RobolectricTestRunner;
 
 @RunWith(RobolectricTestRunner.class)
