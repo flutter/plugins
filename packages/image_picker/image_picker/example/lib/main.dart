60e0b9f35d641a06a9afca8ff40d37168a2972aa
balvinderz <balvindersi2@gmail.com>
2021-09-30 22:12:15 +0530

    remove path dependancy

--- packages/image_picker/image_picker/example/lib/main.dart
+++ packages/image_picker/image_picker/example/lib/main.dart
@@ -115,7 +115,6 @@ class _MyHomePageState extends State<MyHomePage> {
             maxHeight: maxHeight,
             imageQuality: quality,
           );
-          print(pickedFile?.name ??"");
           setState(() {
             _imageFile = pickedFile;
           });
