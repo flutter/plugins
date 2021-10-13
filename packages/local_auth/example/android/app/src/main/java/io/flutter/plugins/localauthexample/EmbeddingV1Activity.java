8279bcbe7dd0f10791d235422a7007c9785dd814
Chukwuemeka Ibebugwu <emex4gman@gmail.com>
2021-10-13 13:32:18 -0400

    fixed format

--- packages/local_auth/example/android/app/src/main/java/io/flutter/plugins/localauthexample/EmbeddingV1Activity.java
+++ packages/local_auth/example/android/app/src/main/java/io/flutter/plugins/localauthexample/EmbeddingV1Activity.java
@@ -4,15 +4,8 @@
 
 package io.flutter.plugins.localauthexample;
 
-import dev.flutter.plugins.integration_test.IntegrationTestPlugin;
 
-import androidx.annotation.NonNull;
-import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.embedding.android.FlutterFragmentActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
-import io.flutter.embedding.android.FlutterActivity;
-
 
 @SuppressWarnings("deprecation")
-public class EmbeddingV1Activity extends FlutterFragmentActivity {
-}
+public class EmbeddingV1Activity extends FlutterFragmentActivity {}
