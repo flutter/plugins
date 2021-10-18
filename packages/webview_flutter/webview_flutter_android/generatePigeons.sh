ff4b6cfc64284a4cc8c1be81e421029ea4317b15
Maurice Parrish <bmparr@google.com>
2021-10-18 10:34:48 -0700

    add dart files

---
+++ packages/webview_flutter/webview_flutter_android/generatePigeons.sh
@@ -0,0 +1,5 @@
+flutter pub run pigeon \
+--input pigeons/android_webview.dart \
+--dart_out lib/src/android_webview.pigeon.dart \
+--java_out ./android/src/main/java/io/flutter/plugins/webviewflutter/GeneratedAndroidWebView.java \
+--java_package io.flutter.plugins.webviewflutter
