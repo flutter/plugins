e129b9e1ebf171e8ea8e21d04e13a34f9f3fd250
Chris Yang <ychris@google.com>
2021-10-21 15:55:13 -0700

    add commented out test back

--- packages/google_sign_in/google_sign_in/test/google_sign_in_test.dart
+++ packages/google_sign_in/google_sign_in/test/google_sign_in_test.dart
@@ -351,8 +351,8 @@ void main() {
 
       expect(auth.accessToken, '456');
       expect(auth.idToken, '123');
-      // fix deprecated_member_use_from_same_package
-      // expect(auth.serverAuthCode, '789');
+      // ignore: deprecated_member_use_from_same_package
+      expect(auth.serverAuthCode, '789');
       expect(
         log,
         <Matcher>[
