6cf641125463726b4d6c2f1906b765c5ebc6f5ce
BeMacized <info@bemacized.net>
2021-10-25 15:42:41 +0200

    Add missing license

--- packages/webview_flutter/webview_flutter_platform_interface/test/src/types/webview_request_test.dart
+++ packages/webview_flutter/webview_flutter_platform_interface/test/src/types/webview_request_test.dart
@@ -1,5 +1,8 @@
-import 'dart:typed_data';
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
+import 'dart:typed_data';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:webview_flutter_platform_interface/src/types/types.dart';
 
