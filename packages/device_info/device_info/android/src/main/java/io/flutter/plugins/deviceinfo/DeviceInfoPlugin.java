05361b793e92e9f90da0d32bc8c1b7082f976b81
Aaron Clarke <aaclarke@google.com>
2021-10-27 16:41:51 -0700

    [device_info] started using Background Platform Channels

--- packages/device_info/device_info/android/src/main/java/io/flutter/plugins/deviceinfo/DeviceInfoPlugin.java
+++ packages/device_info/device_info/android/src/main/java/io/flutter/plugins/deviceinfo/DeviceInfoPlugin.java
@@ -5,13 +5,18 @@
 package io.flutter.plugins.deviceinfo;
 
 import android.content.Context;
+import android.util.Log;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodCodec;
+import io.flutter.plugin.common.StandardMethodCodec;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 /** DeviceInfoPlugin */
 public class DeviceInfoPlugin implements FlutterPlugin {
-
+  static final String TAG = "DeviceInfoPlugin";
   MethodChannel channel;
 
   /** Plugin registration. */
@@ -32,7 +37,24 @@ public class DeviceInfoPlugin implements FlutterPlugin {
   }
 
   private void setupMethodChannel(BinaryMessenger messenger, Context context) {
-    channel = new MethodChannel(messenger, "plugins.flutter.io/device_info");
+    String channelName = "plugins.flutter.io/device_info";
+    // TODO(gaaclarke): Remove reflection guard when https://github.com/flutter/engine/pull/29147
+    // becomes available on the stable branch.
+    try {
+      Class methodChannelClass = Class.forName("io.flutter.plugin.common.MethodChannel");
+      Class taskQueueClass = Class.forName("io.flutter.plugin.common.BinaryMessenger$TaskQueue");
+      Method makeBackgroundTaskQueue = messenger.getClass().getMethod("makeBackgroundTaskQueue");
+      Object taskQueue = makeBackgroundTaskQueue.invoke(messenger);
+      Constructor<MethodChannel> constructor =
+          methodChannelClass.getConstructor(
+              BinaryMessenger.class, String.class, MethodCodec.class, taskQueueClass);
+      channel =
+          constructor.newInstance(messenger, channelName, StandardMethodCodec.INSTANCE, taskQueue);
+      Log.d(TAG, "Use TaskQueues.");
+    } catch (Exception ex) {
+      channel = new MethodChannel(messenger, channelName);
+      Log.d(TAG, "Don't use TaskQueues.");
+    }
     final MethodCallHandlerImpl handler =
         new MethodCallHandlerImpl(context.getContentResolver(), context.getPackageManager());
     channel.setMethodCallHandler(handler);
