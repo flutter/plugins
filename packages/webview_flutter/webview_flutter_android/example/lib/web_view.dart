06e24d10975ae472804653358d39db4f2ad0d0b2
Nick Bradshaw <nibradshaw@ebay.com>
2021-10-28 10:35:55 -0700

    Fixing some formatting issues after some cherry picking from previous PRS

--- packages/webview_flutter/webview_flutter_android/example/lib/web_view.dart
+++ packages/webview_flutter/webview_flutter_android/example/lib/web_view.dart
@@ -567,7 +567,6 @@ class WebViewController {
     bool? debuggingEnabled;
     bool? zoomEnabled;
     WebSetting<String?> userAgent = WebSetting.absent();
-    bool? zoomEnabled;
     if (currentValue.javascriptMode != newValue.javascriptMode) {
       javascriptMode = newValue.javascriptMode;
     }
