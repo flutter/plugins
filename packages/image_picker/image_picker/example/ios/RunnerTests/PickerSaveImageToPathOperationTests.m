aa57cf454713ac987a06170a509bcbde2b124faa
Tran Giang Long <trangianglong4003@gmail.com>
2021-10-30 14:45:30 +0200

    fix style

--- packages/image_picker/image_picker/example/ios/RunnerTests/PickerSaveImageToPathOperationTests.m
+++ packages/image_picker/image_picker/example/ios/RunnerTests/PickerSaveImageToPathOperationTests.m
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <XCTest/XCTest.h>
 #import <OCMock/OCMock.h>
 #import <UniformTypeIdentifiers/UniformTypeIdentifiers.h>
+#import <XCTest/XCTest.h>
 
 @import image_picker;
 
@@ -14,63 +14,72 @@
 
 @implementation PickerSaveImageToPathOperationTests
 
-- (void)testSaveWebPImage API_AVAILABLE(ios(14))  {
-    NSURL *imageURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"webpImage" withExtension:@"webp"];
-    NSItemProvider *itemProvider = [[NSItemProvider alloc] initWithContentsOfURL: imageURL];
-    PHPickerResult *result = [self createPickerResult:itemProvider withIdentifier:UTTypeWebP.identifier];
-    
-    [self saveImageToPath:result];
+- (void)testSaveWebPImage API_AVAILABLE(ios(14)) {
+  NSURL *imageURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"webpImage"
+                                                             withExtension:@"webp"];
+  NSItemProvider *itemProvider = [[NSItemProvider alloc] initWithContentsOfURL:imageURL];
+  PHPickerResult *result = [self createPickerResult:itemProvider
+                                     withIdentifier:UTTypeWebP.identifier];
+
+  [self saveImageToPath:result];
 }
 
 - (void)testSavePNGImage API_AVAILABLE(ios(14)) {
-    NSURL *imageURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"pngImage" withExtension:@"png"];
-    NSItemProvider *itemProvider = [[NSItemProvider alloc] initWithContentsOfURL: imageURL];
-    PHPickerResult *result = [self createPickerResult:itemProvider withIdentifier:UTTypeWebP.identifier];
-    
-    [self saveImageToPath:result];
+  NSURL *imageURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"pngImage"
+                                                             withExtension:@"png"];
+  NSItemProvider *itemProvider = [[NSItemProvider alloc] initWithContentsOfURL:imageURL];
+  PHPickerResult *result = [self createPickerResult:itemProvider
+                                     withIdentifier:UTTypeWebP.identifier];
+
+  [self saveImageToPath:result];
 }
 
 - (void)testSaveJPGImage API_AVAILABLE(ios(14)) {
-    NSURL *imageURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"jpgImage" withExtension:@"jpg"];
-    NSItemProvider *itemProvider = [[NSItemProvider alloc] initWithContentsOfURL: imageURL];
-    PHPickerResult *result = [self createPickerResult:itemProvider withIdentifier:UTTypeWebP.identifier];
-    
-    [self saveImageToPath:result];
+  NSURL *imageURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"jpgImage"
+                                                             withExtension:@"jpg"];
+  NSItemProvider *itemProvider = [[NSItemProvider alloc] initWithContentsOfURL:imageURL];
+  PHPickerResult *result = [self createPickerResult:itemProvider
+                                     withIdentifier:UTTypeWebP.identifier];
+
+  [self saveImageToPath:result];
 }
 
 - (void)testSaveGIFImage API_AVAILABLE(ios(14)) {
-    NSURL *imageURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"gifImage" withExtension:@"gif"];
-    NSItemProvider *itemProvider = [[NSItemProvider alloc] initWithContentsOfURL: imageURL];
-    PHPickerResult *result = [self createPickerResult:itemProvider withIdentifier:UTTypeWebP.identifier];
-    
-    [self saveImageToPath:result];
+  NSURL *imageURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"gifImage"
+                                                             withExtension:@"gif"];
+  NSItemProvider *itemProvider = [[NSItemProvider alloc] initWithContentsOfURL:imageURL];
+  PHPickerResult *result = [self createPickerResult:itemProvider
+                                     withIdentifier:UTTypeWebP.identifier];
+
+  [self saveImageToPath:result];
 }
 
-- (PHPickerResult *)createPickerResult:(NSItemProvider*)itemProvider withIdentifier:(NSString*)identifier API_AVAILABLE(ios(14)) {
-    PHPickerResult *result = OCMClassMock([PHPickerResult class]);
-    
-    OCMStub([result itemProvider]).andReturn(itemProvider);
-    OCMStub([result assetIdentifier]).andReturn(identifier);
-    
-    return result;
+- (PHPickerResult *)createPickerResult:(NSItemProvider *)itemProvider
+                        withIdentifier:(NSString *)identifier API_AVAILABLE(ios(14)) {
+  PHPickerResult *result = OCMClassMock([PHPickerResult class]);
+
+  OCMStub([result itemProvider]).andReturn(itemProvider);
+  OCMStub([result assetIdentifier]).andReturn(identifier);
+
+  return result;
 }
 
 - (void)saveImageToPath:(PHPickerResult *)result API_AVAILABLE(ios(14)) {
-    XCTestExpectation *pathExpectation = [self expectationWithDescription:@"Path was created"];
-    
-    FLTPHPickerSaveImageToPathOperation *operation = [[FLTPHPickerSaveImageToPathOperation alloc]
-                                                      initWithResult:result
-                                                      maxHeight:@100
-                                                      maxWidth:@100
-                                                      desiredImageQuality:@100
-                                                      savedPathBlock:^(NSString *savedPath) {
-        if ([[NSFileManager defaultManager] fileExistsAtPath:savedPath]) {
-            [pathExpectation fulfill];
-        }
-    }];
-    
-    [operation start];
-    [self waitForExpectations:@[pathExpectation] timeout:30];
+  XCTestExpectation *pathExpectation = [self expectationWithDescription:@"Path was created"];
+
+  FLTPHPickerSaveImageToPathOperation *operation = [[FLTPHPickerSaveImageToPathOperation alloc]
+           initWithResult:result
+                maxHeight:@100
+                 maxWidth:@100
+      desiredImageQuality:@100
+           savedPathBlock:^(NSString *savedPath) {
+             if ([[NSFileManager defaultManager] fileExistsAtPath:savedPath]) {
+               [pathExpectation fulfill];
+             }
+           }];
+
+  [operation start];
+  [self waitForExpectations:@[ pathExpectation ] timeout:30];
 }
 
 @end
--- packages/image_picker/image_picker/ios/Classes/FLTPHPickerSaveImageToPathOperation.m
+++ packages/image_picker/image_picker/ios/Classes/FLTPHPickerSaveImageToPathOperation.m
@@ -84,59 +84,63 @@ typedef void (^GetSavedPath)(NSString *);
   }
   if (@available(iOS 14, *)) {
     [self setExecuting:YES];
-    
+
     if ([self.result.itemProvider hasItemConformingToTypeIdentifier:UTTypeWebP.identifier]) {
-        [self.result.itemProvider loadDataRepresentationForTypeIdentifier:UTTypeWebP.identifier completionHandler:^(NSData * _Nullable data, NSError * _Nullable error) {
-            UIImage *image = [[UIImage alloc] initWithData:data];
-            [self processImage:image];
-        }];
-        return;
+      [self.result.itemProvider
+          loadDataRepresentationForTypeIdentifier:UTTypeWebP.identifier
+                                completionHandler:^(NSData *_Nullable data,
+                                                    NSError *_Nullable error) {
+                                  UIImage *image = [[UIImage alloc] initWithData:data];
+                                  [self processImage:image];
+                                }];
+      return;
     }
-    
-    [self.result.itemProvider loadObjectOfClass:[UIImage class] completionHandler:^(__kindof id<NSItemProviderReading> _Nullable image, NSError *_Nullable error) {
-        if ([image isKindOfClass:[UIImage class]]) {
+
+    [self.result.itemProvider
+        loadObjectOfClass:[UIImage class]
+        completionHandler:^(__kindof id<NSItemProviderReading> _Nullable image,
+                            NSError *_Nullable error) {
+          if ([image isKindOfClass:[UIImage class]]) {
             [self processImage:image];
-        }
-    }];
+          }
+        }];
   } else {
     [self setFinished:YES];
   }
 }
 
 - (void)processImage:(UIImage *)localImage API_AVAILABLE(ios(14)) {
-    PHAsset *originalAsset =
-    [FLTImagePickerPhotoAssetUtil getAssetFromPHPickerResult:self.result];
-    
-    if (self.maxWidth != (id)[NSNull null] || self.maxHeight != (id)[NSNull null]) {
-        localImage = [FLTImagePickerImageUtil scaledImage:localImage
-                                                 maxWidth:self.maxWidth
-                                                maxHeight:self.maxHeight
-                                      isMetadataAvailable:originalAsset != nil];
-    }
-    if (!originalAsset) {
-        // Image picked without an original asset (e.g. User pick image without permission)
-        NSString *savedPath =
+  PHAsset *originalAsset = [FLTImagePickerPhotoAssetUtil getAssetFromPHPickerResult:self.result];
+
+  if (self.maxWidth != (id)[NSNull null] || self.maxHeight != (id)[NSNull null]) {
+    localImage = [FLTImagePickerImageUtil scaledImage:localImage
+                                             maxWidth:self.maxWidth
+                                            maxHeight:self.maxHeight
+                                  isMetadataAvailable:originalAsset != nil];
+  }
+  if (!originalAsset) {
+    // Image picked without an original asset (e.g. User pick image without permission)
+    NSString *savedPath =
         [FLTImagePickerPhotoAssetUtil saveImageWithPickerInfo:nil
                                                         image:localImage
                                                  imageQuality:self.desiredImageQuality];
-        [self completeOperationWithPath:savedPath];
-    } else {
-        [[PHImageManager defaultManager]
-         requestImageDataForAsset:originalAsset
-         options:nil
-         resultHandler:^(
-                         NSData *_Nullable imageData, NSString *_Nullable dataUTI,
-                         UIImageOrientation orientation, NSDictionary *_Nullable info) {
-                             // maxWidth and maxHeight are used only for GIF images.
-                             NSString* savedPath = [FLTImagePickerPhotoAssetUtil
-                                          saveImageWithOriginalImageData:imageData
-                                          image:localImage
-                                          maxWidth:self.maxWidth
-                                          maxHeight:self.maxHeight
-                                          imageQuality:self.desiredImageQuality];
-                             [self completeOperationWithPath:savedPath];
-                         }];
-    }
+    [self completeOperationWithPath:savedPath];
+  } else {
+    [[PHImageManager defaultManager]
+        requestImageDataForAsset:originalAsset
+                         options:nil
+                   resultHandler:^(NSData *_Nullable imageData, NSString *_Nullable dataUTI,
+                                   UIImageOrientation orientation, NSDictionary *_Nullable info) {
+                     // maxWidth and maxHeight are used only for GIF images.
+                     NSString *savedPath = [FLTImagePickerPhotoAssetUtil
+                         saveImageWithOriginalImageData:imageData
+                                                  image:localImage
+                                               maxWidth:self.maxWidth
+                                              maxHeight:self.maxHeight
+                                           imageQuality:self.desiredImageQuality];
+                     [self completeOperationWithPath:savedPath];
+                   }];
+  }
 }
 
 @end
