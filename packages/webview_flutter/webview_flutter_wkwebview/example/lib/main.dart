0c23f8f5232dd3db9030659454ce0fa31f716c67
BeMacized <info@bemacized.net>
2021-11-01 09:37:20 +0100

    [webview_flutter] Update webview packages for Android and iOS to implement `runJavascript` and `runJavascriptReturningResult`. (#4402)
    * [webview_flutter_platform_interface] Update webview platform interface with new methods for running JavaScript
    
    * Fix tests
    
    * [webview_flutter] Implemented `runJavaScript` and `runJavaScriptForResult` in iOS and Android packages.
    
    * Remove accidental development team inclusion from project.pbxproj
    
    * Implemented PR feedback
    
    * Updated runJavaScriptForResult behaviour
    
    * Implemented PR feedback partially
    
    * Implement PR feedback
    
    * Update changelog
    
    * Implement PR feedback from interface PR
    
    * Update changelog
    
    * Revert inclusion of development team
    
    * Implement PR feedback
    
    * Implemented platform interface PR feedback
    
    * Update pubspec dependency
    
    * Fixed capitalisation
    
    * Fixed capitalisation
    
    * Fix warning
    
    * Partially implement PR feedback
    
    * Partially implement PR feedback
    
    * Format
    
    * Update podfile
    
    * Update podfile
    
    * Update podfiles
    
    * Update iOS project files
    
    * Update podspec
    
    * Remove unnecessary podfile configuration
    
    * Implemented PR feedback
    
    * Format
    
    * Revert podfile changes
    
    * Implemented PR feedback
    
    * Fix formatting
    
    * Fix formatting
    
    * Fixed test.
    
    * Re-add integration tests for deprecated evaluateJavascript method.
    
    * Fix merge conflicts

--- packages/webview_flutter/webview_flutter_wkwebview/example/lib/main.dart
+++ packages/webview_flutter/webview_flutter_wkwebview/example/lib/main.dart
@@ -199,14 +199,14 @@ class _SampleMenu extends StatelessWidget {
       WebViewController controller, BuildContext context) async {
     // Send a message with the user agent string to the Snackbar JavaScript channel we registered
     // with the WebView.
-    await controller.evaluateJavascript(
+    await controller.runJavascript(
         'Snackbar.postMessage("User Agent: " + navigator.userAgent);');
   }
 
   void _onListCookies(
       WebViewController controller, BuildContext context) async {
     final String cookies =
-        await controller.evaluateJavascript('document.cookie');
+        await controller.runJavascriptReturningResult('document.cookie');
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
       content: Column(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -220,7 +220,7 @@ class _SampleMenu extends StatelessWidget {
   }
 
   void _onAddToCache(WebViewController controller, BuildContext context) async {
-    await controller.evaluateJavascript(
+    await controller.runJavascript(
         'caches.open("test_caches_entry"); localStorage["test_localStorage"] = "dummy_entry";');
     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
       content: Text('Added a test entry to cache.'),
@@ -228,7 +228,7 @@ class _SampleMenu extends StatelessWidget {
   }
 
   void _onListCache(WebViewController controller, BuildContext context) async {
-    await controller.evaluateJavascript('caches.keys()'
+    await controller.runJavascript('caches.keys()'
         '.then((cacheKeys) => JSON.stringify({"cacheKeys" : cacheKeys, "localStorage" : localStorage}))'
         '.then((caches) => Snackbar.postMessage(caches))');
   }
@@ -340,5 +340,5 @@ class _NavigationControls extends StatelessWidget {
   }
 }
 
-/// Callback type for handling messages sent from Javascript running in a web view.
+/// Callback type for handling messages sent from JavaScript running in a web view.
 typedef void JavascriptMessageHandler(JavascriptMessage message);
