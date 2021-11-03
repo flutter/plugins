d32437e2102763a0950e4a3eaf18330b65435968
Anton Borries <anton.borries@2denker.de>
2021-11-03 15:52:05 +0100

    Add License Block

--- packages/url_launcher/url_launcher_platform_interface/lib/ios_only_ui_modal_presentation_style.dart
+++ packages/url_launcher/url_launcher_platform_interface/lib/ios_only_ui_modal_presentation_style.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /// Modal presentation styles available when presenting view controllers.
 ///
 /// For more info see
