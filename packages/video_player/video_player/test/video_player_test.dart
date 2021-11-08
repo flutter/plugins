flutter/plugins

commit 0b88ffb57d1481d9535aa1a46498ff3f4b2953d8

[video_player] Pause video when it completes (#3727)

packages/video_player/video_player/lib/video_player.dart
@@ -328,8 +328,11 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
           _applyPlayPause();
           break;
         case VideoEventType.completed:
-          value = value.copyWith(isPlaying: false, position: value.duration);
-          _timer?.cancel();
+          // In this case we need to stop _timer, set isPlaying=false, and
+          // position=value.duration. Instead of setting the values directly,
+          // we use pause() and seekTo() to ensure the platform stops playing
+          // and seeks to the last frame of the video.
+          pause().then((void pauseResult) => seekTo(value.duration));
           break;
         case VideoEventType.bufferingUpdate:
           value = value.copyWith(buffered: event.buffered);
@@ -385,10 +388,15 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
 
   /// Starts playing the video.
   ///
+  /// If the video is at the end, this method starts playing from the beginning.
+  ///
   /// This method returns a future that completes as soon as the "play" command
   /// has been sent to the platform, not when playback itself is totally
   /// finished.
   Future<void> play() async {
+    if (value.position == value.duration) {
+      await seekTo(const Duration());
+    }
     value = value.copyWith(isPlaying: true);
     await _applyPlayPause();
   }

packages/video_player/video_player/pubspec.yaml
@@ -3,7 +3,7 @@ description: Flutter plugin for displaying inline video with other Flutter
   widgets on Android, iOS, and web.
 repository: https://github.com/flutter/plugins/tree/master/packages/video_player/video_player
 issue_tracker: https://github.com/flutter/flutter/issues?q=is%3Aissue+is%3Aopen+label%3A%22p%3A+video_player%22
-version: 2.1.9
+version: 2.1.10
 
 environment:
   sdk: ">=2.12.0 <3.0.0"

packages/video_player/video_player/test/video_player_test.dart
@@ -318,6 +318,23 @@ void main() {
       expect(fakeVideoPlayerPlatform.calls.last, 'setPlaybackSpeed');
     });
 
+    test('play restarts from beginning if video is at end', () async {
+      final VideoPlayerController controller = VideoPlayerController.network(
+        'https://127.0.0.1',
+      );
+      await controller.initialize();
+      const Duration nonzeroDuration = Duration(milliseconds: 100);
+      controller.value = controller.value.copyWith(duration: nonzeroDuration);
+      await controller.seekTo(nonzeroDuration);
+      expect(controller.value.isPlaying, isFalse);
+      expect(controller.value.position, nonzeroDuration);
+
+      await controller.play();
+
+      expect(controller.value.isPlaying, isTrue);
+      expect(controller.value.position, Duration.zero);
+    });
+
     test('setLooping', () async {
       final VideoPlayerController controller = VideoPlayerController.network(
         'https://127.0.0.1',
@@ -459,6 +476,8 @@ void main() {
           'https://127.0.0.1',
         );
         await controller.initialize();
+        const Duration nonzeroDuration = Duration(milliseconds: 100);
+        controller.value = controller.value.copyWith(duration: nonzeroDuration);
         expect(controller.value.isPlaying, isFalse);
         await controller.play();
         expect(controller.value.isPlaying, isTrue);
@@ -470,7 +489,7 @@ void main() {
         await tester.pumpAndSettle();
 
         expect(controller.value.isPlaying, isFalse);
-        expect(controller.value.position, controller.value.duration);
+        expect(controller.value.position, nonzeroDuration);
       });
 
       testWidgets('buffering status', (WidgetTester tester) async {

packages/video_player/video_player/example/integration_test/video_player_test.dart
@@ -143,6 +143,51 @@ void main() {
       },
     );
 
+    testWidgets(
+      'stay paused when seeking after video completed',
+      (WidgetTester tester) async {
+        await _controller.initialize();
+        // Mute to allow playing without DOM interaction on Web.
+        // See https://developers.google.com/web/updates/2017/09/autoplay-policy-changes
+        await _controller.setVolume(0);
+        Duration tenMillisBeforeEnd =
+            _controller.value.duration - const Duration(milliseconds: 10);
+        await _controller.seekTo(tenMillisBeforeEnd);
+        await _controller.play();
+        await tester.pumpAndSettle(_playDuration);
+        expect(_controller.value.isPlaying, false);
+        expect(_controller.value.position, _controller.value.duration);
+
+        await _controller.seekTo(tenMillisBeforeEnd);
+        await tester.pumpAndSettle(_playDuration);
+
+        expect(_controller.value.isPlaying, false);
+        expect(_controller.value.position, tenMillisBeforeEnd);
+      },
+    );
+
+    testWidgets(
+      'do not exceed duration on play after video completed',
+      (WidgetTester tester) async {
+        await _controller.initialize();
+        // Mute to allow playing without DOM interaction on Web.
+        // See https://developers.google.com/web/updates/2017/09/autoplay-policy-changes
+        await _controller.setVolume(0);
+        await _controller.seekTo(
+            _controller.value.duration - const Duration(milliseconds: 10));
+        await _controller.play();
+        await tester.pumpAndSettle(_playDuration);
+        expect(_controller.value.isPlaying, false);
+        expect(_controller.value.position, _controller.value.duration);
+
+        await _controller.play();
+        await tester.pumpAndSettle(_playDuration);
+
+        expect(_controller.value.position,
+            lessThanOrEqualTo(_controller.value.duration));
+      },
+    );
+
     testWidgets('test video player view with local asset',
         (WidgetTester tester) async {
       Future<bool> started() async {

commit 0998dd7762ade4a461a6b21310d31406685825c0

[video_player] Don't restart when scrubbing to end
Fixes a regression introduced by #3727 where scrubbing a progress
indicator to the end of the video while playing would unexpectedly
restart playback from the start of the video. Restarting is an
intentional behavior under the assumption that `play` is called for a
deliberate user action to start playback, but in the case of scrubbing
it is a programatic pause/play pair where the decision to call `play`
again should account for scrubbing to the end.

Fixes https://github.com/flutter/flutter/issues/92658

packages/video_player/video_player/lib/video_player.dart
@@ -784,7 +784,8 @@ class _VideoScrubberState extends State<_VideoScrubber> {
         seekToRelativePosition(details.globalPosition);
       },
       onHorizontalDragEnd: (DragEndDetails details) {
-        if (_controllerWasPlaying) {
+        if (_controllerWasPlaying &&
+            controller.value.position != controller.value.duration) {
           controller.play();
         }
       },

packages/video_player/video_player/pubspec.yaml
@@ -3,7 +3,7 @@ description: Flutter plugin for displaying inline video with other Flutter
   widgets on Android, iOS, and web.
 repository: https://github.com/flutter/plugins/tree/master/packages/video_player/video_player
 issue_tracker: https://github.com/flutter/flutter/issues?q=is%3Aissue+is%3Aopen+label%3A%22p%3A+video_player%22
-version: 2.2.6
+version: 2.2.7
 
 environment:
   sdk: ">=2.14.0 <3.0.0"

packages/video_player/video_player/test/video_player_test.dart
@@ -478,6 +478,60 @@ void main() {
       });
     });
 
+    group('scrubbing', () {
+      testWidgets('restarts on release if already playing',
+          (WidgetTester tester) async {
+        final VideoPlayerController controller = VideoPlayerController.network(
+          'https://127.0.0.1',
+        );
+        await controller.initialize();
+        final progressWidget =
+            VideoProgressIndicator(controller, allowScrubbing: true);
+
+        await tester.pumpWidget(Directionality(
+          textDirection: TextDirection.ltr,
+          child: progressWidget,
+        ));
+
+        await controller.play();
+        expect(controller.value.isPlaying, isTrue);
+
+        final Rect progressRect = tester.getRect(find.byWidget(progressWidget));
+        await tester.dragFrom(progressRect.center, Offset(1.0, 0.0));
+        await tester.pumpAndSettle();
+
+        expect(controller.value.position, lessThan(controller.value.duration));
+        expect(controller.value.isPlaying, isTrue);
+
+        await controller.pause();
+      });
+
+      testWidgets('does not restart when dragging to end',
+          (WidgetTester tester) async {
+        final VideoPlayerController controller = VideoPlayerController.network(
+          'https://127.0.0.1',
+        );
+        await controller.initialize();
+        final progressWidget =
+            VideoProgressIndicator(controller, allowScrubbing: true);
+
+        await tester.pumpWidget(Directionality(
+          textDirection: TextDirection.ltr,
+          child: progressWidget,
+        ));
+
+        await controller.play();
+        expect(controller.value.isPlaying, isTrue);
+
+        final Rect progressRect = tester.getRect(find.byWidget(progressWidget));
+        await tester.dragFrom(progressRect.center, progressRect.centerRight);
+        await tester.pumpAndSettle();
+
+        expect(controller.value.position, controller.value.duration);
+        expect(controller.value.isPlaying, isFalse);
+      });
+    });
+
     group('caption', () {
       test('works when seeking', () async {
         final VideoPlayerController controller = VideoPlayerController.network(