45ca717bfa97c5c50eff76e6ab3dd52525c8d672
livetalabs <47648109+livetalabs@users.noreply.github.com>
2021-11-10 12:12:33 +1100

    Update DeviceOrientationManager.java
    Fixed video showing upside-down when recorded in landscape.

--- packages/camera/camera/android/src/main/java/io/flutter/plugins/camera/features/sensororientation/DeviceOrientationManager.java
+++ packages/camera/camera/android/src/main/java/io/flutter/plugins/camera/features/sensororientation/DeviceOrientationManager.java
@@ -179,7 +179,7 @@ public class DeviceOrientationManager {
         angle = 180;
         break;
       case LANDSCAPE_LEFT:
-        angle = 90;
+        angle = 270;
         break;
       case LANDSCAPE_RIGHT:
         angle = 270;
