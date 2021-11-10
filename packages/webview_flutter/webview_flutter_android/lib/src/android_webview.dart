913f6c83f7e0023305cfb28c0cbecbacf05f7af8
Maurice Parrish <bmparr@google.com>
2021-11-10 11:41:11 -0800

    removed unused import

--- packages/webview_flutter/webview_flutter_android/lib/src/android_webview.dart
+++ packages/webview_flutter/webview_flutter_android/lib/src/android_webview.dart
@@ -5,7 +5,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart' show AndroidViewSurface;
 
-import 'android_webview.pigeon.dart';
 import 'android_webview_api_impls.dart';
 
 // TODO(bparrishMines): This can be removed once pigeon supports null values: https://github.com/flutter/flutter/issues/59118
