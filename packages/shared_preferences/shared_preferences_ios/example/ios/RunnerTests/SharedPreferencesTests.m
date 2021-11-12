c86ddd4293c4d01323b5a9d208af56188a750dde
Stuart Morgan <stuartmorgan@google.com>
2021-11-12 11:20:32 -0500

    Fix iOS native tests

--- packages/shared_preferences/shared_preferences_ios/example/ios/RunnerTests/SharedPreferencesTests.m
+++ packages/shared_preferences/shared_preferences_ios/example/ios/RunnerTests/SharedPreferencesTests.m
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-@import shared_preferences;
+@import shared_preferences_ios;
 @import XCTest;
 
 @interface SharedPreferencesTests : XCTestCase
