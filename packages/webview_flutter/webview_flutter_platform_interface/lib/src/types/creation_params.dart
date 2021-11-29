30289bd01fb0497c217ccf2a1952c03cc326d20f
Maurits van Beusekom <maurits@baseflow.com>
2021-11-29 07:12:05 +0100

    [webview_flutter] Migrate webview_flutter_platform_interface to analysis_options.yaml (#4549)

--- packages/webview_flutter/webview_flutter_platform_interface/lib/src/types/creation_params.dart
+++ packages/webview_flutter/webview_flutter_platform_interface/lib/src/types/creation_params.dart
@@ -55,6 +55,6 @@ class CreationParams {
 
   @override
   String toString() {
-    return '$runtimeType(initialUrl: $initialUrl, settings: $webSettings, javascriptChannelNames: $javascriptChannelNames, UserAgent: $userAgent)';
+    return 'CreationParams(initialUrl: $initialUrl, settings: $webSettings, javascriptChannelNames: $javascriptChannelNames, UserAgent: $userAgent)';
   }
 }
