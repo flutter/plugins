6f9742ad6f127299458552478ac9a280bb992b41
Bodhi Mulders (BeMacized) <info@bemacized.net>
2021-11-30 14:29:28 +0100

    Process PR feedback

--- packages/webview_flutter/webview_flutter_platform_interface/lib/src/types/webview_cookie.dart
+++ packages/webview_flutter/webview_flutter_platform_interface/lib/src/types/webview_cookie.dart
@@ -2,42 +2,42 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// Configuration to use when creating a new [WebViewPlatformController].
-///
-/// The `autoMediaPlaybackPolicy` parameter must not be null.
+/// A cookie that can be set globally for all web views
+/// using [WebViewCookieManagerPlatform].
 class WebViewCookie {
-  /// Construct a new [WebViewCookie].
+  /// Constructs a new [WebViewCookie].
   const WebViewCookie(
       {required this.name,
       required this.value,
       required this.domain,
       this.path = '/'});
 
-  /// The name of the cookie.
+  /// The cookie-name of the cookie.
   ///
   /// Its value should match "cookie-name" in RFC6265bis:
   /// https://datatracker.ietf.org/doc/html/draft-ietf-httpbis-rfc6265bis-02#section-4.1.1
   final String name;
 
-  /// The value of the cookie.
+  /// The cookie-value of the cookie.
   ///
   /// Its value should match "cookie-value" in RFC6265bis:
   /// https://datatracker.ietf.org/doc/html/draft-ietf-httpbis-rfc6265bis-02#section-4.1.1
   final String value;
 
-  /// The value of the cookie.
+  /// The domain-value of the cookie.
   ///
   /// Its value should match "domain-value" in RFC6265bis:
   /// https://datatracker.ietf.org/doc/html/draft-ietf-httpbis-rfc6265bis-02#section-4.1.1
   final String domain;
 
-  /// The value of the cookie.
+  /// The path-value of the cookie.
+  /// Is set to `/` in the constructor by default.
   ///
   /// Its value should match "path-value" in RFC6265bis:
   /// https://datatracker.ietf.org/doc/html/draft-ietf-httpbis-rfc6265bis-02#section-4.1.1
   final String path;
 
-  /// Serialize a [WebViewCookie] to a Map<String, String>.
+  /// Serializes a [WebViewCookie] to a Map<String, String>.
   Map<String, String> toJson() {
     return <String, String>{
       'name': name,
