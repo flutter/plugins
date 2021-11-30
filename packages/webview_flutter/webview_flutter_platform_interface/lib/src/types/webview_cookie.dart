c7067c0565af65cbcda45d86ffbb63e8b07048a6
Bodhi Mulders (BeMacized) <info@bemacized.net>
2021-11-30 14:33:15 +0100

    Process PR feedback

--- packages/webview_flutter/webview_flutter_platform_interface/lib/src/types/webview_cookie.dart
+++ packages/webview_flutter/webview_flutter_platform_interface/lib/src/types/webview_cookie.dart
@@ -37,7 +37,7 @@ class WebViewCookie {
   /// https://datatracker.ietf.org/doc/html/draft-ietf-httpbis-rfc6265bis-02#section-4.1.1
   final String path;
 
-  /// Serializes a [WebViewCookie] to a Map<String, String>.
+  /// Serializes the [WebViewCookie] to a Map<String, String>.
   Map<String, String> toJson() {
     return <String, String>{
       'name': name,
