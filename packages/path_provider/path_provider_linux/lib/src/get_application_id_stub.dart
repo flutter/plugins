537cc437c0bf4d502654113bba7d941a9266f4a8
Robert Ancell <robert.ancell@canonical.com>
2021-12-10 11:03:04 +1300

    [path_provider] Use the application ID in the application support path (#2845) (#3077)
    Use the existing executable named directory if it exists, to allow backwards
    compatibility to work.

--- packages/path_provider/path_provider_linux/lib/src/get_application_id_stub.dart
+++ packages/path_provider/path_provider_linux/lib/src/get_application_id_stub.dart
@@ -2,4 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export 'src/path_provider_linux.dart';
+/// Gets the application ID for this app.
+String? getApplicationId() => null;
