cab4624d3ad4928ce8ceb93d16d029ac7431eaa6
Fergus Strangways-Dixon <fergusdixon101@gmail.com>
2021-12-20 08:31:54 +0000

    fix unused imports and add licence headers to new heatmap files

--- packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/heatmap_updates.dart
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/heatmap_updates.dart
@@ -1,10 +1,10 @@
-// Copyright 2020 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:ui' show hashValues;
-
-import 'package:flutter/foundation.dart' show setEquals;
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'types.dart';
 
