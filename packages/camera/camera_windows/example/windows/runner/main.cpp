49e9222e30fc95657bbe8a04d990721ad3b22a85
Joonas Kerttula <joonas.kerttula@codemate.com>
2022-01-05 14:38:11 +0200

    [camera_windows] Fix formatting for windows runner

--- packages/camera/camera_windows/example/windows/runner/main.cpp
+++ packages/camera/camera_windows/example/windows/runner/main.cpp
@@ -19,8 +19,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   flutter::DartProject project(L"data");
 
-  std::vector<std::string> command_line_arguments =
-      GetCommandLineArguments();
+  std::vector<std::string> command_line_arguments = GetCommandLineArguments();
 
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
