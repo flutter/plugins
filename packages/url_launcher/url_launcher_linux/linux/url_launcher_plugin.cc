727930440d173fdcaae8162607eb3b95caa88886
Joonas Kerttula <joonas.kerttula@codemate.com>
2022-01-07 14:46:03 +0200

    [url_launcher_linux] Rename file resource function

--- packages/url_launcher/url_launcher_linux/linux/url_launcher_plugin.cc
+++ packages/url_launcher/url_launcher_linux/linux/url_launcher_plugin.cc
@@ -45,9 +45,9 @@ static gchar* get_url(FlValue* args, GError** error) {
   return g_strdup(fl_value_get_string(url_value));
 }
 
-// Called to check if URL with file scheme can be launched.
-static gboolean can_launch_url_with_file_scheme(FlUrlLauncherPlugin* self,
-                                                const gchar* url) {
+// Check if URI has launchable file resource
+static gboolean can_launch_uri_with_file_resource(FlUrlLauncherPlugin* self,
+                                                  const gchar* url) {
   g_autoptr(GError) error = nullptr;
   g_autoptr(GFile) file = g_file_new_for_uri(url);
   g_autoptr(GAppInfo) app_info =
@@ -76,7 +76,7 @@ FlMethodResponse* can_launch(FlUrlLauncherPlugin* self, FlValue* args) {
     is_launchable = app_info != nullptr;
 
     if (!is_launchable) {
-      is_launchable = can_launch_url_with_file_scheme(self, url);
+      is_launchable = can_launch_uri_with_file_resource(self, url);
     }
   }
 
