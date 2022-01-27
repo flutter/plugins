282e0796bd1c79dbf12b9d51db50ea9169415ec6
Huan Lin <hellohuanlin@gmail.com>
2022-01-26 18:50:31 -0800

    [camera]add unit tests for device orientation

--- packages/camera/camera/ios/Classes/DeviceOrientation.m
+++ packages/camera/camera/ios/Classes/DeviceOrientation.m
@@ -38,6 +38,5 @@ NSString *FLTGetStringForUIDeviceOrientation(UIDeviceOrientation orientation) {
     case UIDeviceOrientationPortrait:
     default:
       return @"portraitUp";
-      break;
   };
 }
