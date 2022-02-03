ee7ce00bb4480cea02b8ef6ebf1ac96c3137dd87
Aaron Clarke <aaclarke@google.com>
2022-02-02 17:35:53 -0800

    updated android

--- packages/video_player/video_player_android/CHANGELOG.md
+++ packages/video_player/video_player_android/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 2.3.0
+
+* Update Pigeon to ^1.0.16.
+
 ## 2.2.17
 
 * Splits from `video_player` as a federated implementation.
--- packages/video_player/video_player_android/test/android_video_player_test.dart
+++ packages/video_player/video_player_android/test/android_video_player_test.dart
@@ -6,7 +6,7 @@ import 'dart:ui';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:video_player_android/src/messages.dart';
+import 'package:video_player_android/src/messages.g.dart';
 import 'package:video_player_android/video_player_android.dart';
 import 'package:video_player_platform_interface/video_player_platform_interface.dart';
 
