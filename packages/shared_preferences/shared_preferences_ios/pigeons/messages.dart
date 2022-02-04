ec4440a510aa7cdf5a258be054772f5c96294538
Aaron Clarke <aaclarke@google.com>
2022-02-03 16:30:53 -0800

    [shared_preferences] upgraded ios to using pigeon

---
+++ packages/shared_preferences/shared_preferences_ios/pigeons/messages.dart
@@ -0,0 +1,23 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(PigeonOptions(
+  dartOut: 'lib/messages.g.dart',
+  objcHeaderOut: 'ios/Classes/messages.g.h',
+  objcSourceOut: 'ios/Classes/messages.g.m',
+  copyrightHeader: 'pigeons/copyright_header.txt',
+))
+@HostApi()
+abstract class SharedPreferencesApi {
+  bool remove(String key);
+  bool setBool(String key, bool value);
+  bool setDouble(String key, double value);
+  bool setInt(String key, int value);
+  bool setString(String key, String value);
+  bool setStringList(String key, List<String> value);
+  bool clear();
+  Map<String?, Object?> getAll();
+}
