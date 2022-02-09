014b9f1ef39e0ae88f9e1353ea9be231da5f5121
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2022-02-09 15:42:18 -0800

    remove unused import

--- packages/webview_flutter/webview_flutter_wkwebview/lib/src/web_kit/web_kit.dart
+++ packages/webview_flutter/webview_flutter_wkwebview/lib/src/web_kit/web_kit.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
-
 /// Times at which to inject script content into a webpage.
 ///
 /// Wraps [WKUserScriptInjectionTime](https://developer.apple.com/documentation/webkit/wkuserscriptinjectiontime?language=objc).
