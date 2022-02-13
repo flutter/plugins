fe101807694160d2e209d8cabd29cce97ff1168f
TowaYamashita <Confirm.Apps.Develop@gmail.com>
2022-02-13 23:08:42 +0900

    Sort directive sections alphabetically

--- packages/file_selector/file_selector/example/test/widget_test.dart
+++ packages/file_selector/file_selector/example/test/widget_test.dart
@@ -5,10 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:example/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:example/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
