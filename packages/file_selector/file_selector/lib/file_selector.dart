6c716473a26133fb22206e4588b9cd4170a11387
TowaYamashita <Confirm.Apps.Develop@gmail.com>
2022-02-13 17:01:20 +0900

    Add detail comment in file_selector

--- packages/file_selector/file_selector/lib/file_selector.dart
+++ packages/file_selector/file_selector/lib/file_selector.dart
@@ -10,6 +10,18 @@ export 'package:file_selector_platform_interface/file_selector_platform_interfac
     show XFile, XTypeGroup;
 
 /// Open file dialog for loading files and return a file path
+///
+/// The [acceptedTypeGroups] argument is the file type that can be selected in the dialog.
+/// When omitted, Open file dialog with all file types.
+///
+/// The [initialDirectory] argument is directory that will be displayed when the dialog is opened.
+/// (NOTICE: specify a directory as a full path, not a relative path.)
+/// When omitted, The result of each platform's `getDirectoryPath()` execution is used.
+///
+/// The [confirmButtonText] argument is the text in the confirmation button of the dialog.
+/// When omitted, the wording specified in the OS standard is used.(e.g. open)
+///
+/// Returns `null` if user cancels the operation.
 Future<XFile?> openFile({
   List<XTypeGroup> acceptedTypeGroups = const <XTypeGroup>[],
   String? initialDirectory,
@@ -22,6 +34,16 @@ Future<XFile?> openFile({
 }
 
 /// Open file dialog for loading files and return a list of file paths
+///
+/// The [acceptedTypeGroups] argument is the file type that can be selected in the dialog.
+/// When omitted, Open file dialog with all file types.
+///
+/// The [initialDirectory] argument is directory that will be displayed when the dialog is opened.
+/// (NOTICE: specify a directory as a full path, not a relative path.)
+/// When omitted, The result of each platform's `getDirectoryPath()` execution is used.
+///
+/// The [confirmButtonText] argument is the text in the confirmation button of the dialog.
+/// When omitted, the wording specified in the OS standard is used.(e.g. open)
 Future<List<XFile>> openFiles({
   List<XTypeGroup> acceptedTypeGroups = const <XTypeGroup>[],
   String? initialDirectory,
@@ -34,6 +56,21 @@ Future<List<XFile>> openFiles({
 }
 
 /// Saves File to user's file system
+///
+/// The [acceptedTypeGroups] argument is the file type that can be selected in the dialog.
+/// When omitted, Open file dialog with all file types.
+///
+/// The [initialDirectory] argument is directory that will be displayed when the dialog is opened.
+/// (NOTICE: specify a directory as a full path, not a relative path.)
+/// When omitted, The result of each platform's `getDirectoryPath()` execution is used.
+///
+/// The [suggestedName] argument is he name of the file to save.
+/// When omitted, use UUID(version4) as file name.
+///
+/// The [confirmButtonText] argument is the text in the confirmation button of the dialog.
+/// When omitted, the wording specified in the OS standard is used.(e.g. open)
+///
+/// Returns `null` if user cancels the operation.
 Future<String?> getSavePath({
   List<XTypeGroup> acceptedTypeGroups = const <XTypeGroup>[],
   String? initialDirectory,
@@ -48,6 +85,15 @@ Future<String?> getSavePath({
 }
 
 /// Gets a directory path from a user's file system
+///
+/// The [initialDirectory] argument is directory that will be displayed when the dialog is opened.
+/// (NOTICE: specify a directory as a full path, not a relative path.)
+/// When omitted, The result of each platform's `getDirectoryPath()` execution is used.
+///
+/// The [confirmButtonText] argument is the text in the confirmation button of the dialog.
+/// When omitted, the wording specified in the OS standard is used.(e.g. open)
+///
+/// Returns `null` if user cancels the operation.
 Future<String?> getDirectoryPath({
   String? initialDirectory,
   String? confirmButtonText,
