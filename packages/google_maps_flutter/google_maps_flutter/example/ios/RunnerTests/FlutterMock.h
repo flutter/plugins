9f483b1b153aaf82709aa7aa72b0166275c42ee5
Guy Kogus <guy.kogus@gmail.com>
2022-02-14 19:29:37 +0100

    Add missing licence

--- packages/google_maps_flutter/google_maps_flutter/example/ios/RunnerTests/FlutterMock.h
+++ packages/google_maps_flutter/google_maps_flutter/example/ios/RunnerTests/FlutterMock.h
@@ -1,10 +1,6 @@
-//
-//  FlutterMock.h
-//  RunnerTests
-//
-//  Created by Guy Kogus on 14/2/22.
-//  Copyright © 2022 The Flutter Authors. All rights reserved.
-//
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 @import Foundation;
 @import Flutter;
