434e3aee1debdb9a805ec41d77ff484c4e5b7df5
The one with the braid (she/her) | Dфҿ mit dem Zopf (sie/ihr) <29313398+TheOneWithTheBraid@users.noreply.github.com>
2022-02-23 18:41:46 +0100

    Update packages/url_launcher/url_launcher_web/lib/src/link.dart
    Co-authored-by: Mouad Debbar <mouad.debbar@gmail.com>

--- packages/url_launcher/url_launcher_web/lib/src/link.dart
+++ packages/url_launcher/url_launcher_web/lib/src/link.dart
@@ -11,8 +11,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-// used to access the current [UrlStrategy]
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart' show urlStrategy;
 import 'package:url_launcher_platform_interface/link.dart';
 
 /// The unique identifier for the view type to be used for link platform views.
