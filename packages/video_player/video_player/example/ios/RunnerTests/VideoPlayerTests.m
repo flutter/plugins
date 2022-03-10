ebbc6ac79ee5e157c5c5ab15065a6f4cb71b346a
Milad Taghavi <milad.taghavi@kernwerk.de>
2022-03-10 15:06:47 +0100

    [video_player] Add native test case

--- packages/video_player/video_player/example/ios/RunnerTests/VideoPlayerTests.m
+++ packages/video_player/video_player/example/ios/RunnerTests/VideoPlayerTests.m
@@ -2,17 +2,80 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+@import AVFoundation;
 @import video_player;
 @import XCTest;
+
+#import <OCMock/OCMock.h>
+
+@interface FLTVideoPlayer : NSObject <FlutterStreamHandler>
+@property(readonly, nonatomic) AVPlayer *player;
+@end
+
+@interface FLTVideoPlayerPlugin (Test) <FLTVideoPlayerApi>
+@property(readonly, strong, nonatomic)
+    NSMutableDictionary<NSNumber *, FLTVideoPlayer *> *playersByTextureId;
+@end
 
 @interface VideoPlayerTests : XCTestCase
 @end
 
 @implementation VideoPlayerTests
 
-- (void)testPlugin {
-  FLTVideoPlayerPlugin* plugin = [[FLTVideoPlayerPlugin alloc] init];
-  XCTAssertNotNil(plugin);
+
+- (void)testSetMixWithOthersForPlayer {
+    FLTVideoPlayerPlugin* videoPlayer = [[FLTVideoPlayerPlugin alloc] init];
+    FLTMixWithOthersMessage* msg = [[FLTMixWithOthersMessage alloc] init];
+    AVAudioSession* session = [AVAudioSession sharedInstance];
+    FlutterError *error;
+
+    [videoPlayer initialize:&error];
+
+    msg.mixWithOthers = @0;
+    msg.ambient = @0;
+    [videoPlayer setMixWithOthers:msg error:&error];
+
+    XCTAssertEqual(session.category, AVAudioSessionCategoryPlayback);
+    XCTAssertNotEqual(session.mode, AVAudioSessionModeVoicePrompt);
+
+    msg.mixWithOthers = @1;
+    msg.ambient = @0;
+    [videoPlayer setMixWithOthers:msg error:&error];
+
+    XCTAssertEqual(session.category, AVAudioSessionCategoryPlayback);
+    XCTAssertEqual(session.categoryOptions, AVAudioSessionCategoryOptionMixWithOthers);
+
+    msg.mixWithOthers = @1;
+    msg.ambient = @1;
+    [videoPlayer setMixWithOthers:msg error:&error];
+
+    XCTAssertEqual(session.category, AVAudioSessionCategoryAmbient);
+    XCTAssertEqual(session.categoryOptions, AVAudioSessionCategoryOptionMixWithOthers);
+    XCTAssertEqual(session.mode, AVAudioSessionModeVoicePrompt);
+
+    msg.mixWithOthers = @0;
+    msg.ambient = @1;
+    [videoPlayer setMixWithOthers:msg error:&error];
+
+    XCTAssertEqual(session.category, AVAudioSessionCategoryAmbient);
+    XCTAssertEqual(session.mode, AVAudioSessionModeVoicePrompt);
+
+    msg.mixWithOthers = @1;
+    msg.ambient = @0;
+    [videoPlayer setMixWithOthers:msg error:&error];
+
+    XCTAssertEqual(session.category, AVAudioSessionCategoryPlayback);
+    XCTAssertEqual(session.categoryOptions, AVAudioSessionCategoryOptionMixWithOthers);
+    XCTAssertNotEqual(session.mode, AVAudioSessionModeVoicePrompt);
+
+    msg.mixWithOthers = @0;
+    msg.ambient = @0;
+    [videoPlayer setMixWithOthers:msg error:&error];
+
+    XCTAssertEqual(session.category, AVAudioSessionCategoryPlayback);
+    XCTAssertNotEqual(session.mode, AVAudioSessionModeVoicePrompt);
 }
+
+
 
 @end
