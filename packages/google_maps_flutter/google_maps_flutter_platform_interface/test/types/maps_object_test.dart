fb32c893ef8897ac9206c8fc285c0934874f884a
campovski <campovski@gmail.com>
2022-04-04 10:57:45 +0200

    SLFLP-1: Fix formatting

--- packages/google_maps_flutter/google_maps_flutter_platform_interface/test/types/maps_object_test.dart
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/test/types/maps_object_test.dart
@@ -43,10 +43,12 @@ void main() {
         ]);
   });
 
-  test('MarkerId equality should preserve inheritance and only check value', () async {
+  test('MarkerId equality should preserve inheritance and only check value',
+      () async {
     const String value = 'id123';
     const MarkerId markerId = MarkerId(value);
-    const TestMapsObjectId<Marker> customMarkerId = TestMapsObjectId<Marker>(value);
+    const TestMapsObjectId<Marker> customMarkerId =
+        TestMapsObjectId<Marker>(value);
     expect(customMarkerId, equals(markerId));
   });
 }
