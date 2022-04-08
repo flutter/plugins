0f436539322f030260d71fbdf2fafc3d456397a5
Piotr Mitkowski <piotr.mitkowski1@gmail.com>
2022-04-08 10:49:31 +0200

    Added missing license comment

--- packages/image_picker/image_picker_platform_interface/lib/src/types/image_picker_options.dart
+++ packages/image_picker/image_picker_platform_interface/lib/src/types/image_picker_options.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:image_picker_platform_interface/src/types/types.dart';
 
 /// Specifies options for picking a single image from the device's camera or gallery.
