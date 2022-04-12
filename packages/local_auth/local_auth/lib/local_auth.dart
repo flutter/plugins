6188c99982420303dde0b66cf7d950b9680e94a2
Bodhi Mulders (BeMacized) <info@bemacized.net>
2022-04-12 15:07:51 +0200

    Add missing license header

--- packages/local_auth/local_auth/lib/local_auth.dart
+++ packages/local_auth/local_auth/lib/local_auth.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 export 'package:local_auth/src/local_auth.dart' show LocalAuthentication;
 
 // TODO(BeMacized): This should be removed the next time a breaking change
