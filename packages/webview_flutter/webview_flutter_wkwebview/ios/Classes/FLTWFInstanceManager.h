d8b0576b963cef4ef7127cd7112af9dcdd092c2d
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2022-04-12 15:47:20 -0700

    formatting

--- packages/webview_flutter/webview_flutter_wkwebview/ios/Classes/FLTWFInstanceManager.h
+++ packages/webview_flutter/webview_flutter_wkwebview/ios/Classes/FLTWFInstanceManager.h
@@ -6,15 +6,45 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ * Maintains instances to intercommunicate with Dart objects.
+ *
+ * When an instance is added with an instanceId, either can be used to retrieve the other.
+ */
 @interface FLTWFInstanceManager : NSObject
+/**
+ * Add a new instance to the manager.
+ *
+ * If an instance or instanceID has already been added, it will be replaced by the new values.
+ */
 - (void)addInstance:(nonnull NSObject *)instance instanceID:(long)instanceID;
 
+/**
+ * Remove the instance with instanceID from the manager.
+ *
+ * Returns the removed instance if the manager contains the instanceId, otherwise nil.
+ */
 - (nullable NSObject *)removeInstanceWithID:(long)instanceId;
 
+/**
+ * Remove the instance from the manager.
+ *
+ * Returns the instanceID of the removed instance if the manager contains the value, otherwise nil.
+ */
 - (nullable NSNumber *)removeInstance:(NSObject *)instance;
 
+/**
+ * Retrieve the Object paired with instanceId.
+ *
+ * Returns the instance stored with the instanceID if the manager contains the value, otherwise nil.
+ */
 - (nullable NSObject *)instanceForID:(long)instanceID;
 
+/**
+ * Retrieve the instanceID paired with an instance.
+ *
+ * Returns the instanceID paired with instance if the manager contains the value, otherwise nil.
+ */
 - (nullable NSNumber *)instanceIDForInstance:(nonnull NSObject *)instance;
 @end
 
