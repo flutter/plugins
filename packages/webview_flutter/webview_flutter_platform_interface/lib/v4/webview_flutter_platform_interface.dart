4cc0f561f386f44201f683bea25ef25554f1d28d
Maurits van Beusekom <maurits@baseflow.com>
2022-04-15 14:15:22 +0200

    Made JavaScriptChannelRegistry available

--- packages/webview_flutter/webview_flutter_platform_interface/lib/v4/webview_flutter_platform_interface.dart
+++ packages/webview_flutter/webview_flutter_platform_interface/lib/v4/webview_flutter_platform_interface.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export 'src/javascript_channel_registry.dart';
 export 'src/navigation_callback_delegate.dart';
 export 'src/types/types.dart';
 export 'src/webview_controller_delegate.dart';
