c1fc9da480838a72de877202b365cdc05290aa4e
Piotr Mitkowski <piotr.mitkowski1@gmail.com>
2022-04-15 08:34:30 +0200

    PR remarks

--- packages/image_picker/image_picker_platform_interface/lib/src/types/image_picker_options.dart
+++ packages/image_picker/image_picker_platform_interface/lib/src/types/image_picker_options.dart
@@ -24,23 +24,25 @@ class ImagePickerOptions {
   /// Otherwise the image will be returned at its original height.
   final double? maxHeight;
 
-  /// Modifies the quality of the image, ranging from 0-100
-  /// where 100 is the original/max quality. If `imageQuality` is null, the image with
-  /// the original quality will be returned. Compression is only supported for certain
-  /// image types such as JPEG. If compression is not supported for the image that is picked,
-  /// a warning message will be logged.
+  /// Modifies the quality of the image, ranging from 0-100 where 100 is the original/max
+  /// quality. If `imageQuality` is null, the image with the originalquality will
+  /// be returned. Compression is only supported for certain image types such as
+  /// JPEG. If compression is not supported for the image that is picked, a warning
+  /// message will be logged.
   final int? imageQuality;
 
   /// Used to specify the camera to use when the `source` is [ImageSource.camera].
-  /// The `preferredCameraDevice` is ignored when `source` is [ImageSource.gallery]. It is also ignored if the chosen camera is not supported on the device.
-  /// Defaults to [CameraDevice.rear]. Note that Android has no documented parameter for an intent to specify if
-  /// the front or rear camera should be opened, this function is not guaranteed
-  /// to work on an Android device.
+  /// The `preferredCameraDevice` is ignored when `source` is [ImageSource.gallery].
+  /// It is also ignored if the chosen camera is not supported on the device.
+  /// Defaults to [CameraDevice.rear]. Note that Android has no documented parameter
+  /// for an intent to specify if the front or rear camera should be opened, this
+  /// function is not guaranteed to work on an Android device.
   final CameraDevice preferredCameraDevice;
 
-  /// `requestFullMetadata` defaults to `true`, so the plugin tries to get the full image metadata which may require
-  /// extra permission requests on certain platforms.
-  /// If `requestFullMetadata` is set to `false`, the plugin fetches the image in a way that reduces permission requests
-  /// from the platform (e.g. on iOS the plugin won’t ask for the `NSPhotoLibraryUsageDescription` permission).
+  /// `requestFullMetadata` defaults to `true`, so the plugin tries to get the
+  /// full image metadata which may require extra permission requests on certain platforms.
+  /// If `requestFullMetadata` is set to `false`, the plugin fetches the image
+  /// in a way that reduces permission requests from the platform (e.g. on iOS
+  /// the plugin won’t ask for the `NSPhotoLibraryUsageDescription` permission).
   final bool requestFullMetadata;
 }
