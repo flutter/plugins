085e363f18969e90d85a996439d46048bde86d56
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2022-04-22 21:53:31 -0700

    add other methods

--- packages/webview_flutter/webview_flutter_wkwebview/ios/Classes/FWFWebViewHostApi.m
+++ packages/webview_flutter/webview_flutter_wkwebview/ios/Classes/FWFWebViewHostApi.m
@@ -79,4 +79,103 @@
       (FWFWebView *)[self.instanceManager instanceForIdentifier:instanceId.longValue];
   return webView.URL.absoluteString;
 }
+
+- (nullable NSNumber *)canGoForwardInstanceId:(nonnull NSNumber *)instanceId
+                                        error:(FlutterError *_Nullable __autoreleasing *_Nonnull)
+                                                  error {
+  // TODO(bparrishMines): Implement
+  @throw [NSException exceptionWithName:@"UnsupportedException" reason:nil userInfo:nil];
+}
+
+- (void)createInstanceId:(nonnull NSNumber *)instanceId
+    configurationInstanceId:(nonnull NSNumber *)configurationInstanceId
+                      error:(FlutterError *_Nullable __autoreleasing *_Nonnull)error {
+  // TODO(bparrishMines): Implement
+  @throw [NSException exceptionWithName:@"UnsupportedException" reason:nil userInfo:nil];
+}
+
+- (void)evaluateJavaScriptInstanceId:(nonnull NSNumber *)instanceId
+                    javascriptString:(nonnull NSString *)javascriptString
+                          completion:(nonnull void (^)(NSString *_Nullable,
+                                                       FlutterError *_Nullable))completion {
+  // TODO(bparrishMines): Implement
+  @throw [NSException exceptionWithName:@"UnsupportedException" reason:nil userInfo:nil];
+}
+
+- (nullable NSNumber *)
+    getEstimatedProgressInstanceId:(nonnull NSNumber *)instanceId
+                             error:(FlutterError *_Nullable __autoreleasing *_Nonnull)error {
+  // TODO(bparrishMines): Implement
+  @throw [NSException exceptionWithName:@"UnsupportedException" reason:nil userInfo:nil];
+}
+
+- (nullable NSString *)getTitleInstanceId:(nonnull NSNumber *)instanceId
+                                    error:(FlutterError *_Nullable __autoreleasing *_Nonnull)error {
+  // TODO(bparrishMines): Implement
+  @throw [NSException exceptionWithName:@"UnsupportedException" reason:nil userInfo:nil];
+}
+
+- (void)goBackInstanceId:(nonnull NSNumber *)instanceId
+                   error:(FlutterError *_Nullable __autoreleasing *_Nonnull)error {
+  // TODO(bparrishMines): Implement
+  @throw [NSException exceptionWithName:@"UnsupportedException" reason:nil userInfo:nil];
+}
+
+- (void)goForwardInstanceId:(nonnull NSNumber *)instanceId
+                      error:(FlutterError *_Nullable __autoreleasing *_Nonnull)error {
+  // TODO(bparrishMines): Implement
+  @throw [NSException exceptionWithName:@"UnsupportedException" reason:nil userInfo:nil];
+}
+
+- (void)loadFileUrlInstanceId:(nonnull NSNumber *)instanceId
+                          url:(nonnull NSString *)url
+                readAccessUrl:(nonnull NSString *)readAccessUrl
+                        error:(FlutterError *_Nullable __autoreleasing *_Nonnull)error {
+  // TODO(bparrishMines): Implement
+  @throw [NSException exceptionWithName:@"UnsupportedException" reason:nil userInfo:nil];
+}
+
+- (void)loadFlutterAssetInstanceId:(nonnull NSNumber *)instanceId
+                               key:(nonnull NSString *)key
+                             error:(FlutterError *_Nullable __autoreleasing *_Nonnull)error {
+  // TODO(bparrishMines): Implement
+  @throw [NSException exceptionWithName:@"UnsupportedException" reason:nil userInfo:nil];
+}
+
+- (void)loadHtmlStringInstanceId:(nonnull NSNumber *)instanceId
+                          string:(nonnull NSString *)string
+                         baseUrl:(nullable NSString *)baseUrl
+                           error:(FlutterError *_Nullable __autoreleasing *_Nonnull)error {
+  // TODO(bparrishMines): Implement
+  @throw [NSException exceptionWithName:@"UnsupportedException" reason:nil userInfo:nil];
+}
+
+- (void)reloadInstanceId:(nonnull NSNumber *)instanceId
+                   error:(FlutterError *_Nullable __autoreleasing *_Nonnull)error {
+  // TODO(bparrishMines): Implement
+  @throw [NSException exceptionWithName:@"UnsupportedException" reason:nil userInfo:nil];
+}
+
+- (void)setAllowsBackForwardNavigationGesturesInstanceId:(nonnull NSNumber *)instanceId
+                                                   allow:(nonnull NSNumber *)allow
+                                                   error:(FlutterError *_Nullable __autoreleasing
+                                                              *_Nonnull)error {
+  // TODO(bparrishMines): Implement
+  @throw [NSException exceptionWithName:@"UnsupportedException" reason:nil userInfo:nil];
+}
+
+- (void)setNavigationDelegateInstanceId:(nonnull NSNumber *)instanceId
+           navigationDelegateInstanceId:(nullable NSNumber *)navigationDelegateInstanceId
+                                  error:(FlutterError *_Nullable __autoreleasing *_Nonnull)error {
+  // TODO(bparrishMines): Implement
+  @throw [NSException exceptionWithName:@"UnsupportedException" reason:nil userInfo:nil];
+}
+
+- (void)setUIDelegateInstanceId:(nonnull NSNumber *)instanceId
+           uiDelegateInstanceId:(nullable NSNumber *)uiDelegateInstanceId
+                          error:(FlutterError *_Nullable __autoreleasing *_Nonnull)error {
+  // TODO(bparrishMines): Implement
+  @throw [NSException exceptionWithName:@"UnsupportedException" reason:nil userInfo:nil];
+}
+
 @end
