271a49e734514504680efdd62d0eb3ee551dbbb4
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2022-05-13 14:40:23 -0700

    Applied more PR feedback.

--- packages/local_auth/local_auth_windows/lib/types/auth_messages_windows.dart
+++ packages/local_auth/local_auth_windows/lib/types/auth_messages_windows.dart
@@ -3,190 +3,20 @@
 // found in the LICENSE file.
 
 import 'package:flutter/foundation.dart';
-import 'package:intl/intl.dart';
 import 'package:local_auth_platform_interface/types/auth_messages.dart';
 
 /// Windows side authentication messages.
 ///
 /// Provides default values for all messages.
+///
+/// Currently unused.
 @immutable
 class WindowsAuthMessages extends AuthMessages {
   /// Constructs a new instance.
-  const WindowsAuthMessages({
-    this.biometricHint,
-    this.biometricNotRecognized,
-    this.biometricRequiredTitle,
-    this.biometricSuccess,
-    this.cancelButton,
-    this.deviceCredentialsRequiredTitle,
-    this.deviceCredentialsSetupDescription,
-    this.goToSettingsButton,
-    this.goToSettingsDescription,
-    this.signInTitle,
-  });
-
-  /// Hint message advising the user how to authenticate with biometrics.
-  /// Maximum 60 characters.
-  final String? biometricHint;
-
-  /// Message to let the user know that authentication was failed.
-  /// Maximum 60 characters.
-  final String? biometricNotRecognized;
-
-  /// Message shown as a title in a dialog which indicates the user
-  /// has not set up biometric authentication on their device.
-  /// Maximum 60 characters.
-  final String? biometricRequiredTitle;
-
-  /// Message to let the user know that authentication was successful.
-  /// Maximum 60 characters
-  final String? biometricSuccess;
-
-  /// Message shown on a button that the user can click to leave the
-  /// current dialog.
-  /// Maximum 30 characters.
-  final String? cancelButton;
-
-  /// Message shown as a title in a dialog which indicates the user
-  /// has not set up credentials authentication on their device.
-  /// Maximum 60 characters.
-  final String? deviceCredentialsRequiredTitle;
-
-  /// Message advising the user to go to the settings and configure
-  /// device credentials on their device.
-  final String? deviceCredentialsSetupDescription;
-
-  /// Message shown on a button that the user can click to go to settings pages
-  /// from the current dialog.
-  /// Maximum 30 characters.
-  final String? goToSettingsButton;
-
-  /// Message advising the user to go to the settings and configure
-  /// biometric on their device.
-  final String? goToSettingsDescription;
-
-  /// Message shown as a title in a dialog which indicates the user
-  /// that they need to scan biometric to continue.
-  /// Maximum 60 characters.
-  final String? signInTitle;
+  const WindowsAuthMessages();
 
   @override
   Map<String, String> get args {
-    return <String, String>{
-      'biometricHint': biometricHint ?? windowsBiometricHint,
-      'biometricNotRecognized':
-          biometricNotRecognized ?? windowsBiometricNotRecognized,
-      'biometricSuccess': biometricSuccess ?? windowsBiometricSuccess,
-      'biometricRequired':
-          biometricRequiredTitle ?? windowsBiometricRequiredTitle,
-      'cancelButton': cancelButton ?? windowsCancelButton,
-      'deviceCredentialsRequired': deviceCredentialsRequiredTitle ??
-          windowsDeviceCredentialsRequiredTitle,
-      'deviceCredentialsSetupDescription': deviceCredentialsSetupDescription ??
-          windowsDeviceCredentialsSetupDescription,
-      'goToSetting': goToSettingsButton ?? goToSettings,
-      'goToSettingDescription':
-          goToSettingsDescription ?? windowsGoToSettingsDescription,
-      'signInTitle': signInTitle ?? windowsSignInTitle,
-    };
+    return <String, String>{};
   }
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is WindowsAuthMessages &&
-          runtimeType == other.runtimeType &&
-          biometricHint == other.biometricHint &&
-          biometricNotRecognized == other.biometricNotRecognized &&
-          biometricRequiredTitle == other.biometricRequiredTitle &&
-          biometricSuccess == other.biometricSuccess &&
-          cancelButton == other.cancelButton &&
-          deviceCredentialsRequiredTitle ==
-              other.deviceCredentialsRequiredTitle &&
-          deviceCredentialsSetupDescription ==
-              other.deviceCredentialsSetupDescription &&
-          goToSettingsButton == other.goToSettingsButton &&
-          goToSettingsDescription == other.goToSettingsDescription &&
-          signInTitle == other.signInTitle;
-
-  @override
-  int get hashCode => Object.hash(
-      super.hashCode,
-      biometricHint,
-      biometricNotRecognized,
-      biometricRequiredTitle,
-      biometricSuccess,
-      cancelButton,
-      deviceCredentialsRequiredTitle,
-      deviceCredentialsSetupDescription,
-      goToSettingsButton,
-      goToSettingsDescription,
-      signInTitle);
 }
-
-// Default strings for WindowsAuthMessages. Currently supports English.
-// Intl.message must be string literals.
-
-/// Message shown on a button that the user can click to go to settings pages
-/// from the current dialog.
-String get goToSettings => Intl.message('Go to settings',
-    desc: 'Message shown on a button that the user can click to go to '
-        'settings pages from the current dialog. Maximum 30 characters.');
-
-/// Hint message advising the user how to authenticate with biometrics.
-String get windowsBiometricHint => Intl.message('Verify identity',
-    desc: 'Hint message advising the user how to authenticate with biometrics. '
-        'Maximum 60 characters.');
-
-/// Message to let the user know that authentication was failed.
-String get windowsBiometricNotRecognized =>
-    Intl.message('Not recognized. Try again.',
-        desc: 'Message to let the user know that authentication was failed. '
-            'Maximum 60 characters.');
-
-/// Message to let the user know that authentication was successful. It
-String get windowsBiometricSuccess => Intl.message('Success',
-    desc: 'Message to let the user know that authentication was successful. '
-        'Maximum 60 characters.');
-
-/// Message shown on a button that the user can click to leave the
-/// current dialog.
-String get windowsCancelButton => Intl.message('Cancel',
-    desc: 'Message shown on a button that the user can click to leave the '
-        'current dialog. Maximum 30 characters.');
-
-/// Message shown as a title in a dialog which indicates the user
-/// that they need to scan biometric to continue.
-String get windowsSignInTitle => Intl.message('Authentication required',
-    desc: 'Message shown as a title in a dialog which indicates the user '
-        'that they need to scan biometric to continue. Maximum 60 characters.');
-
-/// Message shown as a title in a dialog which indicates the user
-/// has not set up biometric authentication on their device.
-String get windowsBiometricRequiredTitle => Intl.message('Biometric required',
-    desc: 'Message shown as a title in a dialog which indicates the user '
-        'has not set up biometric authentication on their device. '
-        'Maximum 60 characters.');
-
-/// Message shown as a title in a dialog which indicates the user
-/// has not set up credentials authentication on their device.
-String get windowsDeviceCredentialsRequiredTitle =>
-    Intl.message('Device credentials required',
-        desc: 'Message shown as a title in a dialog which indicates the user '
-            'has not set up credentials authentication on their device. '
-            'Maximum 60 characters.');
-
-/// Message advising the user to go to the settings and configure
-/// device credentials on their device.
-String get windowsDeviceCredentialsSetupDescription =>
-    Intl.message('Device credentials required',
-        desc: 'Message advising the user to go to the settings and configure '
-            'device credentials on their device.');
-
-/// Message advising the user to go to the settings and configure
-/// biometric on their device.
-String get windowsGoToSettingsDescription => Intl.message(
-    'Biometric authentication is not set up on your device. Go to '
-    "'Settings > Security' to add biometric authentication.",
-    desc: 'Message advising the user to go to the settings and configure '
-        'biometric on their device.');
--- packages/local_auth/local_auth_windows/test/local_auth_test.dart
+++ packages/local_auth/local_auth_windows/test/local_auth_test.dart
@@ -9,7 +9,7 @@ import 'package:local_auth_windows/local_auth_windows.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  group('LocalAuth', () {
+  group('authenticate', () {
     const MethodChannel channel = MethodChannel(
       'plugins.flutter.io/local_auth_windows',
     );
@@ -31,141 +31,49 @@ void main() {
       log.clear();
     });
 
-    test('deviceSupportsBiometrics calls platform', () async {
-      final bool result = await localAuthentication.deviceSupportsBiometrics();
-
+    test('authenticate with no arguments passes expected defaults', () async {
+      await localAuthentication.authenticate(
+          authMessages: <AuthMessages>[const WindowsAuthMessages()],
+          localizedReason: 'My localized reason');
       expect(
         log,
         <Matcher>[
-          isMethodCall('deviceSupportsBiometrics', arguments: null),
-        ],
-      );
-      expect(result, true);
-    });
-
-    test('getEnrolledBiometrics calls platform', () async {
-      final List<BiometricType> result =
-          await localAuthentication.getEnrolledBiometrics();
-
-      expect(
-        log,
-        <Matcher>[
-          isMethodCall('getEnrolledBiometrics', arguments: null),
-        ],
-      );
-      expect(result, <BiometricType>[
-        BiometricType.weak,
-        BiometricType.strong,
-      ]);
-    });
-
-    test('isDeviceSupported calls platform', () async {
-      await localAuthentication.isDeviceSupported();
-      expect(
-        log,
-        <Matcher>[
-          isMethodCall('isDeviceSupported', arguments: null),
+          isMethodCall('authenticate',
+              arguments: <String, dynamic>{
+                'localizedReason': 'My localized reason',
+                'useErrorDialogs': true,
+                'stickyAuth': false,
+                'sensitiveTransaction': true,
+                'biometricOnly': false,
+              }..addAll(const WindowsAuthMessages().args)),
         ],
       );
     });
 
-    test('stopAuthentication returns false', () async {
-      final bool result = await localAuthentication.stopAuthentication();
-      expect(result, false);
-    });
-
-    group('With device auth fail over', () {
-      test('authenticate with no args.', () async {
-        await localAuthentication.authenticate(
-          authMessages: <AuthMessages>[const WindowsAuthMessages()],
-          localizedReason: 'Needs secure',
-          options: const AuthenticationOptions(biometricOnly: true),
-        );
-        expect(
-          log,
-          <Matcher>[
-            isMethodCall('authenticate',
-                arguments: <String, dynamic>{
-                  'localizedReason': 'Needs secure',
-                  'useErrorDialogs': true,
-                  'stickyAuth': false,
-                  'sensitiveTransaction': true,
-                  'biometricOnly': true,
-                }..addAll(const WindowsAuthMessages().args)),
-          ],
-        );
-      });
-
-      test('authenticate with no sensitive transaction.', () async {
-        await localAuthentication.authenticate(
-          authMessages: <AuthMessages>[const WindowsAuthMessages()],
-          localizedReason: 'Insecure',
-          options: const AuthenticationOptions(
-            sensitiveTransaction: false,
-            useErrorDialogs: false,
-            biometricOnly: true,
-          ),
-        );
-        expect(
-          log,
-          <Matcher>[
-            isMethodCall('authenticate',
-                arguments: <String, dynamic>{
-                  'localizedReason': 'Insecure',
-                  'useErrorDialogs': false,
-                  'stickyAuth': false,
-                  'sensitiveTransaction': false,
-                  'biometricOnly': true,
-                }..addAll(const WindowsAuthMessages().args)),
-          ],
-        );
-      });
-    });
-
-    group('With biometrics only', () {
-      test('authenticate with no args.', () async {
-        await localAuthentication.authenticate(
-          authMessages: <AuthMessages>[const WindowsAuthMessages()],
-          localizedReason: 'Needs secure',
-        );
-        expect(
-          log,
-          <Matcher>[
-            isMethodCall('authenticate',
-                arguments: <String, dynamic>{
-                  'localizedReason': 'Needs secure',
-                  'useErrorDialogs': true,
-                  'stickyAuth': false,
-                  'sensitiveTransaction': true,
-                  'biometricOnly': false,
-                }..addAll(const WindowsAuthMessages().args)),
-          ],
-        );
-      });
-
-      test('authenticate with no sensitive transaction.', () async {
-        await localAuthentication.authenticate(
-          authMessages: <AuthMessages>[const WindowsAuthMessages()],
-          localizedReason: 'Insecure',
-          options: const AuthenticationOptions(
-            sensitiveTransaction: false,
-            useErrorDialogs: false,
-          ),
-        );
-        expect(
-          log,
-          <Matcher>[
-            isMethodCall('authenticate',
-                arguments: <String, dynamic>{
-                  'localizedReason': 'Insecure',
-                  'useErrorDialogs': false,
-                  'stickyAuth': false,
-                  'sensitiveTransaction': false,
-                  'biometricOnly': false,
-                }..addAll(const WindowsAuthMessages().args)),
-          ],
-        );
-      });
+    test('authenticate passes all options.', () async {
+      await localAuthentication.authenticate(
+        authMessages: <AuthMessages>[const WindowsAuthMessages()],
+        localizedReason: 'My localized reason',
+        options: const AuthenticationOptions(
+          useErrorDialogs: false,
+          stickyAuth: true,
+          sensitiveTransaction: false,
+          biometricOnly: true,
+        ),
+      );
+      expect(
+        log,
+        <Matcher>[
+          isMethodCall('authenticate',
+              arguments: <String, dynamic>{
+                'localizedReason': 'My localized reason',
+                'useErrorDialogs': false,
+                'stickyAuth': true,
+                'sensitiveTransaction': false,
+                'biometricOnly': true,
+              }..addAll(const WindowsAuthMessages().args)),
+        ],
+      );
     });
   });
 }
