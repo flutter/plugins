00f0f2a223124d67c7bb8d66aa8e5c717eda6661
Kristoffer Vinther <kristoffer@zliide.com>
2022-05-16 18:14:16 +0200

    [url_launcher_web] Revert changes to example app.

--- packages/url_launcher/url_launcher_web/example/lib/main.dart
+++ packages/url_launcher/url_launcher_web/example/lib/main.dart
@@ -3,34 +3,26 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:url_launcher/link.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 /// App for testing
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   /// Default Constructor
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(
-        home: Scaffold(
-          body: _List(),
-        ),
-      );
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _List extends StatelessWidget {
-  const _List({Key? key}) : super(key: key);
-
+class _MyAppState extends State<MyApp> {
   @override
-  Widget build(BuildContext context) => ListView.builder(
-        itemCount: 5000,
-        itemBuilder: (_, int index) => Link(
-          uri: Uri.parse('https://example.com/$index'),
-          builder: (_, __) => Text('#$index', textAlign: TextAlign.center),
-        ),
-      );
+  Widget build(BuildContext context) {
+    return const Directionality(
+      textDirection: TextDirection.ltr,
+      child: Text('Testing... Look at the console output for results!'),
+    );
+  }
 }
