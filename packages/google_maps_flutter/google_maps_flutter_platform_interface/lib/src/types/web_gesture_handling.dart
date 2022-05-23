7c6934d5b015086e54c934303ce7b5aa9741048f
Rexios <rexios@rexios.dev>
2022-05-23 17:27:08 -0400

    Added license text to web_gesture_handling.dart and organized imports

--- packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/web_gesture_handling.dart
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/web_gesture_handling.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /// This setting controls how the API handles gestures on the map
 enum WebGestureHandling {
   /// Scroll events and one-finger touch gestures scroll the page, and do not
