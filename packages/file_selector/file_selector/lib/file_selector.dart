e0eac1a36975b242cbe3443fee921a6b17f1e63a
Stuart Morgan <stuartmorgan@google.com>
2022-06-07 10:49:15 -0400

    Clean up comments

--- packages/file_selector/file_selector/lib/file_selector.dart
+++ packages/file_selector/file_selector/lib/file_selector.dart
@@ -9,19 +9,23 @@ import 'package:file_selector_platform_interface/file_selector_platform_interfac
 export 'package:file_selector_platform_interface/file_selector_platform_interface.dart'
     show XFile, XTypeGroup;
 
-/// Open file dialog for loading files and return a file path.
+/// Opens a file selection dialog and returns the path chosen by the user.
 ///
-/// [acceptedTypeGroups] is the file type that can be selected in the dialog.
-/// there are differences in behavior depending on the platform as follows.
-/// - Windows: Each group will be an entry in a list of filter options.
-/// - macOS: The union of all types allowed by all of the groups will be allowed.
+/// [acceptedTypeGroups] is a list of file type groups that can be selected in
+/// the dialog. How this is displayed depends on the pltaform, for example:
+/// - On Windows and Linux, each group will be an entry in a list of filter
+///   options.
+/// - On macOS, the union of all types allowed by all of the groups will be
+///   allowed.
 ///
-/// [initialDirectory] is the full path to the directory that will be displayed when the dialog is opened.
+/// [initialDirectory] is the full path to the directory that will be displayed
+/// when the dialog is opened. When not provided, the platform will pick an
+/// initial location.
 ///
 /// [confirmButtonText] is the text in the confirmation button of the dialog.
-/// when omitted, the default OS label. (for example, "Open")
+/// When not provided, the default OS label is used (for example, "Open").
 ///
-/// returns `null` if user cancels the operation.
+/// Returns `null` if the user cancels the operation.
 Future<XFile?> openFile({
   List<XTypeGroup> acceptedTypeGroups = const <XTypeGroup>[],
   String? initialDirectory,
@@ -40,10 +44,14 @@ Future<XFile?> openFile({
 /// - Windows: Each group will be an entry in a list of filter options.
 /// - macOS: The union of all types allowed by all of the groups will be allowed.
 ///
-/// [initialDirectory] is the full path to the directory that will be displayed when the dialog is opened.
+/// [initialDirectory] is the full path to the directory that will be displayed
+/// when the dialog is opened. When not provided, the platform will pick an
+/// initial location.
 ///
 /// [confirmButtonText] is the text in the confirmation button of the dialog.
-/// when omitted, the default OS label. (for example, "Open")
+/// When not provided, the default OS label is used (for example, "Open").
+///
+/// Returns an empty list if the user cancels the operation.
 Future<List<XFile>> openFiles({
   List<XTypeGroup> acceptedTypeGroups = const <XTypeGroup>[],
   String? initialDirectory,
@@ -62,14 +70,16 @@ Future<List<XFile>> openFiles({
 /// - Windows: Each group will be an entry in a list of filter options.
 /// - macOS: The union of all types allowed by all of the groups will be allowed.
 ///
-/// [initialDirectory] is the full path to the directory that will be displayed when the dialog is opened.
+/// [initialDirectory] is the full path to the directory that will be displayed
+/// when the dialog is opened. When not provided, the platform will pick an
+/// initial location.
 ///
 /// [suggestedName] is initial value of file name.
 ///
 /// [confirmButtonText] is the text in the confirmation button of the dialog.
-/// when omitted, the default OS label. (for example, "Save")
+/// When not provided, the default OS label is used (for example, "Save").
 ///
-/// returns `null` if user cancels the operation.
+/// Returns `null` if the user cancels the operation.
 Future<String?> getSavePath({
   List<XTypeGroup> acceptedTypeGroups = const <XTypeGroup>[],
   String? initialDirectory,
@@ -85,12 +95,14 @@ Future<String?> getSavePath({
 
 /// Gets a directory path from a user's file system.
 ///
-/// [initialDirectory] is the full path to the directory that will be displayed when the dialog is opened.
+/// [initialDirectory] is the full path to the directory that will be displayed
+/// when the dialog is opened. When not provided, the platform will pick an
+/// initial location.
 ///
 /// [confirmButtonText] is the text in the confirmation button of the dialog.
-/// when omitted, the default OS label. (for example, "Open")
+/// When not provided, the default OS label is used (for example, "Open").
 ///
-/// returns `null` if user cancels the operation.
+/// Returns `null` if the user cancels the operation.
 Future<String?> getDirectoryPath({
   String? initialDirectory,
   String? confirmButtonText,
