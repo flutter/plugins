39020ce73c7e7070e30f3ebcf345492441b477eb
8rine23 <58650422+TowaYamashita@users.noreply.github.com>
2022-06-10 05:43:12 +0900

    [file_selector]Improve API docs and examples (#4824)

--- packages/file_selector/file_selector/example/lib/save_text_page.dart
+++ packages/file_selector/file_selector/example/lib/save_text_page.dart
@@ -5,6 +5,7 @@
 import 'dart:typed_data';
 import 'package:file_selector/file_selector.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 /// Page for showing an example of saving with file_selector
 class SaveTextPage extends StatelessWidget {
@@ -15,17 +16,27 @@ class SaveTextPage extends StatelessWidget {
   final TextEditingController _contentController = TextEditingController();
 
   Future<void> _saveFile() async {
-    final String? path = await getSavePath();
+    final String fileName = _nameController.text;
+    // This demonstrates using an initial directory for the prompt, which should
+    // only be done in cases where the application can likely predict where the
+    // file will be saved. In most cases, this parameter should not be provided.
+    final String initialDirectory =
+        (await getApplicationDocumentsDirectory()).path;
+    final String? path = await getSavePath(
+      initialDirectory: initialDirectory,
+      suggestedName: fileName,
+    );
     if (path == null) {
       // Operation was canceled by the user.
       return;
     }
+
     final String text = _contentController.text;
-    final String fileName = _nameController.text;
     final Uint8List fileData = Uint8List.fromList(text.codeUnits);
     const String fileMimeType = 'text/plain';
     final XFile textFile =
         XFile.fromData(fileData, mimeType: fileMimeType, name: fileName);
+
     await textFile.saveTo(path);
   }
 
