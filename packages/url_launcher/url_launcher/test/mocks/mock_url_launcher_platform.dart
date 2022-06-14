49ce7d85ddf2a963337668129badf58ee4d4acc0
Stuart Morgan <stuartmorgan@google.com>
2022-06-14 11:27:18 -0400

    [url_launcher] Switch to new launchUrl interface
    Switches the new `launchUrl*` API methods to use the new corresponding
    platform interface method instead of the legacy version.
    
    The legacy `launch` API still uses the old interface to ensure that its
    behavior doesn't change.

--- packages/url_launcher/url_launcher/test/mocks/mock_url_launcher_platform.dart
+++ packages/url_launcher/url_launcher/test/mocks/mock_url_launcher_platform.dart
@@ -4,6 +4,7 @@
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import 'package:url_launcher_platform_interface/link.dart';
 import 'package:url_launcher_platform_interface/url_launcher_platform_interface.dart';
 
@@ -11,6 +12,7 @@ class MockUrlLauncher extends Fake
     with MockPlatformInterfaceMixin
     implements UrlLauncherPlatform {
   String? url;
+  PreferredLaunchMode? launchMode;
   bool? useSafariVC;
   bool? useWebView;
   bool? enableJavaScript;
@@ -32,8 +34,9 @@ class MockUrlLauncher extends Fake
 
   void setLaunchExpectations({
     required String url,
-    required bool? useSafariVC,
-    required bool useWebView,
+    PreferredLaunchMode? launchMode,
+    bool? useSafariVC,
+    bool? useWebView,
     required bool enableJavaScript,
     required bool enableDomStorage,
     required bool universalLinksOnly,
@@ -41,6 +44,7 @@ class MockUrlLauncher extends Fake
     required String? webOnlyWindowName,
   }) {
     this.url = url;
+    this.launchMode = launchMode;
     this.useSafariVC = useSafariVC;
     this.useWebView = useWebView;
     this.enableJavaScript = enableJavaScript;
@@ -85,6 +89,17 @@ class MockUrlLauncher extends Fake
     expect(headers, this.headers);
     expect(webOnlyWindowName, this.webOnlyWindowName);
     launchCalled = true;
+    return response!;
+  }
+
+  @override
+  Future<bool> launchUrl(String url, LaunchOptions options) async {
+    expect(url, this.url);
+    expect(options.mode, launchMode);
+    expect(options.webViewConfiguration.enableJavaScript, enableJavaScript);
+    expect(options.webViewConfiguration.enableDomStorage, enableDomStorage);
+    expect(options.webViewConfiguration.headers, headers);
+    expect(options.webOnlyWindowName, webOnlyWindowName);
     return response!;
   }
 
