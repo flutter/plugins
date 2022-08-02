d57973e68bf0e727b09c3294ed6ff9623855d642
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2022-08-02 12:31:19 -0700

    add java objects

---
+++ packages/webview_flutter/webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/JavaObjectHostApiImpl.java
@@ -0,0 +1,28 @@
+package io.flutter.plugins.webviewflutter;
+
+import androidx.annotation.NonNull;
+
+/**
+ * A pigeon Host API implementation that handles creating {@link Object}s and invoking its static
+ * and instance methods.
+ *
+ * <p>{@link Object} instances created by {@link JavaObjectHostApiImpl} are used to intercommunicate
+ * with a paired Dart object.
+ */
+public class JavaObjectHostApiImpl implements GeneratedAndroidWebView.JavaObjectHostApi {
+  private final InstanceManager instanceManager;
+
+  /**
+   * Constructs a {@link JavaObjectHostApiImpl}.
+   *
+   * @param instanceManager maintains instances stored to communicate with Dart objects
+   */
+  public JavaObjectHostApiImpl(InstanceManager instanceManager) {
+    this.instanceManager = instanceManager;
+  }
+
+  @Override
+  public void dispose(@NonNull Long identifier) {
+    instanceManager.remove(identifier);
+  }
+}
---
+++ packages/webview_flutter/webview_flutter_android/android/src/test/java/io/flutter/plugins/webviewflutter/JavaObjectHostApiTest.java
@@ -0,0 +1,28 @@
+package io.flutter.plugins.webviewflutter;
+
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+public class JavaObjectHostApiTest {
+  @Test
+  public void dispose() {
+    final InstanceManager instanceManager = InstanceManager.open(identifier -> {});
+
+    final JavaObjectHostApiImpl hostApi = new JavaObjectHostApiImpl(instanceManager);
+
+    Object object = new Object();
+    instanceManager.addDartCreatedInstance(object, 0);
+
+    // To free object for garbage collection.
+    //noinspection UnusedAssignment
+    object = null;
+
+    hostApi.dispose(0L);
+    Runtime.getRuntime().gc();
+
+    assertNull(instanceManager.getInstance(0));
+
+    instanceManager.close();
+  }
+}
