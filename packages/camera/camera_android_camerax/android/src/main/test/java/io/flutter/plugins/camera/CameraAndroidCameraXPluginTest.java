c9e1578eefea849fd95b09adfe40d5a3aef4ace2
camsim99 <camillesimon90@gmail.com>
2022-08-02 09:56:45 -0700

    Add unit test

---
+++ packages/camera/camera_android_camerax/android/src/main/test/java/io/flutter/plugins/camera/CameraAndroidCameraXPluginTest.java
@@ -0,0 +1,17 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugins.camera;
+
+import static junit.framework.TestCase.assertEquals;
+
+import org.junit.Test;
+
+public class CameraAndroidCameraXPluginTest {
+    
+    @Test
+    public void fakeTest() {
+        assertEquals(true, true);
+    }
+}
