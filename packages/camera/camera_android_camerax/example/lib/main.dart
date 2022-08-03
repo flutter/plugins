d66030ebee2b7dbb4030df66675170acae15cbb3
camsim99 <camillesimon90@gmail.com>
2022-08-03 15:53:53 -0700

    Rev 2

--- packages/camera/camera_android_camerax/example/lib/main.dart
+++ packages/camera/camera_android_camerax/example/lib/main.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -9,7 +13,7 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
--- packages/camera/camera_android_camerax/test/camera_android_camerax_test.dart
+++ packages/camera/camera_android_camerax/test/camera_android_camerax_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:camera_android_camerax/camera_android_camerax.dart';
 import 'package:camera_android_camerax/camera_android_camerax_platform_interface.dart';
