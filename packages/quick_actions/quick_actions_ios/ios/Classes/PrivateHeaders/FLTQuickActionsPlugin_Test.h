01464063c6b63aa44a8e3d732fbfb49969e2b652
hellohuanlin <41930132+hellohuanlin@users.noreply.github.com>
2022-08-04 13:11:06 -0700

    [quick_actions] add custom modulemap with test submodule (#6169)

---
+++ packages/quick_actions/quick_actions_ios/ios/Classes/PrivateHeaders/FLTQuickActionsPlugin_Test.h
@@ -0,0 +1,20 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+@import Flutter;
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// APIs exposed for unit tests.
+@interface FLTQuickActionsPlugin (Test)
+
+/// Initializes a FLTQuickActionsPlugin with the given method channel.
+/// API exposed for unit tests.
+/// @param channel A method channel.
+/// @return The initialized FLTQuickActionsPlugin.
+- (instancetype)initWithChannel:(FlutterMethodChannel *)channel;
+
+@end
+
+NS_ASSUME_NONNULL_END
