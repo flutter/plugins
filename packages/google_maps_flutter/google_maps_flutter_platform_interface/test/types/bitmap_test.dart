2d7bdbdce2782f2348338268c8343e3e469ea8fe
David Iglesias Teixeira <ditman@gmail.com>
2022-08-04 21:20:53 -0700

    Ignore unnecessary_import in test

--- packages/google_maps_flutter/google_maps_flutter_platform_interface/test/types/bitmap_test.dart
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/test/types/bitmap_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore:unnecessary_import
 import 'dart:typed_data';
 import 'dart:ui';
 
