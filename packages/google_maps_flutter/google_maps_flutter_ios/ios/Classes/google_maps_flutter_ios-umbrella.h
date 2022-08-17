686b60a4df62be2d41f7ec2980f19019030f644b
Rexios <rexios@rexios.dev>
2022-08-17 12:07:32 -0400

    Formatting

--- packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/google_maps_flutter_ios-umbrella.h
+++ packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/google_maps_flutter_ios-umbrella.h
@@ -3,10 +3,10 @@
 // found in the LICENSE file.
 
 #import <Foundation/Foundation.h>
+#import <google_maps_flutter_ios/FLTGoogleMapHeatmapController.h>
 #import <google_maps_flutter_ios/FLTGoogleMapJSONConversions.h>
 #import <google_maps_flutter_ios/FLTGoogleMapTileOverlayController.h>
 #import <google_maps_flutter_ios/FLTGoogleMapsPlugin.h>
-#import <google_maps_flutter_ios/FLTGoogleMapHeatmapController.h>
 
 FOUNDATION_EXPORT double google_maps_flutterVersionNumber;
 FOUNDATION_EXPORT const unsigned char google_maps_flutterVersionString[];
