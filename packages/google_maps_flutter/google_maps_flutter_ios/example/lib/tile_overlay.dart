d98a5b73b91a6c0c29ea404af78b9acc57cddef5
tarrinneal <tarrinneal@gmail.com>
2022-08-16 18:17:45 -0700

    remove errors related to analysis option

--- packages/google_maps_flutter/google_maps_flutter_ios/example/lib/tile_overlay.dart
+++ packages/google_maps_flutter/google_maps_flutter_ios/example/lib/tile_overlay.dart
@@ -140,8 +140,7 @@ class _DebugTileProvider implements TileProvider {
     textPainter.layout(
       maxWidth: width.toDouble(),
     );
-    const Offset offset = Offset(0, 0);
-    textPainter.paint(canvas, offset);
+    textPainter.paint(canvas, Offset.zero);
     canvas.drawRect(
         Rect.fromLTRB(0, 0, width.toDouble(), width.toDouble()), boxPaint);
     final ui.Picture picture = recorder.endRecording();
