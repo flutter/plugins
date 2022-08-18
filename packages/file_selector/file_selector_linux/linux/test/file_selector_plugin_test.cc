3920b38d25a5deac2f873c2dbb5b4d77bf9e1ae6
Stuart Morgan <stuartmorgan@google.com>
2022-08-18 12:19:43 -0400

    Licenses

--- packages/file_selector/file_selector_linux/linux/test/file_selector_plugin_test.cc
+++ packages/file_selector/file_selector_linux/linux/test/file_selector_plugin_test.cc
@@ -1,6 +1,7 @@
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 #include "include/file_selector_linux/file_selector_plugin.h"
 
 #include <flutter_linux/flutter_linux.h>
