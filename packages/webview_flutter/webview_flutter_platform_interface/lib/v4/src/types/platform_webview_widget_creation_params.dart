d7da2f5856cad9da8d20e9f4caad6504f77288f1
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2022-08-23 09:39:13 -0400

    dont use dart ui

--- packages/webview_flutter/webview_flutter_platform_interface/lib/v4/src/types/platform_webview_widget_creation_params.dart
+++ packages/webview_flutter/webview_flutter_platform_interface/lib/v4/src/types/platform_webview_widget_creation_params.dart
@@ -2,10 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:ui';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter/painting.dart';
 
 import '../platform_webview_controller.dart';
 
