a00703cde08b81fb5bc2136f10c853a38cf1078b
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2022-08-23 09:28:42 -0400

    start of impl

---
+++ packages/webview_flutter/webview_flutter/lib/src/v4/src/webview_widget.dart
@@ -0,0 +1,48 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:webview_flutter_platform_interface/v4/webview_flutter_platform_interface.dart';
+
+/// Displays a native WebView as a Widget.
+class WebViewWidget extends StatelessWidget {
+  /// Constructs a [WebViewWidget].
+  WebViewWidget({
+    Key? key,
+    required PlatformWebViewController controller,
+    TextDirection layoutDirection = TextDirection.ltr,
+    Set<Factory<OneSequenceGestureRecognizer>> gestureRecognizers =
+        const <Factory<OneSequenceGestureRecognizer>>{},
+  }) : this.withPlatform(
+          platform: PlatformWebViewWidget(
+            PlatformWebViewWidgetCreationParams(
+              key: key,
+              controller: controller,
+              layoutDirection: layoutDirection,
+              gestureRecognizers: gestureRecognizers,
+            ),
+          ),
+        );
+
+  /// Constructs a [WebViewWidget] with creation params for a specific
+  /// platform.
+  WebViewWidget.withPlatformCreationParams(
+    PlatformWebViewWidgetCreationParams params,
+  ) : this.withPlatform(platform: PlatformWebViewWidget(params));
+
+  /// Constructs a [WebViewWidget] with a specific platform implementation.
+  WebViewWidget.withPlatform({required this.platform})
+      : super(key: platform.params.key);
+
+  // TODO: This should be WebViewController! Don't submit.
+  /// Implementation of [PlatformWebViewWidget] for the current platform.
+  final PlatformWebViewWidget platform;
+
+  @override
+  Widget build(BuildContext context) {
+    return platform.build(context);
+  }
+}
---
+++ packages/webview_flutter/webview_flutter/test/v4/webview_widget_test.dart
@@ -0,0 +1,33 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:webview_flutter/src/v4/webview_flutter.dart';
+import 'package:webview_flutter_platform_interface/v4/webview_flutter_platform_interface.dart';
+
+import 'webview_widget_test.mocks.dart';
+
+@GenerateMocks(<Type>[PlatformWebViewWidget])
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('WebViewWidget', () {
+    testWidgets('build', (WidgetTester tester) async {
+      final MockPlatformWebViewWidget mockPlatformWebViewWidget =
+          MockPlatformWebViewWidget();
+      when(mockPlatformWebViewWidget.build(any)).thenReturn(
+        const Text('WebView'),
+      );
+
+      final WebViewWidget webViewWidget = WebViewWidget.withPlatform(
+        platform: mockPlatformWebViewWidget,
+      );
+
+      await tester.pumpWidget(WebViewWidget(controller: controller));
+    });
+  });
+}
