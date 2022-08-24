b52d2957627e26fb5fe2bb78e43757824e31185d
Pierre-Louis Guidez <plg@google.com>
2022-08-24 21:29:22 +0200

    x

--- packages/in_app_purchase/in_app_purchase/example/lib/main.dart
+++ packages/in_app_purchase/in_app_purchase/example/lib/main.dart
@@ -4,12 +4,14 @@
 
 import 'dart:async';
 import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:in_app_purchase_android/billing_client_wrappers.dart';
 import 'package:in_app_purchase_android/in_app_purchase_android.dart';
 import 'package:in_app_purchase_storekit/in_app_purchase_storekit.dart';
 import 'package:in_app_purchase_storekit/store_kit_wrappers.dart';
+
 import 'consumable_store.dart';
 
 void main() {
@@ -194,7 +196,9 @@ class _MyAppState extends State<_MyApp> {
     }
     final Widget storeHeader = ListTile(
       leading: Icon(_isAvailable ? Icons.check : Icons.block,
-          color: _isAvailable ? Colors.green : ThemeData.light().colorScheme.error),
+          color: _isAvailable
+              ? Colors.green
+              : ThemeData.light().colorScheme.error),
       title:
           Text('The store is ${_isAvailable ? 'available' : 'unavailable'}.'),
     );
