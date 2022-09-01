90052310c250ed49d0aa0b7b835b0ccb9c762af0
Rexios <rexios@rexios.dev>
2022-09-01 12:37:29 -0400

    Fixes

--- packages/google_maps_flutter/google_maps_flutter/test/fake_maps_controllers.dart
+++ packages/google_maps_flutter/google_maps_flutter/test/fake_maps_controllers.dart
@@ -4,7 +4,6 @@
 
 import 'dart:typed_data';
 
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
