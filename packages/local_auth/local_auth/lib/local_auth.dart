e0417f7d7dc1fbbf6511a45bf4edfe4990cb6f9b
Anan Alfred <ananalfredcarlos@gmail.com>
2022-09-03 16:08:32 +0700

    show AuthMessage On LocalAuth Import

--- packages/local_auth/local_auth/lib/local_auth.dart
+++ packages/local_auth/local_auth/lib/local_auth.dart
@@ -3,7 +3,14 @@
 // found in the LICENSE file.
 
 export 'package:local_auth/src/local_auth.dart' show LocalAuthentication;
+export 'package:local_auth_android/types/auth_messages_android.dart'
+    show AndroidAuthMessages;
+export 'package:local_auth_ios/types/auth_messages_ios.dart'
+    show IOSAuthMessages;
+export 'package:local_auth_platform_interface/types/auth_messages.dart';
 export 'package:local_auth_platform_interface/types/auth_options.dart'
     show AuthenticationOptions;
 export 'package:local_auth_platform_interface/types/biometric_type.dart'
     show BiometricType;
+export 'package:local_auth_windows/types/auth_messages_windows.dart'
+    show WindowsAuthMessages;
