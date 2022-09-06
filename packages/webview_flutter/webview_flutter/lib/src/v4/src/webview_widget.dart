c2df215d7344490261b41c1a1d589411931eb86b
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2022-09-06 09:17:13 -0400

    update test and add parameters

--- packages/webview_flutter/webview_flutter/lib/src/v4/src/webview_widget.dart
+++ packages/webview_flutter/webview_flutter/lib/src/v4/src/webview_widget.dart
@@ -12,30 +12,18 @@ import 'webview_controller.dart';
 /// Displays a native WebView as a Widget.
 class WebViewWidget extends StatelessWidget {
   /// Constructs a [WebViewWidget].
-  ///
-  /// `controller` - The [WebViewController] that allows controlling the native
-  /// WebView.
-  ///
-  /// `layoutDirection` - The layout direction to use for the embedded WebView.
-  ///
-  /// `gestureRecognizers` - specifies which gestures should be consumed by the
-  /// web view. When `gestureRecognizers` is empty (default), the web view will
-  /// only handle pointer events for gestures that were not claimed by any other
-  /// gesture recognizer.
   WebViewWidget({
     Key? key,
     required WebViewController controller,
     TextDirection layoutDirection = TextDirection.ltr,
     Set<Factory<OneSequenceGestureRecognizer>> gestureRecognizers =
         const <Factory<OneSequenceGestureRecognizer>>{},
-  }) : this.fromPlatform(
+  }) : this.fromPlatformCreationParams(
           key: key,
-          platform: PlatformWebViewWidget(
-            PlatformWebViewWidgetCreationParams(
-              controller: controller.platform,
-              layoutDirection: layoutDirection,
-              gestureRecognizers: gestureRecognizers,
-            ),
+          params: PlatformWebViewWidgetCreationParams(
+            controller: controller.platform,
+            layoutDirection: layoutDirection,
+            gestureRecognizers: gestureRecognizers,
           ),
         );
 
@@ -47,11 +35,27 @@ class WebViewWidget extends StatelessWidget {
   }) : this.fromPlatform(key: key, platform: PlatformWebViewWidget(params));
 
   /// Constructs a [WebViewWidget] from a specific platform implementation.
-  const WebViewWidget.fromPlatform({Key? key, required this.platform})
+  WebViewWidget.fromPlatform({Key? key, required this.platform})
       : super(key: key);
 
   /// Implementation of [PlatformWebViewWidget] for the current platform.
   final PlatformWebViewWidget platform;
+
+  /// The layout direction to use for the embedded WebView.
+  late final TextDirection layoutDirection = platform.params.layoutDirection;
+
+  /// Specifies which gestures should be consumed by the web view.
+  ///
+  /// It is possible for other gesture recognizers to be competing with the web
+  /// view on pointer events, e.g if the web view is inside a [ListView] the
+  /// [ListView] will want to handle vertical drags. The web view will claim
+  /// gestures that are recognized by any of the recognizers on this list.
+  ///
+  /// When `gestureRecognizers` is empty (default), the web view will only
+  /// handle pointer events for gestures that were not claimed by any other
+  /// gesture recognizer.
+  late final Set<Factory<OneSequenceGestureRecognizer>> gestureRecognizers =
+      platform.params.gestureRecognizers;
 
   @override
   Widget build(BuildContext context) {
--- packages/webview_flutter/webview_flutter/test/v4/webview_widget_test.dart
+++ packages/webview_flutter/webview_flutter/test/v4/webview_widget_test.dart
@@ -39,7 +39,7 @@ void main() {
           MockPlatformWebViewController();
       final WebViewController webViewController =
           WebViewController.fromPlatform(
-        platform: mockPlatformWebViewController,
+        mockPlatformWebViewController,
       );
 
       final WebViewWidget webViewWidget = WebViewWidget(
