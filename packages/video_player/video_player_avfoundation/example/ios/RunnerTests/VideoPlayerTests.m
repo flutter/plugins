620c1798a1962d8e7debb385e46e4fe3c224e86c
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2022-09-09 13:29:05 +0200

    Make sure the tests compile

--- packages/video_player/video_player_avfoundation/example/ios/RunnerTests/VideoPlayerTests.m
+++ packages/video_player/video_player_avfoundation/example/ios/RunnerTests/VideoPlayerTests.m
@@ -222,6 +222,7 @@
   // Set Picture In Picture
   FLTPreparePictureInPictureMessage *preparePictureInPicture =
       [FLTPreparePictureInPictureMessage makeWithTextureId:textureId
+        enableStartPictureInPictureAutomaticallyFromInline:@1
                                                        top:@0
                                                       left:@0
                                                      width:@300
