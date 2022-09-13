54a93b4ea6a93d26b16f110c4a791b7f3744c1ab
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2022-09-13 10:37:15 -0400

    reset

--- packages/webview_flutter/webview_flutter_android/example/android/app/src/main/java/io/flutter/plugins/webviewflutterexample/DriverExtensionActivity.java
+++ packages/webview_flutter/webview_flutter_android/example/android/app/src/main/java/io/flutter/plugins/webviewflutterexample/DriverExtensionActivity.java
@@ -4,38 +4,13 @@
 
 package io.flutter.plugins.webviewflutterexample;
 
-import android.webkit.WebView;
-
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.webviewflutter.GeneratedAndroidWebView;
-import io.flutter.plugins.webviewflutter.WebViewFlutterPlugin;
-import io.flutter.plugins.webviewflutter.WebViewHostApiImpl;
 
 public class DriverExtensionActivity extends FlutterActivity {
   @Override
   @NonNull
   public String getDartEntrypointFunctionName() {
     return "appMain";
-  }
-
-  @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    super.configureFlutterEngine(flutterEngine);
-
-    final WebViewFlutterPlugin webViewPlugin = (WebViewFlutterPlugin) flutterEngine.getPlugins().get(WebViewFlutterPlugin.class);
-
-    final WebViewHostApiImpl apiImpl = new WebViewHostApiImpl(webViewPlugin.getInstanceManager(),
-        new WebViewHostApiImpl.WebViewProxy(), this, null) {
-      @Override
-      public void create(Long instanceId, Boolean useHybridComposition) {
-        super.create(instanceId, useHybridComposition);
-        final WebView webView = getInstanceManager().getInstance(instanceId);
-        MobileAds.registerWebView(webView);
-      }
-    };
-    
-    GeneratedAndroidWebView.WebViewHostApi.setup(flutterEngine.getDartExecutor(), apiImpl);
   }
 }
