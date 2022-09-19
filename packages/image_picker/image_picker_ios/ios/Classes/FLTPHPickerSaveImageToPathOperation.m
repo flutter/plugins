4ed43b8f9b5485c4d5bb1789ea87aabb2c3a6133
Piotr Mitkowski <piotr.mitkowski1@gmail.com>
2022-09-19 08:23:21 +0200

    PR remarks
    Co-authored-by: Jenn Magder <magder@google.com>

--- packages/image_picker/image_picker_ios/ios/Classes/FLTPHPickerSaveImageToPathOperation.m
+++ packages/image_picker/image_picker_ios/ios/Classes/FLTPHPickerSaveImageToPathOperation.m
@@ -117,6 +117,7 @@ typedef void (^GetSavedPath)(NSString *);
  */
 - (void)processImage:(UIImage *)localImage API_AVAILABLE(ios(14)) {
   PHAsset *originalAsset;
+  // Only if requested, fetch the full "PHAsset" metadata, which requires  "Photo Library Usage" permissions.
   if (self.requestFullMetadata) {
     originalAsset = [FLTImagePickerPhotoAssetUtil getAssetFromPHPickerResult:self.result];
   }
