78e8a60ae8154f97a25c1fadec57d17c7922ed5f
Gary Qian <garyq@google.com>
2022-09-29 09:57:46 -0700

    Bump version and fix test

--- packages/google_maps_flutter/google_maps_flutter_android/android/src/test/java/io/flutter/plugins/googlemaps/PolylineControllerTest.java
+++ packages/google_maps_flutter/google_maps_flutter_android/android/src/test/java/io/flutter/plugins/googlemaps/PolylineControllerTest.java
@@ -7,7 +7,7 @@ package io.flutter.plugins.googlemaps;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 
-import com.google.android.gms.internal.maps.zzad;
+import com.google.android.gms.internal.maps.zzag;
 import com.google.android.gms.maps.model.Polyline;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -16,7 +16,7 @@ public class PolylineControllerTest {
 
   @Test
   public void controller_SetsStrokeDensity() {
-    final zzad z = mock(zzad.class);
+    final zzag z = mock(zzag.class);
     final Polyline polyline = spy(new Polyline(z));
 
     final float density = 5;
