a32c5a4d565ef5c3d1cea4440ed6efa63b253a95
alestiago <dev@alestiago.com>
2022-10-03 13:20:58 +0100

    refactor: removed video constraints

--- packages/camera/camera_web/lib/src/camera_web.dart
+++ packages/camera/camera_web/lib/src/camera_web.dart
@@ -234,12 +234,6 @@ class CameraPlugin extends CameraPlatform {
           video: VideoConstraints(
             facingMode:
                 cameraType != null ? FacingModeConstraint(cameraType) : null,
-            width: VideoSizeConstraint(
-              ideal: videoSize.width.toInt(),
-            ),
-            height: VideoSizeConstraint(
-              ideal: videoSize.height.toInt(),
-            ),
             deviceId: cameraMetadata.deviceId,
           ),
         ),
