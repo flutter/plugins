98e0ca1fa4282a6b6c4277edf5ecb7ed6391f807
Helielson <helielsonsa@gmail.com>
2022-10-06 19:16:04 -0300

    [video_player] Removed AudioSession configuration on iOS
    We have a custom AudioSession configuration, so it shouldn't be set
    on video_player.

--- packages/video_player/video_player_avfoundation/ios/Classes/FLTVideoPlayerPlugin.m
+++ packages/video_player/video_player_avfoundation/ios/Classes/FLTVideoPlayerPlugin.m
@@ -553,8 +553,10 @@ NS_INLINE UIViewController *rootViewController() {
 }
 
 - (void)initialize:(FlutterError *__autoreleasing *)error {
+  // WE HAVE A CUSTOM AUDIO SESSION CONFIGURATION, SO IT SHOULDN'T BE CONFIGURED HERE.
+  //
   // Allow audio playback when the Ring/Silent switch is set to silent
-  [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+  // [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
 
   [self.playersByTextureId
       enumerateKeysAndObjectsUsingBlock:^(NSNumber *textureId, FLTVideoPlayer *player, BOOL *stop) {
@@ -649,13 +651,15 @@ NS_INLINE UIViewController *rootViewController() {
 
 - (void)setMixWithOthers:(FLTMixWithOthersMessage *)input
                    error:(FlutterError *_Nullable __autoreleasing *)error {
-  if (input.mixWithOthers.boolValue) {
-    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback
-                                     withOptions:AVAudioSessionCategoryOptionMixWithOthers
-                                           error:nil];
-  } else {
-    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
-  }
+  // WE HAVE A CUSTOM AUDIO SESSION CONFIGURATION, SO IT SHOULDN'T BE CONFIGURED HERE.
+  //
+  //  if (input.mixWithOthers.boolValue) {
+  //    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback
+  //                                     withOptions:AVAudioSessionCategoryOptionMixWithOthers
+  //                                           error:nil];
+  //  } else {
+  //    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+  //  }
 }
 
 @end
