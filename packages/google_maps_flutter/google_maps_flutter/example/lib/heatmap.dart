1ce1595e1746130c7cd8a5b8f20e5e358339aee6
Rexios <rexios@rexios.dev>
2022-10-17 17:20:46 -0400

    Fixed analysis issues

--- packages/google_maps_flutter/google_maps_flutter/example/lib/heatmap.dart
+++ packages/google_maps_flutter/google_maps_flutter/example/lib/heatmap.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_flutter_example/page.dart';
+import 'page.dart';
 
 class HeatmapPage extends GoogleMapExampleAppPage {
   const HeatmapPage({Key? key})
--- packages/google_maps_flutter/google_maps_flutter/example/lib/main.dart
+++ packages/google_maps_flutter/google_maps_flutter/example/lib/main.dart
@@ -7,8 +7,8 @@ import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
 import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
 import 'animate_camera.dart';
-import 'lite_mode.dart';
 import 'heatmap.dart';
+import 'lite_mode.dart';
 import 'map_click.dart';
 import 'map_coordinates.dart';
 import 'map_ui.dart';
