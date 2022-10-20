3e58062a9cd0bdad1dd70b982a0c41a971cda277
alestiago <dev@alestiago.com>
2022-10-20 16:04:57 +0100

    feat: used real offscreen

--- packages/camera/camera_web/lib/src/camera.dart
+++ packages/camera/camera_web/lib/src/camera.dart
@@ -620,7 +620,7 @@ class Camera {
     final height = int.tryParse(heightString) ?? videoHeight;
 
     final bool canUseOffscreenCanvas =
-        false; //js_util.hasProperty(html.window, 'OffscreenCanvas');
+        js_util.hasProperty(html.window, 'OffscreenCanvas');
     late html.ImageData imageData;
     if (canUseOffscreenCanvas) {
       final html.OffscreenCanvas canvas = html.OffscreenCanvas(width, height);
