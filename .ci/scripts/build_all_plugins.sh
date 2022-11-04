360cd7353eb5c9d646889278ffa249717696ced2
Keyong Han <keyonghan@google.com>
2022-11-04 13:44:55 -0700

    general build_all_plugins

--- .ci/scripts/build_all_plugins.sh
+++ .ci/scripts/build_all_plugins.sh
@@ -3,6 +3,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+arg="$1"
 cd all_packages
-flutter build windows --debug
-flutter build windows --release
+if [[ "$arg" == "web" ]]; then
+  echo "Skipping; web does not support debug builds"
+else
+  flutter build "$arg" --debug
+  flutter build "$arg" --release
+fi
