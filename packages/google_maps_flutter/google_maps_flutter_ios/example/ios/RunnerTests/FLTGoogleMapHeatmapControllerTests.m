58198300d736c6d8af45fde28d4af4c2fc340dc1
Rexios <rexios@rexios.dev>
2022-11-07 16:24:19 -0500

    Expose `VideoScrubber` so it can be used to make custom video timelines

---
+++ packages/google_maps_flutter/google_maps_flutter_ios/example/ios/RunnerTests/FLTGoogleMapHeatmapControllerTests.m
@@ -0,0 +1,9 @@
+//
+//  FLTGoogleMapHeatmapControllerTests.m
+//  RunnerTests
+//
+//  Created by Rexios on 10/24/22.
+//  Copyright © 2022 The Flutter Authors. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
