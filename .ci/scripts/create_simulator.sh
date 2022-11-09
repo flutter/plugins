c3ec25d781155fc2f84623f9abf4eeb5fb88d967
Keyong Han <keyonghan@google.com>
2022-11-08 16:51:07 -0800

    add 0_4 master

---
+++ .ci/scripts/create_simulator.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Copyright 2013 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+xcrun simctl list
+xcrun simctl create Flutter-iPhone com.apple.CoreSimulator.SimDeviceType.iPhone-11 com.apple.CoreSimulator.SimRuntime.iOS-15-0 | xargs xcrun simctl boot
