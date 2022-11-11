bea4b96f06a6b5a79b035f263c604129ee1bef9a
Keyong Han <keyonghan@google.com>
2022-11-11 13:07:19 -0800

    ios runtime update

--- .ci/scripts/create_simulator.sh
+++ .ci/scripts/create_simulator.sh
@@ -4,4 +4,4 @@
 # found in the LICENSE file.
 
 xcrun simctl list
-xcrun simctl create Flutter-iPhone com.apple.CoreSimulator.SimDeviceType.iPhone-11 com.apple.CoreSimulator.SimRuntime.iOS-15-0 | xargs xcrun simctl boot
+xcrun simctl create Flutter-iPhone com.apple.CoreSimulator.SimDeviceType.iPhone-11 com.apple.CoreSimulator.SimRuntime.iOS-16-0 | xargs xcrun simctl boot
