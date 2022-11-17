c24c92d3286bd2736f43e3829ed1badb61116b53
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2022-11-17 10:48:03 -0500

    fix merge conflicts

--- .ci/scripts/build_all_plugins.sh
+++ .ci/scripts/build_all_plugins.sh
@@ -3,13 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-<<<<<<< HEAD
-cd all_packages
-flutter build windows --debug
-flutter build windows --release
-=======
 platform="$1"
 build_mode="$2"
 cd all_packages
 flutter build "$platform" --"$build_mode"
->>>>>>> e500884c758c2d516baf7d5ab30639c81dd6b849
