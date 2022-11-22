12e62d9cfdb9b8042bc3ac3600c5a04dc0dbae1a
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2022-11-22 10:06:54 -0500

    exchange services for material

--- packages/webview_flutter/webview_flutter_android/lib/src/v4/src/android_webview_controller.dart
+++ packages/webview_flutter/webview_flutter_android/lib/src/v4/src/android_webview_controller.dart
@@ -9,7 +9,7 @@ import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 import 'package:webview_flutter_platform_interface/v4/webview_flutter_platform_interface.dart';
 
 import '../../android_webview.dart' as android_webview;
