742081206b2250f2563dc475f92e6ef35fe487fe
ItayBarFaye <106300092+ItayBarFaye@users.noreply.github.com>
2022-12-06 12:46:43 +0200

    fix launching IOS app shortcut type not received

--- packages/quick_actions/quick_actions_ios/ios/Classes/QuickActionsPlugin.swift
+++ packages/quick_actions/quick_actions_ios/ios/Classes/QuickActionsPlugin.swift
@@ -42,7 +42,7 @@ public final class QuickActionsPlugin: NSObject, FlutterPlugin {
       shortcutItemProvider.shortcutItems = []
       result(nil)
     case "getLaunchAction":
-      result(nil)
+      result(launchingShortcutType)
     case _:
       result(FlutterMethodNotImplemented)
     }
