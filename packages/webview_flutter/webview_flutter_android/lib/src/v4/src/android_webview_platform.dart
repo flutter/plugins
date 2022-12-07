720e6e14e64c58324967dd32f1a58afce57cc3d3
Maurits van Beusekom <maurits@vnbskm.nl>
2022-12-07 20:14:52 +0100

    Fixed analysis issues

--- packages/webview_flutter/webview_flutter_android/lib/src/v4/src/android_webview_platform.dart
+++ packages/webview_flutter/webview_flutter_android/lib/src/v4/src/android_webview_platform.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:webview_flutter_platform_interface/v4/webview_flutter_platform_interface.dart';
+import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
 
 import 'android_navigation_delegate.dart';
 import 'android_webview_controller.dart';
