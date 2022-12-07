205f59b6d8a1f27c41d7f9d081102e0c2323ca11
Maurits van Beusekom <maurits@vnbskm.nl>
2022-12-07 19:28:43 +0100

    Added implementation of AndroidWebViewPlatform

---
+++ packages/webview_flutter/webview_flutter_android/lib/src/v4/src/android_webview_platform.dart
@@ -0,0 +1,40 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:webview_flutter_platform_interface/v4/webview_flutter_platform_interface.dart';
+
+import 'android_navigation_delegate.dart';
+import 'android_webview_controller.dart';
+import 'android_webview_cookie_manager.dart';
+
+/// Implementation of [WebViewPlatform] using the WebKit API.
+class AndroidWebViewPlatform extends WebViewPlatform {
+  @override
+  AndroidWebViewController createPlatformWebViewController(
+    PlatformWebViewControllerCreationParams params,
+  ) {
+    return AndroidWebViewController(params);
+  }
+
+  @override
+  AndroidNavigationDelegate createPlatformNavigationDelegate(
+    PlatformNavigationDelegateCreationParams params,
+  ) {
+    return AndroidNavigationDelegate(params);
+  }
+
+  @override
+  AndroidWebViewWidget createPlatformWebViewWidget(
+    PlatformWebViewWidgetCreationParams params,
+  ) {
+    return AndroidWebViewWidget(params);
+  }
+
+  @override
+  AndroidWebViewCookieManager createPlatformCookieManager(
+    PlatformWebViewCookieManagerCreationParams params,
+  ) {
+    return AndroidWebViewCookieManager(params);
+  }
+}
