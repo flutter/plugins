c40f5729cc27539c68ea1707e57f36990570432b
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2022-12-08 12:57:00 -0500

    undo iOS changes

--- packages/webview_flutter/webview_flutter_wkwebview/lib/src/webkit_webview_controller.dart
+++ packages/webview_flutter/webview_flutter_wkwebview/lib/src/webkit_webview_controller.dart
@@ -575,10 +575,9 @@ class WebKitNavigationDelegate extends PlatformNavigationDelegate {
                 .fromPlatformNavigationDelegateCreationParams(params)) {
     final WeakReference<WebKitNavigationDelegate> weakThis =
         WeakReference<WebKitNavigationDelegate>(this);
-    _navigationDelegate =
-        (this.params as WebKitNavigationDelegateCreationParams)
-            .webKitProxy
-            .createNavigationDelegate(
+    _navigationDelegate = (this.params as WebKitNavigationDelegateCreationParams)
+        .webKitProxy
+        .createNavigationDelegate(
       didFinishNavigation: (WKWebView webView, String? url) {
         if (weakThis.target?._onPageFinished != null) {
           weakThis.target!._onPageFinished!(url ?? '');
