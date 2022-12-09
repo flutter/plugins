13c5ad4e6af6130edb433d32e8b61883be01e647
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2022-12-08 21:42:26 -0500

    move stuff to legacy folder and create new main

--- packages/webview_flutter/webview_flutter_android/example/integration_test/legacy/webview_flutter_test.dart
+++ packages/webview_flutter/webview_flutter_android/example/integration_test/legacy/webview_flutter_test.dart
@@ -21,9 +21,9 @@ import 'package:integration_test/integration_test.dart';
 import 'package:webview_flutter_android/src/instance_manager.dart';
 import 'package:webview_flutter_android/src/weak_reference_utils.dart';
 import 'package:webview_flutter_android/webview_flutter_android_legacy.dart';
-import 'package:webview_flutter_android_example/navigation_decision.dart';
-import 'package:webview_flutter_android_example/navigation_request.dart';
-import 'package:webview_flutter_android_example/web_view.dart';
+import 'package:webview_flutter_android_example/legacy/navigation_decision.dart';
+import 'package:webview_flutter_android_example/legacy/navigation_request.dart';
+import 'package:webview_flutter_android_example/legacy/web_view.dart';
 import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface_legacy.dart';
 
 Future<void> main() async {
