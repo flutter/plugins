15c634078c50e0a1af557cc411d8ab4fcd1c0531
ItayBarFaye <106300092+ItayBarFaye@users.noreply.github.com>
2022-12-13 10:23:51 +0200

    Update RunnerUITests.swift

--- packages/quick_actions/quick_actions_ios/example/ios/RunnerUITests/RunnerUITests.swift
+++ packages/quick_actions/quick_actions_ios/example/ios/RunnerUITests/RunnerUITests.swift
@@ -2,7 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import Flutter
 import XCTest
+@testable import quick_actions_ios
 
 private let elementWaitingTime: TimeInterval = 30
 
