cde4774129736767664e65a9befafdd1d106edf1
ItayBarFaye <106300092+ItayBarFaye@users.noreply.github.com>
2022-12-13 15:44:15 +0200

    Update RunnerUITests.swift
    revert

--- packages/quick_actions/quick_actions_ios/example/ios/RunnerUITests/RunnerUITests.swift
+++ packages/quick_actions/quick_actions_ios/example/ios/RunnerUITests/RunnerUITests.swift
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import Flutter
 import XCTest
 
 private let elementWaitingTime: TimeInterval = 30
@@ -51,64 +50,5 @@ class RunnerUITests: XCTestCase {
     }
 
     XCTAssert(actionTwoConfirmation.exists)
-  }
-
-  func testQuickActionWhenAppIsInBackground() {
-    exampleApp.launch()
-
-    let actionsReady = exampleApp.otherElements["actions ready"]
-
-    if !actionsReady.waitForExistence(timeout: elementWaitingTime) {
-      XCTFail(
-        "Failed due to not able to find the actionsReady in the app with \(elementWaitingTime) seconds. App debug description: \(exampleApp.debugDescription)"
-      )
-    }
-
-    XCUIDevice.shared.press(.home)
-
-    let springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
-    let quickActionsAppIcon = springboard.icons["quick_actions_example"]
-    if !quickActionsAppIcon.waitForExistence(timeout: elementWaitingTime) {
-      XCTFail(
-        "Failed due to not able to find the example app from springboard with \(elementWaitingTime) seconds. Springboard debug description: \(springboard.debugDescription)"
-      )
-    }
-
-    quickActionsAppIcon.press(forDuration: 2)
-
-    let actionOne = springboard.buttons["Action one"]
-    if !actionOne.waitForExistence(timeout: elementWaitingTime) {
-      XCTFail(
-        "Failed due to not able to find the actionOne button from springboard with \(elementWaitingTime) seconds. Springboard debug description: \(springboard.debugDescription)"
-      )
-    }
-
-    actionOne.tap()
-
-    let actionOneConfirmation = exampleApp.otherElements["action_one"]
-    if !actionOneConfirmation.waitForExistence(timeout: elementWaitingTime) {
-      XCTFail(
-        "Failed due to not able to find the actionOneConfirmation in the app with \(elementWaitingTime) seconds. Springboard debug description: \(springboard.debugDescription)"
-      )
-    }
-
-    XCTAssert(actionOneConfirmation.exists)
-  }
-  
-  func testFetchQuickActionAfterFreshStart() {   
-    let call = FlutterMethodCall(methodName: "getLaunchAction", arguments: nil)
-    let springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")     
-    let quickActionsAppIcon = springboard.icons["quick_actions_example"]
-    if !quickActionsAppIcon.waitForExistence(timeout: elementWaitingTime) {
-      XCTFail(
-        "Failed due to not able to find the example app from springboard with \(elementWaitingTime) seconds. Springboard debug description: \(springboard.debugDescription)"
-      )
-    }
-    
-    quickActionsAppIcon.press(forDuration: 2)
-    
-    springboard.handle(call) { result in
-      XCTAssertEqual(result, "quick_actions_example")      
-    }
   }
 }
