0496af6c9653739d79142c6a1a0e0a30b44db1ba
ItayBarFaye <106300092+ItayBarFaye@users.noreply.github.com>
2022-12-20 09:46:07 +0200

    Update RunnerUITests.swift

--- packages/quick_actions/quick_actions_ios/example/ios/RunnerUITests/RunnerUITests.swift
+++ packages/quick_actions/quick_actions_ios/example/ios/RunnerUITests/RunnerUITests.swift
@@ -51,4 +51,46 @@ class RunnerUITests: XCTestCase {
 
     XCTAssert(actionTwoConfirmation.exists)
   }
+  
+  func testQuickActionWhenAppIsInBackground() {
+    exampleApp.launch()
+
+    let actionsReady = exampleApp.otherElements["actions ready"]
+
+    if !actionsReady.waitForExistence(timeout: elementWaitingTime) {
+      XCTFail(
+        "Failed due to not able to find the actionsReady in the app with \(elementWaitingTime) seconds. App debug description: \(exampleApp.debugDescription)"
+      )
+    }
+
+    XCUIDevice.shared.press(.home)
+
+    let springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
+    let quickActionsAppIcon = springboard.icons["quick_actions_example"]
+    if !quickActionsAppIcon.waitForExistence(timeout: elementWaitingTime) {
+      XCTFail(
+        "Failed due to not able to find the example app from springboard with \(elementWaitingTime) seconds. Springboard debug description: \(springboard.debugDescription)"
+      )
+    }
+
+    quickActionsAppIcon.press(forDuration: 2)
+
+    let actionOne = springboard.buttons["Action one"]
+    if !actionOne.waitForExistence(timeout: elementWaitingTime) {
+      XCTFail(
+        "Failed due to not able to find the actionOne button from springboard with \(elementWaitingTime) seconds. Springboard debug description: \(springboard.debugDescription)"
+      )
+    }
+
+    actionOne.tap()
+
+    let actionOneConfirmation = exampleApp.otherElements["action_one"]
+    if !actionOneConfirmation.waitForExistence(timeout: elementWaitingTime) {
+      XCTFail(
+        "Failed due to not able to find the actionOneConfirmation in the app with \(elementWaitingTime) seconds. Springboard debug description: \(springboard.debugDescription)"
+      )
+    }
+
+    XCTAssert(actionOneConfirmation.exists)
+  }
 }
