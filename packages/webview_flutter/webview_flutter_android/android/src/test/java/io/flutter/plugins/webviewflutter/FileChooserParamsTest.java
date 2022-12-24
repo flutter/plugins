73110076a56b0d13d955a184ca60652a4b667ef7
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2022-12-24 13:01:05 -0500

    flutterapi create test

---
+++ packages/webview_flutter/webview_flutter_android/android/src/test/java/io/flutter/plugins/webviewflutter/FileChooserParamsTest.java
@@ -0,0 +1,73 @@
+package io.flutter.plugins.webviewflutter;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import android.content.Intent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebChromeClient.FileChooserParams;
+
+import androidx.annotation.Nullable;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import io.flutter.plugin.common.BinaryMessenger;
+
+public class FileChooserParamsTest {
+  @Rule
+  public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+  @Mock
+  public FileChooserParams mockFileChooserParams;
+
+  @Mock public BinaryMessenger mockBinaryMessenger;
+
+  InstanceManager instanceManager;
+
+  @Before
+  public void setUp() {
+    instanceManager = InstanceManager.open(identifier -> {});
+  }
+
+  @After
+  public void tearDown() {
+    instanceManager.close();
+  }
+
+  @Test
+  public void flutterApiCreate() {
+    final FileChooserParamsFlutterApiImpl spyFlutterApi =
+        spy(new FileChooserParamsFlutterApiImpl(mockBinaryMessenger, instanceManager));
+
+    when(mockFileChooserParams.isCaptureEnabled()).thenReturn(true);
+    when(mockFileChooserParams.getAcceptTypes()).thenReturn(new String[]{"my", "list"});
+    when(mockFileChooserParams.getMode()).thenReturn(FileChooserParams.MODE_OPEN_MULTIPLE);
+    when(mockFileChooserParams.getFilenameHint()).thenReturn("filenameHint");
+    spyFlutterApi.create(mockFileChooserParams, reply -> {});
+
+    final long identifier =
+        Objects.requireNonNull(instanceManager.getIdentifierForStrongReference(mockFileChooserParams));
+    final ArgumentCaptor<GeneratedAndroidWebView.FileChooserModeEnumData> modeCaptor = ArgumentCaptor.forClass(GeneratedAndroidWebView.FileChooserModeEnumData.class);
+
+    verify(spyFlutterApi).create(eq(identifier), eq(true),
+        eq(Arrays.asList("my", "list")),
+        modeCaptor.capture(),
+        eq("filenameHint"),
+        any());
+    assertEquals(modeCaptor.getValue().getValue(), GeneratedAndroidWebView.FileChooserMode.OPEN_MULTIPLE);
+  }
+}
