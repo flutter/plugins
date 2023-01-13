7113c31e9ec7e52ee8db846fce42bc6f9b43e785
Stuart Morgan <stuartmorgan@google.com>
2023-01-13 11:48:44 -0500

    Revert "[shared_preferences_foundation] Add Swift runtime search paths for Objective-C apps (#6952)"
    This reverts commit be2e3de7a7a605848d2ba74b572b4f4b4b411025.

--- packages/shared_preferences/shared_preferences_foundation/example/ios/Runner/Runner-Bridging-Header.h
+++ packages/shared_preferences/shared_preferences_foundation/example/ios/Runner/Runner-Bridging-Header.h
@@ -2,9 +2,4 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <Flutter/Flutter.h>
-#import <UIKit/UIKit.h>
-
-@interface AppDelegate : FlutterAppDelegate
-
-@end
+#import "GeneratedPluginRegistrant.h"
