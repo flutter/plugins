65548a0f41f8689a52098baaf270d395cdf74035
dmytro diachenko <dmytro.diachenko@spsoft.com>
2023-01-16 18:37:29 +0000

    hardcode quality

--- packages/image_picker/image_picker_ios/ios/Classes/FLTPHPickerSaveImageToPathOperation.m
+++ packages/image_picker/image_picker_ios/ios/Classes/FLTPHPickerSaveImageToPathOperation.m
@@ -13,9 +13,9 @@ API_AVAILABLE(ios(14))
 @interface FLTPHPickerSaveImageToPathOperation ()
 
 @property(strong, nonatomic) PHPickerResult *result;
-@property(strong, nonatomic) NSNumber *maxHeight;
-@property(strong, nonatomic) NSNumber *maxWidth;
-@property(strong, nonatomic) NSNumber *desiredImageQuality;
+@property(assign, nonatomic) NSNumber *maxHeight;
+@property(assign, nonatomic) NSNumber *maxWidth;
+@property(assign, nonatomic) NSNumber *desiredImageQuality;
 @property(assign, nonatomic) BOOL requestFullMetadata;
 
 @end
@@ -175,7 +175,7 @@ API_AVAILABLE(ios(14))
     NSString *savedPath =
         [FLTImagePickerPhotoAssetUtil saveImageWithPickerInfo:nil
                                                         image:localImage
-                                                 imageQuality:self.desiredImageQuality];
+                                                 imageQuality: @0.5];
     [self completeOperationWithPath:savedPath error:nil];
   }
 }
