95cec5f12984e55525228e7c4f31f717420bad9e
Ross Wang <imagipioneer@gmail.com>
2023-01-17 05:32:04 -0800

    [google_maps_flutter_web] Fix licenses
    Lock to 2013, and add omissions.

--- packages/google_maps_flutter/google_maps_flutter_web/lib/src/overlays.dart
+++ packages/google_maps_flutter/google_maps_flutter_web/lib/src/overlays.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 part of google_maps_flutter_web;
 
 /// This class manages all the [TileOverlayController]s associated to a [GoogleMapController].
