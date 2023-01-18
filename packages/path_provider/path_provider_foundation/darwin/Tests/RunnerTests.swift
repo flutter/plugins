ef71c1d59707b95985dde111b947de1c0a7a7af6
Stuart Morgan <stuartmorgan@google.com>
2023-01-18 11:00:01 -0500

    Move unit test to shared location, and add iOS unit test target using it

--- packages/path_provider/path_provider_foundation/darwin/Tests/RunnerTests.swift
+++ packages/path_provider/path_provider_foundation/darwin/Tests/RunnerTests.swift
@@ -2,9 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import FlutterMacOS
 import XCTest
 @testable import path_provider_foundation
+
+#if os(iOS)
+import Flutter
+#elseif os(macOS)
+import FlutterMacOS
+#endif
 
 class RunnerTests: XCTestCase {
   func testGetTemporaryDirectory() throws {
