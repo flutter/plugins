c56da37dd505c65a87e08508b030f8bb4583e62d
camsim99 <camillesimon90@gmail.com>
2023-01-18 14:07:39 -0800

    Add )

--- packages/camera/camera/lib/src/camera_controller.dart
+++ packages/camera/camera/lib/src/camera_controller.dart
@@ -506,8 +506,8 @@ class CameraController extends ValueNotifier<CameraValue> {
           isRecordingVideo: true,
           isRecordingPaused: false,
           recordingOrientation: Optional<DeviceOrientation>.of(
-              value.lockedCaptureOrientation ?? value.deviceOrientation)),
-          isStreamingImages: onAvailable != null,
+              value.lockedCaptureOrientation ?? value.deviceOrientation),
+          isStreamingImages: onAvailable != null);
     } on PlatformException catch (e) {
       throw CameraException(e.code, e.message);
     }
