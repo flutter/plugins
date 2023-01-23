4ea5bf31ddc30c9b87b43058781dd07e192d5b0e
amannegi <akuro787898@gmail.com>
2023-01-23 15:17:46 +0530

    [shared_preferences]: Switched to Piegon, created setters

--- packages/shared_preferences/shared_preferences_android/android/src/main/java/io/flutter/plugins/sharedpreferences/SharedPreferencesPlugin.java
+++ packages/shared_preferences/shared_preferences_android/android/src/main/java/io/flutter/plugins/sharedpreferences/SharedPreferencesPlugin.java
@@ -8,39 +8,113 @@ import android.content.Context;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
+import android.content.SharedPreferences;
+
+
+import io.flutter.plugins.sharedpreferences.Messages.SharedPreferencesApi;
 
 /** SharedPreferencesPlugin */
-public class SharedPreferencesPlugin implements FlutterPlugin {
-  private static final String CHANNEL_NAME = "plugins.flutter.io/shared_preferences_android";
-  private MethodChannel channel;
-  private MethodCallHandlerImpl handler;
+public class SharedPreferencesPlugin implements FlutterPlugin , SharedPreferencesApi{
+
+
+  private static final String SHARED_PREFERENCES_NAME = "FlutterSharedPreferences";
+
+  // Fun fact: The following is a base64 encoding of the string "This is the prefix for a list."
+  private static final String LIST_IDENTIFIER = "VGhpcyBpcyB0aGUgcHJlZml4IGZvciBhIGxpc3Qu";
+  private static final String BIG_INTEGER_PREFIX = "VGhpcyBpcyB0aGUgcHJlZml4IGZvciBCaWdJbnRlZ2Vy";
+  private static final String DOUBLE_PREFIX = "VGhpcyBpcyB0aGUgcHJlZml4IGZvciBEb3VibGUu";
+
+  private final android.content.SharedPreferences preferences;
+
+  SharedPreferencesPlugin(){
+      preferences = context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
+  }
+
+  // private static final String CHANNEL_NAME = "plugins.flutter.io/shared_preferences_android";
+  // private MethodChannel channel;
+
+  // private MethodCallHandlerImpl handler;
+
+  private void setup(BinaryMessenger messenger, Context context) {
+    TaskQueue taskQueue = messenger.makeBackgroundTaskQueue();
+
+    try {
+      SharedPreferencesApi.setup(messenger, this);
+    } catch (Exception ex) {
+      Log.e(TAG, "Received exception while setting up SharedPreferencesPlugin", ex);
+    }
+
+    // this.context = context;
+  }
 
   @SuppressWarnings("deprecation")
   public static void registerWith(io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
     final SharedPreferencesPlugin plugin = new SharedPreferencesPlugin();
-    plugin.setupChannel(registrar.messenger(), registrar.context());
+    plugin.setup(registrar.messenger(), registrar.context());
   }
 
   @Override
   public void onAttachedToEngine(FlutterPlugin.FlutterPluginBinding binding) {
-    setupChannel(binding.getBinaryMessenger(), binding.getApplicationContext());
+    setup(binding.getBinaryMessenger(), binding.getApplicationContext());
+  }
+
+  @Override 
+  public void onDetachedFromEngine(FlutterPlugin.FlutterPluginBinding binding) {
+    SharedPreferencesApi.setup(binding.getBinaryMessenger(), null);
+  } 
+
+  @Override
+  public Boolean setBool(String key, Boolean value){
+    return preferences.edit().putBoolean(key, value).commit();
+  }
+   @Override
+  public Boolean setString(String key, String value){
+   if (value.startsWith(LIST_IDENTIFIER)
+              || value.startsWith(BIG_INTEGER_PREFIX)
+              || value.startsWith(DOUBLE_PREFIX)) {
+            result.error(
+                "StorageError",
+                "This string cannot be stored as it clashes with special identifier prefixes.",
+                null);
+            return false;
+          }
+    return preferences.edit().putString(key, value).commit();
   }
 
   @Override
-  public void onDetachedFromEngine(FlutterPlugin.FlutterPluginBinding binding) {
-    teardownChannel();
+  public Boolean setInt(String key, Object value){
+     Number number =  value;
+    if (number instanceof BigInteger) {
+      BigInteger integerValue = (BigInteger) number;
+      return preferences.edit().putString(key, BIG_INTEGER_PREFIX + integerValue.toString(Character.MAX_RADIX)).commit();
+    } else {
+      return preferences.edit().putLong(key, number.longValue()).commit();
+    }
   }
 
-  private void setupChannel(BinaryMessenger messenger, Context context) {
-    channel = new MethodChannel(messenger, CHANNEL_NAME);
-    handler = new MethodCallHandlerImpl(context);
-    channel.setMethodCallHandler(handler);
+  @Override
+  public Boolean setDouble(String key, double value){
+    String doubleValueStr = Double.toString(value);
+    return preferences.edit().putString(key, DOUBLE_PREFIX + doubleValueStr).commit();
   }
 
-  private void teardownChannel() {
-    handler.teardown();
-    handler = null;
-    channel.setMethodCallHandler(null);
-    channel = null;
+  @Override
+  public Boolean setStringList(String key,List<String> value){
+    return preferences.edit().putString(key, LIST_IDENTIFIER + encodeList(list)).commit();
   }
+
+
+
+  // private void setupChannel(BinaryMessenger messenger, Context context) {
+  //   channel = new MethodChannel(messenger, CHANNEL_NAME);
+  //   handler = new MethodCallHandlerImpl(context);
+  //   channel.setMethodCallHandler(handler);
+  // }
+
+  // private void teardownChannel() {
+  //   handler.teardown();
+  //   handler = null;
+  //   channel.setMethodCallHandler(null);
+  //   channel = null;
+  // }
 }
---
+++ packages/shared_preferences/shared_preferences_android/pigeons/messages.dart
@@ -0,0 +1,38 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(PigeonOptions(
+  input: 'pigeons/messages.dart',
+  javaOut:
+      'android/src/main/java/io/flutter/plugins/sharedpreferences/Messages.java',
+  javaOptions: JavaOptions(
+      className: 'Messages', package: 'io.flutter.plugins.sharedpreferences'),
+  dartOut: 'lib/messages.g.dart',
+  dartTestOut: 'test/messages_test.g.dart',
+  copyrightHeader: 'pigeons/copyright.txt',
+))
+@HostApi(dartHostTestHandler: 'TestSharedPreferencesApi')
+abstract class SharedPreferencesApi {
+  @TaskQueue(type: TaskQueueType.serialBackgroundThread)
+  bool remove(String key);
+  @TaskQueue(type: TaskQueueType.serialBackgroundThread)
+  bool? setBool(String key, bool value);
+  @TaskQueue(type: TaskQueueType.serialBackgroundThread)
+  bool? setString(String key, String value);
+  @TaskQueue(type: TaskQueueType.serialBackgroundThread)
+  bool? setInt(String key, Object value);
+
+  @TaskQueue(type: TaskQueueType.serialBackgroundThread)
+  bool? setDouble(String key, double value);
+
+  @TaskQueue(type: TaskQueueType.serialBackgroundThread)
+  bool? setStringList (String key, List<String>value);
+
+  @TaskQueue(type: TaskQueueType.serialBackgroundThread)
+  bool? clear();
+  @TaskQueue(type: TaskQueueType.serialBackgroundThread)
+  Map<String, Object>? getAll();
+}
