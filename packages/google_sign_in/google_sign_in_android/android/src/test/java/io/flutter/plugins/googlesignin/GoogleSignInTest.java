2898b577171293237742bddc68858a91367d26be
Todd Volkert <tvolkert@google.com>
2023-01-23 17:17:56 -0800

    Slight cleanup in GoogleSignInPlugin
    1) Use switch instead of if/else and show all possible states
    2) Handle the case of synchronous failure in signInSilently()
       by checking isComplete() instead of isSuccessful()

--- packages/google_sign_in/google_sign_in_android/android/src/test/java/io/flutter/plugins/googlesignin/GoogleSignInTest.java
+++ packages/google_sign_in/google_sign_in_android/android/src/test/java/io/flutter/plugins/googlesignin/GoogleSignInTest.java
@@ -17,7 +17,10 @@ import android.content.res.Resources;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.Task;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -43,6 +46,7 @@ public class GoogleSignInTest {
   @Mock GoogleSignInWrapper mockGoogleSignIn;
   @Mock GoogleSignInAccount account;
   @Mock GoogleSignInClient mockClient;
+  @Mock Task<GoogleSignInAccount> mockSignInTask;
   private GoogleSignInPlugin plugin;
 
   @Before
@@ -202,6 +206,22 @@ public class GoogleSignInTest {
         mock(BinaryMessenger.class), mock(Context.class), mock(GoogleSignInWrapper.class));
 
     plugin.onMethodCall(new MethodCall("signIn", null), null);
+  }
+
+  @Test
+  public void signInSilentlyThatImmediatelyCompletesWithoutResultFinishesWithError() {
+    final String clientId = "fakeClientId";
+    MethodCall methodCall = buildInitMethodCall(clientId, null);
+    initAndAssertServerClientId(methodCall, clientId);
+
+    ApiException exception = new ApiException(new Status(CommonStatusCodes.SIGN_IN_REQUIRED, "Error text"));
+    when(mockClient.silentSignIn()).thenReturn(mockSignInTask);
+    when(mockSignInTask.isComplete()).thenReturn(true);
+    when(mockSignInTask.getResult(ApiException.class)).thenThrow(exception);
+
+    MethodCall methodCall = new MethodCall("signInSilently", null);
+    plugin.onMethodCall(methodCall, result);
+    verify(result).error("sign_in_required", "Error text", null);
   }
 
   @Test
