c987dfe558724fb515355ebbd9a1b248b6a908d9
Andrew Coutts <andrewjohncoutts@gmail.com>
2023-01-25 08:04:18 -0500

    Add test for nv21

--- packages/camera/camera/test/camera_image_test.dart
+++ packages/camera/camera/test/camera_image_test.dart
@@ -139,6 +139,30 @@ void main() {
       expect(cameraImage.format.group, ImageFormatGroup.yuv420);
     });
 
+    test('$CameraImage has ImageFormatGroup.nv21 for Android', () {
+      debugDefaultTargetPlatformOverride = TargetPlatform.android;
+
+      final CameraImage cameraImage =
+          CameraImage.fromPlatformData(<dynamic, dynamic>{
+        'format': 17,
+        'height': 1,
+        'width': 4,
+        'lensAperture': 1.8,
+        'sensorExposureTime': 9991324,
+        'sensorSensitivity': 92.0,
+        'planes': <dynamic>[
+          <dynamic, dynamic>{
+            'bytes': Uint8List.fromList(<int>[1, 2, 3, 4]),
+            'bytesPerPixel': 1,
+            'bytesPerRow': 4,
+            'height': 1,
+            'width': 4
+          }
+        ]
+      });
+      expect(cameraImage.format.group, ImageFormatGroup.nv21);
+    });
+
     test('$CameraImage has ImageFormatGroup.bgra8888 for iOS', () {
       debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
 
