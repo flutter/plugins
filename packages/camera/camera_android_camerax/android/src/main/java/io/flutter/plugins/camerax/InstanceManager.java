e6d8c746dec32db66c1654927cb6f0925e288003
camsim99 <camillesimon90@gmail.com>
2023-01-26 10:17:10 -0800

    Add comment

--- packages/camera/camera_android_camerax/android/src/main/java/io/flutter/plugins/camerax/InstanceManager.java
+++ packages/camera/camera_android_camerax/android/src/main/java/io/flutter/plugins/camerax/InstanceManager.java
@@ -122,7 +122,8 @@ public class InstanceManager {
   /**
    * Adds a new instance that was instantiated from the host platform.
    *
-   * <p>If an instance has already been added, the identifier of the instance will be returned.
+   * <p>If an instance has already been added, this will re-create it. {@code #containsInstance} can
+   * be used to check if the object has already been added to avoid this.
    *
    * @param instance the instance to be stored.
    * @return the unique identifier stored with instance.
