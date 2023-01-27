38ec627fbd1dc0456918fe9737171faf0fd8249c
Rexios <rexios@rexios.dev>
2023-01-27 17:23:32 -0500

    Fixing issues

--- packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/heatmap.dart
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/heatmap.dart
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/foundation.dart' show listEquals, objectRuntimeType;
-import 'package:flutter/foundation.dart' show immutable;
+import 'package:flutter/foundation.dart'
+    show listEquals, objectRuntimeType, immutable;
 import 'package:flutter/material.dart' show Color;
 
 import 'types.dart';
