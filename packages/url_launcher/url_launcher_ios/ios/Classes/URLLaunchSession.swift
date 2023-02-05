3dbda712883b47f8bd6de9c70061cffaf93656ac
Andres Gutierrez <andresgutierrez535@gmail.com>
2023-02-05 00:46:49 -0800

    migrate to Swift

---
+++ packages/url_launcher/url_launcher_ios/ios/Classes/URLLaunchSession.swift
@@ -0,0 +1,39 @@
+// Copyright 2022 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import Flutter
+import SafariServices
+
+class URLLaunchSession: NSObject, SFSafariViewControllerDelegate {
+    var flutterResult: FlutterResult?
+    var url: URL
+    var safari: SFSafariViewController
+    var didFinish: (() -> Void)?
+
+    init(url: URL, result: FlutterResult?) {
+        self.url = url
+        self.flutterResult = result
+        self.safari = SFSafariViewController(url: url)
+
+        super.init()
+        self.safari.delegate = self
+    }
+
+    func safariViewController(_ controller: SFSafariViewController, didCompleteInitialLoad didLoadSuccessfully: Bool) {
+        if didLoadSuccessfully {
+            self.flutterResult?(true)
+        } else {
+            self.flutterResult?(FlutterError(code: "Error", message: "Error while launching \(self.url.absoluteString)", details: nil))
+        }
+    }
+
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        controller.dismiss(animated: true, completion: nil)
+        self.didFinish?()
+    }
+
+    func close() {
+        self.safariViewControllerDidFinish(self.safari)
+    }
+}
--- packages/url_launcher/url_launcher_ios/ios/url_launcher_ios.podspec
+++ packages/url_launcher/url_launcher_ios/ios/url_launcher_ios.podspec
@@ -13,8 +13,12 @@ A Flutter plugin for making the underlying platform (Android or iOS) launch a UR
   s.author           = { 'Flutter Dev Team' => 'flutter-dev@googlegroups.com' }
   s.source           = { :http => 'https://github.com/flutter/plugins/tree/main/packages/url_launcher/url_launcher_ios' }
   s.documentation_url = 'https://pub.dev/packages/url_launcher'
-  s.source_files = 'Classes/**/*'
-  s.public_header_files = 'Classes/**/*.h'
+  s.swift_version = '5.0'
+  s.source_files = 'Classes/**/*.swift'
+  s.xcconfig = {
+    'LIBRARY_SEARCH_PATHS' => '$(TOOLCHAIN_DIR)/usr/lib/swift/$(PLATFORM_NAME)/ $(SDKROOT)/usr/lib/swift',
+    'LD_RUNPATH_SEARCH_PATHS' => '/usr/lib/swift',
+  }
   s.dependency 'Flutter'
   s.platform = :ios, '11.0'
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES' }
