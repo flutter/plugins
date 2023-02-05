f0c11bb952006f4c6f486563def4dd4145a9e40d
balvinderz <balvindersi2@gmail.com>
2023-02-05 10:57:42 +0530

    fix format

--- packages/google_maps_flutter/google_maps_flutter_android/android/src/test/java/io/flutter/plugins/googlemaps/ConvertTest.java
+++ packages/google_maps_flutter/google_maps_flutter_android/android/src/test/java/io/flutter/plugins/googlemaps/ConvertTest.java
@@ -8,12 +8,12 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ConvertTest{
+public class ConvertTest {
 
     @Test
-    public void toPointsConvertsThePointsWithFullPrecision(){
-            double latitude = 43.03725568057;
-            double longitude = -87.90466904649;
+    public void toPointsConvertsThePointsWithFullPrecision() {
+        double latitude = 43.03725568057;
+        double longitude = -87.90466904649;
         ArrayList<Double> point = new ArrayList<Double>();
         point.add(latitude);
         point.add(longitude);
@@ -21,8 +21,7 @@ public class ConvertTest{
         pointsList.add(point);
         List<LatLng> latLngs = Convert.toPoints(pointsList);
         LatLng latLng = latLngs.get(0);
-        Assert.assertEquals(String.valueOf(latitude), String.valueOf(latLng.latitude));
-        Assert.assertEquals(String.valueOf(longitude), String.valueOf(latLng.longitude));
-
+        Assert.assertEquals(latitude, latLng.latitude, 1e-15);
+        Assert.assertEquals(longitude, latLng.longitude, 1e-15);
     }
 }
