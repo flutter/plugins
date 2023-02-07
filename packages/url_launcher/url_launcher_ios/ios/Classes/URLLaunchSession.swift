f04747fce6c0efe6853e5e32d5bf99a6e406222f
Andres Gutierrez <andresgutierrez535@gmail.com>
2023-02-07 05:26:55 -0800

    swift-format

--- packages/url_launcher/url_launcher_ios/ios/Classes/URLLaunchSession.swift
+++ packages/url_launcher/url_launcher_ios/ios/Classes/URLLaunchSession.swift
@@ -6,34 +6,38 @@ import Flutter
 import SafariServices
 
 class URLLaunchSession: NSObject, SFSafariViewControllerDelegate {
-    var flutterResult: FlutterResult?
-    var url: URL
-    var safari: SFSafariViewController
-    var didFinish: (() -> Void)?
+  var flutterResult: FlutterResult?
+  var url: URL
+  var safari: SFSafariViewController
+  var didFinish: (() -> Void)?
 
-    init(url: URL, result: FlutterResult?) {
-        self.url = url
-        self.flutterResult = result
-        self.safari = SFSafariViewController(url: url)
+  init(url: URL, result: FlutterResult?) {
+    self.url = url
+    self.flutterResult = result
+    self.safari = SFSafariViewController(url: url)
 
-        super.init()
-        self.safari.delegate = self
+    super.init()
+    self.safari.delegate = self
+  }
+
+  func safariViewController(
+    _ controller: SFSafariViewController, didCompleteInitialLoad didLoadSuccessfully: Bool
+  ) {
+    if didLoadSuccessfully {
+      self.flutterResult?(true)
+    } else {
+      self.flutterResult?(
+        FlutterError(
+          code: "Error", message: "Error while launching \(self.url.absoluteString)", details: nil))
     }
+  }
 
-    func safariViewController(_ controller: SFSafariViewController, didCompleteInitialLoad didLoadSuccessfully: Bool) {
-        if didLoadSuccessfully {
-            self.flutterResult?(true)
-        } else {
-            self.flutterResult?(FlutterError(code: "Error", message: "Error while launching \(self.url.absoluteString)", details: nil))
-        }
-    }
+  func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+    controller.dismiss(animated: true, completion: nil)
+    self.didFinish?()
+  }
 
-    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
-        controller.dismiss(animated: true, completion: nil)
-        self.didFinish?()
-    }
-
-    func close() {
-        self.safariViewControllerDidFinish(self.safari)
-    }
+  func close() {
+    self.safariViewControllerDidFinish(self.safari)
+  }
 }
