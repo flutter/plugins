830f3018e3adeb40483ae6573c4e78e1150eb65c
Stuart Morgan <stuartmorgan@google.com>
2023-02-08 14:40:47 -0500

    Update creation as well

--- .ci/scripts/create_simulator.sh
+++ .ci/scripts/create_simulator.sh
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-device=com.apple.CoreSimulator.SimDeviceType.iPhone-11
+device=com.apple.CoreSimulator.SimDeviceType.iPhone-13
 os=com.apple.CoreSimulator.SimRuntime.iOS-16-0
 
 xcrun simctl list
