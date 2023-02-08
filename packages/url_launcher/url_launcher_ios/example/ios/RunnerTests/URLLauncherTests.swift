7a4093e2cdc60b8c29d44f9e3ee884d6ed5a2329
Andres Gutierrez <andresgutierrez535@gmail.com>
2023-02-07 22:58:35 -0800

    swift-format

--- packages/url_launcher/url_launcher_ios/example/ios/RunnerTests/URLLauncherTests.swift
+++ packages/url_launcher/url_launcher_ios/example/ios/RunnerTests/URLLauncherTests.swift
@@ -2,120 +2,125 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import Flutter
 import Foundation
 import XCTest
-import Flutter
 
 @testable import url_launcher_ios
 
 class URLLauncherTests: XCTestCase {
-    override func setUp() {
-        self.continueAfterFailure = false
+  override func setUp() {
+    self.continueAfterFailure = false
+  }
+
+  func testCreatePlugin() {
+    let plugin = FLTURLLauncherPlugin()
+    XCTAssertNotNil(plugin)
+  }
+
+  func testHandleMethodCall_canLaunch() {
+    let plugin = FLTURLLauncherPlugin()
+
+    let testCases = [
+      [
+        "url": "https://www.flutter.dev",
+        "expected": true,
+      ],
+      [
+        "url": "invalidURL",
+        "expected": false,
+      ],
+    ]
+
+    for testCase in testCases {
+      let call = FlutterMethodCall(methodName: "canLaunch", arguments: ["url": testCase["url"]])
+      let resultExpectation = expectation(description: "result block must be called.")
+
+      plugin.handle(call) { result in
+        XCTAssertEqual(
+          result as? Bool, Optional(testCase["expected"]! as! Bool),
+          "result of canLaunch was not as expected")
+        resultExpectation.fulfill()
+      }
+
+      waitForExpectations(timeout: 5)
+    }
+  }
+
+  func testHandleMethodCall_launch() {
+    let plugin = FLTURLLauncherPlugin()
+    let call = FlutterMethodCall(methodName: "launch", arguments: ["url": "https://flutter.dev"])
+    let resultExpectation = expectation(description: "result block must be called.")
+
+    plugin.handle(call) { result in
+      XCTAssertEqual(
+        result as? Bool, Optional(true),
+        "result block should be called with true on successful launch")
+      resultExpectation.fulfill()
     }
 
-    func testCreatePlugin() {
-        let plugin = FLTURLLauncherPlugin()
-        XCTAssertNotNil(plugin)
+    waitForExpectations(timeout: 5)
+  }
+
+  func testHandleMethodCall_launchInVC() {
+    let plugin = FLTURLLauncherPlugin()
+    let call = FlutterMethodCall(
+      methodName: "launch", arguments: ["url": "https://flutter.dev", "useSafariVC": true])
+    let resultExpectation = expectation(description: "result block must be called.")
+
+    plugin.handle(call) { result in
+      XCTAssertEqual(
+        result as? Bool, Optional(true),
+        "result block should be called with true on successful launch")
+      resultExpectation.fulfill()
     }
 
-    func testHandleMethodCall_canLaunch() {
-        let plugin = FLTURLLauncherPlugin()
+    waitForExpectations(timeout: 5)
+  }
 
-        let testCases = [[
-            "url": "https://www.flutter.dev",
-            "expected": true
-        ], [
-            "url": "invalidURL",
-            "expected": false
-        ]]
+  func testHandleMethodCall_closeWebView() {
+    let plugin = FLTURLLauncherPlugin()
 
-        for testCase in testCases {
-            let call = FlutterMethodCall(methodName: "canLaunch", arguments: ["url": testCase["url"]])
-            let resultExpectation = expectation(description: "result block must be called.")
+    // launch webview in separate VC
+    var call = FlutterMethodCall(
+      methodName: "launch", arguments: ["url": "https://flutter.dev", "useSafariVC": true])
+    var resultExpectation = expectation(description: "result block must be called.")
 
-            plugin.handle(call) { result in
-                XCTAssertEqual(
-                    result as? Bool, Optional(testCase["expected"]! as! Bool),
-                    "result of canLaunch was not as expected")
-                resultExpectation.fulfill()
-            }
-
-            waitForExpectations(timeout: 5)
-        }
+    plugin.handle(call) { result in
+      XCTAssertEqual(
+        result as? Bool, Optional(true),
+        "result block should be called with true on successful launch")
+      resultExpectation.fulfill()
     }
 
-    func testHandleMethodCall_launch() {
-        let plugin = FLTURLLauncherPlugin()
-        let call = FlutterMethodCall(methodName: "launch", arguments: ["url": "https://flutter.dev"])
-        let resultExpectation = expectation(description: "result block must be called.")
+    waitForExpectations(timeout: 5)
 
-        plugin.handle(call) { result in
-            XCTAssertEqual(
-                result as? Bool, Optional(true),
-                "result block should be called with true on successful launch")
-            resultExpectation.fulfill()
-        }
+    // close webview
+    call = FlutterMethodCall(methodName: "close", arguments: nil)
+    resultExpectation = expectation(description: "result block must be called.")
 
-        waitForExpectations(timeout: 5)
+    plugin.handle(call) { result in
+      XCTAssertEqual(
+        result as? Bool, nil,
+        "result block should be called with nil on close")
+      resultExpectation.fulfill()
     }
 
-    func testHandleMethodCall_launchInVC() {
-        let plugin = FLTURLLauncherPlugin()
-        let call = FlutterMethodCall(methodName: "launch", arguments: ["url": "https://flutter.dev", "useSafariVC": true])
-        let resultExpectation = expectation(description: "result block must be called.")
+    waitForExpectations(timeout: 5)
+  }
 
-        plugin.handle(call) { result in
-            XCTAssertEqual(
-                result as? Bool, Optional(true),
-                "result block should be called with true on successful launch")
-            resultExpectation.fulfill()
-        }
+  func testHandleMethodCall_nonExistentMethod() {
+    let plugin = FLTURLLauncherPlugin()
+    let call = FlutterMethodCall(methodName: "nonExistent", arguments: nil)
+    let resultExpectation = expectation(description: "result block must be called.")
 
-        waitForExpectations(timeout: 5)
+    plugin.handle(call) { result in
+      XCTAssertEqual(
+        result as? NSObject, FlutterMethodNotImplemented,
+        "result block must be called with FlutterMethodNotImplemented")
+      resultExpectation.fulfill()
     }
 
-    func testHandleMethodCall_closeWebView() {
-        let plugin = FLTURLLauncherPlugin()
-
-        // launch webview in separate VC
-        var call = FlutterMethodCall(methodName: "launch", arguments: ["url": "https://flutter.dev", "useSafariVC": true])
-        var resultExpectation = expectation(description: "result block must be called.")
-
-        plugin.handle(call) { result in
-            XCTAssertEqual(
-                result as? Bool, Optional(true),
-                "result block should be called with true on successful launch")
-            resultExpectation.fulfill()
-        }
-
-        waitForExpectations(timeout: 5)
-
-        // close webview
-        call = FlutterMethodCall(methodName: "close", arguments: nil)
-        resultExpectation = expectation(description: "result block must be called.")
-
-        plugin.handle(call) { result in
-            XCTAssertEqual(
-                result as? Bool, nil,
-                "result block should be called with nil on close")
-            resultExpectation.fulfill()
-        }
-
-        waitForExpectations(timeout: 5)
-    }
-
-    func testHandleMethodCall_nonExistentMethod() {
-        let plugin = FLTURLLauncherPlugin()
-        let call = FlutterMethodCall(methodName: "nonExistent", arguments: nil)
-        let resultExpectation = expectation(description: "result block must be called.")
-
-        plugin.handle(call) { result in
-            XCTAssertEqual(
-                result as? NSObject, FlutterMethodNotImplemented,
-                "result block must be called with FlutterMethodNotImplemented")
-            resultExpectation.fulfill()
-        }
-
-        waitForExpectations(timeout: 5)
-    }
+    waitForExpectations(timeout: 5)
+  }
 }
