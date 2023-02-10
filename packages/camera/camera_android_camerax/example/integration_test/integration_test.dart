6ee77021e58b2ca0b1a106eedc991daeb33d03f7
camsim99 <camillesimon90@gmail.com>
2023-02-10 12:25:41 -0800

    Add integration test

--- packages/camera/camera_android_camerax/example/integration_test/integration_test.dart
+++ packages/camera/camera_android_camerax/example/integration_test/integration_test.dart
@@ -2,11 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:camera_android_camerax/camera_android_camerax.dart';
+import 'package:camera_platform_interface/camera_platform_interface.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('placeholder test', (WidgetTester tester) async {});
+  setUpAll(() async {
+    CameraPlatform.instance = AndroidCameraCameraX();
+  });
+
+  testWidgets('availableCameras only supports valid back or front cameras',
+      (WidgetTester tester) async {
+    final List<CameraDescription> availableCameras =
+        await CameraPlatform.instance.availableCameras();
+
+    for (final CameraDescription cameraDescription in availableCameras) {
+      expect(
+          cameraDescription.lensDirection, isNot(CameraLensDirection.external));
+      expect(cameraDescription.sensorOrientation, anyOf(0, 90, 180, 270));
+    }
+  });
 }
