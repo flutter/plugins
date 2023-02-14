382ed3d703a9a6d83a969220f2426b1e95801dbc
David Iglesias Teixeira <ditman@gmail.com>
2023-02-14 22:49:08 +0000

    Remove dependency in package:jwt_decoder

--- packages/google_sign_in/google_sign_in_web/example/integration_test/src/jwt_examples.dart
+++ packages/google_sign_in/google_sign_in_web/example/integration_test/src/jwt_examples.dart
@@ -6,13 +6,19 @@ import 'package:google_identity_services_web/id.dart';
 
 import 'jsify_as.dart';
 
-/// A JWT token with null `credential`.
+/// A CredentialResponse with null `credential`.
 final CredentialResponse nullCredential =
     jsifyAs<CredentialResponse>(<String, Object?>{
   'credential': null,
 });
 
-/// A JWT token for predefined values.
+/// A CredentialResponse wrapping a known good JWT Token as its `credential`.
+final CredentialResponse goodCredential =
+    jsifyAs<CredentialResponse>(<String, Object?>{
+  'credential': goodJwtToken,
+});
+
+/// A JWT token with predefined values.
 ///
 /// 'email': 'adultman@example.com',
 /// 'sub': '123456',
@@ -20,15 +26,21 @@ final CredentialResponse nullCredential =
 /// 'picture': 'https://thispersondoesnotexist.com/image?x=.jpg',
 ///
 /// Signed with HS256 and the private key: 'symmetric-encryption-is-weak'
-final CredentialResponse okCredential =
-    jsifyAs<CredentialResponse>(<String, Object?>{
-  'credential':
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkdWx0bWFuQGV4YW1wbGUuY29tIiwic3ViIjoiMTIzNDU2IiwibmFtZSI6IlZpbmNlbnQgQWR1bHRtYW4iLCJwaWN0dXJlIjoiaHR0cHM6Ly90aGlzcGVyc29uZG9lc25vdGV4aXN0LmNvbS9pbWFnZT94PS5qcGcifQ.lqzULA_U3YzEl_-fL7YLU-kFXmdD2ttJLTv-UslaNQ4',
-});
+const String goodJwtToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.$goodPayload.lqzULA_U3YzEl_-fL7YLU-kFXmdD2ttJLTv-UslaNQ4';
 
-// More encrypted credential responses may be created on https://jwt.io.
+/// The payload of a JWT token that contains predefined values.
+///
+/// 'email': 'adultman@example.com',
+/// 'sub': '123456',
+/// 'name': 'Vincent Adultman',
+/// 'picture': 'https://thispersondoesnotexist.com/image?x=.jpg',
+const String goodPayload =
+    'eyJlbWFpbCI6ImFkdWx0bWFuQGV4YW1wbGUuY29tIiwic3ViIjoiMTIzNDU2IiwibmFtZSI6IlZpbmNlbnQgQWR1bHRtYW4iLCJwaWN0dXJlIjoiaHR0cHM6Ly90aGlzcGVyc29uZG9lc25vdGV4aXN0LmNvbS9pbWFnZT94PS5qcGcifQ';
+
+// More encrypted JWT Tokens may be created on https://jwt.io.
 //
-// First, decode the credential that's listed above, modify to your heart's
+// First, decode the `goodJwtToken` above, modify to your heart's
 // content, and add a new credential here.
 //
-// (It can also be done with `package:jose` and `dart:convert`.)
+// (New tokens can also be created with `package:jose` and `dart:convert`.)
--- packages/google_sign_in/google_sign_in_web/lib/src/utils.dart
+++ packages/google_sign_in/google_sign_in_web/lib/src/utils.dart
@@ -2,10 +2,57 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:convert';
+
 import 'package:google_identity_services_web/id.dart';
 import 'package:google_identity_services_web/oauth2.dart';
 import 'package:google_sign_in_platform_interface/google_sign_in_platform_interface.dart';
-import 'package:jwt_decoder/jwt_decoder.dart' as jwt;
+
+/// A codec that can encode/decode JWT payloads.
+///
+/// See https://www.rfc-editor.org/rfc/rfc7519#section-3
+final Codec<Object?, String> jwtCodec = json.fuse(utf8).fuse(base64);
+
+/// A RegExp that can match, and extract parts from a JWT Token.
+///
+/// A JWT token consists of 3 base-64 encoded parts of data separated by periods:
+///
+///   header.payload.signature
+///
+/// More info: https://regexr.com/789qc
+final RegExp jwtTokenRegexp = RegExp(
+    r'^(?<header>[^\.\s]+)\.(?<payload>[^\.\s]+)\.(?<signature>[^\.\s]+)$');
+
+/// Decodes the `claims` of a JWT token and returns them as a Map.
+///
+/// JWT `claims` are stored as a JSON object in the `payload` part of the token.
+///
+/// (This method does not validate the signature of the token.)
+///
+/// See https://www.rfc-editor.org/rfc/rfc7519#section-3
+Map<String, Object?>? getJwtTokenPayload(String? token) {
+  if (token != null) {
+    final RegExpMatch? match = jwtTokenRegexp.firstMatch(token);
+    if (match != null) {
+      return decodeJwtPayload(match.namedGroup('payload'));
+    }
+  }
+
+  return null;
+}
+
+/// Decodes a JWT payload using the [jwtCodec].
+Map<String, Object?>? decodeJwtPayload(String? payload) {
+  try {
+    // Payload must be normalized before passing it to the codec
+    return (jwtCodec.decode(base64.normalize(payload!))
+            as Map<String, Object?>?)
+        ?.cast<String, Object?>();
+  } catch (_) {
+    // Do nothing, we always return null for any failure.
+  }
+  return null;
+}
 
 /// Converts a [CredentialResponse] into a [GoogleSignInUserData].
 ///
@@ -18,7 +65,7 @@ GoogleSignInUserData? gisResponsesToUserData(
   }
 
   final Map<String, Object?>? payload =
-      jwt.JwtDecoder.tryDecode(credentialResponse.credential!);
+      getJwtTokenPayload(credentialResponse.credential);
 
   if (payload == null) {
     return null;
