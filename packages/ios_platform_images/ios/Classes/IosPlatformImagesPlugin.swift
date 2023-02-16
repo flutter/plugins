be6b8634d8865ddf19acc43269720ef07f3e40d5
Atsumi3 <atsumi@mail.bizen.jp>
2023-02-16 10:56:15 +0900

    add license comments

--- packages/ios_platform_images/ios/Classes/IosPlatformImagesPlugin.swift
+++ packages/ios_platform_images/ios/Classes/IosPlatformImagesPlugin.swift
@@ -1,6 +1,12 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import Foundation
 import Flutter
 
+/// A plugin for Flutter that allows Flutter to load images in a platform
+/// specific way on iOS.
 public class IosPlatformImagesPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(
